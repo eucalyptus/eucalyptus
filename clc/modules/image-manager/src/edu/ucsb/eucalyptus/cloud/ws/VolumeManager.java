c17cdf76f47a3a9b2ad5efb41b341b3d2551f48d
decker <decker@hawaii>
2009-01-30 13:02:01 -0800

    my bad

--- clc/modules/image-manager/src/edu/ucsb/eucalyptus/cloud/ws/VolumeManager.java
+++ clc/modules/image-manager/src/edu/ucsb/eucalyptus/cloud/ws/VolumeManager.java
@@ -268,7 +268,6 @@ public class VolumeManager {
     if( volume == null )
         throw new EucalyptusCloudException( "Volume is not attached: "+ request.getVolumeId() );
 
-    request.
     request.setRemoteDevice( volume.getRemoteDevice() );
     QueuedEvent<DetachVolumeType> event = QueuedEvent.make( new VolumeDetachCallback( cluster ), request);
     cluster.getMessageQueue().enqueue( event );
