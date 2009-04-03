25df5d978f6fd190510f269c7ae08e37c8496851
nekro <nekro@deafbabe>
2009-04-02 19:05:13 -0700

    if walrus is terminated before caching is complete, it will try to cache again on the next get

--- clc/modules/storage-manager/src/edu/ucsb/eucalyptus/cloud/ws/Bukkit.java
+++ clc/modules/storage-manager/src/edu/ucsb/eucalyptus/cloud/ws/Bukkit.java
@@ -66,6 +66,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.zip.GZIPInputStream;
 
 public class Bukkit {
@@ -80,6 +81,7 @@ public class Bukkit {
 
     private static boolean sharedMode = false;
     private final long CACHE_PROGRESS_TIMEOUT = 60000L; //a minute
+    private ConcurrentHashMap<String, ImageCacher> imageCachers = new ConcurrentHashMap<String, ImageCacher>();
 
     static {
         storageManager = new FileSystemStorageManager(WalrusProperties.bucketRootDirectory);
@@ -1863,23 +1865,32 @@ public class Bukkit {
     }
 
     private synchronized void cacheImage(String bucketName, String manifestKey, String userId, boolean isAdministrator) throws EucalyptusCloudException {
+
         EntityWrapper<ImageCacheInfo> db = new EntityWrapper<ImageCacheInfo>();
         ImageCacheInfo searchImageCacheInfo = new ImageCacheInfo(bucketName, manifestKey);
         List<ImageCacheInfo> imageCacheInfos = db.query(searchImageCacheInfo);
-
-        if(imageCacheInfos.size() == 0) {
-            String decryptedImageKey = decryptImage(bucketName, manifestKey, userId, isAdministrator);
-//decryption worked. Add it.
+        String decryptedImageKey;
+        if(imageCacheInfos.size() != 0) {
+            ImageCacheInfo icInfo = imageCacheInfos.get(0);
+            if(!icInfo.getInCache())
+                decryptedImageKey = icInfo.getImageName();
+            else
+                decryptedImageKey = "invalid";
+        } else {
+            decryptedImageKey = decryptImage(bucketName, manifestKey, userId, isAdministrator);
+            //decryption worked. Add it.
             ImageCacheInfo foundImageCacheInfo = new ImageCacheInfo(bucketName, manifestKey);
             foundImageCacheInfo.setImageName(decryptedImageKey);
             foundImageCacheInfo.setInCache(false);
-            foundImageCacheInfo.setCaching(true);
             foundImageCacheInfo.setUseCount(0);
             foundImageCacheInfo.setSize(0L);
             db.add(foundImageCacheInfo);
-            db.commit();
-//decrypt, unzip, untar image in the background
-            ImageCacher imageCacher = new ImageCacher(bucketName, manifestKey, decryptedImageKey);
+        }
+        db.commit();
+        //unzip, untar image in the background
+        ImageCacher imageCacher = imageCachers.putIfAbsent(bucketName + manifestKey, new ImageCacher(bucketName, manifestKey, decryptedImageKey));
+        if(imageCacher == null) {
+            imageCacher = imageCachers.get(bucketName + manifestKey);
             imageCacher.start();
         }
     }
@@ -1934,8 +1945,8 @@ public class Bukkit {
 
         if(foundImageCacheInfos.size() > 0) {
             ImageCacheInfo foundImageCacheInfo = foundImageCacheInfos.get(0);
-            if(foundImageCacheInfo.getInCache() && !foundImageCacheInfo.getCaching()) {
-//check that there are no operations in progress and then flush cache and delete image file
+            if(foundImageCacheInfo.getInCache() && (imageCachers.get(bucketName + manifestKey) == null)) {
+                //check that there are no operations in progress and then flush cache and delete image file
                 db.commit();
                 ImageCacheFlusher imageCacheFlusher = new ImageCacheFlusher(bucketName, manifestKey);
                 imageCacheFlusher.start();
@@ -1969,7 +1980,7 @@ public class Bukkit {
 
             if(foundImageCacheInfos.size() > 0) {
                 ImageCacheInfo foundImageCacheInfo = foundImageCacheInfos.get(0);
-                if(foundImageCacheInfo.getInCache() && !foundImageCacheInfo.getCaching()) {
+                if(foundImageCacheInfo.getInCache() && (imageCachers.get(bucketName + objectKey) == null)) {
                     db.delete(foundImageCacheInfo);
                     storageManager.deleteObject(bucketName, foundImageCacheInfo.getImageName());
                 }
@@ -1996,6 +2007,7 @@ public class Bukkit {
             flushCachedImage(bucketName, objectKey);
         }
     }
+
 
     private class ImageCacher extends Thread {
 
@@ -2050,6 +2062,7 @@ public class Bukkit {
                 monitor.notifyAll();
             }
             imageMessenger.removeMonitor(bucketName + "/" + manifestKey);
+            imageCachers.remove(bucketName + manifestKey);
         }
 
         public void run() {
@@ -2100,7 +2113,6 @@ public class Bukkit {
                     ImageCacheInfo foundImageCacheInfo = foundImageCacheInfos.get(0);
                     foundImageCacheInfo.setImageName(imageKey);
                     foundImageCacheInfo.setInCache(true);
-                    foundImageCacheInfo.setCaching(false);
                     foundImageCacheInfo.setSize(unencryptedSize);
                     db.commit();
                     //wake up waiters
