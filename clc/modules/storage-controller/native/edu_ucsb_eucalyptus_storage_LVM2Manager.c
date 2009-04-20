81647be9766f922277b35b38ae7697e3bfbcf3f4
nekro <nekro@deafbabe>
2009-04-19 22:27:01 -0700

    this file shouldn't have been committed! reverting.

--- clc/modules/storage-controller/native/edu_ucsb_eucalyptus_storage_LVM2Manager.c
+++ clc/modules/storage-controller/native/edu_ucsb_eucalyptus_storage_LVM2Manager.c
@@ -45,8 +45,16 @@
 
 static const char* blockSize = "1M";
 jstring run_command(JNIEnv *env, char *cmd, int outfd) {
-    char readbuffer[256]; 
-    char absolute_cmd[256];
+	FILE* fd;
+	int pid;
+	char readbuffer[256];
+	char absolute_cmd[256];
+    char* home = getenv (EUCALYPTUS_ENV_VAR_NAME);
+    if (!home) {
+        home = strdup (""); /* root by default */
+    } else {
+        home = strdup (home);
+    }
 
     snprintf(absolute_cmd, 256, "%s/usr/share/eucalyptus/euca_rootwrap %s", home, cmd);
     fprintf(stderr, "command: %s\n", absolute_cmd);
@@ -60,59 +68,11 @@ jstring run_command(JNIEnv *env, char *cmd, int outfd) {
 	}
 	fclose(fd);
 	return (*env)->NewStringUTF(env, readbuffer);
-
-    int pid = -1;
-
-    if ((pid = fork()) == -1) {
-        perror("Could not run command");
-        return -1;
-    }
-
-   if (pid == 0) {
-        //daemonize
-        DIR *proc_fd_dir;
-        struct dirent *fd_dir;
-        int fd_to_close;
-        char fd_path[128];
-        int my_pid = getpid();
-
-        umask(0);
-        int sid = setsid();
-        if(sid < 0)
-            return -1;
-        char* home = getenv (EUCALYPTUS_ENV_VAR_NAME);
-        if (!home) {
-            home = strdup (""); /* root by default */
-        } else {
-        home = strdup (home);
-        }
-        chdir(home);
-
-        //close all open fds
-        snprintf(fd_path, 128, "/proc/%d/fd", my_pid);
-
-        if ((proc_fd_dir = opendir(fd_path)) == NULL) {
-            perror("ERROR: Cannot opendir\n");
-            return -1;
-        }
-
-        while ((fd_dir = readdir(proc_fd_dir)) != NULL) {
-            if (isdigit(fd_dir->d_name[0])) {
-                fd_to_close =  atoi(fd_dir->d_name);
-                close(fd_to_close);
-            }
-        }
-
-        freopen( "/dev/null", "r", stdin);
-        freopen( "/dev/null", "w", stdout);
-        freopen( "/dev/null", "w", stderr);
-        exit(execvp(cmd, args));
-   } 
-   return pid;
-
 }
 
 int run_command_and_get_pid(char *cmd, char **args) {
+    int fd[2];
+    pipe(fd);
     int pid = -1;
 
     if ((pid = fork()) == -1) {
@@ -159,7 +119,9 @@ int run_command_and_get_pid(char *cmd, char **args) {
         freopen( "/dev/null", "w", stdout);
         freopen( "/dev/null", "w", stderr);
         exit(execvp(cmd, args));
-   } 
+   } else {
+        close(fd[1]);
+   }
    return pid;
 }
 
