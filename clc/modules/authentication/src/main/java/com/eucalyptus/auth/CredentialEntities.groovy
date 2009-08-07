aaccc8b3033d4198046a02ea3dcf0fd92eef6ff0
decker <decker@personal-army>
2009-08-06 20:19:17 -0700

    really add authentication code

---
+++ clc/modules/authentication/src/main/java/com/eucalyptus/auth/CredentialEntities.groovy
@@ -0,0 +1,14 @@
+package com.eucalyptus.auth
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import javax.persistence.*;
+
+@Entity
+@Table(name="x509_certificates")
+@Cache( usage = CacheConcurrencyStrategy.READ_WRITE )
+class X509Certificate {
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
+  public Long id
+  public String alias
+  public String certificate
+}
