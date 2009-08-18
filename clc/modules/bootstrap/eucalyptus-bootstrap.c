02818ffac33e4cecfa9503b781fc93c8c965f632
decker <decker@personal-army>
2009-08-17 19:28:13 -0700

    avoid double listing the deps jar which causes double initialization in spring

--- clc/modules/bootstrap/eucalyptus-bootstrap.c
+++ clc/modules/bootstrap/eucalyptus-bootstrap.c
@@ -475,7 +475,7 @@ char* java_library_path(euca_opts *args) {
 	DIR* lib_dir_p = opendir(lib_dir);
 	struct direct *dir_ent;
 	while ((dir_ent = readdir(lib_dir_p))!=0)  {
-		if (strcmp(dir_ent->d_name,".") != 0 && strcmp(dir_ent->d_name,"..") != 0)  {
+		if (strcmp(dir_ent->d_name,".") != 0 && strcmp(dir_ent->d_name,"..") != 0 && strcmp(dir_ent->d_name,"openjdk-crypto.jar") != 0)  {
 				char jar[256];
 				snprintf(jar,255,"%s/%s",lib_dir,dir_ent->d_name);
 				if( CHECK_ISREG(jar) ) wb += snprintf(jar_list+wb,JAVA_PATH_LEN-wb,":%s",jar);
