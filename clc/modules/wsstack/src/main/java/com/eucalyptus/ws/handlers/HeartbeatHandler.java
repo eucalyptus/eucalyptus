e3141c171d6612400f0fda639667969b1d399d18
decker <decker@internet-hate-machine>
2009-09-25 15:16:19 -0700

    fix handling of timeouts for remote components.

--- clc/modules/wsstack/src/main/java/com/eucalyptus/ws/handlers/HeartbeatHandler.java
+++ clc/modules/wsstack/src/main/java/com/eucalyptus/ws/handlers/HeartbeatHandler.java
@@ -79,6 +79,7 @@ import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineCoverage;
 import org.jboss.netty.channel.ChannelUpstreamHandler;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
@@ -104,6 +105,7 @@ import com.eucalyptus.ws.handlers.soap.AddressingHandler;
 import com.eucalyptus.ws.handlers.soap.SoapHandler;
 import com.eucalyptus.ws.handlers.wssecurity.InternalWsSecHandler;
 import com.eucalyptus.ws.stages.UnrollableStage;
+import com.eucalyptus.ws.util.ChannelUtil;
 
 import edu.ucsb.eucalyptus.msgs.ComponentType;
 import edu.ucsb.eucalyptus.msgs.HeartbeatComponentType;
@@ -239,6 +241,7 @@ public class HeartbeatHandler implements ChannelUpstreamHandler, ChannelDownstre
 
   @Override
   public void unrollStage( ChannelPipeline pipeline ) {
+    ChannelUtil.addPipelineMonitors( pipeline );
     pipeline.addLast( "hb-get-handler", new SimpleHeartbeatHandler( ) );
     pipeline.addLast( "deserialize", new SoapMarshallingHandler( ) );
     try {
@@ -278,6 +281,13 @@ public class HeartbeatHandler implements ChannelUpstreamHandler, ChannelDownstre
         ctx.sendUpstream( e );
       }
     }
+
+    @Override
+    public void exceptionCaught( ChannelHandlerContext ctx, ExceptionEvent e ) throws Exception {
+      e.getFuture( ).addListener( ChannelFutureListener.CLOSE );
+      super.exceptionCaught( ctx, e );
+    }
+
 
   }
 
