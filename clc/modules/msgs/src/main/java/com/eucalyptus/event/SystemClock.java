83701fca329d5dabff62af76c260c20bc3c0c2cd
decker <decker@internet-hate-machine>
2009-09-25 15:04:14 -0700

    lower the polling interval to lower the likelihood of corner cases when resources are near-exhausted.

--- clc/modules/msgs/src/main/java/com/eucalyptus/event/SystemClock.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/event/SystemClock.java
@@ -22,7 +22,7 @@ public class SystemClock extends TimerTask implements UncaughtExceptionHandler {
         timer = new Timer("SystemClockTimer");
         clock = new SystemClock();
         ListenerRegistry.getInstance( ).register( ClockTick.class, new Dummy() );
-        timer.scheduleAtFixedRate( clock, 5000, 4000 );
+        timer.scheduleAtFixedRate( clock, 5000, 3000 );
       }
     }
   }
