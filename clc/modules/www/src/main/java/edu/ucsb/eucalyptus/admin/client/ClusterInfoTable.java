115cffe1f89ac9337b81aecfb90fe6f311e8e22b
Neil <neil@pall>
2009-09-24 20:40:33 -0700

    fixes #436313

--- clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/ClusterInfoTable.java
+++ clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/ClusterInfoTable.java
@@ -1,63 +1,63 @@
 /*******************************************************************************
-*Copyright (c) 2009  Eucalyptus Systems, Inc.
-* 
-*  This program is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, only version 3 of the License.
-* 
-* 
-*  This file is distributed in the hope that it will be useful, but WITHOUT
-*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-*  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-*  for more details.
-* 
-*  You should have received a copy of the GNU General Public License along
-*  with this program.  If not, see <http://www.gnu.org/licenses/>.
-* 
-*  Please contact Eucalyptus Systems, Inc., 130 Castilian
-*  Dr., Goleta, CA 93101 USA or visit <http://www.eucalyptus.com/licenses/>
-*  if you need additional information or have any questions.
-* 
-*  This file may incorporate work covered under the following copyright and
-*  permission notice:
-* 
-*    Software License Agreement (BSD License)
-* 
-*    Copyright (c) 2008, Regents of the University of California
-*    All rights reserved.
-* 
-*    Redistribution and use of this software in source and binary forms, with
-*    or without modification, are permitted provided that the following
-*    conditions are met:
-* 
-*      Redistributions of source code must retain the above copyright notice,
-*      this list of conditions and the following disclaimer.
-* 
-*      Redistributions in binary form must reproduce the above copyright
-*      notice, this list of conditions and the following disclaimer in the
-*      documentation and/or other materials provided with the distribution.
-* 
-*    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-*    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-*    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-*    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-*    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-*    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-*    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-*    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-*    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-*    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-*    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. USERS OF
-*    THIS SOFTWARE ACKNOWLEDGE THE POSSIBLE PRESENCE OF OTHER OPEN SOURCE
-*    LICENSED MATERIAL, COPYRIGHTED MATERIAL OR PATENTED MATERIAL IN THIS
-*    SOFTWARE, AND IF ANY SUCH MATERIAL IS DISCOVERED THE PARTY DISCOVERING
-*    IT MAY INFORM DR. RICH WOLSKI AT THE UNIVERSITY OF CALIFORNIA, SANTA
-*    BARBARA WHO WILL THEN ASCERTAIN THE MOST APPROPRIATE REMEDY, WHICH IN
-*    THE REGENTS’ DISCRETION MAY INCLUDE, WITHOUT LIMITATION, REPLACEMENT
-*    OF THE CODE SO IDENTIFIED, LICENSING OF THE CODE SO IDENTIFIED, OR
-*    WITHDRAWAL OF THE CODE CAPABILITY TO THE EXTENT NEEDED TO COMPLY WITH
-*    ANY SUCH LICENSES OR RIGHTS.
-*******************************************************************************/
+ *Copyright (c) 2009  Eucalyptus Systems, Inc.
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, only version 3 of the License.
+ * 
+ * 
+ *  This file is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  Please contact Eucalyptus Systems, Inc., 130 Castilian
+ *  Dr., Goleta, CA 93101 USA or visit <http://www.eucalyptus.com/licenses/>
+ *  if you need additional information or have any questions.
+ * 
+ *  This file may incorporate work covered under the following copyright and
+ *  permission notice:
+ * 
+ *    Software License Agreement (BSD License)
+ * 
+ *    Copyright (c) 2008, Regents of the University of California
+ *    All rights reserved.
+ * 
+ *    Redistribution and use of this software in source and binary forms, with
+ *    or without modification, are permitted provided that the following
+ *    conditions are met:
+ * 
+ *      Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * 
+ *      Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ * 
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ *    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. USERS OF
+ *    THIS SOFTWARE ACKNOWLEDGE THE POSSIBLE PRESENCE OF OTHER OPEN SOURCE
+ *    LICENSED MATERIAL, COPYRIGHTED MATERIAL OR PATENTED MATERIAL IN THIS
+ *    SOFTWARE, AND IF ANY SUCH MATERIAL IS DISCOVERED THE PARTY DISCOVERING
+ *    IT MAY INFORM DR. RICH WOLSKI AT THE UNIVERSITY OF CALIFORNIA, SANTA
+ *    BARBARA WHO WILL THEN ASCERTAIN THE MOST APPROPRIATE REMEDY, WHICH IN
+ *    THE REGENTS’ DISCRETION MAY INCLUDE, WITHOUT LIMITATION, REPLACEMENT
+ *    OF THE CODE SO IDENTIFIED, LICENSING OF THE CODE SO IDENTIFIED, OR
+ *    WITHDRAWAL OF THE CODE CAPABILITY TO THE EXTENT NEEDED TO COMPLY WITH
+ *    ANY SUCH LICENSES OR RIGHTS.
+ *******************************************************************************/
 package edu.ucsb.eucalyptus.admin.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -88,7 +88,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		this.setStyleName("euca-config-component");
 		this.setSpacing (2);
 		this.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
-//		this.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		//		this.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 		Label clustersHeader = new Label( "Clusters:" );
 		clustersHeader.setStyleName ( "euca-section-header" );
 		this.add ( clustersHeader );
@@ -104,18 +104,17 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		hpanel.add ( add_button );
 		hpanel.add ( new Button( "Save cluster configuration", new SaveCallback( this ) ) );
 		hpanel.add ( this.statusLabel );
-//		this.statusLabel.setWidth ("250");
+		//		this.statusLabel.setWidth ("250");
 		this.statusLabel.setText ("");
 		this.statusLabel.setStyleName ("euca-greeting-pending");
 		this.add ( hpanel );
-    rebuildTable();
-    EucalyptusWebBackend.App.getInstance().getClusterList(
+		rebuildTable();
+		EucalyptusWebBackend.App.getInstance().getClusterList(
 				this.sessionId, new GetClusterListCallback( this ) );
 		EucalyptusWebBackend.App.getInstance().getSystemConfig(
 				this.sessionId, new GetSystemConfigCallback( this ) );
 		EucalyptusWebBackend.App.getInstance().getStorageList(
 				this.sessionId, new GetStorageListCallback(this));
-    rebuildTable();
 	}
 
 	public void onClick( final Widget widget ) // Add cluster button
@@ -193,7 +192,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 
 		i++; // next row
 		g.setWidget( i, 1, new Label( "Cluster Controller" ));
-		
+
 		i++; // next row
 		g.setWidget( i, 0, new Label( "Host:" ) );
 		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
@@ -214,81 +213,81 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		pb.addFocusListener (new FocusHandler (this.hint, this.warningMessage));
 		g.setWidget( i, 1, pb );
 
-    final TextBox reservedAddressesBox = new TextBox(); // declare here, set up after the checkbox later
+		final TextBox reservedAddressesBox = new TextBox(); // declare here, set up after the checkbox later
 
-    i++; // next row
-    final CheckBox dynamicAddressesCheckbox = new CheckBox ();
-    g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
-    g.setWidget (i, 0, dynamicAddressesCheckbox );
-    if (systemConfig.isDoDynamicPublicAddresses()) {
-      dynamicAddressesCheckbox.setChecked(true);
-      reservedAddressesBox.setEnabled(false);
-    } else {
-      dynamicAddressesCheckbox.setChecked(false);
-      reservedAddressesBox.setEnabled(true);
-    }
-    dynamicAddressesCheckbox.addClickListener (new ClickListener() {
-      public void onClick( Widget sender )
-      {
-        if (((CheckBox)sender).isChecked()) {
-          reservedAddressesBox.setEnabled(false);
-          systemConfig.setDoDynamicPublicAddresses( true );
-        } else {
-          reservedAddressesBox.setEnabled(true);
-          systemConfig.setDoDynamicPublicAddresses( false );
-        }
-      }
-    });
-    g.setWidget( i, 1, new Label ("Dynamic public IP address assignment") );
+		i++; // next row
+		final CheckBox dynamicAddressesCheckbox = new CheckBox ();
+		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
+		g.setWidget (i, 0, dynamicAddressesCheckbox );
+		if (systemConfig.isDoDynamicPublicAddresses()) {
+			dynamicAddressesCheckbox.setChecked(true);
+			reservedAddressesBox.setEnabled(false);
+		} else {
+			dynamicAddressesCheckbox.setChecked(false);
+			reservedAddressesBox.setEnabled(true);
+		}
+		dynamicAddressesCheckbox.addClickListener (new ClickListener() {
+			public void onClick( Widget sender )
+			{
+				if (((CheckBox)sender).isChecked()) {
+					reservedAddressesBox.setEnabled(false);
+					systemConfig.setDoDynamicPublicAddresses( true );
+				} else {
+					reservedAddressesBox.setEnabled(true);
+					systemConfig.setDoDynamicPublicAddresses( false );
+				}
+			}
+		});
+		g.setWidget( i, 1, new Label ("Dynamic public IP address assignment") );
 
-    i++; // next row
-    g.setWidget( i, 0, new Label( "Reserve for assignment" ) );
-    g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
-    reservedAddressesBox.addChangeListener (new ChangeCallback (this, row));
-    reservedAddressesBox.setVisibleLength( 5 );
-    reservedAddressesBox.setText( "" + systemConfig.getSystemReservedPublicAddresses());
-    final HorizontalPanel reservedAddressesPanel = new HorizontalPanel ();
-    reservedAddressesPanel.setSpacing(4);
-    reservedAddressesPanel.add (reservedAddressesBox);
-    reservedAddressesPanel.add (new HTML ("public IP addresses"));
-    reservedAddressesBox.setText(""+systemConfig.getSystemReservedPublicAddresses());
-    g.setWidget( i, 1, reservedAddressesPanel );
-    
-    i++; // next row
-    g.setWidget( i, 0, new Label( "Maximum of" ) );
-    g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
-    final TextBox publicAddressesBox = new TextBox();
-    publicAddressesBox.addChangeListener (new ChangeCallback (this, row));
-    publicAddressesBox.setVisibleLength( 5 );
-    publicAddressesBox.setText( "" + systemConfig.getMaxUserPublicAddresses());
-    final HorizontalPanel publicAddressesPanel = new HorizontalPanel ();
-    publicAddressesPanel.setSpacing(4);
-    publicAddressesPanel.add (publicAddressesBox);
-    publicAddressesPanel.add (new HTML ("public IP addresses per user"));
-    g.setWidget( i, 1, publicAddressesPanel );
-    
-    i++;
-    g.setWidget( i, 0, new Label( "Use VLAN tags" ) );
-    g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
-    final TextBox minVlanBox = new TextBox();
-    minVlanBox.addChangeListener (new ChangeCallback (this, row));
-    minVlanBox.setVisibleLength( 4 );
-    minVlanBox.setText(String.valueOf(clusterInfo.getMinVlans()));
-    final TextBox maxVlanBox = new TextBox();
-    maxVlanBox.addChangeListener (new ChangeCallback (this, row));
-    maxVlanBox.setVisibleLength( 4 );
-    maxVlanBox.setText(String.valueOf(clusterInfo.getMaxVlans()));
-    final HorizontalPanel vlanPanel = new HorizontalPanel ();
-    vlanPanel.setSpacing(4);
-    vlanPanel.add (minVlanBox);
-    vlanPanel.add (new HTML ("through"));
-    vlanPanel.add (maxVlanBox);
-    g.setWidget( i, 1, vlanPanel );
+		i++; // next row
+		g.setWidget( i, 0, new Label( "Reserve for assignment" ) );
+		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
+		reservedAddressesBox.addChangeListener (new ChangeCallback (this, row));
+		reservedAddressesBox.setVisibleLength( 5 );
+		reservedAddressesBox.setText( "" + systemConfig.getSystemReservedPublicAddresses());
+		final HorizontalPanel reservedAddressesPanel = new HorizontalPanel ();
+		reservedAddressesPanel.setSpacing(4);
+		reservedAddressesPanel.add (reservedAddressesBox);
+		reservedAddressesPanel.add (new HTML ("public IP addresses"));
+		reservedAddressesBox.setText(""+systemConfig.getSystemReservedPublicAddresses());
+		g.setWidget( i, 1, reservedAddressesPanel );
 
-		
+		i++; // next row
+		g.setWidget( i, 0, new Label( "Maximum of" ) );
+		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
+		final TextBox publicAddressesBox = new TextBox();
+		publicAddressesBox.addChangeListener (new ChangeCallback (this, row));
+		publicAddressesBox.setVisibleLength( 5 );
+		publicAddressesBox.setText( "" + systemConfig.getMaxUserPublicAddresses());
+		final HorizontalPanel publicAddressesPanel = new HorizontalPanel ();
+		publicAddressesPanel.setSpacing(4);
+		publicAddressesPanel.add (publicAddressesBox);
+		publicAddressesPanel.add (new HTML ("public IP addresses per user"));
+		g.setWidget( i, 1, publicAddressesPanel );
+
+		i++;
+		g.setWidget( i, 0, new Label( "Use VLAN tags" ) );
+		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
+		final TextBox minVlanBox = new TextBox();
+		minVlanBox.addChangeListener (new ChangeCallback (this, row));
+		minVlanBox.setVisibleLength( 4 );
+		minVlanBox.setText(String.valueOf(clusterInfo.getMinVlans()));
+		final TextBox maxVlanBox = new TextBox();
+		maxVlanBox.addChangeListener (new ChangeCallback (this, row));
+		maxVlanBox.setVisibleLength( 4 );
+		maxVlanBox.setText(String.valueOf(clusterInfo.getMaxVlans()));
+		final HorizontalPanel vlanPanel = new HorizontalPanel ();
+		vlanPanel.setSpacing(4);
+		vlanPanel.add (minVlanBox);
+		vlanPanel.add (new HTML ("through"));
+		vlanPanel.add (maxVlanBox);
+		g.setWidget( i, 1, vlanPanel );
+
+
 		i++; // next row
 		g.setWidget( i, 1, new Label( "Storage Controller" ));
-		
+
 		i++; // next row
 		g.setWidget( i, 0, new Label( "Host:" ) );
 		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
@@ -298,7 +297,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		sb.setText( storageInfo.getHost() ); 
 		sb.addFocusListener (new FocusHandler (this.hint, this.warningMessage));
 		g.setWidget ( i, 1, sb );
-		
+
 		i++; // next row
 		g.setWidget( i, 0, new Label( "Interface:" ) );
 		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
@@ -405,7 +404,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 			cluster.setName (((Label)p.getWidget(0)).getText());
 			storage.setName (((Label)p.getWidget(0)).getText());
 		}
-		
+
 		// CC section
 		cluster.setHost (((TextBox)g.getWidget(2, 1)).getText());
 		cluster.setPort (Integer.parseInt(((TextBox)g.getWidget(3, 1)).getText()));
@@ -415,10 +414,10 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		systemConfig.setMaxUserPublicAddresses(Integer.parseInt(((TextBox)p.getWidget(0)).getText()));
 		p = (HorizontalPanel)g.getWidget(7, 1);
 		cluster.setMinVlans(Integer.parseInt(((TextBox)p.getWidget(0)).getText()));
-		cluster.setMaxVlans(Integer.parseInt(((TextBox)p.getWidget(1)).getText()));
-		
+		cluster.setMaxVlans(Integer.parseInt(((TextBox)p.getWidget(2)).getText()));
+
 		// SC section
-		storage.setHost (((TextBox)g.getWidget(9, 1)).getText());	
+		storage.setHost (((TextBox)g.getWidget(9, 1)).getText());
 		storage.setStorageInterface(((TextBox)g.getWidget(10, 1)).getText());
 		storage.setVolumesPath (((TextBox)g.getWidget(11, 1)).getText());
 		p = (HorizontalPanel)g.getWidget(12, 1);
