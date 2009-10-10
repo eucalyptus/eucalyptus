6b4add35288825a9027acdaec4f320f3476776dd
Dmitrii Zagorodnov <dmitrii@eucalyptus.com>
2009-10-10 16:36:29 -0700

    bzr behaving strangely

--- clc/modules/msgs/src/main/java/edu/ucsb/eucalyptus/msgs/VMwareBroker.groovy
+++ clc/modules/msgs/src/main/java/edu/ucsb/eucalyptus/msgs/VMwareBroker.groovy
@@ -151,7 +151,7 @@ public class EucalyptusNCNcDescribeResourceType extends VMwareBrokerRequestType 
   	@Override
     public String toString() {
     	//return super.toString( + " resourceType=" + getResourceType());
-    	return ">>>>>" + getClass().getSimpleName() + " resourceType=" + getResourceType();
+    	return "}}}}}" + getClass().getSimpleName() + " resourceType=" + getResourceType();
 
     }
 }
