9b08c89bcfb20b2931b355711aab8d1ee0576193
root <root@ubuntu>
2009-10-17 12:39:12 -0700

    incorrect hashcode. reverting.

--- clc/modules/core/src/main/java/edu/ucsb/eucalyptus/cloud/entities/WalrusInfo.java
+++ clc/modules/core/src/main/java/edu/ucsb/eucalyptus/cloud/entities/WalrusInfo.java
@@ -157,7 +157,10 @@ public class WalrusInfo {
 
 	@Override
 	public int hashCode() {
-		return name.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
 
 	@Override
