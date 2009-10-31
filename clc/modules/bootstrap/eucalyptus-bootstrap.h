608174493f549e239fa8038c33ebc4423a4efd37
root <root@linux-hpg1>
2009-10-31 14:11:56 -0700

    changed version to 1.6.1 for full testing with correct version number

--- clc/modules/bootstrap/eucalyptus-bootstrap.h
+++ clc/modules/bootstrap/eucalyptus-bootstrap.h
@@ -117,7 +117,7 @@ static char *jvm_default_opts[] = {
 	    "-Deuca.lib.dir=%1$s/usr/share/eucalyptus",
 	    "-Deuca.conf.dir=%1$s/etc/eucalyptus/cloud.d",
 	    "-Deuca.log.dir=%1$s/var/log/eucalyptus",
-	    "-Deuca.version=1.6-devel",
+	    "-Deuca.version=1.6.1",
 	    NULL,
 };
 static char *libjvm_paths[ ] = {
