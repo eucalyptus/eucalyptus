f5e3526318c749a74bb18763383c04df17434d7a
decker <decker@personal-army>
2009-11-18 01:50:04 -0800

    It is always wrong to try to use the database before it is setup; All static initializers are defered until after DB setup and called explicitly.  Any attempt to use the DB before it is setup results in a RuntimeException and System.exit 1

--- clc/modules/database/src/main/java/com/eucalyptus/util/DatabaseUtil.java
+++ clc/modules/database/src/main/java/com/eucalyptus/util/DatabaseUtil.java
@@ -1,5 +1,6 @@
 package com.eucalyptus.util;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -14,14 +15,18 @@ import com.eucalyptus.bootstrap.SystemBootstrapper;
 import com.eucalyptus.event.ClockTick;
 import com.eucalyptus.event.Event;
 import com.eucalyptus.event.EventListener;
+import com.google.common.collect.Lists;
 
+import edu.emory.mathcs.backport.java.util.Collections;
+
+@SuppressWarnings( "unchecked" )
 public class DatabaseUtil implements EventListener {
 	static Logger                                        LOG           = Logger.getLogger( DatabaseUtil.class );
 	public static int                                    MAX_FAIL      = 5;
 	public static int                                    MAX_XTREME_FAIL      = 7;
 	private static int                                   failCount     = 0;
 	private static Map<String, EntityManagerFactoryImpl> emf           = new ConcurrentSkipListMap<String, EntityManagerFactoryImpl>( );
-
+	private static List<Exception> illegalAccesses = Collections.synchronizedList( Lists.newArrayList( ) );
 
 	public static EntityManagerFactoryImpl getEntityManagerFactory( ) {
 		return getEntityManagerFactory( "eucalyptus_general" );
@@ -29,7 +34,14 @@ public class DatabaseUtil implements EventListener {
 
 	 public static EntityManagerFactoryImpl registerPersistenceContext( final String persistenceContext, final Ejb3Configuration config ) {
 	    synchronized ( EntityWrapper.class ) {
-	      if ( !emf.containsKey( persistenceContext ) ) {
+	      if( illegalAccesses != null && !illegalAccesses.isEmpty( ) ) {
+	        for( Exception e : illegalAccesses ) {
+	          LOG.fatal( e, e );
+	        }
+	        LogUtil.header( "Illegal Access to Persistence Context.  Database not yet configured. This is always a BUG." );
+	        System.exit( 1 );
+	      } else if ( !emf.containsKey( persistenceContext ) ) {
+          illegalAccesses = null;
 	        EntityManagerFactoryImpl entityManagerFactory = (EntityManagerFactoryImpl) config.buildEntityManagerFactory( );//Persistence.createEntityManagerFactory( persistenceContext );
           LOG.info( "-> Setting up persistence context for : " + persistenceContext );
           LOG.info( LogUtil.subheader( LogUtil.dumpObject( config ) ) );
@@ -44,7 +56,8 @@ public class DatabaseUtil implements EventListener {
 	public static EntityManagerFactoryImpl getEntityManagerFactory( final String persistenceContext ) {
 			if ( !emf.containsKey( persistenceContext ) ) {
 			  RuntimeException e = new RuntimeException ("Attempting to access an entity wrapper before the database has been configured." );
-			  LOG.error( e, e );
+			  illegalAccesses = illegalAccesses == null ? Collections.synchronizedList( Lists.newArrayList( ) ) : illegalAccesses;			  
+			  illegalAccesses.add( e );
 			  throw e;
 			}
 			return emf.get( persistenceContext );
