8c52f6731bfb38d0e384b202d1e0606ab58a52f0
decker <decker@personal-army>
2009-12-19 20:28:24 -0800

    add missing files.

---
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/address/AddressCategory.java
@@ -0,0 +1,38 @@
+package com.eucalyptus.address;
+
+import java.util.NoSuchElementException;
+import org.apache.log4j.Logger;
+import com.eucalyptus.cluster.SuccessCallback;
+import com.eucalyptus.util.EucalyptusCloudException;
+import com.eucalyptus.util.LogUtil;
+import com.eucalyptus.util.NotEnoughResourcesAvailable;
+import edu.ucsb.eucalyptus.cloud.cluster.QueuedEventCallback;
+import edu.ucsb.eucalyptus.cloud.cluster.VmInstance;
+import edu.ucsb.eucalyptus.cloud.cluster.VmInstances;
+
+public class AddressCategory {
+  private static Logger LOG = Logger.getLogger( AddressCategory.class );
+  
+  @SuppressWarnings( "unchecked" )
+  public static QueuedEventCallback unassign( final Address addr ) {
+    final String instanceId = addr.getInstanceId( );
+      return addr.unassign( ).getCallback( ).onSuccess( new SuccessCallback( ) {
+        public void apply( Object response ) {
+          try {
+            VmInstance vm = VmInstances.getInstance( ).lookup( instanceId );
+            Addresses.system( vm );
+          } catch ( NoSuchElementException e ) {}
+        }
+      } );
+  }
+  
+  @SuppressWarnings( "unchecked" )
+  public static QueuedEventCallback assign( final Address addr, final VmInstance vm ) {
+    return addr.assign( vm.getInstanceId( ), vm.getNetworkConfig( ).getIpAddress( ) ).getCallback( ).onSuccess( new SuccessCallback() {
+      public void apply( Object response ) {
+        vm.getNetworkConfig( ).setIgnoredPublicIp( addr.getName( ) );
+      }
+    });
+  }
+  
+}
