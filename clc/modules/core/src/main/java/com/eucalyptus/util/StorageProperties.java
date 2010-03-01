5648ad5ec764da8db900afde4ccefcc5f501bac5
Neil <neil@pall>
2010-03-01 14:08:35 -0800

    changed default transfer block size.

--- clc/modules/core/src/main/java/com/eucalyptus/util/StorageProperties.java
+++ clc/modules/core/src/main/java/com/eucalyptus/util/StorageProperties.java
@@ -95,7 +95,7 @@ public class StorageProperties {
 	public static final long GB = 1024*1024*1024;
 	public static final long MB = 1024*1024;
 	public static final long KB = 1024;
-	public static int TRANSFER_CHUNK_SIZE = 102400;
+	public static int TRANSFER_CHUNK_SIZE = 8192;
 	public static boolean enableSnapshots = false;
 	public static boolean enableStorage = false;
 	public static boolean shouldEnforceUsageLimits = true;
