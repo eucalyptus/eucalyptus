a93f0dc357f35b74f3f0b18e563c591226fa1510
Neil <neil@pall>
2010-03-02 16:33:14 -0800

    remove unnecessary fields from the UI.

--- clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/ClusterInfoTable.java
+++ clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/ClusterInfoTable.java
@@ -164,7 +164,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 
 	private Grid addClusterEntry ( int row, ClusterInfoWeb clusterInfo, final StorageInfoWeb storageInfo)
 	{
-		Grid g = new Grid (18, 2);
+		Grid g = new Grid (15, 2);
 		g.setStyleName( "euca-table" );
 		if (row > 0) {
 			g.setStyleName( "euca-nonfirst-cluster-entry" );
@@ -288,7 +288,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		i++; // next row
 		g.setWidget( i, 1, new Label( "Storage Controller" ));
 
-		i++; // next row
+		/*i++; // next row
 		g.setWidget( i, 0, new Label( "Host:" ) );
 		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
 		final TextBox sb = new TextBox();
@@ -316,7 +316,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		volumesPathBox.setVisibleLength( 30 );
 		volumesPathBox.setText( storageInfo.getVolumesPath() );
 		volumesPathBox.addFocusListener (new FocusHandler (this.hint, this.warningMessage));
-		g.setWidget( i, 1, volumesPathBox );
+		g.setWidget( i, 1, volumesPathBox );*/
 
 		i++; // next row
 		g.setWidget( i, 0, new Label( "Max volume size:" ) );
@@ -346,7 +346,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		volumesTotalPanel.add (new Label ("GB"));
 		g.setWidget( i, 1, volumesTotalPanel );
 
-		i++; // next row
+		/*i++; // next row
 		final CheckBox zeroFillVolumesCheckbox = new CheckBox ();
 		g.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
 		g.setWidget( i, 0, zeroFillVolumesCheckbox );
@@ -365,7 +365,7 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 				}
 			}
 		});
-		g.setWidget( i, 1, new Label ("Zero-fill volumes") );
+		g.setWidget( i, 1, new Label ("Zero-fill volumes") );*/
 
 		i++; // next row
 		g.setWidget( i, 0, new Label( "SAN Host:" ) );
@@ -454,17 +454,17 @@ public class ClusterInfoTable extends VerticalPanel implements ClickListener {
 		cluster.setMaxVlans(Integer.parseInt(((TextBox)p.getWidget(2)).getText()));
 
 		// SC section
-		storage.setHost (((TextBox)g.getWidget(9, 1)).getText());
-		storage.setStorageInterface(((TextBox)g.getWidget(10, 1)).getText());
-		storage.setVolumesPath (((TextBox)g.getWidget(11, 1)).getText());
-		p = (HorizontalPanel)g.getWidget(12, 1);
+		//storage.setHost (((TextBox)g.getWidget(9, 1)).getText());
+		//storage.setStorageInterface(((TextBox)g.getWidget(10, 1)).getText());
+		//storage.setVolumesPath (((TextBox)g.getWidget(11, 1)).getText());
+		p = (HorizontalPanel)g.getWidget(9, 1);
 		storage.setMaxVolumeSizeInGB (Integer.parseInt(((TextBox)p.getWidget(0)).getText()));
-		p = (HorizontalPanel)g.getWidget(13, 1);
+		p = (HorizontalPanel)g.getWidget(10, 1);
 		storage.setTotalVolumesSizeInGB((Integer.parseInt(((TextBox)p.getWidget(0)).getText())));
 	
-		storage.setSanHost (((TextBox)g.getWidget(15, 1)).getText());
-		storage.setSanUser (((TextBox)g.getWidget(16, 1)).getText());
-		storage.setSanPassword (((TextBox)g.getWidget(17, 1)).getText());
+		storage.setSanHost (((TextBox)g.getWidget(11, 1)).getText());
+		storage.setSanUser (((TextBox)g.getWidget(12, 1)).getText());
+		storage.setSanPassword (((TextBox)g.getWidget(13, 1)).getText());
 
 		//storage.setDASPartition(((TextBox)g.getWidget(15, 1)).getText());
 		//    systemConfig.setDoDynamicPublicAddresses( !((TextBox)p.getWidget(0)).isEnabled() ? true : false );
