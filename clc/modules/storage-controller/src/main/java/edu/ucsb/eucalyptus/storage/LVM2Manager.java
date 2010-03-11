a87df17664c912ef8179bea52c039c6c55b3a06e
Neil <neil@pall>
2010-03-10 17:55:21 -0800

    fixes LP: #537058

--- clc/modules/storage-controller/src/main/java/edu/ucsb/eucalyptus/storage/LVM2Manager.java
+++ clc/modules/storage-controller/src/main/java/edu/ucsb/eucalyptus/storage/LVM2Manager.java
@@ -350,7 +350,7 @@ public class LVM2Manager implements LogicalStorageManager {
 		return createLoopback(fileName);
 	}
 
-	public String createLoopback(String fileName) throws EucalyptusCloudException, ExecutionException {
+	public synchronized String createLoopback(String fileName) throws EucalyptusCloudException, ExecutionException {
 		int number_of_retries = 0;
 		int status = -1;
 		String loDevName;
