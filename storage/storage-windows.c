04fa7d120360cc46ba2c4ebafc17273c7c6d23ad
root <root@thunderbird>
2010-03-19 13:51:17 -0700

    add encryptWindowsPassword and decryptWindowsPassword

--- storage/storage-windows.c
+++ storage/storage-windows.c
@@ -11,35 +11,138 @@
 #include <errno.h>
 #include <dirent.h> /* open|read|close dir */
 #include <time.h> /* time() */
+#include <stdint.h>
+
+#include <openssl/sha.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 
 #include <storage-windows.h>
+#include <euca_auth.h>
+
+int decryptWindowsPassword(char *encpass, int encsize, char *pkfile, char **out) {
+  FILE *PKFP;
+  RSA *pr=NULL;
+  char *dec64;
+  int rc;
+  
+  if (!encpass || encsize <= 0 || !*pkfile || !out) {
+    return(1);
+  }
+
+  PKFP = fopen(pkfile, "r");
+  if (!PKFP) {
+    return(1);
+  }
+  if (PEM_read_RSAPrivateKey(PKFP, &pr, NULL, NULL) == NULL) {
+    return(1);
+  }
+  
+  dec64 = base64_dec((unsigned char *)encpass, strlen(encpass));
+  if (!dec64) {
+    return(1);
+  }
+
+  *out = malloc(512);
+  bzero(*out, 512);  
+  rc = RSA_private_decrypt(encsize, (unsigned char *)dec64, (unsigned char *)*out, pr, RSA_PKCS1_PADDING);    
+  if (!*out) {
+    return(1);
+  }
+
+  return(0);
+}
+
+
+int encryptWindowsPassword(char *pass, char *key, char **out, int *outsize) {
+  char *sshkey_dec, *modbuf, *exponentbuf;
+  char *ptr, *tmp, hexstr[2], *enc64;
+  char *dec64, encpassword[512];
+
+  uint32_t len, exponent;
+  int size, ilen, i, encsize=0, rc;
+  RSA *r=NULL;
+  
+  if (!pass || !key || !out || !outsize) {
+    return(1);
+  }
+  
+  size = strlen(key);
+  sshkey_dec = base64_dec((unsigned char *)key, size);
+  if (!sshkey_dec) {
+    return(1);
+  }
+  
+  ptr = sshkey_dec;
+  memcpy(&len, ptr, 4);
+  len = htonl(len);
+  ptr+=4+len;
+
+  memcpy(&len, ptr, 4);
+  len = htonl(len);
+  ptr+=4;
+  
+  // read public exponent
+  exponentbuf = malloc(32768);
+  exponent = 0;
+  memcpy(&exponent, ptr, len);
+  exponent = htonl(exponent);
+  exponent = htonl(exponent);
+  snprintf(exponentbuf, 128, "%08X", exponent);
+  ptr+=len;
+  
+  memcpy(&len, ptr, 4);
+  len = htonl(len);
+  ptr+=4;
+  
+  // read modulus material
+  modbuf = malloc(32768);
+  bzero(modbuf, 32768);
+  ilen = (int)len;
+  for (i=0; i<ilen; i++) {
+    tmp = strndup(ptr, 1);
+    len = *tmp;
+    sprintf(hexstr, "%02X", (len<<24)>>24);
+    strcat(modbuf, hexstr);
+    ptr++;
+  }
+  //  printf("MOD: |%s|\n", modbuf);
+  //  printf("EXPONENT: |%s|\n", exponentbuf);
+    
+  r = RSA_new();
+  if (!r) {
+    free(modbuf); free(exponentbuf);
+    return(1);
+  }
+  if (!BN_hex2bn(&(r->e), exponentbuf) || !BN_hex2bn(&(r->n), modbuf)) {
+    free(modbuf); free(exponentbuf);
+    return(1);
+  }
+
+  bzero(encpassword, 512);
+  encsize = RSA_public_encrypt(strlen(pass), (unsigned char *)pass, (unsigned char *)encpassword, r, RSA_PKCS1_PADDING);
+  if (encsize <= 0) {
+    free(modbuf); free(exponentbuf);
+    return(1);
+  }
+
+  *out = base64_enc((unsigned char *)encpassword, encsize);
+  *outsize = encsize;
+  if (!*out || *outsize <= 0) {
+    free(modbuf); free(exponentbuf);
+    return(1);
+  }
+  
+  return(0);
+}
+
 
 int makeWindowsFloppy(char *euca_home, char *rundir_path, char *keyName, char *password) {
   int fd, rc, rbytes, count;
   char *buf, *ptr, *tmp, *newpass, dest_path[1024], source_path[1024], fname[1024];
-
-  /*
-    char *password, *rundir_path, *euca_home;
-    password = strdup("HALLOTHAR");
-    snprintf(source_path, 1024, "flop");
-    snprintf(dest_path, 1024, "flop.new");
-  */
-
-  /*
-  // generate the pem
-  snprintf(fname, 1024, "%s/key.pub", rundir_path);
-  rc = write2file(fname, keyName);
-  if (rc) {
-    //logprintfl(EUCAERROR, "makeWindowsFloppy(): failed to write public key to file %s\n", fname);
-    return(1);
-  } else {
-    rc = vrun("ssh-keygen -e -f %s > %s.pem", fname, fname);
-    if (rc) {
-      //      logprintfl(EUCAERROR, "makeWindowsFloppy(): failed to convert to PEM\n");
-      return(1);
-    }
-  }
-  */
 
   snprintf(source_path, 1024, "%s/usr/share/eucalyptus/floppy", euca_home);
   snprintf(dest_path, 1024, "%s/floppy", rundir_path);
@@ -48,25 +151,25 @@ int makeWindowsFloppy(char *euca_home, char *rundir_path, char *keyName, char *p
   if (!buf) {
     return(1);
   }
-
+  
   fd = open(source_path, O_RDONLY);
   if (fd < 0) {
     if (buf) free(buf);
     return(1);
   }
-
+  
   rbytes = read(fd, buf, 1024 * 2048);
   close(fd);
-
+  
   ptr = buf;
   count=0;
   tmp = malloc(sizeof(char) * strlen("MAGICEUCALYPTUSPASSWORDPLACEHOLDER")+1);
   newpass = malloc(sizeof(char) * strlen("MAGICEUCALYPTUSPASSWORDPLACEHOLDER")+1);
-
+  
   bzero(tmp, strlen("MAGICEUCALYPTUSPASSWORDPLACEHOLDER")+1);
   bzero(newpass, strlen("MAGICEUCALYPTUSPASSWORDPLACEHOLDER")+1);
   snprintf(newpass, strlen(password)+1, "%s", password);
-
+  
   while(count < rbytes) {
     memcpy(tmp, ptr, strlen("MAGICEUCALYPTUSPASSWORDPLACEHOLDER"));
     if (!strcmp(tmp, "MAGICEUCALYPTUSPASSWORDPLACEHOLDER")) {
--- storage/storage-windows.h
+++ storage/storage-windows.h
@@ -2,5 +2,7 @@
 #define INCLUDE_STORAGE_WINDOWS_H
 
 int makeWindowsFloppy(char *euca_home, char *rundir_path, char *keyName, char *password);
+int encryptWindowsPassword(char *pass, char *key, char **out, int *outsize);
+int decryptWindowsPassword(char *encpass, int encsize, char *pkfile, char **out);
 
 #endif
