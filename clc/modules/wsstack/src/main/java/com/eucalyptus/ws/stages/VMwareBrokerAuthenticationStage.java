2228afa092039c1f4d311f40c94a5f4bcc9551f2
decker <decker@hawaii>
2010-05-06 20:14:24 -0700

    work around to ws-sec issue in vmware

--- clc/modules/wsstack/src/main/java/com/eucalyptus/ws/stages/VMwareBrokerAuthenticationStage.java
+++ clc/modules/wsstack/src/main/java/com/eucalyptus/ws/stages/VMwareBrokerAuthenticationStage.java
@@ -64,10 +64,8 @@
 package com.eucalyptus.ws.stages;
 
 import org.jboss.netty.channel.ChannelPipeline;
-
 import com.eucalyptus.ws.handlers.SoapMarshallingHandler;
 import com.eucalyptus.ws.handlers.soap.SoapHandler;
-import com.eucalyptus.ws.handlers.wssecurity.InternalWsSecHandler;
 import com.eucalyptus.ws.handlers.wssecurity.UserWsSecHandler;
 
 public class VMwareBrokerAuthenticationStage implements UnrollableStage {
@@ -76,7 +74,7 @@ public class VMwareBrokerAuthenticationStage implements UnrollableStage {
   public void unrollStage( final ChannelPipeline pipeline ) {
     pipeline.addLast( "deserialize", new SoapMarshallingHandler( ) );
     pipeline.addLast( "build-soap-envelope", new SoapHandler( ) );
-    //pipeline.addLast( "ws-security", new UserWsSecHandler( ) );
+    pipeline.addLast( "ws-security", new UserWsSecHandler( ) );
   }
 
   @Override
