0e85e0a7dcf7ec966fe0b52113601d42d5e0eaaf
root <root@z2>
2010-05-12 08:40:03 -0700

    forgot new files

---
+++ storage/cmd_create.c
@@ -0,0 +1,380 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // strcmp, strstr
+#include <unistd.h> // access
+#include <assert.h> // duh
+#include "eucalyptus.h" 
+#include "misc.h"
+#include "imager.h"
+#include "cache.h"
+#include "map.h"
+#include "img.h"
+#include "diskfile.h"
+
+#define _M     "* " // mandatory
+#define _CACHE "cache"
+#define _FMT   "format"
+#define _MBR   "mbr"
+#define _OUT   "out"
+#define _PCONT "p#-content"
+#define _PFMT  "p#-format"
+#define _PSIZE "p#-size"
+#define _SIZE  "size"
+#define _WORK  "work"
+
+#define _MAX_PARTS 3 // maximum number of partitions supported by 'create'
+
+static char * params [] = {
+	_CACHE,     "cache the object? (default=no)",
+	_FMT,       "format object as {ext3|swap} partition",
+	_MBR,       "format object as disk with MBR of type {msdos}",
+	_M _OUT,    "local name for the output object",
+	_PCONT,     "copy this object to disk partition #",
+	_PFMT,      "format disk partition # as {ext3|swap}",
+	_PSIZE,     "size, in bytes, of disk partition #",
+    _SIZE,      "size, in bytes of the object (i.e., disk/partition)",
+	_WORK,      "create a work copy? (default=yes)",
+	NULL
+};
+
+typedef struct _create_params {
+	boolean cache;
+	enum pformat_t format;
+	enum mbr_t mbr;
+	char * out;
+	struct _part_type {
+		char * content;
+		enum pformat_t format;
+		long long size;
+	} parts [_MAX_PARTS]; 
+	long long size;
+	boolean work;
+	boolean disk;
+	int nparts;
+	int num_ext_inputs;
+} create_params;
+
+#define PFORMAT_DEFAULT PFORMAT_EXT3 // when format isn't specified for content
+
+static int verify_part (struct _part_type * p, const int n)
+{
+	if (p->content) {
+		if (p->size > 0)
+			err ("in partition %d, 'size' is not needed with 'content'", n);
+		if (p->format == PFORMAT_UNKNOWN) {
+			logprintfl (EUCAWARN, "warning: assuming '%s' as format for partition %d\n", enum_format_as_string (PFORMAT_DEFAULT), n);
+			p->format = PFORMAT_DEFAULT;
+		}
+	} else if (p->format != PFORMAT_UNKNOWN) {
+		if (p->size < 1)
+			err ("in partition %d, 'size' is needed with 'format'", n);
+		long long min = get_min_size (pformat_to_diskpart_t (p->format)); // argh, is type checking of enums too much to ask for?
+		if (p->size < min)
+			err ("in partition %d, of type '%s', 'size' must be at least %lld bytes", n, enum_format_as_string (p->format), min);
+	} else if (p->size > 0) {
+		err ("in partition %d, 'format' is needed with 'size'", n);
+	} else {
+		return 0;
+	}
+	return 1;
+}
+
+char ** create_parameters () 
+{
+	return params;
+}
+
+static int parse_part (char * key, char * subpar) 
+{
+	if (key [0] != 'p') return -1;
+	char * hyphen = strstr (key, subpar);
+	if (hyphen == NULL) return -1;
+	char buf [10];
+	strncpy (buf, key + 1, hyphen - key - 1);
+	int part = atoi (buf);
+	if (part>=_MAX_PARTS) 
+		err ("partition %d is outside of the range of supported values: [0-%d]", part, _MAX_PARTS-1);
+	return part;
+}
+
+int create_validate (imager_request * req)
+{
+	print_req (req);
+
+	create_params * state = calloc (sizeof (create_params), 1);
+	if (state==NULL) 
+		err ("out of memory");
+
+	// default values
+	state->cache = FALSE;
+	state->work = TRUE;
+
+	// record in 'state' all specified parameters
+	boolean have_partitions = FALSE;
+	for (imager_param * p = req->params; p!=NULL && p->key!=NULL; p++) {
+		if      (strcmp (p->key, _CACHE)==0) {state->cache    = parse_boolean (p->val); }
+		else if (strcmp (p->key, _FMT)==0)   {state->format   = parse_pformat_t_enum (p->val); }
+		else if (strcmp (p->key, _MBR)==0)   {state->mbr      = parse_mbr_t_enum (p->val); }
+		else if (strcmp (p->key, _OUT)==0)   {state->out      = p->val; }
+		else if (strcmp (p->key, _SIZE)==0)  {state->size     = atoll (p->val); }
+		else if (strcmp (p->key, _WORK)==0)  {state->work     = parse_boolean (p->val); }
+		else {
+			int part;
+
+			if ((part = parse_part (p->key, "-content"))>=0) {
+				state->parts [part].content = p->val;
+				have_partitions = TRUE;
+			} else if ((part = parse_part (p->key, "-format"))>=0) {
+				state->parts [part].format = parse_pformat_t_enum (p->val);
+				have_partitions = TRUE;
+			} else if ((part = parse_part (p->key, "-size"))>=0) {
+				long long size = atoll (p->val);
+				if (size<1) { // TODO: what's a reasonable minimum for object?
+					err ("in partition %d, invalid size spec (%lld)", part, size);
+				}
+				state->parts [part].size = size;
+				have_partitions = TRUE;
+			} else {
+				err ("invalid parameter '%s' for command 'create'", p->key);
+			}
+		}
+	}
+
+	// ensure mandatory params are present
+	if (state->out==NULL) 
+		err ("missing mandatory parameter '" _OUT "'");
+	if (state->format==PFORMAT_UNKNOWN && state->mbr==MBR_NONE) 
+		err ("either 'format' (for partition) or 'mbr' (for disk) must be specified");
+
+	// if creating a partition
+	if (state->format!=PFORMAT_UNKNOWN) {
+		if (state->mbr!=MBR_NONE)
+			err ("'format' (for partition) and 'mbr' (for disk) are mutually exclusive");
+		if (have_partitions)
+			err ("p#-* parameters are for use with disks, not with partitions");
+		if (state->size<1)
+			err ("'size' must be specified for the partition");
+
+	} else { // if creating a disk
+		if (have_partitions) {
+			boolean no_prev = FALSE;
+			for (int i=0; i<_MAX_PARTS; i++) { // verify all partition specs
+				if (verify_part (&(state->parts [i]), i)) {
+					if (no_prev == TRUE)  // ensure they are contiguous
+						err ("partition %d follows unspecified partition %d", i, i-1);
+					state->nparts++;
+				} else {
+					no_prev = TRUE;
+				}
+			}
+		} else {
+			if (state->size<1)
+				err ("either give overall disk 'size' or the individual partitions");
+		}
+		state->disk = TRUE;
+	}
+	req->internal = (void *) state; // save pointer to find it later
+
+	return 0;
+}
+
+int create_requirements (imager_request * req)
+{
+	create_params * state = (create_params *) req->internal;
+	artifacts_spec * input_specs [_MAX_PARTS];
+	artifacts_spec * output_spec;
+	int num_ext_inputs = 0;
+	long long size = 0;
+	char hash [32];
+
+	char * attrs [_MAX_PARTS*2+8+1];
+	if (attrs==NULL) {
+		logprintfl (EUCAERROR, "error: out of memory for attributes\n");
+		return ERROR;
+	}
+#define _SET_ATTR(_s1,_s2) attrs [n++] = _s1; attrs [n++] = _s2
+	int n = 0;
+	_SET_ATTR("id", state->out);
+	
+	if (state->disk) { // creating a disk, which may have external inputs
+		_SET_ATTR("type", "disk");
+
+		// run through specified partitions, if any
+		for (int i=0; i<state->nparts; i++) {
+			struct _part_type * p = &(state->parts [i]);
+			char * format;
+			long long part_size;
+
+			if (p->content) { // the partition will consist of external copied content
+				format = p->content;
+				artifacts_spec * input_spec = map_get (get_artifacts_map(), p->content);
+				if (input_spec==NULL) { // no artifact from an earlier stage
+					part_size = file_size (p->content); // assume that file size won't change in conversion
+				} else {
+					input_specs [num_ext_inputs++] = input_spec;
+					part_size = input_spec->size; // assume that file size won't change in conversion
+				}
+				if (part_size<0) {
+					logprintfl (EUCAINFO, "failed to locate required input '%s'\n", p->content);
+					return ERROR;
+				}
+				p->size = part_size; // record the size of the input file
+			} else { // partition will be formatted
+				format = enum_format_as_string (p->format);
+				part_size = p->size;
+			}
+			_SET_ATTR("includes-partition", format);
+			size += round_up_sec (part_size);
+		} 
+		
+	} else { // creating a partition doesn't take external inputs
+		_SET_ATTR("type", "partition");
+		_SET_ATTR("format", enum_format_as_string (state->format));
+		size = round_up_sec (state->size);
+	}
+
+	// increase the size required to accommodate MBR, etc
+	size += mbr_size_bytes ();
+
+	char size_str [30];
+	snprintf (size_str, sizeof (size_str), "%lld", size);
+	_SET_ATTR("size", size_str);
+	//_SET_ATTR("hash", hash); // TODO: hash
+	assert (sizeof(attrs)/sizeof(char *)>(n-1));
+	attrs [n++] = NULL;
+
+	output_spec = alloc_artifacts_spec (req, attrs);
+	if (output_spec==NULL) {
+		logprintfl (EUCAERROR, "error: out of memory for artifacts\n");
+		return ERROR;
+	}
+	
+	for (int i=0; i<num_ext_inputs; i++) {
+		output_spec->deps [i] = input_specs [i]; // point back to inputs we depend on
+	}
+
+	state->num_ext_inputs = num_ext_inputs;
+	output_spec->size = round_up_sec (size);
+	map_set (get_artifacts_map(), state->out, (void *)output_spec);
+	
+	return OK;
+}
+
+int create_execute (imager_request * req)
+{
+	create_params * state = (create_params *) req->internal;
+	char * id = state->out;
+	boolean success = TRUE;
+	int ret = OK;
+
+	// look up the artifacts from the preceding requirements() invocation
+	artifacts_spec * spec = map_get (get_artifacts_map(), id);
+	if (spec==NULL) {
+		logprintfl (EUCAERROR, "error: execute() called before requirements() for 'create'\n");
+		return ERROR;
+	}
+
+	output_file * o = preprocess_output_path (id, spec, state->work, state->cache, NULL);
+	if (o==NULL) {
+		free (spec);
+		return ERROR;
+	}
+
+	if (strlen (o->path)) { // valid output file does not exist
+
+		// if expecting local input files
+		if (state->num_ext_inputs > 0) {
+			for (int i=0; i<state->num_ext_inputs; i++) {
+
+                // run the dependent stage(s) to bring in the input
+				logprintfl (EUCAINFO, "stage '%s' executing previous %d stage(s)...\n", req->cmd->name, state->num_ext_inputs);
+				ret = spec->deps[i]->req->cmd->execute(spec->deps[i]->req);
+				if (ret!=OK) 
+					goto cleanup;
+			}
+
+/*
+			// update partition sizes based on sizes of input files
+			for (int i=0; i<state->nparts; i++) {
+				struct _part_type * p = &(state->parts [i]);
+				if (p->size==0) {
+					p->size = file_size (p->content);
+					if (p->size < 1) {
+						logprintfl (EUCAERROR, "input file '%s' is missing\n", p->content);
+						ret = ERROR;
+						goto cleanup;
+					}
+				}
+			}
+*/
+		}
+
+		// create the output file
+		logprintfl (EUCAINFO, "creating '%s'...\n", id);
+		diskfile * df = df_create (o->path, spec->size, FALSE);
+		if (df==NULL) {
+			ret = ERROR;
+			goto close;
+		}
+
+		if (state->disk) {
+			// build up a list of partitions
+			diskpart * dp = calloc (state->nparts + 1, sizeof (diskpart));				
+			if (dp==NULL) {
+				logprintfl (EUCAERROR, "out of memory in execute()\n");
+				ret = ERROR;
+				goto close;
+			}
+			for (int i=0; i<state->nparts; i++) {
+				struct _part_type * p = &(state->parts [i]);
+				dp [i].size_bytes = p->size;
+				dp [i].type = pformat_to_diskpart_t (p->format);
+			}
+			
+#define     _CALL_CHECK(f) if (f!=OK) { \
+				ret = ERROR; \
+				goto close; \
+			}
+
+			// create the partitions
+			_CALL_CHECK(df_partition (df, MBR_MSDOS, dp));
+				
+			// either format or populate the partition
+			for (int i=0; i<state->nparts; i++) {
+				struct _part_type * p = &(state->parts [i]);
+				if (p->content) {
+					_CALL_CHECK(df_dd (df, i, p->content));
+				} else {
+					_CALL_CHECK(df_format (df, i, p->format));
+				}
+			}
+		} else { // a partition => format it
+			_CALL_CHECK(df_format (df, 0, state->format));
+		}
+
+close:
+
+		df_close (df);
+	}
+
+cleanup:
+
+	if (ret!=OK) {
+		logprintfl (EUCAERROR, "error: failed to create '%s'\n", id);
+		success = FALSE;
+	}
+	postprocess_output_path (o, success);
+	// TODO: free spec
+	
+	return ret;
+}
+
+int create_cleanup (imager_request * req)
+{
+	create_params * state = (create_params *) req->internal;
+
+	logprintfl (EUCAINFO, "cleaning up for '%s'...\n", req->cmd->name);
+	free (state);
+
+	return 0;
+}
---
+++ storage/imager.c
@@ -0,0 +1,377 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h> // getopt
+#include <fcntl.h> // open
+#include <ctype.h> // tolower
+#include <errno.h> // errno
+
+#include "euca_auth.h"
+#include "eucalyptus.h"
+#include "misc.h"
+#include "imager.h"
+#include "cmd.h"
+#include "map.h"
+#include "cache.h"
+
+extern char ** environ;
+
+#define MAX_REQS 32
+#define MAX_PARAMS 32
+
+static imager_request reqs [MAX_REQS];
+static char * euca_home = NULL;
+static map * artifacts_map;
+
+static void usage (const char * msg) 
+{ 
+    if (msg!=NULL)
+        fprintf (stderr, "error: %s\n\n", msg);
+        
+    fprintf (stderr, "Usage: euca_imager [command param=value param2=value ...] [command2 ...]\n"); 
+
+	if (msg==NULL) 
+		fprintf (stderr, "Try 'euca_imager help' for list of commands\n");
+
+    exit (1); 
+}
+
+void err (const char *format, ...)
+{
+    va_list ap;
+
+    va_start(ap, format);
+    fprintf (stderr, "error: ");
+    vfprintf(stderr, format, ap);
+    fprintf (stderr, "\n");
+    fflush(stderr);
+    va_end(ap);
+
+    exit (1);
+}
+
+// finds a command by name among known_cmds[] defined in cmd.h
+static imager_command * find_struct (char * name) 
+{
+	for (int i=0; i<(sizeof(known_cmds)/sizeof(imager_command)); i++) {
+		if (strcmp(name, known_cmds[i].name)==0) {
+			return &known_cmds[i];
+		}
+	}
+	return NULL;
+}
+
+// either prints help messages or runs command-specific validator
+static imager_command * validate_cmd (int index, char *this_cmd, imager_param *params, char *next_cmd)
+{
+	if (this_cmd==NULL) 
+		return NULL;
+
+	char * cmd = this_cmd;
+	char help = 0;
+
+	// see if it is a help request
+	if (strcmp(cmd, "help")==0) {
+		help = 1;
+		if (next_cmd==NULL) { // not specific
+			fprintf (stderr, "supported commands:\n");
+			for (int i=0; i<(sizeof(known_cmds)/sizeof(imager_command)); i++) {
+				fprintf (stderr, "\t%s\n", known_cmds[i].name);
+			}
+			exit (0);
+		} else { // help for a specific command
+			cmd = next_cmd;
+		}
+	}
+
+	// find the function pointers for the command
+	imager_command *cmd_struct = find_struct (cmd);
+	if (cmd_struct==NULL)
+		err ("command '%s' not found", cmd);
+
+	// print command-specific help
+	if (help) {
+		char ** p = cmd_struct->parameters();
+		if (p==NULL || *p==NULL) {
+			fprintf (stderr, "command '%s' has no parameters\n", cmd_struct->name);
+		} else {
+			fprintf (stderr, "parameters for '%s' (* - mandatory):\n", cmd_struct->name);
+			while (*p && *(p+1)) {
+				fprintf (stderr, "%18s - %s\n", *p, *(p+1));
+				p+=2;
+			}
+		}
+		exit (0);
+	}
+
+	// fill out the request struct and pass to the validator
+	imager_request * req = & reqs [index];
+	req->cmd = cmd_struct;
+	req->params = params;
+	req->internal = NULL;
+	if (cmd_struct->validate (req))
+		err ("incorrect parameters for command '%s'", cmd_struct->name);
+
+	return cmd_struct;
+}
+
+static void set_global_parameter (char * key, char * val)
+{
+	if (strcmp (key, "work")==0) {
+		set_work_dir (val);
+	} else if (strcmp (key, "work_size")==0) {
+		set_work_limit (atoll(val));
+	} else if (strcmp (key, "cache")==0) {
+		set_cache_dir (val);
+	} else if (strcmp (key, "cache_size")==0) {
+		set_cache_limit (atoll(val));
+	} else {
+		err ("unknown global parameter '%s'", key);
+	}
+}
+
+int main (int argc, char * argv[])
+{
+    logfile (NULL, EUCADEBUG); // so euca libs will log to stdout
+
+	// initialize globals
+	artifacts_map = map_create (10);
+
+	// use $EUCALYPTUS env var if available
+    char root [] = "";
+	euca_home = getenv (EUCALYPTUS_ENV_VAR_NAME);
+    if (!euca_home) {
+        euca_home = root;
+    }
+
+	// parse command-line parameters
+	char * cmd_name = NULL;
+	imager_param * cmd_params = NULL;
+	int nparams = 0;
+	int ncmds = 0;
+	while ( *(++argv) ) {
+		char * eq = strstr(*argv, "="); // all params have '='s
+
+		if (eq==NULL) { // it's a command
+			// process previous command, if any
+			if (validate_cmd (ncmds, cmd_name, cmd_params, *argv)!=NULL)
+				ncmds++; // increment only if there was a previous command
+			
+			if (ncmds+1>MAX_REQS)
+				err ("too many commands (max is %d)", MAX_REQS);
+
+			cmd_name = * argv;
+			cmd_params = NULL;
+			nparams = 0;
+
+		} else { // this is a parameter
+			if (strlen (eq) == 1)
+				usage ("parameters must have non-empty values");
+			* eq = '\0'; // split key from value
+			if (strlen (* argv) == 1)
+				usage ("parameters must have non-empty names");
+			char * key = * argv;
+			char * val = eq + 1;
+			if (key==NULL || val==NULL) 
+				usage ("syntax error in parameters");
+			if (key[0]=='-') key++; // skip '-' if any
+			if (key[0]=='-') key++; // skip second '-' if any
+
+			if (cmd_name==NULL) { // without a preceding command => global parameter
+				set_global_parameter (key, val);
+				continue;
+			}
+
+			if (cmd_params==NULL) {
+				cmd_params = calloc (MAX_PARAMS+1, sizeof(imager_param)); // +1 for terminating NULL
+			}
+			if (nparams+1>MAX_PARAMS)
+				err ("too many parameters (max is %d)", MAX_PARAMS);
+			cmd_params[nparams].key = key;
+			cmd_params[nparams].val = val;
+			nparams++;
+		}
+	}
+	if (validate_cmd (ncmds, cmd_name, cmd_params, *argv)!=NULL)
+		ncmds++;
+
+	logprintfl (EUCAINFO, "verified all parameters for %d command(s)\n", ncmds);
+
+	// invoke the requirements checkers in the same order as on command line
+	for (int i=0; i<ncmds; i++)
+		if (reqs[i].cmd->requirements!=NULL)
+			if (reqs[i].cmd->requirements (&reqs[i])!=OK)
+				err ("failed while verifying requirements");
+
+	// invoke the last command, which will trigger the commands it depends on
+	for (int i=ncmds-1; i>=0; i--) {
+		if (reqs[ncmds-1].cmd->execute!=NULL) {
+			reqs[ncmds-1].cmd->execute(&reqs[ncmds-1]);
+			break;
+		}
+	}
+
+	// invoke the cleaners for each command to tidy up disk space and memory allocations
+	for (int i=0; i<ncmds; i++)
+		if (reqs[i].cmd->cleanup!=NULL)
+			reqs[i].cmd->cleanup (&reqs[i]);
+
+	exit (0);
+}
+
+// common functions used by commands
+
+void print_req (imager_request * req) 
+{
+	logprintfl (EUCAINFO, "command: %s\n", req->cmd->name);
+	for (imager_param * p = req->params; p!=NULL && p->key!=NULL; p++) {
+		logprintfl (EUCAINFO, "\t%s=%s\n", p->key, p->val);
+	}
+}
+
+// tolower for strings
+
+char * strduplc (const char * s)
+{
+	char * lc = strdup (s);
+	for (int i=0; i<strlen(s); i++) {
+		lc [i] = tolower(lc [i]);
+	}
+	return lc;
+}
+
+// turn a string into a boolean (returned as a char)
+
+char parse_boolean (const char * s)
+{
+	char * lc = strduplc (s);
+	char val;
+
+	if (strcmp (lc, "y")==0 ||
+		strcmp (lc, "yes")==0 ||
+		strcmp (lc, "t")==0 ||
+		strcmp (lc, "true")==0) { val = 1; }
+	else if (strcmp (lc, "n")==0 ||
+			 strcmp (lc, "no")==0 ||
+			 strcmp (lc, "f")==0 ||
+			 strcmp (lc, "false")==0) {	val = 0; }
+	else 
+		err ("failed to parse value '%s' as boolean", lc);
+	free (lc);
+	
+	return val;
+}
+
+// read in login or password from command line or from a file
+
+char * parse_loginpassword (char * s)
+{
+	char * val = s;
+	FILE * fp;
+	if ((fp = fopen (s, "r"))!=NULL) {
+		val = fp2str (fp);
+		if (val==NULL) {
+			err ("failed to read file '%s'", s);
+		} else {
+			logprintfl (EUCAINFO, "read in contents from '%s'\n", s);
+		}
+	}
+
+	return val;
+}
+
+// make sure the path exists and is readable
+
+int verify_readability (const char * path)
+{
+	if (fopen (path, "r")==NULL)
+		err ("unable to read '%s'", path);
+	return 0;
+}
+
+// return eucalyptus root
+
+char * get_euca_home (void)
+{
+	return euca_home;
+}
+
+// return global artifacts map
+
+map * get_artifacts_map (void)
+{
+	return artifacts_map;
+}
+
+// if path=A/B/C but only A exists, this will try to create B and C
+int ensure_path_exists (const char * path, mode_t mode)
+{
+	int len = strlen (path);
+	char * path_copy = strdup (path);
+	int i;
+	
+	if (path_copy==NULL) 
+		return errno;
+	
+	for (i=0; i<len; i++) {
+		struct stat buf;
+		char try_it = 0;
+		
+		if (path[i]=='/' && i>0) {
+			path_copy[i] = '\0';
+			try_it = 1;
+		} else if (path[i]!='/' && i+1==len) { // last one
+			try_it = 1;
+		}
+		
+		if ( try_it ) {
+			if ( stat (path_copy, &buf) == -1 ) {
+				logprintfl (EUCAINFO, "trying to create path %s\n", path_copy);
+				
+				if ( mkdir (path_copy, mode) == -1) {
+					logprintfl (EUCAERROR, "error: failed to create path %s: %s\n", path_copy, strerror (errno));
+					
+					if (path_copy) 
+						free (path_copy);
+					return errno;
+				}
+			} else {
+				if ( chmod (path_copy, mode) == -1) {
+					logprintfl (EUCAERROR, "error: failed to chmod path %s: %s\n", path_copy, strerror (errno));
+
+					if (path_copy)
+						free (path_copy);
+					return errno;
+				}
+			}
+			path_copy[i] = '/'; // restore the slash
+		}
+	}
+	
+	free (path_copy);
+	return 0;
+}
+
+// if path=A/B/C but only A exists, this will try to create B, but not C
+int ensure_dir_exists (const char * path, mode_t mode)
+{
+  int len = strlen (path);
+  char * path_copy = strdup (path);
+  int i, err = 0;
+  
+  if (path_copy==NULL) 
+    return errno;
+  
+  for (i=len-1; i>0; i--) {
+    if (path[i]=='/') {
+      path_copy[i] = '\0';
+      err = ensure_path_exists (path_copy, mode);
+      break;
+    }
+  }
+  
+  free (path_copy);
+  return err;
+}
