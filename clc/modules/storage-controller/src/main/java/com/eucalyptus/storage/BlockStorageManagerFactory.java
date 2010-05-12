39a67d64df5e31e1b1ccb972bdddf531512d4421
Neil <neil@pall>
2010-05-12 09:38:39 -0700

    don't require fqn for selecting storage backend. Allow other SAN provider to be selected.

--- clc/modules/storage-controller/src/main/java/com/eucalyptus/storage/BlockStorageManagerFactory.java
+++ clc/modules/storage-controller/src/main/java/com/eucalyptus/storage/BlockStorageManagerFactory.java
@@ -70,13 +70,14 @@ import org.apache.log4j.Logger;
 public class BlockStorageManagerFactory {
 	private static Logger LOG = Logger.getLogger(BlockStorageManagerFactory.class);
 	public static LogicalStorageManager getBlockStorageManager() {
-		String ebsManager = "com.eucalyptus.storage.LVM2Manager";
+		String ebsManager = "LVM2Manager";
 		if(System.getProperty("euca.disable.san") == null) {
 			if(System.getProperty("ebs.storage.manager") != null) {
 				ebsManager = System.getProperty("ebs.storage.manager");
 			}
 		}
 		try {
+			ebsManager = "com.eucalyptus.storage." + ebsManager;
 			return (LogicalStorageManager) ClassLoader.getSystemClassLoader().loadClass(ebsManager).newInstance();
 		} catch (InstantiationException e) {
 			LOG.error(e, e); 
