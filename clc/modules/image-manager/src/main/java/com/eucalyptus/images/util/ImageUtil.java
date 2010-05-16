26e8c2dd99c2bc9aa164706d140f2fdcb645488c
decker <decker@hawaii>
2010-05-16 16:28:49 -0700

    fix windows constraints

--- clc/modules/image-manager/src/main/java/com/eucalyptus/images/util/ImageUtil.java
+++ clc/modules/image-manager/src/main/java/com/eucalyptus/images/util/ImageUtil.java
@@ -257,7 +257,7 @@ public class ImageUtil {
   }
   public static VmImageInfo getVmImageInfo( final String walrusUrl, final Image diskInfo, final Image kernelInfo, final Image ramdiskInfo, final ArrayList<String> productCodes ) throws EucalyptusCloudException {
     String diskUrl = getImageUrl( walrusUrl, diskInfo );
-    String kernelUrl = getImageUrl( walrusUrl, kernelInfo );
+    String kernelUrl = kernelInfo != null ? getImageUrl( walrusUrl, kernelInfo ) : null;
     String ramdiskUrl = null;
     if ( ramdiskInfo != null ) ramdiskUrl = getImageUrl( walrusUrl, ramdiskInfo );
     //:: create the response assets now since we might not have a ramdisk anyway :://
