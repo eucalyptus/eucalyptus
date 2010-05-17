4366a440354f9bd08f4d920aad76171fed562894
Dmitrii Zagorodnov <dmitrii@eucalyptus.com>
2010-05-16 19:51:50 -0700

    added debug=yes to imager; refactored bundling ops; removed setting of LD_LIBRARY_PATH

--- clc/modules/msgs/src/main/java/edu/ucsb/eucalyptus/msgs/VMwareBroker.groovy
+++ clc/modules/msgs/src/main/java/edu/ucsb/eucalyptus/msgs/VMwareBroker.groovy
@@ -1,7 +1,11 @@
 package edu.ucsb.eucalyptus.msgs
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 
+import com.eucalyptus.cloud.ws.VMwareInstance;
 import com.eucalyptus.util.EucalyptusCloudException;
 
 /*
@@ -187,6 +191,18 @@ public class BundleTaskType extends EucalyptusData {
 		this.instanceId = instanceId;
 		this.state = state;
 		this.manifest = manifest;
+	}
+
+	public static ArrayList<BundleTaskType> convertFromInstances(Set<VMwareInstance> instances) {
+		ArrayList<BundleTaskType> bundleTasks = new ArrayList<BundleTaskType>();
+		for (VMwareInstance instance : instances.values()) {
+			BundleTaskType bundleTask = new BundleTaskType (
+					instance.getInstanceId(), 
+					instance.getBundleTaskStateName(),
+					null);
+			bundleTasks.add (bundleTask);
+		}
+		return bundleTasks;
 	}
 }
 
