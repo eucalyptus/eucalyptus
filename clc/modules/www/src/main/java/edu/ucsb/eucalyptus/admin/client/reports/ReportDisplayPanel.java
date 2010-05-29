3e5921fe370937906193dc87d5642c6c0a6dd6cb
decker <decker@personal-army>
2010-05-28 21:19:50 -0700

    revert table names for all the image related stuff

--- clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/reports/ReportDisplayPanel.java
+++ clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/reports/ReportDisplayPanel.java
@@ -33,34 +33,6 @@ public class ReportDisplayPanel extends VerticalPanel implements Observer {
     this.rightBar.setHorizontalAlignment( ALIGN_RIGHT );
     this.report = new Frame( );
     this.report.setStyleName( AccountingControl.RESOURCES.REPORT_FRAME_STYLE );
-    DatePicker datePicker = new DatePicker();
-    final Label text = new Label();
-
-    // Set the value in the text box when the user selects a date
-    datePicker.addValueChangeHandler(new ValueChangeHandler<Date>() {
-      public void onValueChange(ValueChangeEvent<Date> event) {
-        Date date = event.getValue();
-        String dateString = DateTimeFormat.getMediumDateFormat().format(date);
-        text.setText(dateString);
-      }
-    });
-
-    // Set the default value
-    datePicker.setValue(new Date(), true);
-
-    // Create a DateBox
-    DateTimeFormat dateFormat = DateTimeFormat.getLongDateFormat();
-    DateBox dateBox = new DateBox();
-    dateBox.setFormat(new DateBox.DefaultFormat(dateFormat));
-
-    // Combine the widgets into a panel and return them
-    VerticalPanel vPanel = new VerticalPanel();
-    vPanel.add(new HTML(constants.cwDatePickerLabel()));
-    vPanel.add(text);
-    vPanel.add(datePicker);
-    vPanel.add(new HTML(constants.cwDatePickerBoxLabel()));
-    vPanel.add(dateBox);
-    return vPanel;
   }
   
   public void update( ) {
