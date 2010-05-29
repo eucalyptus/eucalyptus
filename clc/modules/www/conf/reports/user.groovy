e11c9e39bdae1f3f0b6241b516dc021f6327058f
decker <decker@personal-army>
2010-05-28 20:50:50 -0700

    tweak user script

--- clc/modules/www/conf/reports/user.groovy
+++ clc/modules/www/conf/reports/user.groovy
@@ -1,12 +1,14 @@
-def class UserReportInfo {
-  String userName;
-  Integer imageCount;
-}
-for( Integer i = 0; i < 20; i++ ) {
+for( int i = 0; i < 20; i++ ) {
   def u = new UserReportInfo() {{
           userName = "test-${i}" 
           imageCount = i
         }};
   results.add( u )
+}
+println "HELLOOOO ${results}"
+
+def class UserReportInfo {
+  String userName;
+  Integer imageCount;
 }
 
