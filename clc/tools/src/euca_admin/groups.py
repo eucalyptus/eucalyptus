6e2242c2e21b6d8ac5047ea5a2527d10f51e389d
Neil <neil@pall>
2010-06-07 11:49:15 -0700

    fixes to cmdline admin tools.

--- clc/tools/src/euca_admin/groups.py
+++ clc/tools/src/euca_admin/groups.py
@@ -1,6 +1,8 @@
 import boto,sys,euca_admin
 from boto.exception import EC2ResponseError
 from euca_admin.generic import BooleanResponse
+from euca_admin.generic import StringList
+from boto.resultset import ResultSet
 from euca_admin import EucaAdmin
 from optparse import OptionParser
 
@@ -10,62 +12,79 @@ class Group():
   
   def __init__(self, groupName=None):
     self.group_groupName = groupName
-    self.group_users = []
+    self.group_users = StringList()
+    self.group_auths = StringList()
     self.euca = EucaAdmin(path=SERVICE_PATH)
           
   def __repr__(self):
-    r = ''
-    for s in self.group_users:
-      r = '%s\t%s' % (r,s)
-    r = 'GROUP\t%s\t%s' % (self.group_groupName,r)
+    r = 'GROUP      \t%s\t' % (self.group_groupName)
+    r = '%s\nUSERS\t%s\t%s' % (r,self.group_groupName,self.group_users)
+    r = '%s\nAUTH\t%s\t%s' % (r,self.group_groupName,self.group_auths)
     return r
       
   def startElement(self, name, attrs, connection):
+    if name == 'euca:users':
+      return self.group_users
+    if name == 'euca:authorizations':
+      return self.group_auths
+    else:
       return None
 
   def endElement(self, name, value, connection):
     if name == 'euca:groupName':
       self.group_groupName = value
-    elif name == 'euca:entry':
-      self.user_groups.append(value)
     else:
       setattr(self, name, value)
           
   def get_describe_parser(self):
-    parser = OptionParser("usage: %prog [options]",version="Eucalyptus %prog VERSION")
-    return parser
+    parser = OptionParser("usage: %prog [GROUPS...]",version="Eucalyptus %prog VERSION")
+    return parser.parse_args()
   
-  def describe(self):
+  def cli_describe(self):
+    (options, args) = self.get_describe_parser()
+    self.group_describe(args)
+    
+  def group_describe(self,groups=None):
+    params = {}
+    if groups:
+      self.euca.connection.build_list_params(params,groups,'GroupNames')
     try:
-      list = self.euca.connection.get_list('DescribeGroups', {}, [('euca:item', Group)])
+      list = self.euca.connection.get_list('DescribeGroups', params, [('euca:item', Group)])
       for i in list:
         print i
     except EC2ResponseError, ex:
       self.euca.handle_error(ex)
 
+  def get_single_parser(self):
+    parser = OptionParser("usage: %prog GROUPNAME",version="Eucalyptus %prog VERSION")
+    (options,args) = parser.parse_args()    
+    if len(args) != 1:
+      print "ERROR  Required argument GROUPNAME is missing or malformed."
+      parser.print_help()
+      sys.exit(1)
+    else:
+      return (options,args)  
 
-  def get_add_parser(self):
-    parser = OptionParser("usage: %prog [options]",version="Eucalyptus %prog VERSION")
-    parser.add_option("-n","--name",dest="groupName",help="Name of the group.")
-    return parser
+  def cli_add(self):
+    (options, args) = self.get_single_parser()
+    self.group_add(args[0])
 
-
-  def add(self, groupName):
+  def group_add(self, groupName):
     try:
       reply = self.euca.connection.get_object('AddGroup', {'GroupName':groupName}, BooleanResponse)
       print reply
     except EC2ResponseError, ex:
       self.euca.handle_error(ex)
 
-  def get_delete_parser(self):
-    parser = OptionParser("usage: %prog [options]",version="Eucalyptus %prog VERSION")
-    parser.add_option("-n","--name",dest="groupName",help="Name of the Group.")
-    return parser
+  def cli_delete(self):
+    (options, args) = self.get_single_parser()
+    self.group_delete(args[0])
             
-  def delete(self, groupName):
+  def group_delete(self, groupName):
     try:
       reply = self.euca.connection.get_object('DeleteGroup', {'GroupName':groupName},BooleanResponse)
       print reply
     except EC2ResponseError, ex:
       self.euca.handle_error(ex)
-        
+
+
