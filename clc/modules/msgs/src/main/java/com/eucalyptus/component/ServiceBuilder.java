79add405b403cf0a8f86fbff1a0af6aa3e0a76cb
decker <decker@hawaii>
2010-10-18 13:49:41 -0700

    add ServiceBuilder.fireEnable/fireDisable

--- clc/modules/msgs/src/main/java/com/eucalyptus/component/ServiceBuilder.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/component/ServiceBuilder.java
@@ -37,7 +37,10 @@ public interface ServiceBuilder<T extends ServiceConfiguration> {
   public abstract ServiceConfiguration toConfiguration( URI uri ) throws ServiceRegistrationException;
   public abstract void fireStart( ServiceConfiguration config ) throws ServiceRegistrationException;
   public abstract void fireStop( ServiceConfiguration config ) throws ServiceRegistrationException;
+  public abstract void fireEnable( ServiceConfiguration config ) throws ServiceRegistrationException;
+  public abstract void fireDisable( ServiceConfiguration config ) throws ServiceRegistrationException;
   public abstract List<T> list() throws ServiceRegistrationException;
   public abstract T lookupByName( String name ) throws ServiceRegistrationException;
   public abstract T lookupByHost( String name ) throws ServiceRegistrationException;
+  
 }
