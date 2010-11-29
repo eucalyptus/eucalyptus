f1e7cbde8dc6bd8fbe4cbf45b4f3040e3c469b37
wenye <wenye@gushan>
2010-11-28 19:12:37 -0800

    Fix test

--- clc/modules/authentication/src/main/java/com/eucalyptus/auth/AuthTest.java
+++ clc/modules/authentication/src/main/java/com/eucalyptus/auth/AuthTest.java
@@ -154,7 +154,7 @@ public class AuthTest {
         "'Statement':[{" +
           "'Effect':'Allow'," +
           "'Action':'ec2:RunInstances'," +
-          "'NotResource':'emi-1234*'," +
+          "'NotResource':'arn:aws:ec2:::image/emi-1234*'," +
           "'Condition':{" +
             "'DateLessThanEquals':{" +
               "'aws:currenttime':'2010-12-31'" +
