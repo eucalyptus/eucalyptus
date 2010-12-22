c9d0f090d0232b67103fd58d68857a7dceb0902d
twerges <twerges@eucahost-4-234.eucalyptus>
2010-12-22 12:25:29 -0800

    removed superfluous imports and made this annotation public

--- clc/modules/core/src/main/java/com/eucalyptus/upgrade/TestDescription.java
+++ clc/modules/core/src/main/java/com/eucalyptus/upgrade/TestDescription.java
@@ -1,14 +1,9 @@
 package com.eucalyptus.upgrade;
 
 import java.lang.annotation.*;
-import java.lang.reflect.Method;
-
-import org.apache.log4j.Logger;
-import org.junit.runner.notification.Failure;
-
 
 @Retention(RetentionPolicy.RUNTIME)
-@interface TestDescription {
+public @interface TestDescription {
   String value() default "";
 }
 
