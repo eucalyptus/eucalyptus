c326b99755f18badd258a0029ab965c45c04d418
twerges <twerges@eucahost-4-234.eucalyptus>
2010-12-29 14:45:44 -0800

    added pwd to classpath and added commentary

--- clc/tools/runTest.sh
+++ clc/tools/runTest.sh
@@ -1,3 +1,17 @@
+
+# 
+# Runs any static method within eucalyptus. Does not require Eucalyptus to be
+#  running. Initializes everything, sets up database and persistence contexts, etc.
+#
+# Syntax: runTest.sh -t fullyQualifiedClassName:staticMethodName[:arg0,arg1...]
+#
+# Arguments are always of type java.lang.String. If you desire some other
+#  type then your method must convert.
+#
+# Example usage:
+#  runTest.sh -t com.eucalyptus.reporting.instance.FalseDataGenerator:summarizeFalseDataTwoCriteria:cluster,user
+#
+
 set -e
 log() {
   FSTR="${1}\n"
@@ -10,7 +24,7 @@ if [ -z "${EUCALYPTUS}" ]; then
 fi
 
 # setup the classpath
-CLASSPATH=""
+CLASSPATH=".:"
 FILES=$(\ls -1 ${EUCALYPTUS}/usr/share/eucalyptus/*.jar)
 for FILE in $FILES; do
   export CLASSPATH=${FILE}:${CLASSPATH}
@@ -23,5 +37,6 @@ java -Xbootclasspath/p:${EUCALYPTUS}/usr/share/eucalyptus/openjdk-crypto.jar -cl
 	-Deuca.upgrade.new.dir=${EUCALYPTUS} \
 	-Deuca.upgrade.destination=com.eucalyptus.upgrade.MysqldbDestination \
 	-Deuca.log.level=TRACE  \
+	-Deuca.log.appender=console \
 	-Djava.security.egd=file:/dev/./urandom \
 	com.eucalyptus.upgrade.TestHarness ${@}
