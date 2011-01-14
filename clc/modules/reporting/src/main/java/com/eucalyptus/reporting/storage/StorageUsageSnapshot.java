b15e41045233d4f081b50cea6faeb254bd7f6692
twerges <twerges@eucahost-4-234.eucalyptus>
2011-01-13 19:15:39 -0800

    moved properties to storage key so this class is only a container for a key and a data

--- clc/modules/reporting/src/main/java/com/eucalyptus/reporting/storage/StorageUsageSnapshot.java
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/storage/StorageUsageSnapshot.java
@@ -2,96 +2,41 @@ package com.eucalyptus.reporting.storage;
 
 import javax.persistence.*;
 
+/**
+ * <p>StorageUsageSnapshot is a snapshot of disk data usage at some point in
+ * time.
+ * 
+ * @author twerges
+ */
 @Entity
 @PersistenceContext(name="reporting")
 @Table(name="storage_usage_snapshot")
 class StorageUsageSnapshot
 {
-	@Id
-	@GeneratedValue(strategy=GenerationType.TABLE)
-	@Column(name="id", nullable=false)
-	protected Long id;
-	@Column(name="timestamp_ms", nullable=false)
-	protected Long timestampMs;
-	@Column(name="owner_id", nullable=false)
-	protected String ownerId;
-	@Column(name="account_id", nullable=false)
-	protected String accountId;
-	@Column(name="cluster_name", nullable=false)
-	protected String clusterName;
-	@Column(name="availability_zone", nullable=false)
-	protected String availabilityZone;
-	@Column(name="volumes_num", nullable=true)
-	protected Long volumesNum;
-	@Column(name="snapshot_num", nullable=true)
-	protected Long snapshotsNum;
-	@Column(name="volumes_gb", nullable=true)
-	protected Long volumesGb; //Gigabytes, not gigabits; following java caps
-	@Column(name="snapshot_gb", nullable=true)
-	protected Long snapshotsGb;
-	@Column(name="objects_num", nullable=true)
-	protected Long objectsNum;
-	@Column(name="objects_gb", nullable=true)
-	protected Long objectsGb;
-
-	public Long getId()
+	@EmbeddedId
+	protected SnapshotKey key;
+	@Embedded
+	protected StorageUsageData usageData;
+	
+	public StorageUsageSnapshot(SnapshotKey key, StorageUsageData usageData)
 	{
-		return id;
+		this.key = key;
+		this.usageData = usageData;
 	}
 
-	public Long getTimestampMs()
+	public SnapshotKey getSnapshotKey()
 	{
-		return timestampMs;
+		return key;
+	}
+	
+	public StorageUsageData getUsageData()
+	{
+		return usageData;
 	}
 
-	public String getOwnerId()
+	public String toString()
 	{
-		return ownerId;
-	}
-
-	public String getAccountId()
-	{
-		return accountId;
-	}
-
-	public String getClusterName()
-	{
-		return clusterName;
-	}
-
-	public String getAvailabilityZone()
-	{
-		return availabilityZone;
-	}
-
-	public Long getVolumesNum()
-	{
-		return volumesNum;
-	}
-
-	public Long getSnapshotsNum()
-	{
-		return snapshotsNum;
-	}
-
-	public Long getVolumesGb()
-	{
-		return volumesGb;
-	}
-
-	public Long getSnapshotsGb()
-	{
-		return snapshotsGb;
-	}
-
-	public Long getObjectsNum()
-	{
-		return objectsNum;
-	}
-
-	public Long getObjectsGb()
-	{
-		return objectsGb;
+		return String.format("[key:%s,usageData:%s]", this.key, this.usageData);
 	}
 
 }
