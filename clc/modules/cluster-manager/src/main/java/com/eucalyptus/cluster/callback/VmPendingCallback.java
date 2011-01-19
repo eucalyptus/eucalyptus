754fca5fff2e76e2c79f3e2b3916bdd7a64561d0
decker <decker@personal-army>
2011-01-18 18:02:37 -0800

    normalize vm pending callback

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/VmPendingCallback.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/VmPendingCallback.java
@@ -1,31 +1,20 @@
 package com.eucalyptus.cluster.callback;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.concurrent.CancellationException;
 import org.apache.log4j.Logger;
 import com.eucalyptus.cluster.Cluster;
-import com.eucalyptus.cluster.Networks;
 import com.eucalyptus.cluster.VmInstance;
 import com.eucalyptus.cluster.VmInstances;
-import com.eucalyptus.cluster.VmTypes;
-import com.eucalyptus.entities.VmType;
 import com.eucalyptus.util.async.FailedRequestException;
 import com.eucalyptus.vm.SystemState;
 import com.eucalyptus.vm.SystemState.Reason;
 import com.eucalyptus.vm.VmState;
-import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Lists;
-import edu.ucsb.eucalyptus.cloud.Network;
 import edu.ucsb.eucalyptus.cloud.VmDescribeResponseType;
 import edu.ucsb.eucalyptus.cloud.VmDescribeType;
 import edu.ucsb.eucalyptus.cloud.VmInfo;
 import edu.ucsb.eucalyptus.msgs.AttachedVolume;
-import edu.ucsb.eucalyptus.msgs.VmTypeInfo;
 
 public class VmPendingCallback extends StateUpdateMessageCallback<Cluster, VmDescribeType, VmDescribeResponseType> {
   private static Logger LOG = Logger.getLogger( VmPendingCallback.class );
