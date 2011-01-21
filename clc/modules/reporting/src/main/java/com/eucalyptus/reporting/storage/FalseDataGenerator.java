3ca4b9916a277a582f1de92fc026c0a8b0aa5ca3
twerges <twerges@eucahost-4-234.eucalyptus>
2011-01-20 19:35:01 -0800

    generated TestPoller and FakeQueue inner classes to fake some of the functionality needed for an isolated test; added various static constants for clarirty

--- clc/modules/reporting/src/main/java/com/eucalyptus/reporting/storage/FalseDataGenerator.java
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/storage/FalseDataGenerator.java
@@ -4,47 +4,117 @@ import java.util.*;
 
 import com.eucalyptus.reporting.GroupByCriterion;
 import com.eucalyptus.reporting.Period;
-import com.eucalyptus.reporting.event.InstanceEvent;
-import com.eucalyptus.reporting.instance.*;
+import com.eucalyptus.reporting.event.*;
+import com.eucalyptus.reporting.event.EventListener;
+import com.eucalyptus.reporting.queue.QueueReceiver;
+import com.eucalyptus.reporting.queue.QueueSender;
 
 public class FalseDataGenerator
-{	
-	private static final long MAX_MS = ((NUM_USAGE+1) * TIME_USAGE_APART);
+{
+	private static final int NUM_USERS    = 256;
+	private static final int NUM_ACCOUNTS = 64;
+	private static final int NUM_CLUSTERS = 16;
+	private static final int NUM_ZONES    = 4;
+	private static final int SNAPSHOTS_PER_USER = 64;
+	private static final int TIME_USAGE_APART = 100000; //ms
+	private static final int WRITE_INTERVAL = 8;
+	private static final long MAX_MS = ((SNAPSHOTS_PER_USER+1) * TIME_USAGE_APART);
 
 	public static void generateFalseData()
 	{
-		List<InstanceAttributes> fakeInstances =
-				new ArrayList<InstanceAttributes>();
-		
 		System.out.println(" ----> GENERATING FALSE DATA");
 
-		StorageUsageLog usageLog = StorageUsageLog.getStorageUsageLog();
+		FakeQueue fakeQueue = new FakeQueue();
+		TestPoller poller = new TestPoller(fakeQueue);
+		long timestampMs = 0l;
+		for (int i = 0; i < SNAPSHOTS_PER_USER; i++) {
+			timestampMs = i * TIME_USAGE_APART;
 
-		
-		for (int i = 0; i < NUM_INSTANCE; i++) {
+			for (int j = 0; j < NUM_USERS; j++) {
+				String userId = String.format("user-%d", j);
+				String accountId = String.format("instance-%d",
+						(j % NUM_ACCOUNTS));
+				String clusterId = String.format("user-%d", (j % NUM_CLUSTERS));
+				String zoneId = String.format("account-%d", (j % NUM_ZONES));
 			
-		}
-
-		TestEventListener listener = new TestEventListener();
-		for (int i=0; i<NUM_USAGE; i++) {
-			listener.setCurrentTimeMillis(i * TIME_USAGE_APART);
-			for (InstanceAttributes insAttrs : fakeInstances) {
-				long instanceNum = Long.parseLong(insAttrs.getUuid());
-				long netIo = instanceNum + i;
-				long diskIo = instanceNum + i*2;
-				InstanceEvent event = new InstanceEvent(insAttrs.getUuid(),
-						insAttrs.getInstanceId(), insAttrs.getInstanceType(),
-						insAttrs.getUserId(), insAttrs.getAccountId(),
-						insAttrs.getClusterName(),
-						insAttrs.getAvailabilityZone(), new Long(netIo),
-						new Long(diskIo));
-				System.out.println("Generating:" + i);
-				listener.receiveEvent(event);
+				for (int k = 0; k < StorageEvent.EventType.values().length; k++) {
+					long sizeGb = 1 + (i * k);
+					StorageEvent.EventType eventType =
+						StorageEvent.EventType.values()[k];
+					StorageEvent event = new StorageEvent(eventType, true,
+							sizeGb, userId, accountId, clusterId, zoneId);
+					fakeQueue.send(event);
+				}
+				
+			}
+			if (i % WRITE_INTERVAL == 0) {
+				poller.setTimestampMs(timestampMs);
+				poller.writeEvents();
 			}
 		}
 
 	}
+	
+	private static class FakeQueue
+		implements QueueReceiver, QueueSender
+	{
+		private final Queue<Event> events;
 
+		FakeQueue()
+		{
+			events = new LinkedList<Event>();
+		}
+		
+		@Override
+		public void send(Event e)
+		{
+			events.add(e);
+		}
+
+		@Override
+		public void addEventListener(EventListener el)
+		{
+			// empty
+		}
+
+		@Override
+		public void removeEventListener(EventListener el)
+		{
+			// empty
+		}
+
+		@Override
+		public Event receiveEventNoWait()
+		{
+			return events.poll();
+		}
+		
+	}
+	
+	private static class TestPoller
+		extends StorageEventPoller
+	{
+		private long timestampMs;
+		
+		TestPoller(QueueReceiver receiver)
+		{
+			super(receiver);
+		}
+
+		void setTimestampMs(long timestampMs)
+		{
+			this.timestampMs = timestampMs;
+		}
+		
+		@Override
+		protected long getTimestampMs()
+		{
+			return timestampMs;
+		}
+	
+		
+	}
+	
 	public static void removeFalseData()
 	{
 		System.out.println(" ----> REMOVING FALSE DATA");
@@ -87,7 +157,7 @@ public class FalseDataGenerator
 
 		StorageUsageLog usageLog = StorageUsageLog.getStorageUsageLog();
 
-		Map<String, StorageUsageData> summaryMap = usageLog.scanSummarize(
+		Map<String, StorageUsageSummary> summaryMap = usageLog.scanSummarize(
 				new Period(0L, MAX_MS), crit);
 		for (String critVal: summaryMap.keySet()) {
 			System.out.printf("%s:%s Summary:%s\n", crit, critVal,
@@ -114,11 +184,11 @@ public class FalseDataGenerator
 
 		StorageUsageLog usageLog = StorageUsageLog.getStorageUsageLog();
 
-		Map<String, Map<String, StorageUsageData>> summaryMap =
+		Map<String, Map<String, StorageUsageSummary>> summaryMap =
 			usageLog.scanSummarize(new Period(0L, MAX_MS),
 					outerCrit, innerCrit);
 		for (String outerCritVal: summaryMap.keySet()) {
-			Map<String, StorageUsageData> innerMap = summaryMap.get(outerCritVal);
+			Map<String, StorageUsageSummary> innerMap = summaryMap.get(outerCritVal);
 			for (String innerCritVal: innerMap.keySet()) {
 				System.out.printf("%s:%s %s:%s Summary:%s\n", outerCrit,
 						outerCritVal, innerCrit, innerCritVal,
