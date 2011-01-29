250bf734444e71f376985eb986865da6bb3a2b69
twerges <twerges@eucahost-4-234.eucalyptus>
2011-01-29 00:33:18 -0800

    altered to run broker separately from QueueFactory; added initial argument for Timer so it repeats; added startTest static method for cmd-line invocation

--- clc/modules/reporting/src/main/java/com/eucalyptus/reporting/ReportingBootstrapper.java
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/ReportingBootstrapper.java
@@ -5,22 +5,25 @@ import java.util.TimerTask;
 
 import org.apache.log4j.*;
 
-import com.eucalyptus.bootstrap.Bootstrapper;
+import com.eucalyptus.bootstrap.*;
 import com.eucalyptus.reporting.instance.*;
 import com.eucalyptus.reporting.storage.*;
 import com.eucalyptus.reporting.queue.*;
 import com.eucalyptus.reporting.queue.QueueFactory.QueueIdentifier;
+import com.eucalyptus.system.Threads;
 
+@Provides(Component.reporting)
+@RunDuring(Bootstrap.Stage.RemoteServicesInit)
 public class ReportingBootstrapper
 	extends Bootstrapper
 {
 	private static Logger log = Logger.getLogger( ReportingBootstrapper.class );
 
-	private static long POLLER_DELAY_MS = 1000l;
+	private static long POLLER_DELAY_MS = 10000l;
 	
 	private StorageEventPoller storagePoller;
 	private InstanceEventListener instanceListener;
-	private QueueBroker broker;
+	private QueueFactory queueFactory;
 	private Timer timer;
 	
 
@@ -28,31 +31,34 @@ public class ReportingBootstrapper
 	{
 	}
 
+	@Override
 	public boolean check()
 	{
 		return true;
 	}
 
+	@Override
 	public void destroy()
 	{
 		return;
 	}
 
+	@Override
 	public boolean enable()
 	{
 		return true;
 	}
 
+	@Override
 	public boolean disable()
 	{
 		return true;
 	}
 
+	@Override
 	public boolean load()
 	{
 		try {
-			broker = QueueBroker.getInstance();
-			timer = new Timer(true);
 			return true;
 		} catch (Exception ex) {
 			log.error("ReportingBootstrapper failed to load", ex);
@@ -60,32 +66,64 @@ public class ReportingBootstrapper
 		}
 	}
 
+	
+	
+	@Override
 	public boolean start()
 	{
 		try {
 			//TODO: brokers must FIND EACH OTHER here...
-			broker.startup();
-			log.info("Queue broker started");
-			QueueFactory queueFactory = QueueFactory.getInstance();
-			final StorageEventPoller poller = new StorageEventPoller(queueFactory.getReceiver(QueueIdentifier.STORAGE));
+
+			/* Start queue broker
+			 */
+			queueFactory = QueueFactory.getInstance();
+			//QueueFactory has been started in SystemBootstrapper.init()
+			
+			/* Start storage receiver and storage queue poller thread
+			 */
+			QueueReceiver storageReceiver = queueFactory.getReceiver(QueueIdentifier.STORAGE);
+			final StorageEventPoller poller = new StorageEventPoller(storageReceiver);
+			timer = new Timer(true);
 			timer.schedule(new TimerTask() {
 				@Override
 				public void run()
 				{
 					poller.writeEvents();
 				}
-			}, POLLER_DELAY_MS);
+			}, 0, POLLER_DELAY_MS);
 			this.storagePoller = poller;
 			log.info("Storage queue poller started");
-			this.instanceListener = new InstanceEventListener();
+			
+			/* Start instance receiver and instance listener
+			 */
+			QueueReceiver instanceReceiver =
+				queueFactory.getReceiver(QueueIdentifier.INSTANCE);
+			instanceListener = new InstanceEventListener();
+			instanceReceiver.addEventListener(instanceListener);
+			
 			log.info("ReportingBootstrapper started");
 			return true;
 		} catch (Exception ex) {
+			ex.printStackTrace();
 			log.error("ReportingBootstrapper failed to start", ex);
 			return false;
 		}
 	}
 
+	public static void startTest()
+	{
+		ReportingBootstrapper bootstrapper = new ReportingBootstrapper();
+		bootstrapper.load();
+		bootstrapper.start();
+		try {
+			System.out.println("Sleeping for 60 secs");
+			Thread.sleep(60000);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Override
 	public boolean stop()
 	{
 		try {
@@ -93,7 +131,7 @@ public class ReportingBootstrapper
 			instanceListener.flush();
 			timer.cancel();
 			storagePoller.writeEvents();
-			broker.shutdown();
+			queueFactory.shutdown();
 			return true;
 		} catch (Exception ex) {
 			log.error("ReportingBootstrapper failed to stop", ex);
