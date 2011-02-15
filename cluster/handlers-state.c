4436abec620ba3ca9fc20e1557ac6923a270e541
root <root@wilshire>
2011-02-15 10:42:44 -0800

    debugging statement

--- cluster/handlers-state.c
+++ cluster/handlers-state.c
@@ -264,6 +264,7 @@ int instNetParamsSet(ccInstance *inst, void *in) {
     if (inst->groupNames[0][0] != '\0' && inst->ownerId[0] != '\0') {
       if ( (vnetconfig->users[inst->ccnet.vlan].netName[0] != '\0' && strcmp(vnetconfig->users[inst->ccnet.vlan].netName, inst->groupNames[0])) || (vnetconfig->users[inst->ccnet.vlan].userName[0] != '\0' && strcmp(vnetconfig->users[inst->ccnet.vlan].userName, inst->ownerId)) ) {
 	// this means that there is a pre-existing network with the passed in vlan tag, but with a different netName or userName
+	logprintfl(EUCAERROR, "instNetParamsSet(): input instance vlan<->user<->netname mapping is incompatible with internal state. Internal - userName=%s netName=%s vlan=%d.  Instance - userName=%s netName=%s vlan=%d\n", vnetconfig->users[inst->ccnet.vlan].userName, vnetconfig->users[inst->ccnet.vlan].netName, inst->ccnet.vlan, inst->ownerId, inst->groupNames[0], inst->ccnet.vlan);
 	ret = 1;
       } else {
 	snprintf(vnetconfig->users[inst->ccnet.vlan].netName, 32, "%s", inst->groupNames[0]);
