0cda0355d6e6e924a0918cac0a9f9d78768d80ac
decker <decker@personal-army>
2011-02-19 00:17:08 -0800

    extract entites from groovy

--- clc/modules/msgs/src/main/java/com/eucalyptus/entities/BaseEntities.groovy
+++ clc/modules/msgs/src/main/java/com/eucalyptus/entities/BaseEntities.groovy
@@ -74,15 +74,22 @@ public class AbstractPersistent implements Serializable {
 public abstract class AccountMetadata extends AbstractPersistent implements Serializable {
   @Column( name = "metadata_account_id" )
   String accountId;
+  @Column( name = "metadata_display_name" )
+  String displayName;
   public AccountMetadata() {}
   public AccountMetadata(AccountFullName account) {
     this.accountId = account.getAccountId( );
+  }
+  public AccountMetadata(AccountFullName account, String displayName) {
+    this.accountId = account.getAccountId( );
+    this.displayName = displayName;
   }
   @Override
   public int hashCode( ) {
     final int prime = 31;
     int result = super.hashCode( );
     result = prime * result + ( ( accountId == null ) ? 0 : accountId.hashCode( ) );
+    result = prime * result + ( ( displayName == null ) ? 0 : displayName.hashCode( ) );
     return result;
   }
   @Override
@@ -94,6 +101,9 @@ public abstract class AccountMetadata extends AbstractPersistent implements Seri
     if ( accountId == null ) {
       if ( other.accountId != null ) return false;
     } else if ( !accountId.equals( other.accountId ) ) return false;
+    if ( displayName == null ) {
+      if ( other.displayName != null ) return false;
+    } else if ( !displayName.equals( other.displayName ) ) return false;
     return true;
   }
 
@@ -102,8 +112,6 @@ public abstract class AccountMetadata extends AbstractPersistent implements Seri
 public abstract class UserMetadata extends AccountMetadata implements Serializable {
   @Column( name = "metadata_user_id" )
   String userId;
-  @Column( name = "metadata_display_name" )
-  String displayName;
   public UserMetadata( ) {
   }
   public UserMetadata( UserFullName user ) {
@@ -111,15 +119,13 @@ public abstract class UserMetadata extends AccountMetadata implements Serializab
     this.userId = user.getUserId( );
   }
   public UserMetadata( UserFullName user, String displayName ) {
-    this( user );
-    this.displayName = displayName;
+    super( user, displayName );
   }  
   @Override
   public int hashCode( ) {
     final int prime = 31;
     int result = super.hashCode( );
     result = prime * result + ( ( userId == null ) ? 0 : userId.hashCode( ) );
-    result = prime * result + ( ( displayName == null ) ? 0 : displayName.hashCode( ) );
     return result;
   }
   @Override
@@ -131,263 +137,9 @@ public abstract class UserMetadata extends AccountMetadata implements Serializab
     if ( userId == null ) {
       if ( other.userId != null ) return false;
     } else if ( !userId.equals( other.userId ) ) return false;
-    if ( displayName == null ) {
-      if ( other.displayName != null ) return false;
-    } else if ( !displayName.equals( other.displayName ) ) return false;
     return true;
   }  
 }
 
 
 
-@Entity
-@PersistenceContext(name="eucalyptus_general")
-@Table( name = "metadata_network_group" )
-@Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-public class NetworkRulesGroup extends UserMetadata implements Serializable {
-  @Column( name = "metadata_network_group_user_network_group_name", unique=true )
-  String uniqueName;//bogus field to enforce uniqueness
-  @Column( name = "metadata_network_group_description" )
-  String            description;
-  @OneToMany( cascade=[CascadeType.ALL], fetch=FetchType.EAGER )
-  @JoinTable( name = "metadata_network_group_has_rules", joinColumns = [ @JoinColumn( name = "id" ) ], inverseJoinColumns = [ @JoinColumn( name = "metadata_network_rule_id" ) ] )
-  @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-  List<NetworkRule> networkRules = new ArrayList<NetworkRule>( );
-  public static String NETWORK_DEFAULT_NAME = "default";
-  public NetworkRulesGroup( ) {
-  }  
-  public NetworkRulesGroup( final AccountFullName account ) {
-    super( account );
-  }
-  public NetworkRulesGroup( final AccountFullName account, final String groupName ) {
-    super( account, groupName );
-    this.uniqueName = account.getAuthority( ) + "/security-groups/" + groupName;
-  }  
-  public NetworkRulesGroup( final AccountFullName account, final String groupName, final String groupDescription ) {
-    this( account, groupName );
-    this.description = groupDescription;
-  }
-  public NetworkRulesGroup( final AccountFullName account, final String groupName, final String description, final List<NetworkRule> networkRules ) {
-    this( account, groupName, description );
-    this.networkRules = networkRules;
-  }
-  public static NetworkRulesGroup getDefaultGroup( AccountFullName account ) {
-    return new NetworkRulesGroup( account, NETWORK_DEFAULT_NAME, "default group", new ArrayList<NetworkRule>( ) );
-  }
-  public Network getVmNetwork( ) {
-    Network vmNetwork = new Network( Accounts.lookupAccontById( this.getAccountId( ) ), this.getDisplayName( ), this.getId( ) );
-    for ( NetworkRule networkRule : this.getNetworkRules( ) ) {
-      PacketFilterRule pfrule = new PacketFilterRule( this.getAccountId( ), this.getDisplayName( ), networkRule.getProtocol( ), networkRule.getLowPort( ), networkRule.getHighPort( ) );
-      for ( IpRange cidr : networkRule.getIpRanges( ) )
-        pfrule.getSourceCidrs( ).add( cidr.getValue( ) );
-      for ( NetworkPeer peer : networkRule.getNetworkPeers( ) )
-        pfrule.addPeer( peer.getUserQueryKey( ), peer.getGroupName( ) );
-      vmNetwork.getRules( ).add( pfrule );
-    }
-    return vmNetwork;
-  }  
-  public static NetworkRulesGroup named( AccountFullName account, String groupName ) {
-    return new NetworkRulesGroup( account, groupName );
-  }
-  @Override
-  public int hashCode( ) {
-    final int prime = 31;
-    int result = super.hashCode( );
-    result = prime * result + ( ( uniqueName == null ) ? 0 : uniqueName.hashCode( ) );
-    return result;
-  }
-  @Override
-  public boolean equals( Object obj ) {
-    if ( this.is( obj ) ) return true;
-    if ( !super.equals( obj ) ) return false;
-    if ( !getClass( ).equals( obj.getClass( ) ) ) return false;
-    NetworkRulesGroup other = ( NetworkRulesGroup ) obj;
-    if ( uniqueName == null ) {
-      if ( other.uniqueName != null ) return false;
-    } else if ( !uniqueName.equals( other.uniqueName ) ) return false;
-    return true;
-  }  
-  @Override
-  public String toString( ) {
-    return String.format( "NetworkRulesGroup:%s:description=%s:networkRules=%s", this.uniqueName, this.description, this.networkRules );
-  }
-}
-
-@Entity
-@PersistenceContext(name="eucalyptus_general")
-@Table( name = "network_rule_peer_network" )
-@Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-public class NetworkPeer {  
-  @Id
-  @GeneratedValue
-  @Column( name = "network_rule_peer_network_id" )
-  Long id = -1l;
-  @Column( name = "network_rule_peer_network_user_query_key" )
-  String userQueryKey;
-  @Column( name = "network_rule_peer_network_user_group" )
-  String groupName;
-  public NetworkPeer() {
-  }
-  public NetworkPeer( final String userQueryKey, final String groupName ) {
-    this.userQueryKey = userQueryKey;
-    this.groupName = groupName;
-  }
-  public boolean equals( final Object o ) {
-    if ( this.is(o) ) return true;
-    if ( o == null || !getClass().equals(o.getClass()) ) return false;
-    
-    NetworkPeer that = ( NetworkPeer ) o;
-    
-    if ( !groupName.equals( that.groupName ) ) return false;
-    if ( !userQueryKey.equals( that.userQueryKey ) ) return false;
-    
-    return true;
-  }
-  
-  public int hashCode() {
-    int result;
-    result = userQueryKey.hashCode();
-    result = 31 * result + groupName.hashCode();
-    return result;
-  }
-  
-  public List<NetworkRule> getAsNetworkRules() {
-    List<NetworkRule> ruleList = new ArrayList<NetworkRule>();
-    ruleList.add( new NetworkRule( "tcp", 0, 65535, new NetworkPeer( this.getUserQueryKey(), this.getGroupName() ) ) );
-    ruleList.add( new NetworkRule( "udp", 0, 65535, new NetworkPeer( this.getUserQueryKey(), this.getGroupName() ) ) );
-    ruleList.add( new NetworkRule( "icmp", -1, -1, new NetworkPeer( this.getUserQueryKey(), this.getGroupName() ) ) );
-    return ruleList;
-  }
-
-  @Override
-  public String toString( ) {
-    return String.format( "NetworkPeer [groupName=%s, userQueryKey=%s]", this.groupName, this.userQueryKey );
-  }
-  
-}
-
-@Entity
-@PersistenceContext(name="eucalyptus_general")
-@Table( name = "metadata_network_rule" )
-@Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-public class NetworkRule {
-  
-  @Id
-  @GeneratedValue
-  @Column( name = "metadata_network_rule_id" )
-  private Long id = -1l;
-  @Column( name = "metadata_network_rule_protocol" )
-  String protocol;
-  @Column( name = "metadata_network_rule_low_port" )
-  Integer lowPort;
-  @Column( name = "metadata_network_rule_high_port" )
-  Integer highPort;
-  @OneToMany( cascade=[CascadeType.REMOVE,CascadeType.MERGE,CascadeType.PERSIST], fetch=FetchType.EAGER )
-  @JoinTable( name = "metadata_network_rule_has_ip_range", joinColumns = [ @JoinColumn( name = "metadata_network_rule_id" ) ], inverseJoinColumns = [ @JoinColumn( name = "metadata_network_rule_ip_range_id" ) ] )
-  @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-  Set<IpRange> ipRanges = new HashSet<IpRange>();
-  @OneToMany( cascade=[CascadeType.REMOVE,CascadeType.MERGE,CascadeType.PERSIST], fetch=FetchType.EAGER )
-  @JoinTable( name = "metadata_network_rule_has_peer_network", joinColumns = [ @JoinColumn( name = "metadata_network_rule_id" ) ], inverseJoinColumns = [ @JoinColumn( name = "metadata_network_rule_peer_network_id" ) ] )
-  @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-  Set<NetworkPeer> networkPeers = new HashSet<NetworkPeer>();
-  public NetworkRule() {
-  }
-  public NetworkRule( final String protocol, final Integer lowPort, final Integer highPort, final List<IpRange> ipRanges ) {
-    this.protocol = protocol;
-    this.lowPort = lowPort;
-    this.highPort = highPort;
-    this.ipRanges = ipRanges;
-  }
-  public NetworkRule( final String protocol, final Integer lowPort, final Integer highPort, final NetworkPeer peer ) {
-    this.protocol = protocol;
-    this.lowPort = lowPort;
-    this.highPort = highPort;
-    this.networkPeers.add( peer );
-  }
-  public NetworkRule( final String protocol, final Integer lowPort, final Integer highPort ) {
-    this.protocol = protocol;
-    this.lowPort = lowPort;
-    this.highPort = highPort;
-  }
-  
-  public boolean isValid() {
-    return "tcp".equals( this.protocol ) || "udp".equals( this.protocol ) || "icmp".equals( this.protocol );
-  }
-  
-  @Override
-  public int hashCode( ) {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ( ( highPort == null ) ? 0 : highPort.hashCode( ) );
-    result = prime * result + ( ( lowPort == null ) ? 0 : lowPort.hashCode( ) );
-    result = prime * result + ( ( protocol == null ) ? 0 : protocol.hashCode( ) );
-    return result;
-  }
-  @Override
-  public boolean equals( Object obj ) {
-    if ( this.is(obj) ) return true;
-    if ( obj == null ) return false;
-    if ( !getClass( ).equals( obj.getClass( ) ) ) return false;
-    NetworkRule other = ( NetworkRule ) obj;
-    if ( highPort == null ) {
-      if ( other.highPort != null ) return false;
-    } else if ( !highPort.equals( other.highPort ) ) return false;
-    if ( lowPort == null ) {
-      if ( other.lowPort != null ) return false;
-    } else if ( !lowPort.equals( other.lowPort ) ) return false;
-    if ( protocol == null ) {
-      if ( other.protocol != null ) return false;
-    } else if ( !protocol.equals( other.protocol ) ) return false;
-    return true;
-  }
-
-  @Override
-  public String toString( ) {
-    return String.format( "NetworkRule:%s:%d:%d:ipRanges=%s:networkPeers=%s:",
-                          this.protocol, this.lowPort, this.highPort, this.ipRanges, this.networkPeers );
-  }
-  
-  
-}
-
-@Entity
-@PersistenceContext(name="eucalyptus_general")
-@Table( name = "metadata_network_rule_ip_range" )
-@Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
-public class IpRange {
-  @Id
-  @GeneratedValue
-  @Column( name = "metadata_network_rule_ip_range_id" )
-  Long id = -1l;
-  @Column( name = "metadata_network_rule_ip_range_value" )
-  String value;
-  public IpRange(){
-  }
-  public IpRange( final String value ) {
-    this.value = value;
-  }
-
-  @Override
-  public String toString( ) {
-    return String.format( "IpRange:%s", this.value );
-  }
-
-  @Override
-  public int hashCode( ) {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ( ( this.value == null ) ? 0 : this.value.hashCode( ) );
-    return result;
-  }
-  @Override
-  public boolean equals( Object obj ) {
-    if ( this.is( obj ) ) return true;
-    if ( obj == null ) return false;
-    if ( !getClass( ).equals( obj.getClass( ) ) ) return false;
-    IpRange other = ( IpRange ) obj;
-    if ( this.value == null ) {
-      if ( other.value != null ) return false;
-    } else if ( !this.value.equals( other.value ) ) return false;
-    return true;
-  }  
-}
---
+++ clc/modules/msgs/src/main/java/com/eucalyptus/entities/NetworkRule.java
@@ -0,0 +1,200 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Eucalyptus Systems, Inc.
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, only version 3 of the License.
+ * 
+ * 
+ *  This file is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  Please contact Eucalyptus Systems, Inc., 130 Castilian
+ *  Dr., Goleta, CA 93101 USA or visit <http://www.eucalyptus.com/licenses/>
+ *  if you need additional information or have any questions.
+ * 
+ *  This file may incorporate work covered under the following copyright and
+ *  permission notice:
+ * 
+ *    Software License Agreement (BSD License)
+ * 
+ *    Copyright (c) 2008, Regents of the University of California
+ *    All rights reserved.
+ * 
+ *    Redistribution and use of this software in source and binary forms, with
+ *    or without modification, are permitted provided that the following
+ *    conditions are met:
+ * 
+ *      Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * 
+ *      Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ * 
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ *    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. USERS OF
+ *    THIS SOFTWARE ACKNOWLEDGE THE POSSIBLE PRESENCE OF OTHER OPEN SOURCE
+ *    LICENSED MATERIAL, COPYRIGHTED MATERIAL OR PATENTED MATERIAL IN THIS
+ *    SOFTWARE, AND IF ANY SUCH MATERIAL IS DISCOVERED THE PARTY DISCOVERING
+ *    IT MAY INFORM DR. RICH WOLSKI AT THE UNIVERSITY OF CALIFORNIA, SANTA
+ *    BARBARA WHO WILL THEN ASCERTAIN THE MOST APPROPRIATE REMEDY, WHICH IN
+ *    THE REGENTS’ DISCRETION MAY INCLUDE, WITHOUT LIMITATION, REPLACEMENT
+ *    OF THE CODE SO IDENTIFIED, LICENSING OF THE CODE SO IDENTIFIED, OR
+ *    WITHDRAWAL OF THE CODE CAPABILITY TO THE EXTENT NEEDED TO COMPLY WITH
+ *    ANY SUCH LICENSES OR RIGHTS.
+ *******************************************************************************
+ * @author chris grzegorczyk <grze@eucalyptus.com>
+ */
+
+package com.eucalyptus.entities;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import com.google.common.collect.Sets;
+
+@Entity
+@PersistenceContext(name="eucalyptus_general")
+@Table( name = "metadata_network_rule" )
+@Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
+public class NetworkRule {
+  
+  @Id
+  @GeneratedValue
+  @Column( name = "metadata_network_rule_id" )
+  private Long id = -1l;
+  @Column( name = "metadata_network_rule_protocol" )
+  String protocol;
+  @Column( name = "metadata_network_rule_low_port" )
+  Integer lowPort;
+  @Column( name = "metadata_network_rule_high_port" )
+  Integer highPort;
+  @OneToMany( cascade={CascadeType.REMOVE,CascadeType.MERGE,CascadeType.PERSIST}, fetch=FetchType.EAGER )
+  @JoinTable( name = "metadata_network_rule_has_ip_range", joinColumns = { @JoinColumn( name = "metadata_network_rule_id" ) }, inverseJoinColumns = { @JoinColumn( name = "metadata_network_rule_ip_range_id" ) } )
+  @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
+  Set<IpRange> ipRanges = new HashSet<IpRange>();
+  @OneToMany( cascade={CascadeType.REMOVE,CascadeType.MERGE,CascadeType.PERSIST}, fetch=FetchType.EAGER )
+  @JoinTable( name = "metadata_network_rule_has_peer_network", joinColumns = { @JoinColumn( name = "metadata_network_rule_id" ) }, inverseJoinColumns = { @JoinColumn( name = "metadata_network_rule_peer_network_id" ) } )
+  @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
+  Set<NetworkPeer> networkPeers = new HashSet<NetworkPeer>();
+  public NetworkRule() {
+  }
+  public NetworkRule( final String protocol, final Integer lowPort, final Integer highPort, final List<IpRange> ipRanges ) {
+    this.protocol = protocol;
+    this.lowPort = lowPort;
+    this.highPort = highPort;
+    this.ipRanges = Sets.newHashSet( ipRanges );
+  }
+  public NetworkRule( final String protocol, final Integer lowPort, final Integer highPort, final NetworkPeer peer ) {
+    this.protocol = protocol;
+    this.lowPort = lowPort;
+    this.highPort = highPort;
+    this.networkPeers.add( peer );
+  }
+  public NetworkRule( final String protocol, final Integer lowPort, final Integer highPort ) {
+    this.protocol = protocol;
+    this.lowPort = lowPort;
+    this.highPort = highPort;
+  }
+  
+  public boolean isValid() {
+    return "tcp".equals( this.protocol ) || "udp".equals( this.protocol ) || "icmp".equals( this.protocol );
+  }
+  
+  public Long getId( ) {
+    return this.id;
+  }
+  public void setId( Long id ) {
+    this.id = id;
+  }
+  public String getProtocol( ) {
+    return this.protocol;
+  }
+  public void setProtocol( String protocol ) {
+    this.protocol = protocol;
+  }
+  public Integer getLowPort( ) {
+    return this.lowPort;
+  }
+  public void setLowPort( Integer lowPort ) {
+    this.lowPort = lowPort;
+  }
+  public Integer getHighPort( ) {
+    return this.highPort;
+  }
+  public void setHighPort( Integer highPort ) {
+    this.highPort = highPort;
+  }
+  public Set<IpRange> getIpRanges( ) {
+    return this.ipRanges;
+  }
+  public void setIpRanges( Set<IpRange> ipRanges ) {
+    this.ipRanges = ipRanges;
+  }
+  public Set<NetworkPeer> getNetworkPeers( ) {
+    return this.networkPeers;
+  }
+  public void setNetworkPeers( Set<NetworkPeer> networkPeers ) {
+    this.networkPeers = networkPeers;
+  }
+  @Override
+  public int hashCode( ) {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ( ( highPort == null ) ? 0 : highPort.hashCode( ) );
+    result = prime * result + ( ( lowPort == null ) ? 0 : lowPort.hashCode( ) );
+    result = prime * result + ( ( protocol == null ) ? 0 : protocol.hashCode( ) );
+    return result;
+  }
+  @Override
+  public boolean equals( Object obj ) {
+    if ( this == obj ) return true;
+    if ( obj == null ) return false;
+    if ( !getClass( ).equals( obj.getClass( ) ) ) return false;
+    NetworkRule other = ( NetworkRule ) obj;
+    if ( highPort == null ) {
+      if ( other.highPort != null ) return false;
+    } else if ( !highPort.equals( other.highPort ) ) return false;
+    if ( lowPort == null ) {
+      if ( other.lowPort != null ) return false;
+    } else if ( !lowPort.equals( other.lowPort ) ) return false;
+    if ( protocol == null ) {
+      if ( other.protocol != null ) return false;
+    } else if ( !protocol.equals( other.protocol ) ) return false;
+    return true;
+  }
+
+  @Override
+  public String toString( ) {
+    return String.format( "NetworkRule:%s:%d:%d:ipRanges=%s:networkPeers=%s:",
+                          this.protocol, this.lowPort, this.highPort, this.ipRanges, this.networkPeers );
+  }
+}
