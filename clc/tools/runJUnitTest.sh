5d7e87675e3d97f1ffc1303368b0f892fd841916
Dmitrii Zagorodnov <dmitrii@eucalyptus.com>
2011-03-09 11:25:38 -0800

    added runJUnitTest.sh

---
+++ clc/tools/runJUnitTest.sh
@@ -0,0 +1,48 @@
+
+# 
+# Runs any static method within eucalyptus. Does not require Eucalyptus to be
+#  running. Initializes everything, sets up database and persistence contexts, etc.
+#
+# Syntax: runTest.sh -t fullyQualifiedClassName:staticMethodName[:arg0,arg1...]
+#
+# Arguments are always of type java.lang.String. If you desire some other
+#  type then your method must convert.
+#
+# Example usage:
+#  runTest.sh -t com.eucalyptus.reporting.instance.FalseDataGenerator:summarizeFalseDataTwoCriteria:cluster,user
+#
+
+set -e
+log() {
+  FSTR="${1}\n"
+  shift
+  printf "${FSTR}" ${@} | tee -a ${UP_LOG}
+}
+
+if [ -z "${EUCALYPTUS}" ]; then
+	echo "EUCALYPTUS must be set to run tests."
+fi
+
+# setup the classpath
+CLASSPATH=".:"
+FILES=$(\ls -1 ${EUCALYPTUS}/usr/share/eucalyptus/*.jar)
+for FILE in $FILES; do
+  export CLASSPATH=${FILE}:${CLASSPATH}
+done
+CLASSPATH=${EUCALYPTUS}/etc/eucalyptus/cloud.d/upgrade:${EUCALYPTUS}/etc/eucalyptus/cloud.d/scripts:${CLASSPATH}
+#echo $CLASSPATH
+
+# set a bunch of other variables for euca_imager to work
+export VDDK_HOME="/usr/local"
+export LD_LIBRARY_PATH="$EUCALYPTUS/packages/axis2c-1.6.0/lib:$EUCALYPTUS/packages/axis2c-1.6.0/modules/rampart:$EUCALYPTUS/usr/lib/eucalyptus:$VDDK_HOME/lib:$VDDK_HOME/lib/vmware-vix-disklib/lib32:$VDDK_HOME/lib/vmware-vix-disklib/lib64/" # to ensure euca_imager finds VDDK libs
+export PATH="$EUCALYPTUS/usr/lib/eucalyptus:$PATH" # to ensure euca_imager has euca_rootwrap
+
+java -Xbootclasspath/p:${EUCALYPTUS}/usr/share/eucalyptus/openjdk-crypto.jar -classpath ${CLASSPATH} \
+	-Deuca.home=${EUCALYPTUS} \
+	-Deuca.lib.dir=${EUCALYPTUS} \
+	-Deuca.upgrade.new.dir=${EUCALYPTUS} \
+	-Deuca.upgrade.destination=com.eucalyptus.upgrade.MysqldbDestination \
+	-Deuca.log.level=TRACE  \
+	-Deuca.log.appender=console \
+	-Djava.security.egd=file:/dev/./urandom \
+	org.junit.runner.JUnitCore ${@}
