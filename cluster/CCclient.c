ce16060960b84a933d54678313146de931c1108e
root <root@wilshire>
2011-03-10 15:00:37 -0800

    add initialization function to C web services

--- cluster/CCclient.c
+++ cluster/CCclient.c
@@ -220,15 +220,6 @@ int main(int argc, char **argv) {
 	printf("cc_disableService() failed\n");
 	exit(1);
       }
-    } else if (!strcmp(argv[2], "describeBundleTasks")) {
-      char **meh;
-      meh = argv;
-      meh++; meh++; meh++;
-      rc = cc_describeBundleTasks(meh, 1, env, stub);
-      if (rc != 0) {
-	printf("cc_describeBundleTasks() failed\n");
-	exit(1);
-      }
     } else if (!strcmp(argv[2], "getConsoleOutput")) {
       rc = cc_getConsoleOutput(argv[3], env, stub);
       if (rc != 0) {
