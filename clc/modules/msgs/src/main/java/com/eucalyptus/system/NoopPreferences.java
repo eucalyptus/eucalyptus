6e20ea5ab1b9267f077bcf70da8a00950837a560
grze <decker@irony>
2011-03-10 07:29:00 -0800

    tweak java prefs factory

--- clc/modules/msgs/src/main/java/com/eucalyptus/system/NoopPreferences.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/system/NoopPreferences.java
@@ -1,5 +1,9 @@
 package com.eucalyptus.system;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.prefs.AbstractPreferences;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
@@ -8,6 +12,8 @@ import java.util.prefs.PreferencesFactory;
 public class NoopPreferences extends AbstractPreferences {
   private static final String[] EMPTY = {};
   private static final NoopPreferences ROOT = new NoopPreferences();
+  private Map<String,String> fakePrefs = new HashMap<String,String>();
+  private ConcurrentMap<String,AbstractPreferences> fakeKids = new ConcurrentHashMap<String,AbstractPreferences>();
   
   public NoopPreferences( AbstractPreferences parent, String name ) {
     super( parent, name );
@@ -18,44 +24,53 @@ public class NoopPreferences extends AbstractPreferences {
   }
 
   public static class Factory implements PreferencesFactory {
+    private static final Preferences SYSTEM = new NoopPreferences( );
+    private static final Preferences USER = new NoopPreferences( );
     @Override
     public Preferences systemRoot( ) {
-      return new NoopPreferences( );
+      return SYSTEM;
     }
     
     @Override
     public Preferences userRoot( ) {
-      return new NoopPreferences( );
+      return USER;
     }
   }
   
   @Override
-  protected void putSpi( String key, String value ) {}
-  
-  @Override
-  protected String getSpi( String key ) {
-    return null;
+  protected void putSpi( String key, String value ) {
+    this.fakePrefs.put( key, value );
   }
   
   @Override
-  protected void removeSpi( String key ) {}
+  protected String getSpi( String key ) {
+    return this.fakePrefs.get( key );
+  }
   
   @Override
-  protected void removeNodeSpi( ) throws BackingStoreException {}
+  protected void removeSpi( String key ) {
+    this.fakePrefs.remove( key );
+  }
+  
+  @Override
+  protected void removeNodeSpi( ) throws BackingStoreException {
+    this.fakeKids.clear( );
+    this.fakePrefs.clear( );
+  }
   
   @Override
   protected String[] keysSpi( ) throws BackingStoreException {
-    return EMPTY;
+    return this.fakePrefs.keySet( ).toArray( EMPTY );
   }
   
   @Override
   protected String[] childrenNamesSpi( ) throws BackingStoreException {
-    return EMPTY;
+    return this.fakeKids.keySet( ).toArray( EMPTY );
   }
   
   @Override
   protected AbstractPreferences childSpi( String name ) {
-    return new NoopPreferences( this, name );
+    return this.fakeKids.putIfAbsent( name, new NoopPreferences( this, name ) );
   }
   
   @Override
