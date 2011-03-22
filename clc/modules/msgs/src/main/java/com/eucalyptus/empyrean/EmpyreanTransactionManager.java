e62a5a841a7bb4623565af02155b76b1c5aaa25d
grze <decker@irony>
2011-03-22 02:19:48 -0700

    updated tm config

---
+++ clc/modules/msgs/src/main/java/com/eucalyptus/empyrean/EmpyreanTransactionManager.java
@@ -0,0 +1,63 @@
+package com.eucalyptus.empyrean;
+
+import java.util.Properties;
+import javax.naming.Context;
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import com.eucalyptus.bootstrap.SystemIds;
+import com.eucalyptus.system.SubDirectory;
+import bitronix.tm.Configuration;
+import bitronix.tm.TransactionManagerServices;
+import bitronix.tm.jndi.BitronixContext;
+
+public class EmpyreanTransactionManager implements org.jboss.cache.transaction.TransactionManagerLookup, org.hibernate.transaction.TransactionManagerLookup {
+  private static Logger  LOG = Logger.getLogger( EmpyreanTransactionManager.class );
+  private static Context ctx;
+  
+  private static Context getContext( ) {
+    if ( ctx != null ) {
+      return ctx;
+    } else {
+      synchronized ( EmpyreanTransactionManager.class ) {
+        if ( ctx == null ) {
+          configureTm( );
+          ctx = new BitronixContext( );
+        }
+        return ctx;
+      }
+    }
+  }
+  
+  public TransactionManager getTransactionManager( ) throws Exception {
+    return ( TransactionManager ) getContext( ).lookup( "java:comp/UserTransaction" );
+  }
+  
+  private static void configureTm( ) {
+    Configuration tm_conf = TransactionManagerServices.getConfiguration( );
+    tm_conf.setServerId( SystemIds.createCloudUniqueName( "transaction-manager" ) );
+    tm_conf.setAsynchronous2Pc( false );
+    tm_conf.setLogPart1Filename( SubDirectory.DB.toString( ) + "/btm1.tx" );
+    tm_conf.setLogPart2Filename( SubDirectory.DB.toString( ) + "/btm2.tx" );
+//    tm_conf.setJndiUserTransactionName(  )
+  }
+  
+  public String getUserTransactionName( ) {
+    return "java:comp/UserTransaction";
+  }
+  
+  public Object getTransactionIdentifier( Transaction transaction ) {
+    return transaction;
+  }
+  
+  @Override
+  public TransactionManager getTransactionManager( Properties arg0 ) throws HibernateException {
+    try {
+      return getTransactionManager( );
+    } catch ( Exception ex ) {
+      LOG.error( ex, ex );
+      return TransactionManagerServices.getTransactionManager( );
+    }
+  }
+}
