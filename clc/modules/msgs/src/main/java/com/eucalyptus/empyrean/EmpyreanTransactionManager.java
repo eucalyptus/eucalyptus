195680a8e3c9f7ef88f44fb32169733a322570aa
grze <decker@irony>
2011-03-22 23:24:08 -0700

    fix jndi naming for transaction manager to avoid conflict w/ jetty

--- clc/modules/msgs/src/main/java/com/eucalyptus/empyrean/EmpyreanTransactionManager.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/empyrean/EmpyreanTransactionManager.java
@@ -2,19 +2,23 @@ package com.eucalyptus.empyrean;
 
 import java.util.Properties;
 import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import com.eucalyptus.bootstrap.SystemIds;
 import com.eucalyptus.system.SubDirectory;
+import bitronix.tm.BitronixTransactionManager;
 import bitronix.tm.Configuration;
 import bitronix.tm.TransactionManagerServices;
 import bitronix.tm.jndi.BitronixContext;
 
-public class EmpyreanTransactionManager implements org.jboss.cache.transaction.TransactionManagerLookup, org.hibernate.transaction.TransactionManagerLookup {
+public class EmpyreanTransactionManager extends org.mortbay.component.AbstractLifeCycle implements org.jboss.cache.transaction.TransactionManagerLookup, org.hibernate.transaction.TransactionManagerLookup {
+
   private static Logger  LOG = Logger.getLogger( EmpyreanTransactionManager.class );
   private static Context ctx;
+  private static TransactionManager tm;
   
   private static Context getContext( ) {
     if ( ctx != null ) {
@@ -22,7 +26,7 @@ public class EmpyreanTransactionManager implements org.jboss.cache.transaction.T
     } else {
       synchronized ( EmpyreanTransactionManager.class ) {
         if ( ctx == null ) {
-          configureTm( );
+          tm = configureTm( );
           ctx = new BitronixContext( );
         }
         return ctx;
@@ -34,17 +38,18 @@ public class EmpyreanTransactionManager implements org.jboss.cache.transaction.T
     return ( TransactionManager ) getContext( ).lookup( "java:comp/UserTransaction" );
   }
   
-  private static void configureTm( ) {
+  private static TransactionManager configureTm( ) {
     Configuration tm_conf = TransactionManagerServices.getConfiguration( );
     tm_conf.setServerId( SystemIds.createCloudUniqueName( "transaction-manager" ) );
     tm_conf.setAsynchronous2Pc( false );
     tm_conf.setLogPart1Filename( SubDirectory.DB.toString( ) + "/btm1.tx" );
     tm_conf.setLogPart2Filename( SubDirectory.DB.toString( ) + "/btm2.tx" );
-//    tm_conf.setJndiUserTransactionName(  )
+    tm_conf.setJndiUserTransactionName( "eucalyptusTransactionManager" );
+    return TransactionManagerServices.getTransactionManager( );
   }
   
   public String getUserTransactionName( ) {
-    return "java:comp/UserTransaction";
+    return TransactionManagerServices.getConfiguration( ).getJndiUserTransactionName( );
   }
   
   public Object getTransactionIdentifier( Transaction transaction ) {
