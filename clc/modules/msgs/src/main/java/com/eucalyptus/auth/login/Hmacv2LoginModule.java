f66fc1b352b6bf27198da0446b7679b9d6ea217e
wenye <wenye@gushan>
2011-03-29 21:48:39 -0700

    debugging.

--- clc/modules/msgs/src/main/java/com/eucalyptus/auth/login/Hmacv2LoginModule.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/auth/login/Hmacv2LoginModule.java
@@ -88,14 +88,14 @@ public class Hmacv2LoginModule extends BaseLoginModule<HmacCredentials> {
     sortedKeys.addAll( parameters.keySet( ) );
     String firstKey = sortedKeys.pollFirst( );
     if( firstKey != null ) { 
-      sb.append( codec.encode( firstKey ,"UTF-8" ) ).append( "=" ).append( codec.encode( parameters.get( firstKey ).replaceAll( " ", "%20" ).replaceAll( "\\+", " " ), "UTF-8" ) );
+      sb.append( codec.encode( firstKey ,"UTF-8" ) ).append( "=" ).append( codec.encode( parameters.get( firstKey ).replaceAll( "\\+", " " ), "UTF-8" ) );
     } 
     while ( ( firstKey = sortedKeys.pollFirst( ) ) != null ) {
-      sb.append( "&" ).append( codec.encode( firstKey, "UTF-8" ) ).append( "=" ).append( codec.encode( parameters.get( firstKey ).replaceAll( " ", "%20" ).replaceAll( "\\+", " " ), "UTF-8" ) );
+      sb.append( "&" ).append( codec.encode( firstKey, "UTF-8" ) ).append( "=" ).append( codec.encode( parameters.get( firstKey ).replaceAll( "\\+", " " ), "UTF-8" ) );
     }
     String subject = prefix + sb.toString( );
     LOG.trace( "VERSION2: " + subject );
-    LOG.trace( ">>>>>>>> URLCodec(hello hello)=" + codec.encode( "hello hello", "UTF-8" ) );
+    LOG.trace( ">>>>>>>> URLCodec(hello+hello)=" + codec.encode( "hello+hello", "UTF-8" ) );
     return subject;
   }
 
