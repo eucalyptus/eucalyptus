db3cfd7266012b7617451882d007028367b652c1
wenye <wenye@gushan>
2011-03-29 20:14:52 -0700

    Debugging.

--- clc/modules/msgs/src/main/java/com/eucalyptus/auth/login/Hmacv2LoginModule.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/auth/login/Hmacv2LoginModule.java
@@ -36,17 +36,28 @@ public class Hmacv2LoginModule extends BaseLoginModule<HmacCredentials> {
     String canonicalStringWithPort = this.makeSubjectString( credentials.getVerb( ), credentials.getHeaderHost( ) + ":" + credentials.getHeaderPort( ), credentials.getServicePath( ), credentials.getParameters( ) );
     String computedSig = this.getSignature( secretKey, canonicalString, credentials.getSignatureMethod( ) );
     String computedSigWithPort = this.getSignature( secretKey, canonicalStringWithPort, credentials.getSignatureMethod( ) );
-    LOG.debug( "=========>" + "incoming sig=" + sig );
-    LOG.debug( "=========>" + "sig=" + computedSig + ", sigWithPort=" + computedSigWithPort );
+    LOG.debug( "=========>" + "incoming: canonicalString=" + canonicalString );
+    LOG.debug( "=========>" + "incoming: canonicalStringWithPort=" + canonicalStringWithPort );
+    LOG.debug( "=========>" + "incoming: sig=" + sig );
+    LOG.debug( "=========>" + "computed: computedSig=" + computedSig );
+    LOG.debug( "=========>" + "computed: computedSigWithPort=" + computedSigWithPort );
     if ( !computedSig.equals( sig ) && !computedSigWithPort.equals( sig ) ) {
       sig = URLDecoder.decode( sig ).replaceAll("=","");
       computedSig = this.getSignature( secretKey, canonicalString.replaceAll("\\+","%2B"), credentials.getSignatureMethod( ) ).replaceAll("\\+"," ");
       computedSigWithPort = this.getSignature( secretKey, canonicalStringWithPort.replaceAll("\\+","%2B"), credentials.getSignatureMethod( ) ).replaceAll("\\+"," ");
-      LOG.debug( "=========>" + "sig=" + computedSig + ", sigWithPort=" + computedSigWithPort );
+      LOG.debug( "=========>" + "incoming: canonicalString=" + canonicalString );
+      LOG.debug( "=========>" + "incoming: canonicalStringWithPort=" + canonicalStringWithPort );
+      LOG.debug( "=========>" + "incoming: sig=" + sig );
+      LOG.debug( "=========>" + "computed: computedSig=" + computedSig );
+      LOG.debug( "=========>" + "computed: computedSigWithPort=" + computedSigWithPort );
       if( !computedSig.equals( sig ) && !computedSigWithPort.equals( sig ) ) {
         computedSig = this.getSignature( secretKey, canonicalString.replaceAll("\\+","%20"), credentials.getSignatureMethod( ) ).replaceAll("\\+"," ");
         computedSigWithPort = this.getSignature( secretKey, canonicalStringWithPort.replaceAll("\\+","%20"), credentials.getSignatureMethod( ) ).replaceAll("\\+"," ");
-        LOG.debug( "=========>" + "sig=" + computedSig + ", sigWithPort=" + computedSigWithPort );
+        LOG.debug( "=========>" + "incoming: canonicalString=" + canonicalString );
+        LOG.debug( "=========>" + "incoming: canonicalStringWithPort=" + canonicalStringWithPort );
+        LOG.debug( "=========>" + "incoming: sig=" + sig );
+        LOG.debug( "=========>" + "computed: computedSig=" + computedSig );
+        LOG.debug( "=========>" + "computed: computedSigWithPort=" + computedSigWithPort );
         if( !computedSig.equals( sig ) && !computedSigWithPort.equals( sig ) ) {
           return false;
         }
