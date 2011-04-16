d0a4e4cd1b4183c2e5a042c22b3bafdd013ae38a
twerges <twerges@eucahost-3-120.eucalyptus>
2011-04-16 16:13:17 -0700

    added

---
+++ clc/modules/www/src/main/java/com/eucalyptus/www/ReportingGenerator.java
@@ -0,0 +1,74 @@
+package com.eucalyptus.www;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+/**
+ * <p>This servlet will generate or remove false data, for testing the reporting
+ * system. It accepts two HTTP params: <code>command</code> and
+ * <code>target</code>. Command can be either "generate" or "remove", and target
+ * can be either "instance" (which means generate fake instance usage data) or
+ * "storage" (which means generate fake storage data).
+ */
+public class ReportingGenerator extends HttpServlet
+{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException
+	{
+		String command = req.getParameter("command");
+		String target = req.getParameter("target");
+
+		if (command == null || target == null) {
+			throw new ServletException(
+					"Command and target params must both be present");
+		}
+
+		int rv = 0;
+		if (command.equalsIgnoreCase("generate")
+				&& target.equalsIgnoreCase("instance")) {
+
+			rv = com.eucalyptus.reporting.instance.FalseDataGenerator
+					.generateFalseData();
+			res.setStatus((rv == 0)
+					? HttpServletResponse.SC_ACCEPTED
+					: HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+		} else if (command.equalsIgnoreCase("remove")
+				&& target.equalsIgnoreCase("instance")) {
+
+			rv = com.eucalyptus.reporting.instance.FalseDataGenerator
+					.removeFalseData();
+			res.setStatus((rv == 0)
+					? HttpServletResponse.SC_ACCEPTED
+					: HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+		} else if (command.equalsIgnoreCase("generate")
+				&& target.equalsIgnoreCase("storage")) {
+
+			rv = com.eucalyptus.reporting.storage.FalseDataGenerator
+					.generateFalseData();
+			res.setStatus((rv == 0)
+					? HttpServletResponse.SC_ACCEPTED
+					: HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+		} else if (command.equalsIgnoreCase("remove")
+				&& target.equalsIgnoreCase("storage")) {
+
+			rv = com.eucalyptus.reporting.storage.FalseDataGenerator
+					.removeFalseData();
+			res.setStatus((rv == 0)
+					? HttpServletResponse.SC_ACCEPTED
+					: HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+		} else {
+
+			res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+		}
+
+	}
+}
