7d8ef05e17b79ddb4205508c0ed00e306029768f
twerges <twerges@eucahost-3-120.eucalyptus>
2011-04-19 13:43:54 -0700

    added

---
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/storage/StorageEventListener.java
@@ -0,0 +1,266 @@
+package com.eucalyptus.reporting.storage;
+
+import java.util.*;
+
+import org.apache.log4j.Logger;
+
+import com.eucalyptus.entities.EntityWrapper;
+import com.eucalyptus.event.EventListener;
+import com.eucalyptus.reporting.Period;
+import com.eucalyptus.reporting.event.Event;
+import com.eucalyptus.reporting.event.StorageEvent;
+
+public class StorageEventListener
+	implements EventListener<Event>
+{
+	private static Logger LOG = Logger.getLogger( StorageEventListener.class );
+	
+	private static long WRITE_INTERVAL_MS = 10800000l; //write every 3 hrs
+
+	private Map<UsageDataKey, StorageUsageData> usageDataMap;
+	private long lastStoredMs = 0l;
+
+	@Override
+	public void fireEvent(Event event)
+	{
+		if (event instanceof StorageEvent) {
+			StorageEvent storageEvent = (StorageEvent) event;
+			long timeMillis = getCurrentTimeMillis();
+
+			final StorageUsageLog usageLog = StorageUsageLog.getStorageUsageLog();
+
+			EntityWrapper<StorageUsageSnapshot> entityWrapper =
+				EntityWrapper.get( StorageUsageSnapshot.class );
+			try {
+
+				LOG.info("Receive event:" + storageEvent.toString());
+
+				/* Load usageDataMap if starting up
+				 */
+				if (usageDataMap == null) {
+					this.usageDataMap = new HashMap<UsageDataKey, StorageUsageData>();
+					// TODO: optimize so we don't have to scan entire log on
+					// startup
+					Iterator<StorageUsageSnapshot> iter = usageLog
+							.scanLog(new Period(0l, Long.MAX_VALUE));
+					while (iter.hasNext()) {
+						StorageUsageSnapshot snapshot = iter.next();
+						UsageDataKey key = new UsageDataKey(
+								snapshot.getSnapshotKey());
+						usageDataMap.put(key, snapshot.getUsageData());
+						System.out.println("Loaded key:" + key);
+					}
+					LOG.info("Loaded usageDataMap");
+					lastStoredMs = timeMillis; //TODO
+				}
+
+				
+				/* Update usageDataMap
+				 */
+				UsageDataKey key = new UsageDataKey(storageEvent.getOwnerId(),
+						storageEvent.getAccountId(),
+						storageEvent.getClusterName(),
+						storageEvent.getAvailabilityZone());
+				StorageUsageData usageData;
+				if (usageDataMap.containsKey(key)) {
+					usageData = usageDataMap.get(key);
+				} else {
+					usageData = new StorageUsageData();
+					usageDataMap.put(key, usageData);
+				}
+				long addAmountMegs = (storageEvent.isCreateOrDelete())
+						? storageEvent.getSizeMegs()
+						: -storageEvent.getSizeMegs();
+				long addNum = (storageEvent.isCreateOrDelete()) ? 1 : -1;
+				switch(storageEvent.getEventType()) {
+					case S3Object:
+						Long newObjectsNum =
+							addLong(usageData.getObjectsNum(), addNum);
+						usageData.setObjectsNum(newObjectsNum);
+						Long newObjectsMegs =
+							addLong(usageData.getObjectsMegs(), addAmountMegs);
+						usageData.setObjectsMegs(newObjectsMegs);
+						break;
+					case EbsSnapshot:
+						Long newSnapshotsNum =
+							addLong(usageData.getSnapshotsNum(), addNum);
+						usageData.setSnapshotsNum(newSnapshotsNum);
+						Long newSnapshotsMegs =
+							addLong(usageData.getSnapshotsMegs(), addAmountMegs);
+						usageData.setSnapshotsMegs(newSnapshotsMegs);
+						break;
+					case EbsVolume:
+						Long newVolumesNum =
+							addLong(usageData.getVolumesNum(), addNum);
+						usageData.setVolumesNum(newVolumesNum);
+						Long newVolumesMegs =
+							addLong(usageData.getVolumesMegs(), addAmountMegs);
+						usageData.setVolumesMegs(newVolumesMegs);						
+						break;
+
+				}
+
+				/* Write data to DB
+				 */
+				if ((timeMillis - lastStoredMs) > WRITE_INTERVAL_MS) {
+					/* Write all snapshots
+					 */
+					for (UsageDataKey udk: usageDataMap.keySet()) {
+						SnapshotKey snapshotKey = udk.newSnapshotKey(timeMillis);
+						StorageUsageSnapshot sus =
+							new StorageUsageSnapshot(snapshotKey, usageDataMap.get(key));
+						System.out.println("Storing:" + sus);
+						entityWrapper.add(sus);						
+					}
+				} else {
+					/* Write this snapshot
+					 */
+					SnapshotKey snapshotKey = key.newSnapshotKey(timeMillis);
+					StorageUsageSnapshot sus =
+						new StorageUsageSnapshot(snapshotKey, usageDataMap.get(key));
+					System.out.println("Storing:" + sus);
+					entityWrapper.add(sus);
+				}
+
+			} catch (Exception ex) {
+				entityWrapper.rollback();
+				LOG.error(ex);		
+			}
+		}
+
+	}
+	
+	private static final Long addLong(Long a, long b)
+	{
+		return new Long(a.longValue() + b);
+	}
+
+	protected long getCurrentTimeMillis()
+	{
+		return (this.testCurrentTimeMillis == null)
+				? System.currentTimeMillis()
+				: this.testCurrentTimeMillis.longValue();
+	}
+
+	private Long testCurrentTimeMillis = null;
+
+	/**
+	 * Used only for testing. Sets the poller into test mode and overrides the
+	 * actual timestamp with a false one.
+	 */
+	public void setCurrentTimeMillis(long testCurrentTimeMillis)
+	{
+		this.testCurrentTimeMillis = new Long(testCurrentTimeMillis);
+	}
+
+	private class UsageDataKey
+	{
+		private final String ownerId;
+		private final String accountId;
+		private final String clusterName;
+		private final String availabilityZone;
+
+		public UsageDataKey(String ownerId, String accountId, String clusterName,
+				String availabilityZone)
+		{
+			this.ownerId = ownerId;
+			this.accountId = accountId;
+			this.clusterName = clusterName;
+			this.availabilityZone = availabilityZone;
+		}
+		
+		public UsageDataKey(SnapshotKey key)
+		{
+			this.ownerId = key.getOwnerId();
+			this.accountId = key.getAccountId();
+			this.clusterName = key.getClusterName();
+			this.availabilityZone = key.getAvailabilityZone();
+		}
+
+		public String getOwnerId()
+		{
+			return ownerId;
+		}
+
+		public String getAccountId()
+		{
+			return accountId;
+		}
+
+		public String getClusterName()
+		{
+			return clusterName;
+		}
+
+		public String getAvailabilityZone()
+		{
+			return availabilityZone;
+		}
+		
+		public SnapshotKey newSnapshotKey(long timestampMs)
+		{
+			return new SnapshotKey(ownerId, accountId, clusterName, availabilityZone, timestampMs);
+		}
+
+		@Override
+		public int hashCode()
+		{
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result
+					+ ((accountId == null) ? 0 : accountId.hashCode());
+			result = prime
+					* result
+					+ ((availabilityZone == null) ? 0 : availabilityZone
+							.hashCode());
+			result = prime * result
+					+ ((clusterName == null) ? 0 : clusterName.hashCode());
+			result = prime * result
+					+ ((ownerId == null) ? 0 : ownerId.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			UsageDataKey other = (UsageDataKey) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (accountId == null) {
+				if (other.accountId != null)
+					return false;
+			} else if (!accountId.equals(other.accountId))
+				return false;
+			if (availabilityZone == null) {
+				if (other.availabilityZone != null)
+					return false;
+			} else if (!availabilityZone.equals(other.availabilityZone))
+				return false;
+			if (clusterName == null) {
+				if (other.clusterName != null)
+					return false;
+			} else if (!clusterName.equals(other.clusterName))
+				return false;
+			if (ownerId == null) {
+				if (other.ownerId != null)
+					return false;
+			} else if (!ownerId.equals(other.ownerId))
+				return false;
+			return true;
+		}
+
+		private StorageEventListener getOuterType()
+		{
+			return StorageEventListener.this;
+		}
+
+
+	}
+}
