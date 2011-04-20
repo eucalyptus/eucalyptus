391f8e592ed6132bd1e6a7e92b60c23401f82219
twerges <twerges@eucahost-3-120.eucalyptus>
2011-04-20 16:30:53 -0700

    added

---
+++ clc/modules/msgs/src/main/java/com/eucalyptus/util/ExposedCommand.java
@@ -0,0 +1,11 @@
+package com.eucalyptus.www;
+
+/**
+ * <p>Must be present on all methods executed by <code>CommandServlet</code>
+ * 
+ * @see CommandServlet
+ * @author tom.werges
+ */
+public @interface ExposedCommand
+{
+}
