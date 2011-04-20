608cf337182a67553faf348a9725d437b253f85b
twerges <twerges@eucahost-3-120.eucalyptus>
2011-04-20 16:08:46 -0700

    minor commentary

--- clc/modules/www/src/main/java/com/eucalyptus/www/ExposedCommand.java
+++ clc/modules/www/src/main/java/com/eucalyptus/www/ExposedCommand.java
@@ -1,5 +1,11 @@
 package com.eucalyptus.www;
 
-public @interface ExposedCommand {
-
+/**
+ * <p>Must be present on all methods executed by <code>CommandServlet</code>
+ * 
+ * @see CommandServlet
+ * @author tom.werges
+ */
+public @interface ExposedCommand
+{
 }
