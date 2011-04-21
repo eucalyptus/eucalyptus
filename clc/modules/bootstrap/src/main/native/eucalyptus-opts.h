366708e1980b46df390e37c062332e8eb6e825a8
decker <decker@personal-army>
2011-04-20 20:58:49 -0700

    remove gengetopt file; manually deleting eucalyptus-opts.h will trigger regeneration of gengetopts

--- clc/modules/bootstrap/src/main/native/eucalyptus-opts.h
+++ clc/modules/bootstrap/src/main/native/eucalyptus-opts.h
@@ -31,7 +31,7 @@ extern "C" {
 
 #ifndef ARGUMENTS_VERSION
 /** @brief the program version */
-#define ARGUMENTS_VERSION "eee-2.1.0"
+#define ARGUMENTS_VERSION "eee-3.0.0"
 #endif
 
 /** @brief Where the command line options are stored */
