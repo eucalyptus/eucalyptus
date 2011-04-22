7540a0d645fea6d58441449d3c8a87a16e25f1bc
twerges <twerges@eucahost-3-120.eucalyptus>
2011-04-22 13:34:15 -0700

    now verifies admin pw. also, minor re-ordering and javadoc changes

--- clc/modules/www/src/main/java/com/eucalyptus/www/CommandServlet.java
+++ clc/modules/www/src/main/java/com/eucalyptus/www/CommandServlet.java
@@ -8,14 +8,19 @@ import javax.servlet.http.*;
 
 import org.apache.log4j.Logger;
 
+import com.eucalyptus.crypto.Crypto;
 import com.eucalyptus.util.ExposedCommand;
+import com.google.gwt.user.client.rpc.SerializableException;
+
+import edu.ucsb.eucalyptus.admin.server.EucalyptusWebBackendImpl;
 
 /**
  * <p>CommandServlet invokes a single static method within a running Eucalyptus
- * system. It requires you to pass admin credentials. It accepts the following
- * params: className, methodName, and args; the <code>args</code> param may be
- * omitted, but if it's present, it must be a comma-delimited list of String
- * arguments to be passed to the invoked method.
+ * system. It accepts the following params: className, methodName, methodArgs,
+ * and adminPw. The <code>args</code> param may be omitted, but if it's present,
+ * it must be a comma-delimited list of String arguments to be passed to the
+ * invoked method. The <code>adminPw</code> param must contain the password of
+ * the user "admin" or this servlet will return an error code.
  * 
  * <p>Every method invoked from this servlet must be static, must take only
  * String paramaters, and must have the <code>ExposedCommand</code> annotation.
@@ -24,15 +29,14 @@ import com.eucalyptus.util.ExposedCommand;
  * Eucalyptus functionality; 2) to allow infrequently-used administration
  * commands.
  * 
- * <p>CommandServlet is normally used by the <code>wget</code> shell
- * command with the appropriate URL. For example,
- * <code>wget --no-check-certificate
- * 'https://localhost:8443/commandservlet?className=Foo&methodName=Bar&args=one,two,three
- * ' </code>
+ * <p>CommandServlet is normally used by the <code>wget</code> shell command
+ * with the appropriate URL. For example, <code>wget --no-check-certificate
+ * 'https://localhost:8443/commandservlet?adminPw=admin&className=Foo&
+ * methodName=Bar&args=one,two,three' </code>
  * 
  * <p>Any invoked command is presumed to be successful if it returns normally
  * without exceptions, in which case, an HTTP 200 is returned from this servlet.
- * If an exception is encountered while invoking the specified command, then an
+ * If an exception is caught while invoking the specified command, then an
  * HTTP 500 is returned.
  * 
  * @author tom.werges
@@ -47,29 +51,38 @@ public class CommandServlet
 	protected void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException
 	{
-		final String className = req.getParameter("className");
+		final String className  = req.getParameter("className");
 		final String methodName = req.getParameter("methodName");
-		final String args = req.getParameter("args");
+		final String methodArgs = req.getParameter("methodArgs");
+		final String adminPw    = req.getParameter("adminPw");
+
+		final String[] methodArgsArray;
+		if (methodArgs == null) {
+			methodArgsArray = new String[0];
+		} else {
+			methodArgsArray = methodArgs.split(",");
+		}
+
+		if (className == null || methodName == null || adminPw == null) {
+			throw new ServletException(
+					"className, methodName, adminPw params must be present");
+		}
 
 		LOG.info(String.format(
 				"CommandServlet called, class:%s method:%s args:%s", className,
-				methodName, args));
-		
-		if (className == null || methodName == null) {
-			throw new ServletException(
-					"className and methodName params must both be present");
+				methodName, methodArgs));
+
+
+		/* Verify admin pw */
+		try {
+			new EucalyptusWebBackendImpl().getNewSessionID("admin", adminPw);
+		} catch (SerializableException e) {
+			LOG.error(e);
+			throw new ServletException("Incorrect admin password");
 		}
 
-		String[] methodArgsArray = new String[0];
-		if (args != null) {
-			methodArgsArray = args.split(",");
-			LOG.info(String.format("Executing class:%s method:%s args:%s\n",
-					className, methodName, args));
-		} else {
-			LOG.info(String.format("Executing class:%s method:%s\n", className,
-					methodName));
-		}
-		
+
+		// (Only get methods which have n String params corresponding to passed args)
 		Class[] params = new Class[methodArgsArray.length];
 		for (int j = 0; j < params.length; j++) {
 			params[j] = String.class;
@@ -83,9 +96,11 @@ public class CommandServlet
 			throw new ServletException(ex);
 		}
 
+
 		if (! method.isAnnotationPresent(ExposedCommand.class)) {
 			throw new ServletException("Method lacks ExposedCommand annotation");			
 		}
+
 
 		try {
 			method.invoke(null, (Object[]) methodArgsArray);
