03ce71a4799879e72e609989f7969779e2293666
decker <decker@personal-army>
2011-04-28 21:16:24 -0700

    adjust eclipse project

--- devel/dot-fsm.sh
+++ devel/dot-fsm.sh
@@ -4,14 +4,12 @@ echo 'digraph bootstrap {
   nodesep=.2;
   clusterrank="local";
   rankdir="TB";'
-sed '
-	s/)\./)\n/g;
-	s/^ *//g;
-	s/State\.//g;
-	s/Transition\.//g;
-	s/\.class//g;
-	s/\(\w\)( *\(.*\));$/\1( \2/g
-' | \
+sed 's/)\./)\n/g;
+s/^ *//g;
+s/State\.//g;
+s/Transition\.//g;
+s/\.class//g;
+s/\(\w\)( *\(.*\));$/\1( \2/g' | \
 awk '
 /new StateMachineBuilder/{start="true"}
 /newAtomicStateMachine/{start="false"}
@@ -20,9 +18,10 @@ awk '
 /error\(/{err=$2}
 /on\(/{on=$2}
 /(run|condition)\(/ && start == "true" {
-	run=gensub("\.run.|\.condition.","","g",$0);
-	printf("%s -> %s [label=\"%s\\n%s\"];\n",from,to,on,run);
-	if(err) printf("%s -> %s [label=\"error\"];\n", from, err)
+	labelProps="fontsize=\"8.0\"";
+	action=gensub("(.*run.|.*condition\\()","","g",$0);
+	printf("%s -> %s [label=\"%s\\n%s\",%s];\n",from,to,on,action,labelProps);
+	if(err) printf("%s -> %s [label=\"error\",color=\"gray\",%s];\n", from, err,labelProps)
 }' | \
 sed 's/\./=/g'
 echo "}"
