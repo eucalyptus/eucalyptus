1e5b1cceeca8ec05c9b98738656889385b67c9df
decker <decker@irony>
2011-04-30 02:53:53 -0700

    clarify and improve the error handling and related apis for async events; esp. for service state handling

--- clc/modules/configuration/src/main/java/com/eucalyptus/config/Configuration.java
+++ clc/modules/configuration/src/main/java/com/eucalyptus/config/Configuration.java
@@ -67,7 +67,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import org.apache.log4j.Logger;
 import com.eucalyptus.component.Component;
 import com.eucalyptus.component.ComponentId;
@@ -84,68 +83,70 @@ import com.eucalyptus.util.EucalyptusCloudException;
 import com.google.common.collect.Sets;
 
 public class Configuration {
-  public static Logger        LOG                 = Logger.getLogger( Configuration.class );
+  public static Logger LOG                 = Logger.getLogger( Configuration.class );
   static String        CLUSTER_KEY_FSTRING = "cc-%s";
   static String        NODE_KEY_FSTRING    = "nc-%s";
   
-  public RegisterComponentResponseType registerComponent( RegisterComponentType request ) throws EucalyptusCloudException {
-    Component component = Components.oneWhichHandles( request.getClass( ) );
-    ComponentId componentId = component.getComponentId( );
-    RegisterComponentResponseType reply = ( RegisterComponentResponseType ) request.getReply( );
-    String name = request.getName( );
-    String hostName = request.getHost( );
-    Integer port = request.getPort( );
+  public RegisterComponentResponseType registerComponent( final RegisterComponentType request ) throws EucalyptusCloudException {
+    final Component component = Components.oneWhichHandles( request.getClass( ) );
+    final ComponentId componentId = component.getComponentId( );
+    final RegisterComponentResponseType reply = ( RegisterComponentResponseType ) request.getReply( );
+    final String name = request.getName( );
+    final String hostName = request.getHost( );
+    final Integer port = request.getPort( );
     Assertions.assertNotNull( name, "Name must not be null: " + request );
     Assertions.assertNotNull( hostName, "Hostname must not be null: " + request );
     Assertions.assertNotNull( port, "Port must not be null: " + request );
-
+    
     String partition = request.getPartition( );
-    if( !component.getComponentId( ).isPartitioned( ) ) {//TODO:GRZE: convert to @NotNull
-      partition = componentId.getPartition( ); 
-      LOG.error( "Unpartitioned component (" + componentId.getFullName( ) + ") is being registered w/o a partition.  Using fixed partition=" + partition + " for request: " + request );
-    } else if( component.getComponentId( ).isPartitioned( ) && partition == null ) {
+    if ( !component.getComponentId( ).isPartitioned( ) ) {//TODO:GRZE: convert to @NotNull
+      partition = componentId.getPartition( );
+      LOG.error( "Unpartitioned component (" + componentId.getFullName( ) + ") is being registered w/o a partition.  Using fixed partition=" + partition
+                 + " for request: " + request );
+    } else if ( component.getComponentId( ).isPartitioned( ) && ( partition == null ) ) {
       partition = name;
       LOG.error( "Partitioned component is being registered w/o a partition.  Using partition=name=" + partition + " for request: " + request );
     }
     try {
       reply.set_return( ComponentRegistrationHandler.register( component, partition, name, hostName, port ) );
-    } catch ( Throwable ex ) {
+    } catch ( final Throwable ex ) {
       throw new EucalyptusCloudException( "Component registration failed because: " + ex.getMessage( ), ex );
     }
     return reply;
   }
   
-  public DeregisterComponentResponseType deregisterComponent( DeregisterComponentType request ) throws EucalyptusCloudException {
-    Component component = Components.oneWhichHandles( request.getClass( ) );
-    DeregisterComponentResponseType reply = ( DeregisterComponentResponseType ) request.getReply( );
+  public DeregisterComponentResponseType deregisterComponent( final DeregisterComponentType request ) throws EucalyptusCloudException {
+    final Component component = Components.oneWhichHandles( request.getClass( ) );
+    final DeregisterComponentResponseType reply = ( DeregisterComponentResponseType ) request.getReply( );
     try {
       reply.set_return( ComponentRegistrationHandler.deregister( component, request.getPartition( ), request.getName( ) ) );
-    } catch ( Throwable ex ) {
+    } catch ( final Throwable ex ) {
       throw new EucalyptusCloudException( "Component deregistration failed because: " + ex.getMessage( ), ex );
     }
     return reply;
   }
-    
-  public DescribeNodesResponseType listComponents( DescribeNodesType request ) throws EucalyptusCloudException {
-    DescribeNodesResponseType reply = ( DescribeNodesResponseType ) request.getReply( );
+  
+  public DescribeNodesResponseType listComponents( final DescribeNodesType request ) throws EucalyptusCloudException {
+    final DescribeNodesResponseType reply = ( DescribeNodesResponseType ) request.getReply( );
     reply.setRegistered( ( ArrayList<NodeComponentInfoType> ) GroovyUtil.evaluateScript( "describe_nodes" ) );
     return reply;
   }
   
   private static final Set<String> attributes = Sets.newHashSet( "partition", "state" );
-  public ModifyComponentAttributeResponseType modify( ModifyComponentAttributeType request ) throws EucalyptusCloudException {
-    ModifyComponentAttributeResponseType reply = request.getReply( );
+  
+  public ModifyComponentAttributeResponseType modify( final ModifyComponentAttributeType request ) throws EucalyptusCloudException {
+    final ModifyComponentAttributeResponseType reply = request.getReply( );
     if ( !attributes.contains( request.getAttribute( ) ) ) {
       throw new EucalyptusCloudException( "Request to modify unknown attribute: " + request.getAttribute( ) );
     }
-    Component component = Components.oneWhichHandles( request.getClass( ) );
-    ServiceBuilder builder = component.getBuilder( );
+    final Component component = Components.oneWhichHandles( request.getClass( ) );
+    final ServiceBuilder builder = component.getBuilder( );
     LOG.info( "Using builder: " + builder.getClass( ).getSimpleName( ) );
     if ( "state".equals( request.getAttribute( ) ) ) {
       ServiceConfiguration conf;
       try {
         conf = builder.lookupByName( request.getName( ) );
-      } catch ( ServiceRegistrationException e ) {
+      } catch ( final ServiceRegistrationException e ) {
         LOG.info( builder.getClass( ).getSimpleName( ) + ": lookupByName failed." );
         LOG.error( e, e );
         throw e;
@@ -153,8 +154,8 @@ public class Configuration {
       if ( "enable".startsWith( request.getValue( ).toLowerCase( ) ) ) {
         try {
           builder.getComponent( ).enableTransition( conf ).get( );
-        } catch ( Exception ex ) {
-          LOG.error( ex , ex );
+        } catch ( final Exception ex ) {
+          LOG.error( ex, ex );
           throw new EucalyptusCloudException( ex.getMessage( ), ex );
         }
       } else if ( "disable".startsWith( request.getValue( ).toLowerCase( ) ) ) {
@@ -164,42 +165,48 @@ public class Configuration {
     return reply;
   }
   
-  public DescribeComponentsResponseType listComponents( DescribeComponentsType request ) throws EucalyptusCloudException {
-    DescribeComponentsResponseType reply = ( DescribeComponentsResponseType ) request.getReply( );
-    List<ComponentInfoType> listConfigs = reply.getRegistered( );
+  public DescribeComponentsResponseType listComponents( final DescribeComponentsType request ) throws EucalyptusCloudException {
+    final DescribeComponentsResponseType reply = ( DescribeComponentsResponseType ) request.getReply( );
+    final List<ComponentInfoType> listConfigs = reply.getRegistered( );
     if ( DescribeComponentsType.class.equals( request.getClass( ) ) ) {
-      for ( Component c : Components.list( ) ) {
-        if( c.lookupServices( ).isEmpty( ) ) {
-          listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", c.getComponentId( ).name( ).toUpperCase( ) ), c.getComponentId( ).name( ), "", c.getState( ).toString( ), "" ) );
+      for ( final Component c : Components.list( ) ) {
+        if ( c.lookupServices( ).isEmpty( ) ) {
+          listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", c.getComponentId( ).name( ).toUpperCase( ) ), c.getComponentId( ).name( ), "",
+                                                  c.getState( ).toString( ), "" ) );
         } else {
-          for ( ServiceConfiguration conf : c.lookupServiceConfigurations( ) ) {
+          for ( final ServiceConfiguration conf : c.lookupServiceConfigurations( ) ) {
             try {
-              listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", conf.getComponentId( ).name( ).toUpperCase( ) ) + ( conf.getPartition( ) != null
-                ? conf.getPartition( )
-                : "-" ),
-                                                      conf.getFullName( ).toString( ), conf.getHostName( ), conf.lookupStateMachine( ).getState( ).toString( ), "" ) );
-            } catch ( Exception ex ) {
-              LOG.error( ex , ex );
-              listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", conf.getComponentId( ).name( ).toUpperCase( ) ) + ( conf.getPartition( ) != null
-                  ? conf.getPartition( )
-                    : "-" ),
-                    conf.getFullName( ).toString( ), conf.getHostName( ), "none", "" ) );
+              listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", conf.getComponentId( ).name( ).toUpperCase( ) )
+                                                      + ( conf.getPartition( ) != null
+                                                        ? conf.getPartition( )
+                                                        : "-" ),
+                                                      conf.getFullName( ).toString( ), conf.getHostName( ), conf.lookupStateMachine( ).getState( ).toString( ),
+                                                      "" ) );
+            } catch ( final Exception ex ) {
+              LOG.error( ex, ex );
+              listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", conf.getComponentId( ).name( ).toUpperCase( ) )
+                                                      + ( conf.getPartition( ) != null
+                                                        ? conf.getPartition( )
+                                                        : "-" ),
+                                                      conf.getFullName( ).toString( ), conf.getHostName( ), "none", "" ) );
             }
-            for ( String d : s.getDetails( ) ) {
-              listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", conf.getComponentId( ).name( ).toUpperCase( ) ) + ( conf.getPartition( ) != null
-                ? conf.getPartition( )
-                : "-" ),
+            for ( final String d : conf.lookupService( ).getDetails( ) ) {
+              listConfigs.add( new ComponentInfoType( String.format( "%-15.15s", conf.getComponentId( ).name( ).toUpperCase( ) )
+                                                      + ( conf.getPartition( ) != null
+                                                        ? conf.getPartition( )
+                                                        : "-" ),
                                                       conf.getName( ), "detail", d, "" ) );
             }
           }
         }
       }
     } else {
-      for ( ServiceConfiguration conf : ServiceBuilderRegistry.handles( request.getClass( ) ).list( ) ) {
+      for ( final ServiceConfiguration conf : ServiceBuilderRegistry.handles( request.getClass( ) ).list( ) ) {
         try {
-          Service s = Components.lookup( conf.getComponentId( ) ).lookupService( conf );
-          listConfigs.add( new ComponentInfoType( conf.getPartition( ), conf.getName( ), conf.getHostName( ), conf.lookupStateMachine( ).getState( ).toString( ), s.getDetails( ) ) );
-        } catch ( NoSuchElementException ex ) {
+          final Service s = Components.lookup( conf.getComponentId( ) ).lookupService( conf );
+          listConfigs.add( new ComponentInfoType( conf.getPartition( ), conf.getName( ), conf.getHostName( ),
+                                                  conf.lookupStateMachine( ).getState( ).toString( ), s.getDetails( ) ) );
+        } catch ( final NoSuchElementException ex ) {
           listConfigs.add( new ComponentInfoType( conf.getPartition( ), conf.getName( ), conf.getHostName( ), Component.State.NOTREADY.toString( ), "unknown" ) );
           LOG.error( ex, ex );
         }
