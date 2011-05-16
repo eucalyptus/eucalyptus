801fe86e98e89f7fd37bfaa76fb2c09e5f7b54bf
decker <decker@personal-army>
2011-05-15 17:25:24 -0700

    intermediate commit

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/Cluster.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/Cluster.java
@@ -270,8 +270,8 @@ public class Cluster implements HasFullName<Cluster>, EventListener, HasStateMac
         @Override
         public final void leave( final Cluster parent, final Callback.Completion transitionCallback ) {
           try {
-            AsyncRequests.newRequest( factory.newInstance( ) )
-                         .sendSync( parent.getLogServiceConfiguration( ) );
+            CheckedListenableFuture res = AsyncRequests.newRequest( factory.newInstance( ) ).dispatch( parent.getLogServiceConfiguration( ) );
+            res.get( );
             transitionCallback.fire( );
           } catch ( final ExecutionException e ) {
             if ( e.getCause( ) instanceof FailedRequestException ) {
