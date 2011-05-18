62b4d046b00da36462f6ab54f39fba9b1c4ce374
twerges <twerges@eucahost-3-120.eucalyptus>
2011-05-17 18:21:44 -0700

    added logic to verify that the generated report has the correct number of entries

--- clc/modules/reporting/src/test/sh/runS3ReportingTest.sh
+++ clc/modules/reporting/src/test/sh/runS3ReportingTest.sh
@@ -19,6 +19,14 @@ wget --no-check-certificate -O /tmp/nothing "https://localhost:8443/commandservl
 # Generate storage report, based upon data generated above
 wget -O /tmp/s3Report.csv --no-check-certificate "https://localhost:8443/reports?session=$SESSIONID&name=user_s3&type=csv&page=0&flush=false&start=1104566400000&end=1304566400000&criterionId=2&groupById=0"
 
-# Verify that the report is correct, using the CsvChecker tool.
-#java -jar CsvChecker.jar 0.2 s3Reference.csv /tmp/s3Report.csv
+
+# Verify that the resulting report has the correct number of entries in it
+REPORT_USERS_CNT=`cat /tmp/s3Report.csv |  grep 'user-[0-9]\+' | wc -l`
+if [ "$REPORT_USERS_CNT" -ne "32" ]; then
+	echo "Report users count is incorrect."
+	exit -1
+fi
+
+# Verify that the data in the report is correct, using the CsvChecker tool.
+java -jar CsvChecker.jar 0.2 s3Reference.csv /tmp/s3Report.csv
 
