8f6e3ee4be469236b8e059f537b54df442cbeb49
twerges <twerges@eucahost-3-120.eucalyptus>
2011-05-17 18:35:50 -0700

    added logic to verify that the generated report has the correct number of entries

--- clc/modules/reporting/src/test/sh/runInstanceReportingTest.sh
+++ clc/modules/reporting/src/test/sh/runInstanceReportingTest.sh
@@ -19,6 +19,13 @@ wget --no-check-certificate -O /tmp/nothing "https://localhost:8443/commandservl
 # Generate instance report, based upon data generated above
 wget -O /tmp/instanceReport.csv --no-check-certificate "https://localhost:8443/reports?session=$SESSIONID&name=user_vms&type=csv&page=0&flush=false&start=1104566400000&end=1304566400000&criterionId=2&groupById=0"
 
+# Verify that the resulting report has the correct number of entries in it
+REPORT_USERS_CNT=`cat /tmp/instanceReport.csv |  grep 'user-[0-9]\+' | wc -l`
+if [ "$REPORT_USERS_CNT" -ne "8" ]; then
+	echo "Report users count is incorrect."
+	exit -1
+fi
+
 # Verify that the report is correct, using the CsvChecker tool.
 java -jar CsvChecker.jar 0.2 instanceReference.csv /tmp/instanceReport.csv
 
