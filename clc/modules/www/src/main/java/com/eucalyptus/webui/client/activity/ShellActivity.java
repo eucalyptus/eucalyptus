8130575556d49864d452b42e418836cab76d754b
wenye <wenye@gushan>
2011-05-19 19:52:45 -0700

    Quick fix.

--- clc/modules/www/src/main/java/com/eucalyptus/webui/client/activity/ShellActivity.java
+++ clc/modules/www/src/main/java/com/eucalyptus/webui/client/activity/ShellActivity.java
@@ -211,7 +211,7 @@ public class ShellActivity extends AbstractActivity implements FooterView.Presen
   public void search( String search ) {
     if ( search != null ) {
       LOG.log( Level.INFO, "New search: " + search );
-      this.clientFactory.getMainHistorian( ).newItem( SearchPlace.encode( search ), true/*issueEvent*/ );
+      this.clientFactory.getMainHistorian( ).newItem( search, true/*issueEvent*/ );
     } else {
       LOG.log( Level.INFO, "Empty search!" );
     }
