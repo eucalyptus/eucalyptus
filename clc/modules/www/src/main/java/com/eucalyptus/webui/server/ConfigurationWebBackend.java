8e2c3954617dc3e99b541673a10a58ec2ffc79e2
wenye <wenye@gushan>
2011-05-23 23:20:57 -0700

    Fix cloud config setting. Fix detail view issue.

--- clc/modules/www/src/main/java/com/eucalyptus/webui/server/ConfigurationWebBackend.java
+++ clc/modules/www/src/main/java/com/eucalyptus/webui/server/ConfigurationWebBackend.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import org.apache.log4j.Logger;
 import com.eucalyptus.address.AddressingConfiguration;
 import com.eucalyptus.component.Components;
@@ -21,7 +22,9 @@ import com.eucalyptus.util.DNSProperties;
 import com.eucalyptus.util.EucalyptusCloudException;
 import com.eucalyptus.util.Internets;
 import com.eucalyptus.util.LogUtil;
+import com.eucalyptus.util.Transactions;
 import com.eucalyptus.util.WalrusProperties;
+import com.eucalyptus.util.async.Callback;
 import com.eucalyptus.webui.client.service.EucalyptusServiceException;
 import com.eucalyptus.webui.client.service.SearchResultFieldDesc;
 import com.eucalyptus.webui.client.service.SearchResultFieldDesc.TableDisplay;
@@ -66,6 +69,7 @@ public class ConfigurationWebBackend {
   public static final String DNS_IP = "DNS IP";
   public static final String MAX_USER_PUBLIC_ADDRESSES = "Max public addresses per user";
   public static final String ENABLE_DYNAMIC_PUBLIC_ADDRESSES = "Enable dynamic public addresses";
+  public static final String SYSTEM_RESERVED_PUBLIC_ADDRESSES = "System reserved public addresses";
   
   public static final String PORT = "Port";
   public static final String MAX_VLAN = "Max VLAN tag";
@@ -89,13 +93,14 @@ public class ConfigurationWebBackend {
   // Cloud config extra fields
   public static final ArrayList<SearchResultFieldDesc> CLOUD_CONFIG_EXTRA_FIELD_DESCS = Lists.newArrayList( );
   static {
-    CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "defaultKernel", DEFAULT_KERNEL, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
-    CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "defaultRamdisk", DEFAULT_RAMDISK, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
     CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "dnsDomain", DNS_DOMAIN, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
     CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "nameserver", DNS_NAMESERVER, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
     CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "nameserverAddress", DNS_IP, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
+    CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "defaultKernel", DEFAULT_KERNEL, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
+    CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "defaultRamdisk", DEFAULT_RAMDISK, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
     CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "doDynamicPublicAddresses", ENABLE_DYNAMIC_PUBLIC_ADDRESSES, false, "0px", TableDisplay.NONE, Type.BOOLEAN, true, false ) );
     CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "maxUserPublicAddresses", MAX_USER_PUBLIC_ADDRESSES, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
+    CLOUD_CONFIG_EXTRA_FIELD_DESCS.add( new SearchResultFieldDesc( "systemReservedPublicAddresses", SYSTEM_RESERVED_PUBLIC_ADDRESSES, false, "0px", TableDisplay.NONE, Type.TEXT, true, false ) );
   }
   // Cluster config extra fields
   public static final ArrayList<SearchResultFieldDesc> CLUSTER_CONFIG_EXTRA_FIELD_DESCS = Lists.newArrayList( );
@@ -122,13 +127,14 @@ public class ConfigurationWebBackend {
     result.addField( Internets.localHostAddress( ) );               // host
     result.addField( "" );                                    // port
     // Then fill in the specific fields
-    result.addField( ImageConfiguration.getInstance( ).getDefaultKernelId( ) );           // default kernel
-    result.addField( ImageConfiguration.getInstance( ).getDefaultRamdiskId( ) );          // default ramdisk
     result.addField( sysConf.getDnsDomain( ) );               // dns domain
     result.addField( sysConf.getNameserver( ) );              // dns nameserver
     result.addField( sysConf.getNameserverAddress( ) );       // dns IP
+    result.addField( ImageConfiguration.getInstance( ).getDefaultKernelId( ) );           // default kernel
+    result.addField( ImageConfiguration.getInstance( ).getDefaultRamdiskId( ) );          // default ramdisk
     result.addField( AddressingConfiguration.getInstance( ).getDoDynamicPublicAddresses( ).toString( ) );// enable dynamic public addresses
-    result.addField( AddressingConfiguration.getInstance( ).getMaxUserPublicAddresses( ).toString( ) ); // max public addresses per user    
+    result.addField( AddressingConfiguration.getInstance( ).getMaxUserPublicAddresses( ).toString( ) ); // max public addresses per user
+    result.addField( AddressingConfiguration.getInstance( ).getSystemReservedPublicAddresses( ).toString( ) ); // system reserved addresses
   }
   
   /**
@@ -144,38 +150,31 @@ public class ConfigurationWebBackend {
     return result;
   }
   
-  private static void deserializeSystemConfiguration( SystemConfiguration sysConf, SearchResultRow input ) {
-    int i = COMMON_FIELD_DESCS.size( );
-    ImageConfiguration.getInstance( ).setDefaultKernelId( input.getField( i++ ) );
-    ImageConfiguration.getInstance( ).setDefaultRamdiskId( input.getField( i++ ) );
-    sysConf.setDnsDomain( input.getField( i++ ) );
-    sysConf.setNameserver( input.getField( i++ ) );
-    sysConf.setNameserverAddress( input.getField( i++ ) );
-    AddressingConfiguration.getInstance( ).setDoDynamicPublicAddresses( Boolean.parseBoolean( input.getField( i++ ) ) );
-    try {
-      Integer val = Integer.parseInt( input.getField( i++ ) );
-      AddressingConfiguration.getInstance( ).setMaxUserPublicAddresses( val );
-    } catch ( Exception e ) { }
+  private static void deserializeSystemConfiguration( SystemConfiguration sysConf, SearchResultRow input, int index ) {
+    sysConf.setDnsDomain( input.getField( index++ ) );
+    sysConf.setNameserver( input.getField( index++ ) );
+    sysConf.setNameserverAddress( input.getField( index++ ) );
   }
-  
+
   /**
    * Set the cloud configuration using UI input.
    * 
    * @param input
    */
-  public static void setCloudConfiguration( SearchResultRow input ) throws EucalyptusServiceException {
+  public static void setCloudConfiguration( final SearchResultRow input ) throws EucalyptusServiceException {
+    final int i = COMMON_FIELD_DESCS.size( );
     EntityWrapper<SystemConfiguration> db = EntityWrapper.get( SystemConfiguration.class );
     SystemConfiguration sysConf = null;
     try {
       sysConf = db.getUnique( new SystemConfiguration( ) );
-      deserializeSystemConfiguration( sysConf, input );
+      deserializeSystemConfiguration( sysConf, input, i );
       db.commit( );
       DNSProperties.update( );      
     } catch ( EucalyptusCloudException e ) {
       try {
         LOG.debug( e, e );
         sysConf = new SystemConfiguration( );
-        deserializeSystemConfiguration( sysConf, input );
+        deserializeSystemConfiguration( sysConf, input, i );
         db.persist( sysConf );
         db.commit( );
         DNSProperties.update( );
@@ -188,6 +187,46 @@ public class ConfigurationWebBackend {
       ListenerRegistry.getInstance( ).fireEvent( new SystemConfigurationEvent( sysConf ) );
     } catch ( EventFailedException e ) {
       LOG.debug( e, e );
+    }
+    final int j = i + 3;
+    try {
+      Transactions.one( ImageConfiguration.getInstance( ), new Callback<ImageConfiguration>( ) {
+        @Override
+        public void fire( ImageConfiguration t ) {
+          int n = j;
+          t.setDefaultKernelId( input.getField( n++ ) );
+          t.setDefaultRamdiskId( input.getField( n++ ) );
+        }
+      } );
+    } catch ( ExecutionException e ) {
+      LOG.error( "Failed to set image configuration", e );
+      LOG.debug( e, e );
+      throw new EucalyptusServiceException( "Failed to set image configuration", e );
+    }
+    final int k = j + 2;
+    try {
+      Transactions.one( AddressingConfiguration.getInstance( ), new Callback<AddressingConfiguration>( ) {
+        
+        @Override
+        public void fire( AddressingConfiguration t ) {
+          int n = k;
+          t.setDoDynamicPublicAddresses( Boolean.parseBoolean( input.getField( n++ ) ) );
+          try {
+            t.setMaxUserPublicAddresses( Integer.parseInt( input.getField( n++ ) ) );
+          } catch ( Exception e ) {
+            LOG.error( e, e );
+          }
+          try {
+            t.setSystemReservedPublicAddresses( Integer.parseInt( input.getField( n++ ) ) );
+          } catch ( Exception e ) {
+            LOG.error( e, e );
+          }
+        }
+      } );
+    } catch ( ExecutionException e ) {
+      LOG.error( "Failed to set addressing configuration", e );
+      LOG.debug( e, e );
+      throw new EucalyptusServiceException( "Failed to set image configuration", e );
     }    
   }
   
