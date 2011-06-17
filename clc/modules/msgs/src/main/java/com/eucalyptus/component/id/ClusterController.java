00867a05ae338509a56fa44c379b13c196726eae
decker <decker@irony>
2011-06-16 23:20:22 -0700

    fix service context restart timing

--- clc/modules/msgs/src/main/java/com/eucalyptus/component/id/ClusterController.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/component/id/ClusterController.java
@@ -1,15 +1,17 @@
 package com.eucalyptus.component.id;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import com.eucalyptus.component.ComponentId;
 import com.eucalyptus.util.Internets;
 
 public class ClusterController extends ComponentId {
-
+  
   public ClusterController( ) {
     super( "cluster" );
   }
-
+  
   @Override
   public Integer getPort( ) {
     return 8774;
@@ -17,18 +19,18 @@ public class ClusterController extends ComponentId {
   
   @Override
   public String getLocalEndpointName( ) {
-    return String.format( getUriPattern(), Internets.localHostAddress( ), this.getPort( ) );
+    return String.format( getUriPattern( ), Internets.localHostAddress( ), this.getPort( ) );
   }
   
   @Override
   public String getUriPattern( ) {
     return "http://%s:%d/axis2/services/EucalyptusCC";
   }
-
+  
   @Override
   public String getExternalUriPattern( ) {
     return "http://%s:%d/axis2/services/EucalyptusCC";
-  }  
+  }
   
   private static ChannelPipelineFactory clusterPipeline;
   
@@ -38,5 +40,14 @@ public class ClusterController extends ComponentId {
         ? clusterPipeline
           : helpGetClientPipeline( "com.eucalyptus.ws.client.pipeline.ClusterClientPipelineFactory" ) ) );
   }
-    
+  
+  @Override
+  public List<Class<? extends ComponentId>> serviceDependencies( ) {
+    return new ArrayList( ) {
+      {
+        add( Eucalyptus.class );
+      }
+    };
+  }
+  
 }
