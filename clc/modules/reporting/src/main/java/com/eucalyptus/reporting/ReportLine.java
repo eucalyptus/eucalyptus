fa8ec7338b801ad2d73c09f3e0cc080648ef6da9
twerges <>
2011-06-27 12:51:50 -0700

    minor commentary

--- clc/modules/reporting/src/main/java/com/eucalyptus/reporting/ReportLine.java
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/ReportLine.java
@@ -1,7 +1,10 @@
 package com.eucalyptus.reporting;
 
 /**
- * <p>ReportLine represents a line on a visual report.
+ * <p>ReportLine represents a line on a visual report. It contains all the
+ * printable values for each line on a visual report. Each class which
+ * implements ReportLine has numeric properties which have already undergone
+ * unit conversion and are suitable for display.
  * 
  * @author tom.werges
  */
