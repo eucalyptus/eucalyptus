8e9a5d06355b6164fa3ac9540320369f2a74cb10
decker <decker@hawaii>
2011-07-18 16:38:41 -0700

    fix service key creation

--- clc/modules/msgs/src/main/java/com/eucalyptus/component/Topology.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/component/Topology.java
@@ -382,7 +382,7 @@ public class Topology implements EventListener<Event> {
     private final ComponentId componentId;
     
     static ServiceKey create( final ServiceConfiguration config ) throws ServiceRegistrationException {
-      if ( config.getComponentId( ).isAlwaysLocal( ) || config.getComponentId( ).isCloudLocal( ) ) {
+      if ( config.getComponentId( ).isAlwaysLocal( ) || ( config.getComponentId( ).isCloudLocal( ) && !config.getComponentId( ).isRegisterable( ) ) ) {
         Partition p = Partitions.lookupInternal( config );
         return new ServiceKey( config.getComponentId( ), p );
       } else if ( config.getComponentId( ).isPartitioned( ) ) {
