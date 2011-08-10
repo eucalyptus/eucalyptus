f6e4ad555a7a56ea64fa594d85cb8e9164f98953
twerges <>
2011-08-10 15:26:24 -0700

    added

---
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/user/ReportingAccountDao.java
@@ -0,0 +1,105 @@
+package com.eucalyptus.reporting.user;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.eucalyptus.entities.EntityWrapper;
+
+public class ReportingAccountDao
+{
+	private static Logger log = Logger.getLogger( ReportingUserDao.class );
+
+	private final Map<String,String> accounts = new HashMap<String,String>();
+	
+	public ReportingAccountDao()
+	{
+		
+	}
+
+	public void addUpdateAccount(String id, String name)
+	{
+
+		loadIfNeeded();
+		if (accounts.containsKey(id) && accounts.get(id).equals(name)) {
+			return;
+		} else if (accounts.containsKey(id)) {
+			update(id, name);
+		} else {
+			add(id, name);
+		}
+		
+	}
+
+	public String getAccountName(String id)
+	{
+		loadIfNeeded();
+		return accounts.get(id);
+	}
+	
+
+	
+	private void loadIfNeeded()
+	{
+		if (accounts.keySet().size() == 0) {
+
+			EntityWrapper<ReportingAccount> entityWrapper =
+				EntityWrapper.get(ReportingAccount.class);
+
+			try {
+				@SuppressWarnings("rawtypes")
+				List reportingAccounts = (List)
+					entityWrapper.createQuery("from ReportingAccount")
+					.list();
+
+				for (Object obj: reportingAccounts) {
+					ReportingAccount Account = (ReportingAccount) obj;
+					accounts.put(Account.getId(), Account.getName());
+				}
+				
+				entityWrapper.commit();
+			} catch (Exception ex) {
+				log.error(ex);
+				entityWrapper.rollback();
+				throw new RuntimeException(ex);
+			}			
+		}
+	}
+	
+	private void update(String id, String name)
+	{
+		EntityWrapper<ReportingAccount> entityWrapper =
+			EntityWrapper.get(ReportingAccount.class);
+
+		try {
+			ReportingAccount reportingAccount = (ReportingAccount)
+			entityWrapper.createQuery("from ReportingAccount where id = ?")
+				.setString(0, id)
+				.uniqueResult();
+			reportingAccount.setName(name);
+			entityWrapper.commit();
+		} catch (Exception ex) {
+			log.error(ex);
+			entityWrapper.rollback();
+			throw new RuntimeException(ex);
+		}			
+	}
+	
+	private void add(String id, String name)
+	{
+		EntityWrapper<ReportingAccount> entityWrapper =
+			EntityWrapper.get(ReportingAccount.class);
+
+		try {
+			entityWrapper.add(new ReportingAccount(id, name));
+			entityWrapper.commit();
+		} catch (Exception ex) {
+			log.error(ex);
+			entityWrapper.rollback();
+			throw new RuntimeException(ex);
+		}					
+	}
+
+}
---
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/user/ReportingUserDao.java
@@ -0,0 +1,103 @@
+package com.eucalyptus.reporting.user;
+
+import java.util.*;
+
+import org.apache.log4j.Logger;
+
+import com.eucalyptus.entities.EntityWrapper;
+
+public class ReportingUserDao
+{
+	private static Logger log = Logger.getLogger( ReportingUserDao.class );
+
+	private final Map<String,String> users = new HashMap<String,String>();
+	
+	public ReportingUserDao()
+	{
+		
+	}
+
+	public void addUpdateUser(String id, String name)
+	{
+
+		loadIfNeeded();
+		if (users.containsKey(id) && users.get(id).equals(name)) {
+			return;
+		} else if (users.containsKey(id)) {
+			update(id, name);
+		} else {
+			add(id, name);
+		}
+		
+	}
+
+	public String getUserName(String id)
+	{
+		loadIfNeeded();
+		return users.get(id);
+	}
+	
+
+	
+	private void loadIfNeeded()
+	{
+		if (users.keySet().size() == 0) {
+
+			EntityWrapper<ReportingUser> entityWrapper =
+				EntityWrapper.get(ReportingUser.class);
+
+			try {
+				@SuppressWarnings("rawtypes")
+				List reportingUsers = (List)
+					entityWrapper.createQuery("from ReportingUser")
+					.list();
+
+				for (Object obj: reportingUsers) {
+					ReportingUser user = (ReportingUser) obj;
+					users.put(user.getId(), user.getName());
+				}
+				
+				entityWrapper.commit();
+			} catch (Exception ex) {
+				log.error(ex);
+				entityWrapper.rollback();
+				throw new RuntimeException(ex);
+			}			
+		}
+	}
+	
+	private void update(String id, String name)
+	{
+		EntityWrapper<ReportingUser> entityWrapper =
+			EntityWrapper.get(ReportingUser.class);
+
+		try {
+			ReportingUser reportingUser = (ReportingUser)
+			entityWrapper.createQuery("from ReportingUser where id = ?")
+				.setString(0, id)
+				.uniqueResult();
+			reportingUser.setName(name);
+			entityWrapper.commit();
+		} catch (Exception ex) {
+			log.error(ex);
+			entityWrapper.rollback();
+			throw new RuntimeException(ex);
+		}			
+	}
+	
+	private void add(String id, String name)
+	{
+		EntityWrapper<ReportingUser> entityWrapper =
+			EntityWrapper.get(ReportingUser.class);
+
+		try {
+			entityWrapper.add(new ReportingUser(id, name));
+			entityWrapper.commit();
+		} catch (Exception ex) {
+			log.error(ex);
+			entityWrapper.rollback();
+			throw new RuntimeException(ex);
+		}					
+	}
+
+}
