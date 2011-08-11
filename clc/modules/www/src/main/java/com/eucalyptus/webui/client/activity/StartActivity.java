0449f44c35aae99d941660fffa96ffff416f0dde
Ye Wen <wenye@eucalyptus.com>
2011-08-10 21:20:29 -0700

    Don't show certain parts of the start guide for nonsysadmin users.

--- clc/modules/www/src/main/java/com/eucalyptus/webui/client/activity/StartActivity.java
+++ clc/modules/www/src/main/java/com/eucalyptus/webui/client/activity/StartActivity.java
@@ -43,9 +43,12 @@ public class StartActivity extends AbstractActivity {
   }
   
   private void loadSnippets( StartView view, EventBus eventBus ) {
-    new RightScaleActivity( clientFactory ).start( view.getRightScaleSnippetDisplay( ), eventBus );
+	boolean isSystemAdmin = this.clientFactory.getSessionData( ).getLoginUser( ).isSystemAdmin( );
+	if ( isSystemAdmin ) {
+	  new RightScaleActivity( clientFactory ).start( view.getRightScaleSnippetDisplay( ), eventBus );
+	  new GenericGuideActivity( clientFactory, SERVICE_SNIPPET ).start( view.getServiceSnippetDisplay( ), eventBus );
+    }
     new DownloadActivity( clientFactory ).start( view.getDownloadSnippetDisplay( ), eventBus );
-    new GenericGuideActivity( clientFactory, SERVICE_SNIPPET ).start( view.getServiceSnippetDisplay( ), eventBus );
     new GenericGuideActivity( clientFactory, IAM_SNIPPET ).start( view.getIamSnippetDisplay( ), eventBus );
   }
   
