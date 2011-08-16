01f0dced7bf3c952439516960b6677f15432b771
decker <decker@hawaii>
2011-08-16 07:09:27 -0700

    error handling fixes

--- clc/modules/configuration/src/main/java/com/eucalyptus/config/Properties.java
+++ clc/modules/configuration/src/main/java/com/eucalyptus/config/Properties.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import com.eucalyptus.configurable.ConfigurableFieldType;
 import com.eucalyptus.configurable.ConfigurableProperty;
-import com.eucalyptus.configurable.ConfigurationProperties;
 import com.eucalyptus.configurable.PropertyDirectory;
 import com.eucalyptus.context.Contexts;
 import com.eucalyptus.scripting.Groovyness;
