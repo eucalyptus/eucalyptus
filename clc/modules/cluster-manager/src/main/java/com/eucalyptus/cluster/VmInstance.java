e11828d3e602ddeb74cacbfcd740516c6fba749e
decker <decker@hawaii>
2011-08-18 19:32:12 -0700

    temp fix to @Column for sshKeyPair

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/VmInstance.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/VmInstance.java
@@ -175,7 +175,8 @@ public class VmInstance extends UserMetadata<VmState> implements HasName<VmInsta
   private Long                                        blockBytes;
   @Column( name = "vm_network_bytes" )
   private Long                                        networkBytes;
-  @Column( name = "vm_ssh_key_pair" )
+  @Transient
+//  @Column( name = "vm_ssh_key_pair" )
   private SshKeyPair                                  sshKeyPair;
   @Column( name = "vm_type" )
   private final VmType                                vmType;
