589813d5790012348dde8fbfe1ec1f7a2398ed36
decker <decker@hawaii>
2011-08-24 15:22:32 -0700

    clean up relations before attempting to delete

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/VmInstance.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/VmInstance.java
@@ -168,12 +168,12 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
   @Column( name = "metadata_vm_private_networking" )
   private final Boolean           privateNetwork;
   @NotFound( action = NotFoundAction.IGNORE )
-  @ManyToMany( cascade = CascadeType.REMOVE )
+  @ManyToMany( cascade = CascadeType.ALL )
   @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
   private final Set<NetworkGroup> networkGroups    = Sets.newHashSet( );
   
   @NotFound( action = NotFoundAction.IGNORE )
-  @OneToOne( fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true )
+  @OneToOne( fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true )
   @JoinColumn( name = "metadata_vm_network_index", nullable = true, insertable = true, updatable = true )
   @Cache( usage = CacheConcurrencyStrategy.TRANSACTIONAL )
   private PrivateNetworkIndex     networkIndex;
