263f9ea715b9133e84813236d126735c9d23011c
Daniel Nurmi <nurmi@eucalyptus.com>
2011-08-31 15:04:20 -0700

    little build reorg to make room for...the fake backend

--- node/client-marshal-fake.c
+++ node/client-marshal-fake.c
@@ -78,30 +78,16 @@ enum {SHARED_MEM, SHARED_FILE};
 
 typedef struct fakeconfig_t {
   ncInstance global_instances[MAX_FAKE_INSTANCES];
-  int instanceidx;
+  ncResource res;
+  time_t current, last;
 } fakeconfig;
 fakeconfig *myconfig;
 sem_t *fakelock;
 
 void saveNcStuff() {
   int fd, i, done=0;
-
+  
   sem_post(fakelock);
-  /*
-  fd = open("/tmp/global_instances", O_WRONLY | O_CREAT, 0644);
-  if (fd) {
-    write(fd, global_instances, sizeof(ncInstance) * MAX_FAKE_INSTANCES);
-    close(fd);
-  }
-  for (i=0; i<MAX_FAKE_INSTANCES && !done; i++) {
-    if (strlen(global_instances[i].instanceId)) {
-      logprintfl(EUCADEBUG, "HI:\tinstance %d: %s/%s\n", i, global_instances[i].instanceId, global_instances[i].stateName);
-    } else {
-      done++;
-    }
-  }
-  logprintfl(EUCADEBUG, "HI: saved global_instances\n");
-  */
 }
 
 int setup_shared_buffer_fake(void **buf, char *bufname, size_t bytes, sem_t **lock, char *lockname, int mode) {
@@ -163,35 +149,15 @@ int setup_shared_buffer_fake(void **buf, char *bufname, size_t bytes, sem_t **lo
 }
 
 void loadNcStuff() {
-  int fd, i, count=0, done=0, rc;
+  int fd, i, count=0, done=0, rc, j;
   struct stat mystat;
 
-  /*
-  if (stat("/tmp/global_instances", &mystat)) {
-    logprintfl(EUCADEBUG, "HI: initializeing global_instances\n");
-    bzero(global_instances, sizeof(ncInstance) * MAX_FAKE_INSTANCES);
-    lock = sem_open("eucalyptusCCfakesem", O_CREAT, 0644, 1);    
-    sem_wait(lock);
-    saveNcStuff();
-  }
-  */
-  
   rc = setup_shared_buffer_fake((void **)&myconfig, "/eucalyptusCCfakeconfig", sizeof(fakeconfig), &fakelock, "/eucalyptusCCfakelock", SHARED_FILE);
   if (rc) {
-    logprintfl(EUCADEBUG, "HI: error setting up shared mem\n");
+    logprintfl(EUCADEBUG, "fakeNC:  error setting up shared mem\n");
   }  
   sem_wait(fakelock);
 
-  /*
-  fd = open("/tmp/global_instances", O_RDONLY);
-  if (fd) {
-    int rbytes;
-    logprintfl(EUCADEBUG, "HI: reading global_intances\n");
-    rbytes = read(fd, global_instances, sizeof(ncInstance) * MAX_FAKE_INSTANCES);
-    logprintfl(EUCADEBUG, "HI: read %d bytes\n", rbytes);
-    close(fd);
-  }
-  */
   done=0;
   for (i=0; i<MAX_FAKE_INSTANCES && !done; i++) {
     if (!strlen(myconfig->global_instances[i].instanceId)) {
@@ -199,9 +165,35 @@ void loadNcStuff() {
       done++;
     }
   }
-  myconfig->instanceidx = count-1;
+  
+  if (myconfig->last == 0) {
+    myconfig->last = time(NULL);
+    myconfig->current = time(NULL);
+  } else {
+    myconfig->current = time(NULL);
+  }
+  logprintfl(EUCADEBUG, "fakeNC: setup(): last=%d current=%d\n", myconfig->last, myconfig->current);
+  if ( (myconfig->current - myconfig->last) > 30 ) {
+    // do a refresh
+    myconfig->last = time(NULL);
+    myconfig->current = time(NULL);
+    for (i=0; i<MAX_FAKE_INSTANCES; i++) {
+      if (strlen(myconfig->global_instances[i].instanceId)) {
 
-  logprintfl(EUCADEBUG, "HI: instanceidx=%d after load\n", myconfig->instanceidx);
+	if (!strcmp(myconfig->global_instances[i].stateName, "Teardown")) {
+	  logprintfl(EUCADEBUG, "fakeNC: setup(): invalidating instance %s\n", myconfig->global_instances[i].instanceId);
+	  bzero(&(myconfig->global_instances[i]), sizeof(ncInstance));
+	} else {
+	  for (j=0; j<EUCA_MAX_VOLUMES; j++) {
+	    if (strlen(myconfig->global_instances[i].volumes[j].volumeId) && strcmp(myconfig->global_instances[i].volumes[j].stateName, "attached")) {
+	      logprintfl(EUCADEBUG, "fakeNC: setup(): invalidating volume %s\n", myconfig->global_instances[i].volumes[j].volumeId);	    
+	      bzero(&(myconfig->global_instances[i].volumes[j]), sizeof(ncVolume));
+	    }
+	  }
+	}
+      }
+    }
+  }
 }
 
 ncStub * ncStubCreate (char *endpoint_uri, char *logfile, char *homedir) 
@@ -223,11 +215,11 @@ ncStub * ncStubCreate (char *endpoint_uri, char *logfile, char *homedir)
     }
 
     if (client_home == NULL) {
-        logprintfl (EUCAERROR, "ERROR: cannot get AXIS2C_HOME");
+        logprintfl (EUCAERROR, "fakeNC: ERROR: cannot get AXIS2C_HOME");
 	return NULL;
     }
     if (endpoint_uri == NULL) {
-        logprintfl (EUCAERROR, "ERROR: empty endpoint_url");
+        logprintfl (EUCAERROR, "fakeNC: ERROR: empty endpoint_url");
 	return NULL;
     }
 
@@ -236,23 +228,23 @@ ncStub * ncStubCreate (char *endpoint_uri, char *logfile, char *homedir)
     // extract node name from the endpoint
     char * p = strstr (uri, "://"); // find "http[s]://..."
     if (p==NULL) {
-      logprintfl (EUCAERROR, "ncStubCreate received invalid URI %s\n", uri);
+      logprintfl (EUCAERROR, "fakeNC: ncStubCreate received invalid URI %s\n", uri);
       return NULL;
     }
     char * node_name = strdup (p+3); // copy without the protocol prefix
     if (node_name==NULL) {
-      logprintfl (EUCAERROR, "ncStubCreate is out of memory\n");
+      logprintfl (EUCAERROR, "fakeNC: ncStubCreate is out of memory\n");
       return NULL;
     }
     if ((p = strchr (node_name, ':')) != NULL) *p = '\0'; // cut off the port
     if ((p = strchr (node_name, '/')) != NULL) *p = '\0'; // if there is no port
 
-    logprintfl (EUCADEBUG, "DEBUG: requested URI %s\n", uri);
+    logprintfl (EUCADEBUG, "fakeNC: DEBUG: requested URI %s\n", uri);
 
     // see if we should redirect to the VMware broker
     if (strstr (uri, "VMwareBroker")) {
         uri = "http://localhost:8773/services/VMwareBroker";
-        logprintfl (EUCADEBUG, "DEBUG: redirecting request to %s\n", uri);
+        logprintfl (EUCADEBUG, "fakeNC: DEBUG: redirecting request to %s\n", uri);
     }
 
     // TODO: what if endpoint_uri, home, or env are NULL?
@@ -265,10 +257,10 @@ ncStub * ncStubCreate (char *endpoint_uri, char *logfile, char *homedir)
 	st->node_name=(axis2_char_t *)strdup(node_name);
         st->stub=stub;
 	if (st->client_home == NULL || st->endpoint_uri == NULL) {
-            logprintfl (EUCAWARN, "WARNING: out of memory");
+            logprintfl (EUCAWARN, "fakeNC: WARNING: out of memory");
 	}
     } else {
-        logprintfl (EUCAWARN, "WARNING: out of memory");
+        logprintfl (EUCAWARN, "fakeNC: WARNING: out of memory");
     } 
     
     free (node_name);
@@ -286,6 +278,15 @@ int ncStubDestroy (ncStub * st)
 int ncRunInstanceStub (ncStub *st, ncMetadata *meta, char *uuid, char *instanceId, char *reservationId, virtualMachine *params, char *imageId, char *imageURL, char *kernelId, char *kernelURL, char *ramdiskId, char *ramdiskURL, char *ownerId, char *accountId, char *keyName, netConfig *netparams, char *userData, char *launchIndex, char *platform, int expiryTime, char **groupNames, int groupNamesSize, ncInstance **outInstPtr)
 {
   ncInstance *instance;
+  int i, j, foundidx=-1;
+
+  logprintfl(EUCADEBUG, "fakeNC: runInstance(): params: uuid=%s instanceId=%s reservationId=%s ownerId=%s accountId=%s platform=%s\n", SP(uuid), SP(instanceId), SP(reservationId), SP(ownerId), SP(accountId), SP(platform));
+
+  if (!uuid || !instanceId || !reservationId || !ownerId || !accountId || !platform || !meta || !netparams) {
+    logprintfl(EUCAERROR, "fakeNC: runInstance(): bad input params\n");
+    return(0);
+  }
+
   loadNcStuff();
 
   instance = allocate_instance (uuid,
@@ -298,71 +299,123 @@ int ncRunInstanceStub (ncStub *st, ncMetadata *meta, char *uuid, char *instanceI
 				ownerId, accountId,
 				netparams, keyName,
 				userData, launchIndex, platform, expiryTime, groupNames, groupNamesSize);
-  myconfig->instanceidx++;
-  memcpy( &(myconfig->global_instances[myconfig->instanceidx]),instance, sizeof(ncInstance));
-
-  *outInstPtr = instance;
-
+  if (instance) {
+    foundidx=-1;
+    for (i=0; i<MAX_FAKE_INSTANCES && (foundidx < 0); i++) {
+      if (!strlen(myconfig->global_instances[i].instanceId)) {
+	foundidx = i;
+      }
+    }
+    memcpy( &(myconfig->global_instances[foundidx]),instance, sizeof(ncInstance));
+    logprintfl(EUCADEBUG, "fakeNC: runInstance(): decrementing resource by %d/%d/%d\n", params->cores, params->mem, params->disk);
+    myconfig->res.memorySizeAvailable -= params->mem;
+    myconfig->res.numberOfCoresAvailable -= params->cores;
+    myconfig->res.diskSizeAvailable -= params->disk;
+    
+    *outInstPtr = instance;
+    logprintfl(EUCADEBUG, "fakeNC: runInstance(): allocated and stored instance\n");
+  } else {
+    logprintfl(EUCAERROR, "fakeNC: runInstance(): failed to allocate instance\n");
+  }
+  
   saveNcStuff();
   return(0);
-  //  return doRunInstance (meta, uuid, instanceId, reservationId, params, imageId, imageURL, kernelId, kernelURL, ramdiskId, ramdiskURL, keyName, netparams, userData, launchIndex, platform, expiryTime, groupNames, groupNamesSize, outInstPtr);
 }
 
 int ncTerminateInstanceStub (ncStub *st, ncMetadata *meta, char *instanceId, int force, int *shutdownState, int *previousState)
 {
   int i, done=0;
+  
+  logprintfl(EUCADEBUG, "fakeNC: terminateInstance(): params: instanceId=%s force=%d\n", SP(instanceId), force);
+  
+  if (!instanceId) {
+    logprintfl(EUCAERROR, "fakeNC: termianteInstance(): bad input params\n");
+    return(0);
+  }
+  
   loadNcStuff();
 
-  logprintfl(EUCADEBUG, "HI: Terminate for instance %s\n", instanceId);
   for (i=0; i<MAX_FAKE_INSTANCES && !done; i++) {
-    if (!strlen(myconfig->global_instances[i].instanceId)) {
-      logprintfl(EUCADEBUG, "HI:\tfinishing terminate loop at idx %d\n", i);
-      done++;
-    } else if (!strcmp(myconfig->global_instances[i].instanceId, instanceId)) {
-      logprintfl(EUCADEBUG, "HI:\tsetting stateName at idx %d\n", i);
+    if (!strcmp(myconfig->global_instances[i].instanceId, instanceId)) {
+      logprintfl(EUCADEBUG, "fakeNC: terminateInstance():\tsetting stateName at idx %d\n", i);
       snprintf(myconfig->global_instances[i].stateName, 10, "Teardown");
+      myconfig->res.memorySizeAvailable += myconfig->global_instances[i].params.mem;
+      myconfig->res.numberOfCoresAvailable += myconfig->global_instances[i].params.cores;
+      myconfig->res.diskSizeAvailable += myconfig->global_instances[i].params.disk;
+      done++;
     }
   }
   
-  *shutdownState = *previousState = 0;
+  if (shutdownState && previousState) {
+    *shutdownState = *previousState = 0;
+  }
 
   saveNcStuff();
   return(0);
-  //  return doTerminateInstance (meta, instanceId, force, shutdownState, previousState);
 }
+
 int ncAssignAddressStub (ncStub *st, ncMetadata *meta, char *instanceId, char *publicIp){
+  int i, done=0;
+
+  logprintfl(EUCADEBUG, "fakeNC: assignAddress(): params: instanceId=%s publicIp=%s\n", SP(instanceId), SP(publicIp));
+  if (!instanceId || !publicIp) {
+    logprintfl(EUCADEBUG, "fakeNC: assignAddress(): bad input params\n");
+    return(0);
+  }
+
+  loadNcStuff();
+
+  for (i=0; i<MAX_FAKE_INSTANCES && !done; i++) {
+    if (!strcmp(myconfig->global_instances[i].instanceId, instanceId)) {
+      logprintfl(EUCADEBUG, "fakeNC: assignAddress()\tsetting publicIp at idx %d\n", i);
+      snprintf(myconfig->global_instances[i].ncnet.publicIp, 24, "%s", publicIp);
+      done++;
+    }
+  }
+  
+  saveNcStuff();
   return(0);
 }
+
 int ncPowerDownStub (ncStub *st, ncMetadata *meta){
   return(0);
 }
 
 int ncDescribeInstancesStub (ncStub *st, ncMetadata *meta, char **instIds, int instIdsLen, ncInstance ***outInsts, int *outInstsLen)
 {
-  int i;
+  int i, numinsts=0;
+  logprintfl(EUCADEBUG, "fakeNC: describeInstances(): params: instIdsLen=%d\n", instIdsLen);
+  
+  if (instIdsLen < 0) {
+    logprintfl(EUCAERROR, "fakeNC: describeInstances(): bad input params\n");
+    return(0);
+  }
+  
   loadNcStuff();
 
-  *outInstsLen = myconfig->instanceidx+1;
-  * outInsts = malloc (sizeof(ncInstance *) * *outInstsLen);
-  for (i=0; i<*outInstsLen; i++) {
-    ncInstance *newinst;
-    newinst = malloc(sizeof(ncInstance));
-    if (!strcmp(myconfig->global_instances[i].stateName, "Pending")) {
-      snprintf(myconfig->global_instances[i].stateName, 8, "Extant");
+  //  *outInstsLen = myconfig->instanceidx+1;
+  *outInsts = malloc (sizeof(ncInstance *) * MAX_FAKE_INSTANCES);
+  for (i=0; i<MAX_FAKE_INSTANCES; i++) {
+    if (strlen(myconfig->global_instances[i].instanceId)) {
+      ncInstance *newinst;
+      newinst = malloc(sizeof(ncInstance));
+      if (!strcmp(myconfig->global_instances[i].stateName, "Pending")) {
+	snprintf(myconfig->global_instances[i].stateName, 8, "Extant");
+      }
+      memcpy(newinst, &(myconfig->global_instances[i]), sizeof(ncInstance));
+      (* outInsts)[numinsts] = newinst;
+      logprintfl(EUCADEBUG, "fakeNC: describeInstances(): idx=%d numinsts=%d instanceId=%s stateName=%s\n", i, numinsts, newinst->instanceId, newinst->stateName);
+      numinsts++;
     }
-    memcpy(newinst, &(myconfig->global_instances[i]), sizeof(ncInstance));
-    (* outInsts)[i] = newinst;
-    logprintfl(EUCADEBUG, "HI: %s/%s\n", newinst->instanceId, newinst->stateName);
   }
-
+  *outInstsLen = numinsts;
+  
   saveNcStuff();
   return(0);
-
-  //    return doDescribeInstances (meta, instIds, instIdsLen, outInsts, outInstsLen);
 }
 
 int ncBundleInstanceStub (ncStub *stub, ncMetadata *meta, char *instanceId, char *bucketName, char *filePrefix, char *walrusURL, char *userPublicKey, char *S3Policy, char *S3PolicySig) {
-  return(0);
+  return(ncTerminateInstanceStub(stub, meta, instanceId, 0, NULL, NULL));
 }
 
 int ncCancelBundleTaskStub (ncStub *stub, ncMetadata *meta, char *instanceId) {
@@ -375,28 +428,110 @@ int ncDescribeBundleTasksStub (ncStub *stub, ncMetadata *meta, char **instIds, i
 
 int ncDescribeResourceStub (ncStub *st, ncMetadata *meta, char *resourceType, ncResource **outRes)
 {
+  int ret=0;
   ncResource *res;
+
   loadNcStuff();
-
-  res = allocate_resource ("OK", "iqn.1993-08.org.debian:01:736a4e92c588", 1024000, 1024000, 30000000, 30000000, 4096, 4096, "none");
-  *outRes = res;
-
+  
+  if (myconfig->res.memorySizeMax <= 0) {
+    // not initialized?
+    res = allocate_resource ("OK", "iqn.1993-08.org.debian:01:736a4e92c588", 1024000, 1024000, 30000000, 30000000, 4096, 4096, "none");
+    if (!res) {
+      logprintfl(EUCAERROR, "fakeNC: describeResource(): failed to allocate fake resource\n");
+      ret=1;
+    } else {
+      memcpy(&(myconfig->res), res, sizeof(ncResource));
+      free(res);
+    }
+  }
+  
+  if (!ret) {
+    res = malloc(sizeof(ncResource));
+    memcpy(res, &(myconfig->res), sizeof(ncResource));
+    *outRes = res;
+  } else {
+    *outRes = NULL;
+  }
+  
   saveNcStuff();
-  return(0);
-  //    return doDescribeResource (meta, resourceType, outRes);
+  return(ret);
 }
 
 int ncAttachVolumeStub (ncStub *stub, ncMetadata *meta, char *instanceId, char *volumeId, char *remoteDev, char *localDev)
 {
-    return doAttachVolume (meta, instanceId, volumeId, remoteDev, localDev);
+  int i, j, done=0, vdone=0, foundidx=-1;
+  
+  logprintfl(EUCADEBUG, "fakeNC:  attachVolume(): params: instanceId=%s volumeId=%s remoteDev=%s localDev=%s\n", SP(instanceId), SP(volumeId), SP(remoteDev), SP(localDev));
+  if (!instanceId || !volumeId || !remoteDev || !localDev) {
+    logprintfl(EUCADEBUG, "fakeNC:  attachVolume(): bad input params\n");
+    return(0);
+  }
+
+  loadNcStuff();
+
+  for (i=0; i<MAX_FAKE_INSTANCES && !done; i++) {
+    if (!strcmp(myconfig->global_instances[i].instanceId, instanceId)) {
+      logprintfl(EUCADEBUG, "fakeNC: \tsetting volume info at idx %d\n", i);
+      vdone=0;
+      for (j=0; j<EUCA_MAX_VOLUMES; j++) {
+	if (!strlen(myconfig->global_instances[i].volumes[j].volumeId)) {
+	  if (foundidx < 0) {
+	    foundidx=j;
+	  }
+	} else if (!strcmp(myconfig->global_instances[i].volumes[j].volumeId, volumeId)) {
+	  vdone++;
+	}
+      }
+      if (!vdone && foundidx >= 0) {
+	logprintfl(EUCADEBUG, "fakeNC: \tfake attaching volume at idx %d\n", foundidx);
+	snprintf(myconfig->global_instances[i].volumes[foundidx].volumeId, CHAR_BUFFER_SIZE, "%s", volumeId);
+	snprintf(myconfig->global_instances[i].volumes[foundidx].remoteDev, CHAR_BUFFER_SIZE, "%s", remoteDev);
+	snprintf(myconfig->global_instances[i].volumes[foundidx].localDev, CHAR_BUFFER_SIZE, "%s", localDev);
+	snprintf(myconfig->global_instances[i].volumes[foundidx].localDevReal, CHAR_BUFFER_SIZE, "%s", localDev);
+	snprintf(myconfig->global_instances[i].volumes[foundidx].stateName, CHAR_BUFFER_SIZE, "%s", "attached");
+      }
+      done++;
+    }
+  }
+
+  saveNcStuff();
+  return (0);
 }
 
 int ncDetachVolumeStub (ncStub *stub, ncMetadata *meta, char *instanceId, char *volumeId, char *remoteDev, char *localDev, int force)
 {
-    return doDetachVolume (meta, instanceId, volumeId, remoteDev, localDev, force, 1);
+  int i, j, done=0, vdone=0, foundidx=-1;
+
+  logprintfl(EUCADEBUG, "fakeNC:  detachVolume(): params: instanceId=%s volumeId=%s remoteDev=%s localDev=%s\n", SP(instanceId), SP(volumeId), SP(remoteDev), SP(localDev));
+  if (!instanceId || !volumeId || !remoteDev || !localDev) {
+    logprintfl(EUCADEBUG, "fakeNC:  detachVolume(): bad input params\n");
+    return(0);
+  }
+
+  loadNcStuff();
+
+  for (i=0; i<MAX_FAKE_INSTANCES && !done; i++) {
+    if (!strcmp(myconfig->global_instances[i].instanceId, instanceId)) {
+      logprintfl(EUCADEBUG, "fakeNC: \tsetting volume info at idx %d\n", i);
+      vdone=0;
+      for (j=0; j<EUCA_MAX_VOLUMES; j++) {
+	if (!strcmp(myconfig->global_instances[i].volumes[j].volumeId, volumeId)) {
+	  foundidx=j;
+	}
+      }
+      if (foundidx >= 0) {
+	logprintfl(EUCADEBUG, "fakeNC: \tfake detaching volume at idx %d\n", foundidx);
+	snprintf(myconfig->global_instances[i].volumes[foundidx].stateName, CHAR_BUFFER_SIZE, "%s", "detached");
+      }
+      done++;
+    }
+  }
+
+  saveNcStuff();
+  return (0);
 }
 
 int ncCreateImageStub (ncStub *stub, ncMetadata *meta, char *instanceId, char *volumeId, char *remoteDev)
 {
-    return doCreateImage (meta, instanceId, volumeId, remoteDev);
+  return(ncTerminateInstanceStub(stub, meta, instanceId, 0, NULL, NULL));
 }
