177cc4b00bbaa5057df6660662c7356e4a0df386
decker <decker@hawaii>
2011-09-28 05:40:55 -0700

    adjust state terminate to dirty object forcing flush

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
@@ -407,7 +407,7 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
               if ( !exNet.getTag( ).equals( input.getNetParams( ).getVlan( ) ) ) {
                 exNet = null;
               } else {
-                index = exNet.reclaimNetworkIndex( input.getNetParams( ).getNetworkIndex( ) );
+                index = Entities.merge( exNet.reclaimNetworkIndex( input.getNetParams( ).getNetworkIndex( ) ) );
               }
             }
           }
