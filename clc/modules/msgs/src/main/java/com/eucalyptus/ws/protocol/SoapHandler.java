33eed40116a7ba5519f187a4353c982bc37ea0e3
decker <decker@hawaii>
2011-09-29 05:16:38 -0700

    error reporting

--- clc/modules/msgs/src/main/java/com/eucalyptus/ws/protocol/SoapHandler.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/ws/protocol/SoapHandler.java
@@ -155,16 +155,17 @@ public class SoapHandler extends MessageStackHandler {
     }
   }
   
-  @Override
-  public void handleDownstream( ChannelHandlerContext ctx, ChannelEvent channelEvent ) throws Exception {
-    if ( channelEvent instanceof ExceptionEvent ) {
-      Throwable cause = ( ( ExceptionEvent ) channelEvent ).getCause( );
-      ExceptionResponseType errMsg = new ExceptionResponseType( null, cause.getMessage( ), HttpResponseStatus.INTERNAL_SERVER_ERROR, cause );
-      channelEvent.getFuture( ).cancel( );
-      ctx.getChannel( ).write( errMsg );
-    } else {
-      super.handleDownstream( ctx, channelEvent );
-    }
-  }
-  
+//TODO:GRZE: services specific error handling  
+//  @Override
+//  public void handleDownstream( ChannelHandlerContext ctx, ChannelEvent channelEvent ) throws Exception {
+//    if ( channelEvent instanceof ExceptionEvent ) {
+//      Throwable cause = ( ( ExceptionEvent ) channelEvent ).getCause( );
+//      ExceptionResponseType errMsg = new ExceptionResponseType( null, cause.getMessage( ), HttpResponseStatus.INTERNAL_SERVER_ERROR, cause );
+//      channelEvent.getFuture( ).cancel( );
+//      ctx.getChannel( ).write( errMsg );
+//    } else {
+//      super.handleDownstream( ctx, channelEvent );
+//    }
+//  }
+//  
 }
