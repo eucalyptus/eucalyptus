88835029f4c50b0183db64f08f8924946630acdc
twerges <>
2011-09-29 11:39:23 -0700

    added reporting button to account administrator ui, since they can now generate reports for their accounts only

--- clc/modules/www/src/main/java/com/eucalyptus/webui/server/QuickLinks.java
+++ clc/modules/www/src/main/java/com/eucalyptus/webui/server/QuickLinks.java
@@ -17,6 +17,8 @@ public class QuickLinks {
   public static ArrayList<QuickLinkTag> getTags( User login ) throws EucalyptusServiceException {
     if ( login.isSystemAdmin( ) ) {
       return getSystemAdminTags( login );
+    } else if ( login.isAccountAdmin() ) {
+        return getAccountAdminTags( login );
     } else {
       return getUserTags( login );
     }
@@ -61,6 +63,43 @@ public class QuickLinks {
       throw new EucalyptusServiceException( "Failed to load user information for " + login );
     }    
   }
+
+  private static ArrayList<QuickLinkTag> getAccountAdminTags( User login ) throws EucalyptusServiceException {
+	    try {
+	      String accountId = login.getAccount( ).getAccountNumber( );
+	      String userId = login.getUserId( );
+	      return new ArrayList<QuickLinkTag>( Arrays.asList( 
+	              new QuickLinkTag( "System Management", 
+	                               new ArrayList<QuickLink>( Arrays.asList(
+	                                              new QuickLink( "Start", "Start guide", "home",
+	                                                                QueryBuilder.get( ).start( QueryType.start ).query( ) ) ) ) ),
+	              new QuickLinkTag( "Identity Management",
+	                               new ArrayList<QuickLink>( Arrays.asList(
+	                                              new QuickLink( "Accounts", "Accounts", "dollar", 
+	                                                                QueryBuilder.get( ).start( QueryType.account ).query( ) ),
+	                                              new QuickLink( "Groups", "User groups", "group",
+	                                                                QueryBuilder.get( ).start( QueryType.group ).add( EuareWebBackend.ACCOUNTID, accountId ).query( ) ),
+	                                              new QuickLink( "Users", "Users", "user",
+	                                                                QueryBuilder.get( ).start( QueryType.user ).add( EuareWebBackend.ACCOUNTID, accountId ).query( ) ),
+	                                              new QuickLink( "Policies", "Policies", "lock",
+	                                                                QueryBuilder.get( ).start( QueryType.policy ).add( EuareWebBackend.ACCOUNTID, accountId ).query( ) ),
+	                                              new QuickLink( "Keys", "Access keys", "key",
+	                                                                QueryBuilder.get( ).start( QueryType.key ).add( EuareWebBackend.ACCOUNTID, accountId ).query( ) ),
+	                                              new QuickLink( "Certificates", "X509 certificates", "sun",
+	                                                                QueryBuilder.get( ).start( QueryType.cert ).add( EuareWebBackend.ACCOUNTID, accountId ).query( ) ) ) ) ),
+	              new QuickLinkTag( "Resource Management",
+                                 new ArrayList<QuickLink>( Arrays.asList(
+                                 new QuickLink( "Images", "Virtual machine images (EMIs)", "image",
+                                                   QueryBuilder.get( ).start( QueryType.image ).query( ) ),
+                                 new QuickLink( "Usage Report", "Resource usage report", "report",
+                                                   QueryBuilder.get( ).start( QueryType.report ).query( ) ) ) ) ) ) );
+
+	    } catch ( Exception e ) {
+	      LOG.error( "Failed to load user information", e );
+	      LOG.debug( e, e );
+	      throw new EucalyptusServiceException( "Failed to load user information for " + login );
+	    }    
+	  }
 
   private static ArrayList<QuickLinkTag> getUserTags( User login ) throws EucalyptusServiceException {
     try {
