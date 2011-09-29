808cba93b88802d0d4c76e0d2b7d8ea50c115582
decker <decker@hawaii>
2011-09-28 20:07:47 -0700

    fix index reclaim

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
@@ -420,7 +420,6 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
                                                              .placement( partition, partition.getName( ) )
                                                              .networking( networks, index )
                                                              .build( launchIndex );
-          Entities.flush( vmInst );
           vmInst.setNaturalId( input.getUuid( ) );
           Address addr;
           try {
