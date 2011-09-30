dd4b35e21bb98ff69a849ffe3ff3365fdea323d3
decker <decker@hawaii>
2011-09-30 13:20:08 -0700

    dont load props before db is up

--- clc/modules/wsstack/src/main/java/com/eucalyptus/ws/DeferredPropertiesBootstrapper.java
+++ clc/modules/wsstack/src/main/java/com/eucalyptus/ws/DeferredPropertiesBootstrapper.java
@@ -88,38 +88,42 @@ public class DeferredPropertiesBootstrapper extends Bootstrapper {
   
   @Override
   public boolean load( ) throws Exception {
-    List<ConfigurableProperty> staticProps = Lists.newArrayList( );
-    for ( Entry<String, ConfigurableProperty> entry : PropertyDirectory.getPendingPropertyEntries( ) ) {
-      ConfigurableProperty prop = entry.getValue( );
-      if ( prop instanceof StaticPropertyEntry ) {
-        staticProps.add( prop );
-      } else {
-        try {
-          ComponentId compId = ComponentIds.lookup( prop.getEntrySetName( ) );
-          for ( ServiceConfiguration s : ServiceConfigurations.list( compId.getClass( ) ) ) {
-            if ( compId.name( ).equals( prop.getEntrySetName( ) ) ) {
-              if ( prop instanceof SingletonDatabasePropertyEntry ) {
-                PropertyDirectory.addProperty( prop );
-              } else if ( prop instanceof MultiDatabasePropertyEntry ) {
-                PropertyDirectory.addProperty( ( ( MultiDatabasePropertyEntry ) prop ).getClone( s.getPartition( ) ) );
+    if ( !Bootstrap.isFinished( ) && Bootstrap.getCurrentStage( ).ordinal( ) > Bootstrap.Stage.DatabaseInit.ordinal( ) ) {
+      return true;
+    } else {
+      List<ConfigurableProperty> staticProps = Lists.newArrayList( );
+      for ( Entry<String, ConfigurableProperty> entry : PropertyDirectory.getPendingPropertyEntries( ) ) {
+        ConfigurableProperty prop = entry.getValue( );
+        if ( prop instanceof StaticPropertyEntry ) {
+          staticProps.add( prop );
+        } else {
+          try {
+            ComponentId compId = ComponentIds.lookup( prop.getEntrySetName( ) );
+            for ( ServiceConfiguration s : ServiceConfigurations.list( compId.getClass( ) ) ) {
+              if ( compId.name( ).equals( prop.getEntrySetName( ) ) ) {
+                if ( prop instanceof SingletonDatabasePropertyEntry ) {
+                  PropertyDirectory.addProperty( prop );
+                } else if ( prop instanceof MultiDatabasePropertyEntry ) {
+                  PropertyDirectory.addProperty( ( ( MultiDatabasePropertyEntry ) prop ).getClone( s.getPartition( ) ) );
+                }
               }
             }
+          } catch ( Exception ex ) {
+            LOG.error( ex, ex );
           }
-        } catch ( Exception ex ) {
-          LOG.error( ex, ex );
         }
       }
-    }
-    for ( ConfigurableProperty prop : staticProps ) {
-      if ( PropertyDirectory.addProperty( prop ) ) {
-        try {
-          prop.getValue( );
-        } catch ( Exception ex ) {
-          Logs.extreme( ).error( ex, ex );
+      for ( ConfigurableProperty prop : staticProps ) {
+        if ( PropertyDirectory.addProperty( prop ) ) {
+          try {
+            prop.getValue( );
+          } catch ( Exception ex ) {
+            Logs.extreme( ).error( ex, ex );
+          }
         }
       }
+      return true;
     }
-    return true;
   }
   
   @Override
