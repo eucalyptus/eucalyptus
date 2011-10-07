938e8853cb03d4187eb199298f0b7b14834ca4ca
twerges <>
2011-10-07 14:27:13 -0700

    renamed path to image, implemented part of euca-run-instances

--- devel/simulate_usage.pl
+++ devel/simulate_usage.pl
@@ -10,22 +10,22 @@
 # The purpose of this is to verify that reporting events are sent to the
 # reporting system properly when usage occurs, as part of a test.
 #
-# Usage: simulate_usage.pl interval duration s3_url kernel_path ramdisk_path (username/image_path)+
+# Usage: simulate_usage.pl interval duration s3_url kernel_image ramdisk_image (username/image)+
 #
-# Example: simulate_usage.pl 100 10000 kernel_path ramdisk_path s3_url usera/image_patha userb/image_pathb userc/image_pathc
+# Example: simulate_usage.pl 100 10000 kernel_image ramdisk_image s3_url usera/imagea userb/imageb userc/imagec
 #  This example will simluate usage for 10,000 seconds, creating new s3 objects
 #  and storage every 100 seconds for users usera,userb,userc. It will also
-#  start three instances as three users: image_patha as usera, 
-#  image_pathb as userb, and image_pathc as userc, all using the kernel and
-#  ramdisk paths specified, and run them for DURATION. It will do all these
-#  things simultaneously.
+#  start three instances as three users: imagea as usera, imageb as userb,
+#  and imagec as userc, all using the kernel and ramdisk paths specified,
+#  and run them for DURATION. It will do all these things simultaneously.
 #
 # NOTE!! This script has many external dependencies which must be satisfied
 #   for it to run. As follows: 1) A CLC must be running locally; 2) A
 #   Walrus must be running; 3) You must have created all the users
 #   which you pass in; 4) All credentials must be present; 5) You must have
-#   created all the image paths, ramdisk paths, and kernel paths which you
-#   pass in; 6) The s3curl.pl script must be present in this directory.
+#   created and uploaded all the images, ramdisk images, and kernel images
+#   which you pass in; 6) The s3curl.pl script must be present in this
+#   directory.
 #
 # (c)2011, Eucalyptus Systems, Inc. All Rights Reserved.
 # author: tom.werges
@@ -41,10 +41,11 @@ use warnings;
 
 
 # SUB: run_instance
-# Takes a an image path, a kernel path, and a ramdisk path; returns an instance id
+# Takes a an image, a kernel image, and a ramdisk image; returns an instance id
 sub run_instance($$$) {
-	print "run_instance image:$_[1] kernel:$_[2] ramdisk:$_[3]\n";
-	#system("euca-run-instances") or die("starting instance failed");
+	my ($image,$kernel,$ramdisk) = ($_[0],$_[1],$_[2]);
+	print "run_instance image:$image kernel:$kernel ramdisk:$ramdisk\n";
+	#$output = `euca-run-instances --kernel $kernel --ramdisk $ramdisk $image` or die("starting instance failed");
 	my $time = time();
 	return "fakeInstance-$_[0]-$time";
 }
@@ -89,30 +90,31 @@ sub switch_to_user($) {
 #
 
 if ($#ARGV+1 < 6) {
-	print "Usage: simulate_usage.pl interval duration s3_url kernel_path ramdisk_path (username/image_path)+\n";
+	print "Usage: simulate_usage.pl interval duration s3_url kernel_image ramdisk_image (username/image)+\n";
 }
 
 my $interval = shift;
 my $duration = shift;
 my $s3_url = shift;
-my $kernel_path = shift;
-my $ramdisk_path = shift;
+my $kernel_image = shift;
+my $ramdisk_image = shift;
 my @users = ();
 my %user_instance_id_hash = ();
 
-print "interval:$interval duration:$duration s3_url:$s3_url kernel:$kernel_path ramdisk:$ramdisk_path\n";
+print "interval:$interval duration:$duration s3_url:$s3_url kernel:$kernel_image ramdisk:$ramdisk_image\n";
+
 # Parse users into separate list
 foreach my $item (@ARGV) {
-	(my $user,my $image_path)=split("/",$item);
+	(my $user,my $image)=split("/",$item);
 	push(@users,$user);
 }
 
 
-# Run all instances as users and retain instance ids for termination
+# Run all instances as users, and retain instance ids for termination
 foreach my $item (@ARGV) {
-	(my $user,my $image_path)=split("/",$item);
+	(my $user,my $image)=split("/",$item);
 	switch_to_user($user);
-	my $instance_id = run_instance($image_path,$kernel_path,$ramdisk_path);
+	my $instance_id = run_instance($image,$kernel_image,$ramdisk_image);
 	$user_instance_id_hash{$user}=$instance_id;
 }
 
