581248d6290fb46cb9309150128efdf20d021401
grze@eucalyptus.com <>
2011-10-15 11:36:15 -0700

    fix vm instance issue

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
@@ -336,37 +336,37 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
         final UserFullName userFullName = UserFullName.getInstance( input.getOwnerId( ) );
         final List<NetworkGroup> networks = Lists.newArrayList( );
         
+        PrivateNetworkIndex index = null;
+        ExtantNetwork exNet;
         EntityTransaction tx1 = Entities.get( NetworkGroup.class );
         try {
           networks.addAll( Lists.transform( input.getGroupNames( ), transformNetworkNames( userFullName ) ) );
+          final NetworkGroup network = ( !networks.isEmpty( )
+              ? networks.get( 0 )
+              : null );
+          try {
+            if ( network != null ) {
+              if ( !network.hasExtantNetwork( ) ) {
+                exNet = network.reclaim( input.getNetParams( ).getVlan( ) );
+              } else {
+                exNet = network.extantNetwork( );
+                if ( !exNet.getTag( ).equals( input.getNetParams( ).getVlan( ) ) ) {
+                  exNet = null;
+                } else {
+                  index = exNet.reclaimNetworkIndex( input.getNetParams( ).getNetworkIndex( ) );
+                }
+              }
+            }
+          } catch ( Exception ex ) {
+            throw Exceptions.toUndeclared( ex );
+          }
+         
           tx1.commit( );
         } catch ( Exception ex ) {
           tx1.rollback( );
           throw Exceptions.toUndeclared( ex );
         }
         
-        PrivateNetworkIndex index = null;
-        ExtantNetwork exNet;
-        final NetworkGroup network = ( !networks.isEmpty( )
-          ? networks.get( 0 )
-          : null );
-        try {
-          if ( network != null ) {
-            if ( !network.hasExtantNetwork( ) ) {
-              exNet = network.reclaim( input.getNetParams( ).getVlan( ) );
-            } else {
-              exNet = network.extantNetwork( );
-              if ( !exNet.getTag( ).equals( input.getNetParams( ).getVlan( ) ) ) {
-                exNet = null;
-              } else {
-                index = exNet.reclaimNetworkIndex( input.getNetParams( ).getNetworkIndex( ) );
-              }
-            }
-          }
-        } catch ( Exception ex ) {
-          throw Exceptions.toUndeclared( ex );
-        }
-
         final EntityTransaction db = Entities.get( VmInstance.class );
         try {
           final VmType vmType = VmTypes.getVmType( input.getInstanceType( ).getName( ) );
@@ -384,12 +384,12 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
             try {
               kernelId = input.getInstanceType( ).lookupKernel( ).getId( );
             } catch ( Exception ex ) {
-              LOG.error( ex , ex );
+              LOG.error( ex, ex );
             }
             try {
               ramdiskId = input.getInstanceType( ).lookupRamdisk( ).getId( );
             } catch ( Exception ex ) {
-              LOG.error( ex , ex );
+              LOG.error( ex, ex );
             }
           } catch ( final Exception ex2 ) {
             LOG.error( ex2, ex2 );
@@ -621,7 +621,8 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
           throw ex;
         } catch ( final Exception ex ) {
           db.rollback( );
-          throw new NoSuchElementException( "An error occurred while trying to lookup vm instance " + arg0 + ": " + ex.getMessage( ) + "\n" + Exceptions.causeString( ex ) );
+          throw new NoSuchElementException( "An error occurred while trying to lookup vm instance " + arg0 + ": " + ex.getMessage( ) + "\n"
+                                            + Exceptions.causeString( ex ) );
         }
       }
     };
@@ -866,7 +867,7 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
     final boolean dns = !ComponentIds.lookup( Dns.class ).runLimitedServices( );
     final Map<String, String> m = new HashMap<String, String>( );
     m.put( "ami-id", this.getImageId( ) );
-    if ( !this.bootRecord.getMachine( ).getProductCodes( ).isEmpty()) {
+    if ( !this.bootRecord.getMachine( ).getProductCodes( ).isEmpty( ) ) {
       m.put( "product-codes", this.bootRecord.getMachine( ).getProductCodes( ).toString( ).replaceAll( "[\\Q[]\\E]", "" ).replaceAll( ", ", "\n" ) );
     }
     m.put( "ami-launch-index", "" + this.launchRecord.getLaunchIndex( ) );
@@ -1576,25 +1577,24 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
     }
     return true;
   }
-
+  
   /**
    * @return
    */
   public static VmInstance create( ) {
     return new VmInstance( );
   }
-
+  
   /**
    * @param exampleWithPrivateIp
    */
   void setNetworkConfig( VmNetworkConfig networkConfig ) {
     this.networkConfig = networkConfig;
   }
-
+  
   public void release( ) {
     try {
       Transitions.DELETE.apply( this );
-    } catch ( Exception ex ) {
-    }
+    } catch ( Exception ex ) {}
   }
 }
