986094d874d625f0d9d3c783af0e1a1b46d05a08
grze@eucalyptus.com <>
2011-10-18 06:50:26 -0700

    fix bad url construction

--- clc/modules/msgs/conf/scripts/initialize_cloud.groovy
+++ clc/modules/msgs/conf/scripts/initialize_cloud.groovy
@@ -62,7 +62,7 @@ try {
     for ( String ctx : PersistenceContexts.list( ) ) {
       Properties p = new Properties( );
       p.putAll( props );
-      String ctxUrl = "jdbc:${ServiceUris.remote(dbComp,ctx)}?createDatabaseIfNotExist=true";
+      String ctxUrl = "jdbc:${ServiceUris.remote(dbComp,InetAddress.getLocalHost( ),ctx)}?createDatabaseIfNotExist=true";
       p.put( "hibernate.connection.url", ctxUrl );
       p.put("hibernate.cache.region_prefix", "eucalyptus_" + ctx + "_cache" );
       Ejb3Configuration config = new Ejb3Configuration( );
