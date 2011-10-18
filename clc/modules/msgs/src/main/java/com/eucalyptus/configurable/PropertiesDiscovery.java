13f99256739f3bfee4d7c2332b867ca762cde134
grze@eucalyptus.com <>
2011-10-18 16:06:47 -0700

    winner winner chicken dinner

--- clc/modules/msgs/src/main/java/com/eucalyptus/configurable/PropertiesDiscovery.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/configurable/PropertiesDiscovery.java
@@ -3,12 +3,13 @@ package com.eucalyptus.configurable;
 import java.lang.reflect.Field;
 import org.apache.log4j.Logger;
 import com.eucalyptus.bootstrap.ServiceJarDiscovery;
+import com.google.common.collect.ObjectArrays;
 import edu.emory.mathcs.backport.java.util.Arrays;
 
 public class PropertiesDiscovery extends ServiceJarDiscovery {
   private static Logger LOG = Logger.getLogger( PropertiesDiscovery.class );
-
-  public PropertiesDiscovery() {}
+  
+  public PropertiesDiscovery( ) {}
   
   @Override
   public Double getPriority( ) {
@@ -17,17 +18,17 @@ public class PropertiesDiscovery extends ServiceJarDiscovery {
   
   @Override
   public boolean processClass( Class c ) throws Exception {
-    if ( (c.getAnnotation( ConfigurableClass.class ) != null) )  {
+    if ( ( c.getAnnotation( ConfigurableClass.class ) != null ) ) {
       LOG.trace( "-> Registering configuration properties for entry: " + c.getName( ) );
-      LOG.trace( "Checking fields: " + Arrays.asList( c.getDeclaredFields( ) ));
-      for( Field  f : c.getDeclaredFields( ) ) {
+      LOG.trace( "Checking fields: " + Arrays.asList( c.getDeclaredFields( ) ) );
+      for ( Field f : ObjectArrays.concat( c.getFields( ), c.getDeclaredFields( ), Field.class ) ) {
         LOG.trace( "Checking field: " + f );
         try {
           ConfigurableProperty prop = PropertyDirectory.buildPropertyEntry( c, f );
-          if( prop == null ) {
+          if ( prop == null ) {
             continue;
           } else {
-            LOG.info( "--> Registered property: " + prop.getQualifiedName( )  );
+            LOG.info( "--> Registered property: " + prop.getQualifiedName( ) );
           }
         } catch ( Exception e ) {
           LOG.debug( e, e );
