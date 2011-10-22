c99d5219a32e258a5f7e70c9bb0986571a9ade3f
Neil Soman <neil@eucalyptus.com>
2011-10-22 12:12:25 -0700

    arbitrator gateway host prop.

--- clc/eucadmin/eucadmin/describearbitrators.py
+++ clc/eucadmin/eucadmin/describearbitrators.py
@@ -35,9 +35,9 @@ class DescribeArbitrators(eucadmin.describerequest.DescribeRequest):
     ServiceName = 'Arbitrator'
     Description = 'List Arbitrator services.'
     
-    def cli_formatter(self, data):
-        services = getattr(data, 'euca:registered')
-        fmt = '%s\t%s\t%s'
-        for s in services:
-            print fmt % (self.ServiceName.upper(),
-                         a['euca:name'], a['euca:partition'])
+#    def cli_formatter(self, data):
+#        services = getattr(data, 'euca:registered')
+#        fmt = '%s\t%s\t%s'
+#        for s in services:
+#            print fmt % (self.ServiceName.upper(),
+#                         data['euca:name'], data['euca:partition'])
