53750da3ac7646ebe2cc352ad13559db7b92f3c9
grze@eucalyptus.com <>
2011-10-24 15:06:09 -0700

    fix toUpperCase in modify service

--- clc/modules/msgs/src/main/java/com/eucalyptus/ws/EmpyreanService.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/ws/EmpyreanService.java
@@ -227,7 +227,7 @@ public class EmpyreanService {
       if ( NamedTransition.INSTANCE.apply( request ) ) {
         reply.markWinning( );
       } else {
-        Component.State nextState = Component.State.valueOf( request.getState( ) );
+        Component.State nextState = Component.State.valueOf( request.getState( ).toUpperCase( ) );
         ServiceConfiguration config = findService( request.getName( ) );
         Topology.transition( nextState ).apply( config ).get( );
         reply.markWinning( );
