8dc7b11231e0cd333b246a6a4c081f815652d5e5
Daniel Nurmi <nurmi@eucalyptus.com>
2011-10-30 13:14:26 -0700

    revert debugging change

--- net/vnetwork.c
+++ net/vnetwork.c
@@ -2246,7 +2246,7 @@ int vnetStopNetworkManaged(vnetConfig *vnetconfig, int vlan, char *userName, cha
 
     snprintf(newdevname, 32, "%s.%d", vnetconfig->privInterface, vlan);
     rc = check_device(newdevname);
-    if (0 && !rc) {
+    if (!rc) {
       snprintf(cmd, MAX_PATH, "%s/usr/lib/eucalyptus/euca_rootwrap ip link set dev %s down", vnetconfig->eucahome, newdevname);
       rc = system(cmd);
       if (rc) {
