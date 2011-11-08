e4ddd6b6354d3dbda73d3ec942bc518e32d9e1f3
grze@eucalyptus.com <>
2011-11-07 20:47:51 -0800

    reverse ordering of shutdown

--- clc/modules/msgs/src/main/java/com/eucalyptus/bootstrap/OrderedShutdown.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/bootstrap/OrderedShutdown.java
@@ -62,13 +62,9 @@
  */
 package com.eucalyptus.bootstrap;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
-
+import org.apache.commons.collections.ComparatorUtils;
 import org.apache.log4j.Logger;
-
 import com.eucalyptus.component.ComponentId;
 import com.eucalyptus.component.ComponentIds;
 import com.eucalyptus.util.Exceptions;
@@ -80,7 +76,7 @@ import com.eucalyptus.util.Exceptions;
 public class OrderedShutdown {
   
   static Logger                     LOG = Logger.getLogger( OrderedShutdown.class );
-  static PriorityBlockingQueue<ShutdownHook> hooks = new PriorityBlockingQueue<ShutdownHook>();
+  static PriorityBlockingQueue<ShutdownHook> hooks = new PriorityBlockingQueue<ShutdownHook>(100,ComparatorUtils.reversedComparator(ComparatorUtils.naturalComparator()));
 
   public static <T extends ComponentId> void register(Class<T> id, Runnable r) {
 	  ShutdownHook hook = new ShutdownHook(ComponentIds.lookup(id), r);
