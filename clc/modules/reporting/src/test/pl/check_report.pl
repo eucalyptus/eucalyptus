822b9fdb178d44ba1a266add35c682790abdf8ab
twerges <>
2011-11-10 12:59:23 -0800

    added

---
+++ clc/modules/reporting/src/test/pl/check_report.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+
+use strict;
+use warnings;
+
+if ($#ARGV+1 < 2) {
+	die ("Usage: check_report.pl admin_pw (account:user+)+");
+}
+
+my $password = shift;
+my $session_id = "";
+my $report_file = "";
+my $start_ms = 0;
+my $end_ms = 0;
+my $num_users = 0;
+my @usernames = ();
+my @accountnames = ();
+
+while ($#ARGV+1>0) {
+	my ($account,$username_arg) = split(":",shift);
+	push(@accountnames, $account);
+	foreach (split(",",$username_arg)) {
+		push(@usernames, $_);
+		$num_users++;
+	}
+}
+
+my $username_csv = "'" . join("','",@usernames) . "'";
+my $accountname_csv = "'" . join("','",@accountnames) . "'";
+
+sub execute_query($) {
+	print "Executing query:$_[0]\n";
+	my $output = `./db.sh --execute="$_[0]" -D eucalyptus_reporting --skip-column-names`;
+	print "Output:$output\n";
+	return split("\n",$output);
+}
+
+sub runcmd($) {
+	print "Running cmd:$_[0]\n";
+	my $output = `$_[0]`;
+	print "Output:$output\n";
+}
+
+
+# login and gather session id
+runcmd("wget -O /tmp/sessionId --no-check-certificate \"https://localhost:8443/loginservlet?adminPw=$password\"");
+$session_id = `cat /tmp/sessionId`;
+
+my $username="";
+# Get first and last times for instance events
+foreach (execute_query("
+	select
+	  max(ius.timestamp_ms) as max_time,
+	  min(ius.timestamp_ms) as min_time,
+	  ru.user_name
+	from
+	  reporting_instance ri,
+	  instance_usage_snapshot ius,
+	  reporting_user ru,
+	  reporting_account ra
+	where
+	  ri.user_id = ru.user_id
+	and ius.uuid = ri.uuid
+	and ri.account_id = ra.account_id
+	and ru.user_name in ($username_csv)
+	and ra.account_name in ($accountname_csv)
+	group by ru.user_name
+")) {
+	($start_ms,$end_ms,$username) = split("\\s+");
+	print "Found times $start_ms - $end_ms for user:$username\n";
+}
+
+# Generate report WITHIN
+$report_file = "/tmp/report-" . time();
+$start_ms++;
+$end_ms--;
+runcmd("wget -O \"$report_file\" --no-check-certificate \"https://localhost:8443/reports?session=$session_id&name=instance&type=CSV&page=0&flush=false&start=$start_ms&end=$end_ms&criterionId=User&groupById=None\"");
+
+
+# Parse report
+open(REPORT, $report_file);
+
+while (<REPORT>) {
+	print "Report line:$_\n";
+}
+
+# Compare report against total number of instances, instance-hours, instance types
+
