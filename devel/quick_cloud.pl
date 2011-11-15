84610425427cc99476f116a22224810629fbcc15
grze@eucalyptus.com <>
2011-11-15 00:51:00 -0800

    tweaks to quick_ha.pl: combine cluster proxy checks, db connection pool checks, lined up output of svc

--- devel/quick_cloud.pl
+++ devel/quick_cloud.pl
@@ -38,7 +38,7 @@ sub check_mysqld {
 
 sub coordinator_local {
    test_start("is coordinator?");
-   run_on_clc("$euca_dir/usr/sbin/euca-modify-property -p euca=\'com.eucalyptus.bootstrap.Hosts.Coordinator.INSTANCE.isLocalhost( )\'");
+   run_on_clc("$euca_dir/usr/sbin/euca-modify-property -p euca=\'com.eucalyptus.bootstrap.Hosts.isLocalHost() + \\\" => \\\" + com.eucalyptus.bootstrap.Hosts.getCoordinator()\'");
    test_end();
 }
 
