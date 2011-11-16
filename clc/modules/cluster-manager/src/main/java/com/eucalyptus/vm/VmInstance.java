1aabe230fb1d4050d047826bf0f4b4f27578b442
grze@eucalyptus.com <>
2011-11-15 22:15:44 -0800

    fix to instance events

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/vm/VmInstance.java
@@ -840,7 +840,7 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
                                                                     this.getOwner( ).getAccountNumber( ), this.getOwnerAccountName( ),
                                                                     this.placement.getClusterName( ), this.placement.getPartitionName( ),
                                                                     this.usageStats.getNetworkBytes( ), this.usageStats.getBlockBytes( ) ) );
-    } catch ( final EventFailedException ex ) {
+    } catch ( final Exception ex ) {
       LOG.error( ex, ex );
     }
   }
@@ -1320,6 +1320,7 @@ public class VmInstance extends UserMetadata<VmState> implements VmInstanceMetad
             } else {
               this.updateState( runVm );
             }
+            VmInstance.this.fireUsageEvent( );
             db.commit( );
           } catch ( final Exception ex ) {
             Logs.exhaust( ).error( ex, ex );
