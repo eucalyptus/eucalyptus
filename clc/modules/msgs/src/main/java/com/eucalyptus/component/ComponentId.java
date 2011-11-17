a8e5a574db6ac4cb716543370bdf977f45c741af
grze@eucalyptus.com <>
2011-11-16 17:05:28 -0800

    hide info for internal services

--- clc/modules/msgs/src/main/java/com/eucalyptus/component/ComponentId.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/component/ComponentId.java
@@ -406,7 +406,8 @@ public abstract class ComponentId implements HasName<ComponentId>, HasFullName<C
   
   public final boolean isInternal( ) {
     return this.ats.has( InternalService.class )
-      || ( !this.isAdminService( ) && !this.isPublicService( ) )
+      || !this.isAdminService( )
+      || !this.isPublicService( )
       || ( this.partitionParent( ).equals( Empyrean.INSTANCE ) && !this.isRegisterable( ) )
       || ( this.partitionParent( ).equals( Eucalyptus.INSTANCE ) && !this.isRegisterable( ) );
   }
