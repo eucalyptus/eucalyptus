f734452b8a73381005061742e7e95bf76aa4c314
grze@eucalyptus.com <>
2011-11-21 13:39:14 -0800

    logging in Hosts periodic cleanup

--- clc/modules/msgs/src/main/java/com/eucalyptus/bootstrap/Hosts.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/bootstrap/Hosts.java
@@ -258,22 +258,44 @@ public class Hosts {
       if ( !BootstrapArgs.isCloudController( ) && currentHost.hasBootstrapped( ) && Databases.shouldInitialize( ) ) {
         System.exit( 123 );
       }
-      if ( !Topology.isEnabled( Eucalyptus.class ) && Hosts.getCoordinator( ) != null ) {
-        BootstrapComponent.setup( Eucalyptus.class, Hosts.getCoordinator( ).getBindAddress( ) );
+      try {
+        if ( !Topology.isEnabled( Eucalyptus.class ) && Hosts.getCoordinator( ) != null ) {
+          LOG.info( "Setting up new coordinator: " + Hosts.getCoordinator( ) );
+          BootstrapComponent.setup( Eucalyptus.class, Hosts.getCoordinator( ).getBindAddress( ) );
+        }
+      } catch ( Exception ex ) {
+        LOG.debug( ex );
+        Logs.extreme( ).debug( ex, ex );
       }
-      if ( event.isAsserted( 3L ) && Bootstrap.isFinished( ) && !Hosts.list( Predicates.not( BootedFilter.INSTANCE ) ).isEmpty( ) ) {
-        UpdateEntry.INSTANCE.apply( currentHost );
-      } else if ( event.isAsserted( 15L ) ) {
-        UpdateEntry.INSTANCE.apply( currentHost );
+      try {
+        if ( event.isAsserted( 3L ) && Bootstrap.isFinished( ) && !Hosts.list( Predicates.not( BootedFilter.INSTANCE ) ).isEmpty( ) ) {
+          LOG.info( "Updating current host entry: " + currentHost );
+          UpdateEntry.INSTANCE.apply( currentHost );
+        } else if ( event.isAsserted( 15L ) ) {
+          LOG.info( "Updating current host entry: " + currentHost );
+          UpdateEntry.INSTANCE.apply( currentHost );
+        }
+      } catch ( Exception ex ) {
+        LOG.debug( ex );
+        Logs.extreme( ).debug( ex, ex );
       }
-      Set<Address> currentMembers = Sets.newHashSet( hostMap.getChannel( ).getView( ).getMembers( ) );
-      Map<String, Host> hostCopy = Maps.newHashMap( hostMap );
-      Set<Address> currentHosts = Sets.newHashSet( Collections2.transform( hostCopy.values( ), GroupAddressTransform.INSTANCE ) );
-      Set<Address> strayHosts = Sets.difference( currentHosts, currentMembers );
-      for ( Address strayHost : strayHosts ) {
-        Host h = hostCopy.get( strayHost );
-        BootstrapComponent.TEARDOWN.apply( h );
-        hostMap.remove( strayHost );
+      try {
+        Set<Address> currentMembers = Sets.newHashSet( hostMap.getChannel( ).getView( ).getMembers( ) );
+        Map<String, Host> hostCopy = Maps.newHashMap( hostMap );
+        Set<Address> currentHosts = Sets.newHashSet( Collections2.transform( hostCopy.values( ), GroupAddressTransform.INSTANCE ) );
+        Set<Address> strayHosts = Sets.difference( currentHosts, currentMembers );
+        if ( !strayHosts.isEmpty( ) ) {
+          LOG.info( "Pruning orphan host entries: " + strayHosts );
+        }
+        for ( Address strayHost : strayHosts ) {
+          Host h = hostCopy.get( strayHost );
+          LOG.info( "Pruning orphan host: " + h );
+          hostMap.remove( strayHost );
+          BootstrapComponent.TEARDOWN.apply( h );
+        }
+      } catch ( Exception ex ) {
+        LOG.debug( ex );
+        Logs.extreme( ).debug( ex, ex );
       }
     }
   }
