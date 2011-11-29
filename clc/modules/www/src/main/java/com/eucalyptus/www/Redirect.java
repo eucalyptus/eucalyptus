c434519c7aa8d3fb42ab3b02098c68d17c6e89d1
Ye Wen <wenye@eucalyptus.com>
2011-11-28 16:57:31 -0800

    Dashboard: Redirect web ui request to slave CLC to master CLC.

--- clc/modules/www/src/main/java/com/eucalyptus/www/Redirect.java
+++ clc/modules/www/src/main/java/com/eucalyptus/www/Redirect.java
@@ -1,28 +1,68 @@
 package com.eucalyptus.www;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 import org.mortbay.jetty.handler.rewrite.Rule;
 import com.eucalyptus.bootstrap.HttpServerBootstrapper;
+import com.eucalyptus.component.ServiceConfiguration;
+import com.eucalyptus.component.Topology;
+import com.eucalyptus.component.id.Eucalyptus;
 
 public class Redirect extends Rule {
+  
+  private static final Logger LOG = Logger.getLogger( Redirect.class );
+  
   public Redirect( ) {
     setTerminating( true );
   }
   
   @Override
   public String matchAndApply( String target, HttpServletRequest req, HttpServletResponse resp ) throws IOException {
-    String urlStr = req.getRequestURL( ).toString( );
-    URL url = new URL( urlStr );
-    if ( !urlStr.startsWith( "https" ) ) {
-      resp.sendRedirect( String.format( "https://%s:%d/%s", url.getHost( ), HttpServerBootstrapper.HTTPS_PORT,
-                                        url.getPath( ) + ( ( req.getQueryString( ) != null ) ? "?" + req.getQueryString( ) : "" ) ).replaceAll("//","/") );
-      return target;
-    } else {
-      return target;
+    String redirectHost = null; // indicates if a redirect is necessary
+    // First check if the request is for a CLC slave
+    // If ture, redirect to the master CLC.
+    if ( !Topology.isEnabledLocally( Eucalyptus.class ) ) {
+      try {
+        ServiceConfiguration clc = Topology.lookup( Eucalyptus.class );
+        if ( clc == null ) {
+          throw new RuntimeException( "Can not find enabled cloud controller" );
+        }
+        redirectHost = clc.getHostName( );
+      } catch ( Exception e ) {
+        LOG.error( "Failed to lookup service configuration for cloud controller", e );
+        throw new IOException( "No available cloud web service" );
+      }
     }
+    // Then check if the request is plain HTTP
+    // If true, redirect to the HTTPS service
+    String urlStr = req.getRequestURL( ).toString( );
+    if ( !urlStr.startsWith( "https" ) ) {
+      redirectHost = ( new URL( urlStr ) ).getHost( );
+    }
+    if ( redirectHost != null ) {
+      // A redirect is required
+      resp.sendRedirect( getRedirectUrl( redirectHost, req ) );
+    }
+    return target;
+  }
+
+  /**
+   * Reconstruct the correct redirect URL.
+   * 
+   * @param redirectHost
+   * @param req
+   * @return
+   * @throws MalformedURLException
+   */
+  private static String getRedirectUrl(String redirectHost, HttpServletRequest req) throws MalformedURLException {
+    URL url = new URL( req.getRequestURL( ).toString( ) );
+    return String.format( "https://%s:%d/%s", redirectHost, HttpServerBootstrapper.HTTPS_PORT, 
+        url.getPath( ) + ( ( req.getQueryString( ) != null ) ? "?" + req.getQueryString( ) : "" ) ).replaceAll("//","/");
   }
   
 }
