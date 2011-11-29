ec2f1b2137dadff79c86e05470dae2f203370e6d
chris grzegorczyk <grze@eucalyptus.com>
2011-11-29 14:19:27 -0800

    adjust stack filtering

--- clc/modules/msgs/src/main/java/com/eucalyptus/bootstrap/Databases.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/bootstrap/Databases.java
@@ -594,6 +594,7 @@ public class Databases {
   }
   
   private static Predicate<StackTraceElement> stackFilter = Predicates.not( Predicates.or( Threads.filterStackByQualifiedName( "com\\.eucalyptus\\.entities\\..*" ),
+                                                                                           Threads.filterStackByQualifiedName( "java\\.lang\\.Thread.*" ),
                                                                                            Threads.filterStackByQualifiedName( "com\\.eucalyptus\\.bootstrap\\.Databases.*" ) ) );
   
   public static void awaitSynchronized( ) {
