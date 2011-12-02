2b22213a4038560de63f120fdcaf2f2fe83c7954
twerges <>
2011-12-02 11:56:05 -0800

    oops minor bug; .eucarc -> eucarc

--- clc/modules/reporting/src/test/pl/test.sh
+++ clc/modules/reporting/src/test/pl/test.sh
@@ -4,8 +4,8 @@
 # This script is specifically written to be run on Kyo's QA setup and
 #   has pathnames dependent on that.
 
-. /root/.eucarc
-. /root/.iamrc
+. /root/eucarc
+. /root/iamrc
 export EUCALYPTUS=/opt/eucalyptus
 export PATH=$PATH:$EUCALYPTUS/usr/sbin
 export PATH=$PATH:/root/euca_builder/eee/devel
