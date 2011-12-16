20733cfe592d4ecafeae6ba79ef4b9757be6f02b
chris grzegorczyk <grze@eucalyptus.com>
2011-12-15 23:13:52 -0800

    fix error message in pipeline

--- clc/modules/wsstack/src/main/java/com/eucalyptus/ws/server/MetadataPipeline.java
+++ clc/modules/wsstack/src/main/java/com/eucalyptus/ws/server/MetadataPipeline.java
@@ -1,7 +1,6 @@
 package com.eucalyptus.ws.server;
 
 import java.net.InetSocketAddress;
-import java.util.List;
 import java.util.NoSuchElementException;
 import org.apache.log4j.Logger;
 import org.jboss.netty.buffer.ChannelBuffer;
@@ -28,17 +27,28 @@ import com.eucalyptus.context.ServiceDispatchException;
 import com.eucalyptus.http.MappingHttpRequest;
 import com.eucalyptus.records.Logs;
 import com.eucalyptus.util.Exceptions;
-import com.eucalyptus.ws.stages.UnrollableStage;
 
 @ChannelPipelineCoverage( "one" )
 @ComponentPart( Eucalyptus.class )
 public class MetadataPipeline extends FilteredPipeline implements ChannelUpstreamHandler {
-  private static Logger LOG = Logger.getLogger( MetadataPipeline.class );
+  private static final String ERROR_STRING = "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n" +
+                                             "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" +
+                                             "         \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+                                             "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n" +
+                                             " <head>\n" +
+                                             "  <title>404 - Not Found</title>\n" +
+                                             " </head>\n" +
+                                             " <body>\n" +
+                                             "  <h1>404 - Not Found: failed to find %s</h1>\n" +
+                                             "  <pre>%s</pre>\n" +
+                                             " </body>\n" +
+                                             "</html>\n";
+  private static Logger       LOG          = Logger.getLogger( MetadataPipeline.class );
   
   public MetadataPipeline( ) {
     super( );
   }
-
+  
   @Override
   public boolean checkAccepts( HttpRequest message ) {
     return message.getUri( ).matches( "/latest(/.*)*" ) || message.getUri( ).matches( "/\\d\\d\\d\\d-\\d\\d-\\d\\d/.*" );
@@ -85,14 +95,18 @@ public class MetadataPipeline extends FilteredPipeline implements ChannelUpstrea
       Logs.extreme( ).debug( "VmMetadata reply info: " + reply + " " + replyEx );
       if ( replyEx != null || reply == null || reply instanceof NullPayload ) {
         response = new DefaultHttpResponse( request.getProtocolVersion( ), HttpResponseStatus.NOT_FOUND );
+        String errorMessage = String.format(
+          ERROR_STRING,
+          newUri.replaceAll( remoteHost + ":", "" ),
+          replyEx != null && Logs.isDebug( ) ? Exceptions.string( replyEx ) : "" );
         if ( Logs.isDebug( ) ) {
           response.setHeader( HttpHeaders.Names.CONTENT_TYPE, "text/plain" );
           ChannelBuffer buffer = null;
           if ( replyEx != null && !( replyEx instanceof NoSuchElementException ) ) {
-            buffer = ChannelBuffers.wrappedBuffer(  Exceptions.string( replyEx ).getBytes( ) );
+            buffer = ChannelBuffers.wrappedBuffer( errorMessage.getBytes( ) );
             response.setContent( buffer );
           } else {
-            buffer = ChannelBuffers.wrappedBuffer( "".getBytes( ) );
+            buffer = ChannelBuffers.wrappedBuffer( errorMessage.getBytes( ) );
             response.setContent( buffer );
           }
           response.addHeader( HttpHeaders.Names.CONTENT_LENGTH, Integer.toString( buffer.readableBytes( ) ) );
