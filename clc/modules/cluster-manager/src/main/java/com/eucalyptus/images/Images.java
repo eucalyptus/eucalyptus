1af061967de588c515240a0b9b3a606b5434feae
Sang-Min Park <spark@eucalyptus.com>
2011-12-19 16:16:45 -0800

    fix rt5227 - don't show eki/eri with windows images

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/images/Images.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/images/Images.java
@@ -501,7 +501,7 @@ public class Images {
     ImageMetadata.Architecture imageArch = ( requestArch != null )
       ? requestArch
       : manifest.getArchitecture( );
-    ImageMetadata.Platform imagePlatform = manifest.getPlatform( );
+    ImageMetadata.Platform imagePlatform = manifest.getPlatform( );    
     switch ( manifest.getImageType( ) ) {
       case kernel:
         ret = new KernelImageInfo( creator, ImageUtil.newImageId( ImageMetadata.Type.kernel.getTypePrefix( ), manifest.getImageLocation( ) ),
@@ -514,6 +514,10 @@ public class Images {
                                     manifest.getImageLocation( ), manifest.getBundledSize( ), manifest.getChecksum( ), manifest.getChecksumType( ) );
         break;
       case machine:
+    	if(ImageMetadata.Platform.windows.equals(imagePlatform)){
+    	    	eki = null; 
+    	    	eri = null;
+    	}
         ret = new MachineImageInfo( creator, ImageUtil.newImageId( ImageMetadata.Type.machine.getTypePrefix( ), manifest.getImageLocation( ) ),
                                     imageName, imageDescription, manifest.getSize( ), imageArch, imagePlatform,
                                     manifest.getImageLocation( ), manifest.getBundledSize( ), manifest.getChecksum( ), manifest.getChecksumType( ), eki, eri );
