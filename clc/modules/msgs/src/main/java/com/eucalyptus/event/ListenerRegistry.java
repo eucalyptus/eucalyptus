975218a4c85c2518dd8d1a094a1fa64ed1657e15
mmunn1 <mmunn1@eucalyptus>
2012-05-20 18:27:58 -0700

    10838 UL: Lock not released on all paths

--- clc/modules/msgs/src/main/java/com/eucalyptus/event/ListenerRegistry.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/event/ListenerRegistry.java
@@ -208,8 +208,9 @@ public class ListenerRegistry {
     }
     
     public void destroy( T type ) {
-      try {
-        this.modificationLock.lock( );
+     
+      this.modificationLock.lock( );
+      try{
         for ( EventListener e : this.listenerMap.get( type ) ) {
           EventRecord.caller( ReentrantListenerRegistry.class, EventType.LISTENER_DESTROY_ALL, type.getClass( ).getSimpleName( ),
                               e.getClass( ).getCanonicalName( ) ).trace( );
