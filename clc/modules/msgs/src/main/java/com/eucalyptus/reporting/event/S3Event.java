8a9ea517a52ac1427042d6dcfc052458d5500ab4
Tom Werges <tom.werges@eucalyptus.com>
2012-06-11 13:16:44 -0700

    added defensive programming checks so events with negative values or missing information cannot be created

--- clc/modules/msgs/src/main/java/com/eucalyptus/reporting/event/S3Event.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/reporting/event/S3Event.java
@@ -20,6 +20,10 @@ public class S3Event
 	public S3Event(boolean createOrDelete, long sizeMegs, String ownerId,
 			String ownerName, String accountId, String accountName)
 	{
+		if (sizeMegs < 0) throw new IllegalArgumentException("sizeMegs for s3 objects cannot be negative");
+		if (ownerId==null) throw new IllegalArgumentException("ownerId cant be null");
+		if (accountId==null) throw new IllegalArgumentException("ownerId cant be null");
+
 		super();
 		this.objectOrBucket = true;
 		this.createOrDelete = createOrDelete;
@@ -38,6 +42,10 @@ public class S3Event
 	public S3Event(boolean createOrDelete, String ownerId,	String ownerName,
 			String accountId, String accountName)
 	{
+		if (sizeMegs < 0) throw new IllegalArgumentException("sizeMegs for s3 objects cannot be negative");
+		if (ownerId==null) throw new IllegalArgumentException("ownerId cant be null");
+		if (accountId==null) throw new IllegalArgumentException("ownerId cant be null");
+
 		super();
 		this.objectOrBucket = false;
 		this.createOrDelete = createOrDelete;
