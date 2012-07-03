3d0b70e4eb320f87e3f5b251ea70edce03efce26
dkavanagh <dkavanagh@gmail.com>
2012-07-03 12:09:48 -0400

    added session handling. upon login, user/access/secret keys are stored in session. Session ID is set in cookie. Cookie is read upon request for data and access/secret from session are used to make request to API

--- eui-server.py
+++ eui-server.py
@@ -2,6 +2,7 @@ import tornado.ioloop
 import tornado.web
 import os
 import socket
+import random
 import boto
 import json
 from json import JSONEncoder
@@ -10,6 +11,16 @@ from boto.ec2.ec2object import EC2Object
 from boto.regioninfo import RegionInfo
 from boto.ec2.instance import Group
 from boto.ec2.volume import AttachmentSet
+
+# dictionary for in-memory sessions
+sessions = {};
+
+class UserSession(object):
+  def __init__(self, username, password, access_id, secret_key):
+    self.username = username
+    self.password = password
+    self.access_id = access_id
+    self.secret_key = secret_key
 
 class BotoEncoder(JSONEncoder):
   def default(self, obj):
@@ -36,9 +47,14 @@ class EC2Handler(BaseHandler):
     def get(self):
 #        print "Hello, " + self.current_user
         endpoint='http://192.168.25.10:8773/services/Eucalyptus'
-        access_id='L52ISGKFHSEXSPOZYIZ1K'
-        secret_key='YRRpiyw333aq1se5PneZEnskI9MMNXrSoojoJjat'
-        conn = boto.connect_euca(host='192.168.25.10', aws_access_key_id=access_id, aws_secret_access_key=secret_key)
+        session_id = self.get_cookie("session-id")
+        if not(session_id):
+          self.redirect("/login")
+        try:
+          session = sessions[session_id]
+        except KeyError:
+          self.redirect("/login")
+        conn = boto.connect_euca(host='192.168.25.10', aws_access_key_id=session.access_id, aws_secret_access_key=session.secret_key)
         data_type = self.get_argument("type")
         obj = []
         if data_type == "image":
@@ -59,13 +75,26 @@ class EC2Handler(BaseHandler):
 
 class LoginHandler(BaseHandler):
     def get(self):
+        # ensure session is cleared
+        self.set_cookie("session-id", "")
+        # could redirect to login page...
         self.write('<html><body><form action="/login" method="post">'
-                   'Name: <input type="text" name="name">'
+                   'Name: <input type="text" name="name"><br/>'
+                   'Password: <input type="text" name="passwd"><br/>'
                    '<input type="submit" value="Sign in">'
                    '</form></body></html>')
 
     def post(self):
-        self.set_secure_cookie("user", self.get_argument("name"))
+        # validate user from args, get back tokens, then
+        user = self.get_argument("name")
+        passwd = self.get_argument("passwd")
+        access_id='L52ISGKFHSEXSPOZYIZ1K'
+        secret_key='YRRpiyw333aq1se5PneZEnskI9MMNXrSoojoJjat'
+        # create session and store info there, set session id in cookie
+        cookie = os.urandom(16).encode('hex');
+        print "session id : "+cookie
+        self.set_cookie("session-id", cookie);
+        sessions[cookie] = UserSession(user, passwd, access_id, secret_key)
         self.redirect("/static/eui.html")
 
 settings = {
@@ -79,11 +108,10 @@ application = tornado.web.Application([
     (r"/login", LoginHandler),
 ], **settings)
 
-
 if __name__ == "__main__":
-    (hostname, alt_host, ipaddrs) = socket.gethostbyaddr(socket.gethostname())
-    for ip in ipaddrs:
-      print "host IP: "+ip
+#    (hostname, alt_host, ipaddrs) = socket.gethostbyaddr(socket.gethostname())
+#    for ip in ipaddrs:
+#      print "host IP: "+ip
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
 
