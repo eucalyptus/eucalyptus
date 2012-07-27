12bd8fb15749ef3250288404c7055559d0c08a27
Sang-Min Park <spark@eucalyptus.com>
2012-07-27 15:12:31 -0700

    don't use async=false option in jQuery.ajax; use when().done() instead

--- static/js/eucalyptus.js
+++ static/js/eucalyptus.js
@@ -3,61 +3,62 @@
 	$.eucaData = {};
   }
   $(document).ready(function() {
-    // check language and retrieve i18n customization
-    $.ajax({
-      type:"POST",
-      data:"action=lang",
-      dataType:"json",
-      async:"false",
-      success: function(out, textStatus, jqXHR){ 
-        eucalyptus.i18n({'language':out.language});
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        alert("The server is not available");
-        location.href='/';
-      }
-    });
- 
-    // check cookie
-    if ($.cookie('session-id')) {
-       $.ajax({
-  	  type:"POST",
-	  data:"action=session&_xsrf="+$.cookie('_xsrf'),
-	  dataType:"json",
-	  async:"false",
-	  success: function(out, textStatus, jqXHR){
+    $.when( // this is to synchronize a chain of ajax calls 
+      $.ajax({
+        type:"POST",
+        data:"action=lang",
+        dataType:"json",
+        async:"false", // async option deprecated as of jQuery 1.8
+        success: function(out, textStatus, jqXHR){ 
+          eucalyptus.i18n({'language':out.language});
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+          //TODO: should present error screen
+          alert("The server is not available");
+          location.href='/';
+        }
+      })).done(function(out){
+        // check cookie
+        if ($.cookie('session-id')) {
+          $.ajax({
+  	    type:"POST",
+	    data:"action=session&_xsrf="+$.cookie('_xsrf'),
+	    dataType:"json",
+	    async:"false",
+	    success: function(out, textStatus, jqXHR){
 	      $.extend($.eucaData, {'g_session':out.global_session, 'u_session':out.user_session});
               eucalyptus.main($.eucaData);
-          },
-	  error: function(jqXHR, textStatus, errorThrown){
+            },
+	    error: function(jqXHR, textStatus, errorThrown){
               $.cookie('session-id','');
  	      location.href='/';	   
-	  }
-       });
-    } else {
-      eucalyptus.login({
-        doLogin : function(args) {
-          var tok = args.param.account+':'+args.param.username+':'+args.param.password;
-          var hash = btoa(tok);
-	  $.ajax({
-	    type:"POST",
- 	    data:"action=login", 
-            beforeSend: function (xhr) { 
-               xhr.setRequestHeader('Authorization', 'Basic '+hash); 
-            },
-    	    dataType:"json",
-	    async:"false",
-	    success: function(out, textStatus, jqXHR) {
-	       $.extend($.eucaData, {'g_session':out.global_session, 'u_session':out.user_session});
-               args.onSuccess($.eucaData); // call back to login UI
-            },
-            error: function(jqXHR, textStatus, errorThrown){
-	       args.onError(errorThrown);
-            }
- 	  });
-        }
-      });
-    }
+	    }
+          });
+        } else {
+          eucalyptus.login({
+            doLogin : function(args) {
+              var tok = args.param.account+':'+args.param.username+':'+args.param.password;
+              var hash = btoa(tok);
+	      $.ajax({
+	        type:"POST",
+ 	        data:"action=login", 
+                beforeSend: function (xhr) { 
+                   xhr.setRequestHeader('Authorization', 'Basic '+hash); 
+                },
+    	        dataType:"json",
+	        async:"false",
+	        success: function(out, textStatus, jqXHR) {
+	          $.extend($.eucaData, {'g_session':out.global_session, 'u_session':out.user_session});
+                  args.onSuccess($.eucaData); // call back to login UI
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+	          args.onError(errorThrown);
+                }
+ 	     });
+           }
+         }); // end of login
+       } // end of else
+    }); // end of done
   });
 })(jQuery,
    window.eucalyptus ? window.eucalyptus : window.eucalyptus = {});
--- static/js/i18n.js
+++ static/js/i18n.js
@@ -1,7 +1,7 @@
 (function($, eucalyptus) {
   eucalyptus.i18n = function(args) {
     // i18n properties will be loaded before log-in
-    jQuery.i18n.properties({
+    $.i18n.properties({
       name:'Messages', 
       path:'custom/', 
       mode:'both',
@@ -9,33 +9,33 @@
       callback: function() {
         // We specified mode: 'both' so translated values will be
         // available as JS vars/functions and as a map
-        jQuery.i18n.prop('text_footer');
+        $.i18n.prop('text_footer');
      
-        jQuery.i18n.prop('button_explorer');
+        $.i18n.prop('button_explorer');
 
-        jQuery.i18n.prop('menu_dashboard');
-        jQuery.i18n.prop('menu_images');
-        jQuery.i18n.prop('menu_instances');
-        jQuery.i18n.prop('menu_storage');
-        jQuery.i18n.prop('menu_netsec');
-        jQuery.i18n.prop('menu_support');
+        $.i18n.prop('menu_dashboard');
+        $.i18n.prop('menu_images');
+        $.i18n.prop('menu_instances');
+        $.i18n.prop('menu_storage');
+        $.i18n.prop('menu_netsec');
+        $.i18n.prop('menu_support');
 
-        jQuery.i18n.prop('menu_dashboard_dashboard');
-        jQuery.i18n.prop('menu_images_images');
-        jQuery.i18n.prop('menu_instances_instances');
-        jQuery.i18n.prop('menu_storage_volumes');
-        jQuery.i18n.prop('menu_storage_snapshots');
-        jQuery.i18n.prop('menu_storage_buckets');
-        jQuery.i18n.prop('menu_netsec_eip');
-        jQuery.i18n.prop('menu_netsec_sgroup');
-        jQuery.i18n.prop('menu_netsec_keypair');
-        jQuery.i18n.prop('menu_support_guide');
-        jQuery.i18n.prop('menu_support_forum');
-        jQuery.i18n.prop('menu_support_report');
+        $.i18n.prop('menu_dashboard_dashboard');
+        $.i18n.prop('menu_images_images');
+        $.i18n.prop('menu_instances_instances');
+        $.i18n.prop('menu_storage_volumes');
+        $.i18n.prop('menu_storage_snapshots');
+        $.i18n.prop('menu_storage_buckets');
+        $.i18n.prop('menu_netsec_eip');
+        $.i18n.prop('menu_netsec_sgroup');
+        $.i18n.prop('menu_netsec_keypair');
+        $.i18n.prop('menu_support_guide');
+        $.i18n.prop('menu_support_forum');
+        $.i18n.prop('menu_support_report');
 
-        jQuery.i18n.prop('help_dashboard_01');
-        jQuery.i18n.prop('help_dashboard_02');
-        jQuery.i18n.prop('help_dashboard_03');
+        $.i18n.prop('help_dashboard_01');
+        $.i18n.prop('help_dashboard_02');
+        $.i18n.prop('help_dashboard_03');
       }
     });
   }
