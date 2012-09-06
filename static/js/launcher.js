4a8873c14362f86e248d881ad39dc90a9d8cd79d
Sang-Min Park <smpark.uva@gmail.com>
2012-09-06 04:08:46 -0700

    header, buttons, and slide toggle on launch wizard

--- static/js/launcher.js
+++ static/js/launcher.js
@@ -29,6 +29,8 @@
       var $wrapper = $($tmpl.render($.extend($.i18n.map, help_launcher)));
       var $launcher = $wrapper.children().first();
       var $launcherHelp = $wrapper.children().last();
+    
+      thisObj.element.addClass('launch-wizard');
 
       // make section headers
       thisObj._makeSectionHeader($launcher);
@@ -45,17 +47,102 @@
      },
     _destroy : function() { },
 
-    _makeSectionHeader : function($launcher) { },
+    _makeSectionHeader : function($launcher) {
+      var thisObj = this;
+      var $header = $launcher.find('#launch-wizard-image-header');
+      $header.append($('<a>').attr('href', '#').html(launch_instance_section_header_image).click( function(e) {
+        var imgSection = $launcher.find('#launch-wizard-image-contents');
+        thisObj._selectedSection.slideToggle('fast');
+        imgSection.slideToggle('fast');
+        thisObj._selectedSection = imgSection;
+      }));
+      $header = $launcher.find('#launch-wizard-type-header');
+      $header.append($('<a>').attr('href', '#').html(launch_instance_section_header_type).click(function(e) {
+        var typeSection = $launcher.find('#launch-wizard-type-contents');
+        thisObj._selectedSection.slideToggle('fast');
+        typeSection.slideToggle('fast');
+        thisObj._selectedSection = typeSection;
+      }));
+      $header = $launcher.find('#launch-wizard-security-header');
+      $header.append($('<a>').attr('href', '#').html(launch_instance_section_header_security).click(function(e){
+        var secSection = $launcher.find('#launch-wizard-security-contents');
+        thisObj._selectedSection.slideToggle('fast');
+        secSection.slideToggle('fast');
+        thisObj._selectedSection = secSection;
+      }));
+      $header = $launcher.find('#launch-wizard-advanced-header');
+      $header.append($('<a>').attr('href', '#').html(launch_instance_section_header_advanced).click(function(e){
+        var advSection = $launcher.find('#launch-wizard-advanced-contents');
+        thisObj._selectedSection.slideToggle('fast');
+        advSection.slideToggle('fast');
+        thisObj._selectedSection = advSection;
+      }));
+    },
  
-    _makeSectionButton : function($launcher) { },
+    _makeSectionButton : function($launcher) {
+      var thisObj = this;
+      var $imageBtn = $launcher.find('#launch-wizard-buttons-image');
+      $imageBtn.append($('<button>').attr('id', 'launch-wizard-buttons-image-next').html(launch_instance_btn_next_type).click( function(e){
+        var typeSection = $launcher.find('#launch-wizard-type-contents');
+        thisObj._selectedSection.slideToggle('fast');
+        typeSection.slideToggle('fast');
+        thisObj._selectedSection = typeSection;
+      }));
+      var $typeBtn = $launcher.find('#launch-wizard-buttons-type');
+      $typeBtn.append($('<button>').attr('id', 'launch-wizard-buttons-type-next').html(launch_instance_btn_next_security).click( function(e){
+        var secSection = $launcher.find('#launch-wizard-security-contents');
+        thisObj._selectedSection.slideToggle('fast');
+        secSection.slideToggle('fast');
+        thisObj._selectedSection = secSection;
+      }));
+      var $securityBtn = $launcher.find('#launch-wizard-buttons-security');
+      $securityBtn.append(
+        $('<ul>').append(
+          $('<li>').append($('<button>').attr('id','launch-wizard-buttons-security-launch').html(launch_instance_btn_launch).click( function(e){
+        })),
+          $('<li>').append($('<span>').text('Or:'), $('<a>').attr('href','#').html(launch_instance_btn_next_advanced).click( function(e){
+            var advSection = $launcher.find('#launch-wizard-advanced-contents');
+            thisObj._selectedSection.slideToggle('fast');
+            advSection.slideToggle('fast');
+            thisObj._selectedSection = advSection;
+        })))); 
+      var $advancedBtn = $launcher.find('#launch-wizard-buttons-advanced');
+      $advancedBtn.append(
+        $('<button>').attr('id', 'launch-wizard-buttons-advanced-launch').html(launch_instance_btn_launch).click( function(e){
+        })); 
+    },
+    _selectedSection : null,
+    _makeImageSection : function($section){ 
+      var thisObj = this;
+      var $content = $section.find('#launch-wizard-image-main-contents');
+      $.each($content.children(), function(idx, child){
+        $(child).html('image-contents'); 
+      });
 
-    _makeImageSection : function($section){ },
+      $content = $section.find('#launch-wizard-image-contents').slideToggle('fast');
+      thisObj._selectedSection = $content;
+    },
    
-    _makeTypeSection : function($section){ },
+    _makeTypeSection : function($section){
+      var $content = $section.find('#launch-wizard-type-main-contents');
+      $.each($content.children(), function(idx, child){
+        $(child).html('type-contents'); 
+      });
+    },
 
-    _makeSecuritySection : function($section) { },
+    _makeSecuritySection : function($section) {
+      var $content = $section.find('#launch-wizard-security-main-contents');
+      $.each($content.children(), function(idx, child){
+        $(child).html('security-contents'); 
+      });
+    },
 
-    _makeAdvancedSection : function($section) { },
+    _makeAdvancedSection : function($section) { 
+      var $content = $section.find('#launch-wizard-advanced-main-contents');
+      $.each($content.children(), function(idx, child){
+        $(child).html('advanced-contents'); 
+      });
+    },
   });
 })(jQuery,
    window.eucalyptus ? window.eucalyptus : window.eucalyptus = {});
