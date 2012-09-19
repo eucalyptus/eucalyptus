3bf71020ef7361d5107f1a0bbd7d96e4ea58deb2
Dmitrii Zagorodnov <dmitrii@eucalyptus.com>
2012-09-19 13:04:36 -0700

    fixing bug where sensor report was limited to one resource

--- util/sensor.c
+++ util/sensor.c
@@ -100,6 +100,7 @@ static int getstat_refresh (void)
 
     getstat_free(); // free the old stats, regardless of whether we succeed or not
 
+    errno = 0;
     char * output = system_output ("euca_rootwrap getstats.pl"); // invoke th Perl script
     int ret = ERROR;
 
@@ -186,7 +187,7 @@ static int getstat_refresh (void)
     done:
         free (output);
     } else {
-        logprintfl (EUCAWARN, "failed to invoke getstats for sensor data\n");
+        logprintfl (EUCAWARN, "failed to invoke getstats for sensor data (%s)\n", strerror (errno));
     }
     
     return ret;
