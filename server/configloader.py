43ab534d5d47c2145f2e78bb122a77231be05abd
Matt Spaulding <mspaulding06@gmail.com>
2012-09-23 22:29:30 -0700

    ConfigLoader is now a singleton

--- server/configloader.py
+++ server/configloader.py
@@ -6,14 +6,38 @@ CONFIG_FILE_LIST = ['console.ini',
                     'server/console.ini',
                     '/etc/eucalyptus-ui/console.ini']
 
-class ConfigLoader:
-    @staticmethod
-    def getParser(config_file=None):
-        parser = ConfigParser.ConfigParser()
+
+class Singleton(type):
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None 
+
+    def __call__(cls,*args,**kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
+
+class ConfigError(Exception):
+    pass
+
+
+class ConfigLoader(object):
+    __metaclass__ = Singleton
+    def __init__(self):
+        self.parser = None
+        self.config = None
+
+    def getParser(self, config_file=None):
+        if self.parser:
+            return self.parser
+        self.parser = ConfigParser.ConfigParser()
         if config_file:
             CONFIG_FILE_LIST.insert(0, config_file);
         for config in CONFIG_FILE_LIST:
             if os.path.isfile(config):
-                parser.read(config)
-                return parser
+                self.parser.read(config)
+                self.config = config
+                return self.parser
+        raise ConfigError("No valid config file found")
 
