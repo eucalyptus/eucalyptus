f7e7e4ae92414078cdf019117872b427f2c4c5ca
Tom Werges <tom.werges@eucalyptus.com>
2012-10-02 23:59:48 -0700

    InstanceArtGenerator now scans values in order, and we do not correct sensor resets before insertion

--- clc/modules/reporting/src/main/java/com/eucalyptus/reporting/event_store/ReportingInstanceEventStore.java
+++ clc/modules/reporting/src/main/java/com/eucalyptus/reporting/event_store/ReportingInstanceEventStore.java
@@ -61,9 +61,6 @@ public class ReportingInstanceEventStore extends EventStoreSupport {
         availabilityZone ) );
   }
 
-  private final Map<InstanceMetricDimensionKey,MetricInfo> metricInfoMap
-  		= new HashMap<InstanceMetricDimensionKey,MetricInfo>();
-  
   public void insertUsageEvent( @Nonnull final String uuid,
                                 @Nonnull final Long timestamp,
                                 @Nonnull final String metric,
@@ -78,115 +75,10 @@ public class ReportingInstanceEventStore extends EventStoreSupport {
     Preconditions.checkNotNull( dimension, "Dimension is required" );
     Preconditions.checkNotNull( value, "value is required" );
 
-    /* Correct sensor resets before inserting data into the database. Sensors occasionally
-     * reset when an nc reboots or for some other reason. In which case, the sequence
-     * number resets to 0 and the value resets to 0. We correct this by increasing the value
-     * by whatever the last value before reset was (values are cumulative) for every subsequent
-     * event for this metric. To do this, we must retain the last value for each metric value, the
-     * last sequence number, and offsets; the offsets are increased every time there is a sensor
-     * reset.
-     * 
-     * In some circumstances, the CLC will failover and this data will be lost. This will not
-     * affect this algorithm. Only if a sensor has a sequence number lower than a prior remembered
-     * one, will we detect a sequence reset.
-     * 
-     * On rare occasions, the sensor can reset at the same moment that the CLC fails over. In this
-     * rare case, instance usage could go backwards in the database. Programs which read the database
-     * should be aware of this rare possibility.
-     * 
-     * We must not retain prior data forever for all instances, so the data is periodically
-     * purged.
-     */
-    InstanceMetricDimensionKey key = new InstanceMetricDimensionKey(uuid, metric, dimension);
-    if (!metricInfoMap.containsKey(key)) {
-    	metricInfoMap.put(key, new MetricInfo());
-    }
-    MetricInfo info = metricInfoMap.get(key);
-    if (sequenceNum < info.lastSequenceNum) {
-    	/* RESET HAS OCCURRED */
-    	info.sequenceOffset += info.lastSequenceNum;
-    	info.valueOffset += info.lastValue;
-    }
-    info.lastSequenceNum = sequenceNum;
-    info.lastValue = value;
-    info.lastEventArrived = System.currentTimeMillis();
-
-    if (eventCnt++ % PURGE_EVERY_NUM_EVENTS==0) purgeMetricInfo();
-
-    persist( new ReportingInstanceUsageEvent( uuid, metric, sequenceNum+info.sequenceOffset,
-    		dimension, value+info.valueOffset, timestamp ) );
+    persist( new ReportingInstanceUsageEvent( uuid, metric, sequenceNum,
+    		dimension, value, timestamp ) );
   }
 
-  private static final long PURGE_EVERY_NUM_EVENTS = 50000;
-  private static final long PURGE_OLDER_THAN_MS = TimeUnit.DAYS.toMillis(2);
-  private long eventCnt = 0;
-
-  private void purgeMetricInfo()
-  {
-  	for (InstanceMetricDimensionKey key: metricInfoMap.keySet()) {
-		MetricInfo info = metricInfoMap.get(key);
-		if (info.lastEventArrived < System.currentTimeMillis() - PURGE_OLDER_THAN_MS) {
-			metricInfoMap.remove(key);
-		}
-	}
-  }
-  
-  private static class InstanceMetricDimensionKey
-  {
-	  private final String uuid;
-	  private final String metric;
-	  private final String dim;
-	  
-	  InstanceMetricDimensionKey(String uuid, String metric, String dim)
-	  {
-		  this.uuid = uuid;
-		  this.metric = metric;
-		  this.dim = dim;
-	  }
-
-	  @Override
-	  public int hashCode() {
-		  final int prime = 31;
-		  int result = 1;
-		  result = prime * result + ((dim == null) ? 0 : dim.hashCode());
-		  result = prime * result + ((metric == null) ? 0 : metric.hashCode());
-		  result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
-		  return result;
-	  }
-
-	  @Override
-	  public boolean equals(Object obj) {
-		  if (this == obj) return true;
-		  if (obj == null) return false;
-		  if (getClass() != obj.getClass()) return false;
-		  InstanceMetricDimensionKey other = (InstanceMetricDimensionKey) obj;
-		  if (dim == null) {
-			  if (other.dim != null) return false;
-		  } else if (!dim.equals(other.dim)) return false;
-		  if (metric == null) {
-			  if (other.metric != null) return false;
-		  } else if (!metric.equals(other.metric)) return false;
-		  if (uuid == null) {
-			  if (other.uuid != null)	return false;
-		  } else if (!uuid.equals(other.uuid)) return false;
-		  return true;
-	  }
-	  
-	  
-  }
-  
-  private static class MetricInfo
-  {
-	  private long lastSequenceNum = 0l;
-	  private long sequenceOffset = 0l;
-	  private double lastValue = 0d;
-	  private double valueOffset = 0d;
-	  private long lastEventArrived = 0l;
-
-	  MetricInfo()
-	  {
-	  }
-  }
   
 
 }
