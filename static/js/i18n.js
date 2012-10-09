318de72706a9e447c207608b04cdc0947f542922
Sang-Min Park <smpark.uva@gmail.com>
2012-10-09 10:00:13 -0700

    fix malformed Messages.properties

--- static/js/i18n.js
+++ static/js/i18n.js
@@ -28,55 +28,7 @@
       language: args.language, 
       callback: function() {
         // when jsrender is used, the variables should be propped to make it available as $.i18n.map 
-        $.i18n.prop('text_footer');
-        $.i18n.prop('login_title');
-        $.i18n.prop('login_acct');
-        $.i18n.prop('login_uname');
-        $.i18n.prop('login_pwd');
-        $.i18n.prop('login_pwd_help');
-        $.i18n.prop('login_pwd_link');
-        $.i18n.prop('login_btn');
-
-        $.i18n.prop('kp_tbl_hdr_name');
-        $.i18n.prop('kp_tbl_hdr_fingerprint');
-
-        // keypair
-        $.i18n.prop('keypair_dialog_del_text');
-        $.i18n.prop('keypair_dialog_add_text');
-        $.i18n.prop('keypair_dialog_add_name');
-        $.i18n.prop('keypair_dialog_add_help');
-        $.i18n.prop('keypair_dialog_delete_help');
-        $.i18n.prop('keypair_table_help');
-
-        // sgroup
-        $.i18n.prop('sgroup_tbl_hdr_name');
-        $.i18n.prop('sgroup_tbl_hdr_description');
-        $.i18n.prop('sgroup_tbl_hdr_rules');
-        $.i18n.prop('sgroup_dialog_del_text');
-        $.i18n.prop('sgroup_dialog_add_text');
-        $.i18n.prop('sgroup_dialog_add_name');
-        $.i18n.prop('sgroup_dialog_add_description');
-
-        // volume
-        $.i18n.prop('volume_dialog_del_text');
-        $.i18n.prop('volume_dialog_add_text');
-
-        // dashboard
-        $.i18n.prop('dashboard_title');
-        $.i18n.prop('dashboard_az_all');
-        $.i18n.prop('dashboard_instance_running');
-        $.i18n.prop('dashboard_instance_stopped');
-        $.i18n.prop('dashboard_volumes');
-        $.i18n.prop('dashboard_snapshots');
-        $.i18n.prop('dashboard_buckets');
-        $.i18n.prop('dashboard_sgroup');
-        $.i18n.prop('dashboard_eip');
-        $.i18n.prop('dashboard_keypair');
-
-        // instances
-        $.i18n.prop('instance_dialog_term_text');
-        // help
-        $.i18n.prop('page_help_backto_page');
+        ;
       }
     });
   }
