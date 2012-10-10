eac98668a4aa0d13b764c4958a89404217426045
dkavanagh <dkavanagh@gmail.com>
2012-10-10 16:48:58 -0700

    addresses proxy part of EUCA-3686, filters for XSS attack vectors in CLC data

--- server/botojsonencoder.py
+++ server/botojsonencoder.py
@@ -1,6 +1,7 @@
 import boto
 import copy
 import json
+import logging
 
 from json import JSONEncoder
 from json import JSONDecoder
@@ -16,16 +17,31 @@ from boto.ec2.securitygroup import IPPermissions
 from boto.ec2.volume import AttachmentSet
 from .response import ClcError
 from .response import Response
-
+from esapi.codecs.html_entity import HTMLEntityCodec
 
 class BotoJsonEncoder(JSONEncoder):
+    # use this codec directly vs using factory which messes with logging config
+    codec = HTMLEntityCodec()
+    IMMUNE_HTML = ',.-_ '
+    IMMUNE_HTMLATTR = ',.-_'
+    
+    def __sanitize_and_copy__(self, dict):
+        try:
+            ret = copy.copy(dict)
+            for key in ret.keys():
+                if isinstance(ret[key], basestring):
+                    ret[key] = self.codec.encode(self.IMMUNE_HTML, ret[key])
+            return ret
+        except Exception, e:
+            logging.error(e)
+
     def default(self, obj):
         if issubclass(obj.__class__, EC2Object):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = obj.__class__.__name__
             return (values)
         elif isinstance(obj, RegionInfo):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = 'RegionInfo'
             return (values)
         elif isinstance(obj, ClcError):
@@ -35,33 +51,33 @@ class BotoJsonEncoder(JSONEncoder):
         elif isinstance(obj, EC2Connection):
             return []
         elif isinstance(obj, Group):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = 'Group'
             return (values)
         elif isinstance(obj, ConsoleOutput):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = 'ConsoleOutput'
             return (values)
         elif isinstance(obj, ImageAttribute):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = 'ImageAttribute'
             return (values)
         elif isinstance(obj, AttachmentSet):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = 'AttachmentSet'
             return (values)
         elif isinstance(obj, IPPermissions):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             # this is because I found a "parent" property set to self - dak
             values['parent'] = None
             values['__obj_name__'] = 'IPPermissions'
             return (values)
         elif isinstance(obj, GroupOrCIDR):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['__obj_name__'] = 'GroupOrCIDR'
             return (values)
         elif isinstance(obj, BlockDeviceType):
-            values = copy.copy(obj.__dict__)
+            values = self.__sanitize_and_copy__(obj.__dict__)
             values['connection'] = None
             values['__obj_name__'] = 'BlockDeviceType'
             return (values)
