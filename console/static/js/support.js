38e7307bb8549e04b605e62282b4b57bcfe4e2e8
Vasiliy Kochergin <vasya@eucalyptus.com>
2012-10-30 15:13:55 -0700

    Updated key pair and sec group name's max length until EUCA-3785 is fixed

--- console/static/js/support.js
+++ console/static/js/support.js
@@ -38,8 +38,8 @@ var DOM_BINDING = {header:'.euca-container .euca-header-container .inner-contain
                    hidden:'.euca-hidden-container',
                   };
 
-var SGROUP_NAME_PATTERN = new RegExp('^[ A-Za-z0-9_\-]{1,256}$');
-var KEY_PATTERN = new RegExp('^[ A-Za-z0-9_\-]{1,256}$');
+var SGROUP_NAME_PATTERN = new RegExp('^[ A-Za-z0-9_\-]{1,242}$');
+var KEY_PATTERN = new RegExp('^[ A-Za-z0-9_\-]{1,242}$');
 var VOL_ID_PATTERN = new RegExp('^vol-[A-Za-z0-9]{8}$');
 var IP_PATTER = new RegExp('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$');
 var MAX_DESCRIPTION_LEN = 255;
