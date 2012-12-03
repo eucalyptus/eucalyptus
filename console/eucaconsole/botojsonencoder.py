548c04fbd525906ad4707e16bdbc0194769cc618
dkavanagh <dkavanagh@gmail.com>
2012-12-03 14:35:46 -0500

    EUCA-4284, un-white listed console output

--- console/eucaconsole/botojsonencoder.py
+++ console/eucaconsole/botojsonencoder.py
@@ -75,7 +75,6 @@ class BotoJsonEncoder(JSONEncoder):
         'progress',
         'ip_protocol',
         'fingerprint',
-        'output',
     ];
     
     def __sanitize_and_copy__(self, dict):
