f859eef04733c4abadf75d6784049429fb63a30f
chuck <chuck@eucalyptus.com>
2012-12-18 11:05:10 -0800

    Fixes EUCA-3902. Implement template for this file.

--- node/libvirt_tortura.c
+++ node/libvirt_tortura.c
@@ -1,6 +1,3 @@
-// -*- mode: C; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
-// vim: set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
-
 /*************************************************************************
  * Copyright 2009-2012 Eucalyptus Systems, Inc.
  *
@@ -63,10 +60,25 @@
  *   NEEDED TO COMPLY WITH ANY SUCH LICENSES OR RIGHTS.
  ************************************************************************/
 
+// -*- mode: C; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+// vim: set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
+
+//!
+//! @file node/libvirt_tortura.c
+//! Need to provide description
+//!
+
 #define _FILE_OFFSET_BITS 64    // so large-file support works on 32-bit systems
+#define __USE_GNU
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                                  INCLUDES                                  |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#define __USE_GNU
 #include <string.h>             /* strlen, strcpy */
 #include <time.h>
 #include <limits.h>             /* INT_MAX */
@@ -84,158 +96,286 @@
 #include <pwd.h>                /* getpwuid_r */
 #include <libvirt/libvirt.h>
 
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                                  DEFINES                                   |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+#define MAXDOMS                                   100
+#define ITERS                                     300
+#define SITERS                                     20
+#define IITERS                                   1000
+#define THREADS                                    11
+
+#define DUMMY_DISK_FILE                          "libvirt_tortura-dummy-disk"
+#define DUMMY_DISK_SIZE_BYTES                    10000000
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                                  TYPEDEFS                                  |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                                ENUMERATIONS                                |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                                 STRUCTURES                                 |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                             EXTERNAL VARIABLES                             |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+/* Should preferably be handled in header file */
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                             EXPORTED VARIABLES                             |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
 virConnectPtr conn = NULL;
 char uri[] = "qemu:///system";
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                              STATIC VARIABLES                              |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
 static pthread_mutex_t check_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-static virConnectPtr *check_hypervisor_conn()
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                             EXPORTED PROTOTYPES                            |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+int main(int argc, char **argv);
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                              STATIC PROTOTYPES                             |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+static virConnectPtr *check_hypervisor_conn(void);
+static void *checker_thread(void *ptr);
+static void *tortura_thread(void *ptr);
+static void *startup_thread(void *ptr);
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                                   MACROS                                   |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*\
+ |                                                                            |
+ |                               IMPLEMENTATION                               |
+ |                                                                            |
+\*----------------------------------------------------------------------------*/
+
+//!
+//!
+//!
+//! @return a pointer to the LIBVIRT connection structure if successful or NULL otherwize
+//!
+//! @pre
+//!
+//! @note
+//!
+static virConnectPtr *check_hypervisor_conn(void)
 {
-    int rc;
+    int rc = 0;
 
     pthread_mutex_lock(&check_mutex);
+    {
+        if (conn) {
+            if ((rc = virConnectClose(conn)) != 0) {
+                printf("refcount on close was non-zero: %d\n", rc);
+            }
 
-    if (conn) {
-        rc = virConnectClose(conn);
-        if (rc) {
-            printf("refcount on close was non-zero: %d\n", rc);
+            conn = NULL;
+        }
+
+        if ((conn = virConnectOpen(uri)) == NULL) {
+            printf("failed to connect to %s\n", uri);
         }
     }
-    conn = virConnectOpen(uri);
-    if (conn == NULL) {
-        printf("failed to connect to %s\n", uri);
-    }
-
     pthread_mutex_unlock(&check_mutex);
 
-    return &(conn);
+    return (&conn);
 }
 
-#define MAXDOMS 100
-#define ITERS 300
-#define SITERS 20
-#define IITERS 1000
-#define THREADS 11
-
+//!
+//!
+//!
+//! @param[in] ptr
+//!
+//! @return Always returns NULL
+//!
 static void *checker_thread(void *ptr)
 {
-    for (int iter = 0; iter < IITERS; iter++) {
+    int iter = 0;
+
+    for (iter = 0; iter < IITERS; iter++) {
         printf("checker thread starting iteration %d\n", iter);
         check_hypervisor_conn();
     }
 
-    return NULL;
+    return (NULL);
 }
 
+//!
+//!
+//!
+//! @param[in] ptr
+//!
+//! @return Always returns NULL
+//!
 static void *tortura_thread(void *ptr)
 {
-    long long tid = *(long long *)ptr;
+    int i = 0;
+    int iter = 0;
+    int error = 0;
+    int num_doms = 0;
+    int dom_ids[MAXDOMS] = { 0 };
+    long long tid = (*(long long *) ptr);
+    virDomainPtr dom[MAXDOMS] = { NULL };
+    virDomainInfo info[MAXDOMS] = { 0 };
 
     check_hypervisor_conn();
-    for (int iter = 0; iter < ITERS; iter++) {
+    for (iter = 0; iter < ITERS; iter++) {
         printf("thread %lld starting iteration %d\n", tid, iter);
-        {
-            int dom_ids[MAXDOMS];
-            virDomainPtr dom[MAXDOMS];
-            virDomainInfo info[MAXDOMS];
 
+        pthread_mutex_lock(&check_mutex);
+        {
+            num_doms = virConnectListDomains(conn, dom_ids, MAXDOMS);
+        }
+        pthread_mutex_unlock(&check_mutex);
+
+        for (i = 0; i < num_doms; i++) {
             pthread_mutex_lock(&check_mutex);
-            int num_doms = virConnectListDomains(conn, dom_ids, MAXDOMS);
+            {
+                dom[i] = virDomainLookupByID(conn, dom_ids[i]);
+            }
             pthread_mutex_unlock(&check_mutex);
 
-            for (int i = 0; i < num_doms; i++) {
-                pthread_mutex_lock(&check_mutex);
-                dom[i] = virDomainLookupByID(conn, dom_ids[i]);
-                pthread_mutex_unlock(&check_mutex);
-                if (!dom[i]) {
-                    printf("failed to look up domain %d\n", dom_ids[i]);
-                    continue;
-                }
+            if (!dom[i]) {
+                printf("failed to look up domain %d\n", dom_ids[i]);
+                continue;
             }
+        }
 
-            for (int i = 0; i < num_doms; i++) {
-                pthread_mutex_lock(&check_mutex);
-                int error = virDomainGetInfo(dom[i], &info[i]);
-                pthread_mutex_unlock(&check_mutex);
-                if (error < 0) {
-                    printf("failed to get info on domain %d\n", dom_ids[i]);
-                    continue;
-                }
-
+        for (i = 0; i < num_doms; i++) {
+            pthread_mutex_lock(&check_mutex);
+            {
+                error = virDomainGetInfo(dom[i], &info[i]);
             }
+            pthread_mutex_unlock(&check_mutex);
 
-            for (int i = 0; i < num_doms; i++) {
-                pthread_mutex_lock(&check_mutex);
-                int error = virDomainFree(dom[i]);
-                pthread_mutex_unlock(&check_mutex);
-                if (error < 0) {
-                    printf("failed to close domain %d\n", dom_ids[i]);
-                    continue;
-                }
+            if (error < 0) {
+                printf("failed to get info on domain %d\n", dom_ids[i]);
+                continue;
+            }
+        }
+
+        for (i = 0; i < num_doms; i++) {
+            pthread_mutex_lock(&check_mutex);
+            {
+                error = virDomainFree(dom[i]);
+            }
+            pthread_mutex_unlock(&check_mutex);
+
+            if (error < 0) {
+                printf("failed to close domain %d\n", dom_ids[i]);
+                continue;
             }
         }
     }
 
-    return NULL;
+    return (NULL);
 }
 
-#define DUMMY_DISK_FILE "libvirt_tortura-dummy-disk"
-#define DUMMY_DISK_SIZE_BYTES 10000000
-
+//!
+//!
+//!
+//! @param[in] ptr
+//!
+//! @return Always returns NULL
+//!
 static void *startup_thread(void *ptr)
 {
-    long long tid = *(long long *)ptr;
-    char file_name[1024];
+    int fd = 0;
+    int iter = 0;
+    long long tid = (*(long long *) ptr);
+    virDomainPtr dom = NULL;
+    char file_name[1024] = { 0 };
+    char xml[4096] = { 0 };
+    char *xml_template =
+        "<?xml version='1.0' encoding='UTF-8'?>"
+        "<domain type='kvm'>"
+        "  <name>tortura-%04lld</name>"
+        "  <description>Eucalyptus instance i-XXX</description>"
+        "  <os>"
+        "    <type>hvm</type>"
+        "  </os>"
+        "  <features>"
+        "    <acpi/>"
+        "  </features>"
+        "  <clock offset='localtime'/>"
+        "  <on_poweroff>destroy</on_poweroff>"
+        "  <on_reboot>restart</on_reboot>"
+        "  <on_crash>destroy</on_crash>"
+        "  <vcpu>1</vcpu>"
+        "  <memory>52428</memory>"
+        "  <devices>"
+        "    <disk device='disk'>" "      <source file='%s'/>" "      <target bus='virtio' dev='vda'/>" "    </disk>" "  </devices>" "</domain>";
 
     snprintf(file_name, sizeof(file_name), "%s/%s-%lld", get_current_dir_name(), DUMMY_DISK_FILE, tid);
 
     umask(0000);
-    int fd = open(file_name, O_RDWR | O_CREAT | O_TRUNC, 0666);
-    if (fd < 0) {
+    if ((fd = open(file_name, O_RDWR | O_CREAT | O_TRUNC, 0666)) < 0) {
         printf("failed to create %s\n", DUMMY_DISK_FILE);
         perror("libvirt_tortura");
         exit(1);
     }
-    if (lseek(fd, DUMMY_DISK_SIZE_BYTES, SEEK_SET) == (off_t) - 1) {
+
+    if (lseek(fd, DUMMY_DISK_SIZE_BYTES, SEEK_SET) == ((off_t) -1)) {
         printf("failed to seek in %s\n", DUMMY_DISK_FILE);
         perror("libvirt_tortura");
         exit(1);
     }
+
     if (write(fd, "x", 1) != 1) {
         printf("failed to write to %s\n", DUMMY_DISK_FILE);
         perror("libvirt_tortura");
         exit(1);
     }
+
     close(fd);
+
     sync();
 
     check_hypervisor_conn();
-    for (int iter = 0; iter < SITERS; iter++) {
+    for (iter = 0; iter < SITERS; iter++) {
         printf("startup thread %lld starting iteration %d\n", tid, iter);
-
-        char xml[4096];
-        char *xml_template =
-            "<?xml version='1.0' encoding='UTF-8'?>"
-            "<domain type='kvm'>"
-            "  <name>tortura-%04lld</name>"
-            "  <description>Eucalyptus instance i-XXX</description>"
-            "  <os>"
-            "    <type>hvm</type>"
-            "  </os>"
-            "  <features>"
-            "    <acpi/>"
-            "  </features>"
-            "  <clock offset='localtime'/>"
-            "  <on_poweroff>destroy</on_poweroff>"
-            "  <on_reboot>restart</on_reboot>"
-            "  <on_crash>destroy</on_crash>"
-            "  <vcpu>1</vcpu>"
-            "  <memory>52428</memory>"
-            "  <devices>"
-            "    <disk device='disk'>" "      <source file='%s'/>" "      <target bus='virtio' dev='vda'/>" "    </disk>" "  </devices>" "</domain>";
         snprintf(xml, sizeof(xml), xml_template, tid, file_name);
 
         pthread_mutex_lock(&check_mutex);
-        virDomainPtr dom = virDomainCreateLinux(conn, xml, 0);
+        {
+            dom = virDomainCreateLinux(conn, xml, 0);
+        }
         pthread_mutex_unlock(&check_mutex);
 
         sleep(3);
@@ -246,37 +386,53 @@ static void *startup_thread(void *ptr)
         }
 
         pthread_mutex_lock(&check_mutex);
-        virDomainDestroy(dom);
-        virDomainFree(dom);
+        {
+            virDomainDestroy(dom);
+            virDomainFree(dom);
+            dom = NULL;
+        }
         pthread_mutex_unlock(&check_mutex);
     }
 
     unlink(file_name);
 
-    return NULL;
+    return (NULL);
 }
 
+//!
+//! Main entry point of the application
+//!
+//! @param[in] argc the number of parameter passed on the command line
+//! @param[in] argv the list of arguments
+//!
+//! @return EUCA_OK on success or EUCA_ERROR on failure.
+//!
 int main(int argc, char **argv)
 {
-    printf("spawning %d competing threads\n", THREADS);
-    pthread_t threads[THREADS];
-    long long thread_par[THREADS];
+    int j = 0;
     int thread_par_sum = 0;
-    for (int j = 0; j < THREADS; j++) {
+    long long thread_par[THREADS] = { 0 };
+    pthread_t threads[THREADS] = { 0 };
+
+    printf("spawning %d competing threads\n", THREADS);
+
+    for (j = 0; j < THREADS; j++) {
         thread_par[j] = j;
         if (j == 0) {
             pthread_create(&threads[j], NULL, checker_thread, (void *)&thread_par[j]);
         } else {
-            if (j % 2 == 0) {
+            if ((j % 2) == 0) {
                 pthread_create(&threads[j], NULL, tortura_thread, (void *)&thread_par[j]);
             } else {
                 pthread_create(&threads[j], NULL, startup_thread, (void *)&thread_par[j]);
             }
         }
     }
-    for (int j = 0; j < THREADS; j++) {
+
+    for (j = 0; j < THREADS; j++) {
         pthread_join(threads[j], NULL);
         thread_par_sum += (int)thread_par[j];
     }
+
     printf("waited for all competing threads\n");
 }
