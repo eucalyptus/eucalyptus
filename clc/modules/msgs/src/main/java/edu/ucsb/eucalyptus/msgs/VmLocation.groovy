681e966e020a28eb9fe1209299fe675dd8b3668f
chris grzegorczyk <grze@eucalyptus.com>
2013-02-01 08:28:18 -0800

    cleaned up evac frontend types, fixed client handling, added binding ref, fixed WSDL def

--- clc/modules/msgs/src/main/java/edu/ucsb/eucalyptus/msgs/VmLocation.groovy
+++ clc/modules/msgs/src/main/java/edu/ucsb/eucalyptus/msgs/VmLocation.groovy
@@ -84,10 +84,14 @@ public class EvacuateNodeType extends CloudTopologyMessage {
   String host;
 
   public String getServiceTag( ) {
-    return "http://" + host + ":8775/services/axis2/EucalyptusNC";//construct bullshit service tag
+    return "http://" + this.getHost() + ":8775/axis2/services/EucalyptusNC";//construct bullshit service tag
   }
 }
 public class EvacuateNodeResponseType extends CloudTopologyMessage {}
+public class MigrateInstancesType extends CloudTopologyMessage {
+  String host;
+}
+public class MigrateInstancesResponseType extends CloudTopologyMessage {}
 /** *******************************************************************************/
 public class DescribeAvailabilityZonesType extends CloudTopologyMessage {
   //** added 2008-02-01  **/
