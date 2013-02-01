f8c9461114860ea3738e1b637d38f8ca3acd7301
Evan Thomas <ethomas@eucalyptus.com>
2013-01-31 16:10:35 -0800

    EUCA-4801 created entity for list-metrics in CloudWatch, some unit tests as well

--- clc/modules/cloudwatch/src/main/java/com/eucalyptus/cloudwatch/CloudWatchService.java
+++ clc/modules/cloudwatch/src/main/java/com/eucalyptus/cloudwatch/CloudWatchService.java
@@ -1,31 +1,28 @@
 package com.eucalyptus.cloudwatch;
 
-import com.eucalyptus.cloudwatch.DeleteAlarmsResponseType;
-import com.eucalyptus.cloudwatch.DeleteAlarmsType;
-import com.eucalyptus.cloudwatch.DescribeAlarmHistoryResponseType;
-import com.eucalyptus.cloudwatch.DescribeAlarmHistoryType;
-import com.eucalyptus.cloudwatch.DescribeAlarmsForMetricResponseType;
-import com.eucalyptus.cloudwatch.DescribeAlarmsForMetricType;
-import com.eucalyptus.cloudwatch.DescribeAlarmsResponseType;
-import com.eucalyptus.cloudwatch.DescribeAlarmsType;
-import com.eucalyptus.cloudwatch.DisableAlarmActionsResponseType;
-import com.eucalyptus.cloudwatch.DisableAlarmActionsType;
-import com.eucalyptus.cloudwatch.EnableAlarmActionsResponseType;
-import com.eucalyptus.cloudwatch.EnableAlarmActionsType;
-import com.eucalyptus.cloudwatch.GetMetricStatisticsResponseType;
-import com.eucalyptus.cloudwatch.GetMetricStatisticsType;
-import com.eucalyptus.cloudwatch.ListMetricsResponseType;
-import com.eucalyptus.cloudwatch.ListMetricsType;
-import com.eucalyptus.cloudwatch.PutMetricAlarmResponseType;
-import com.eucalyptus.cloudwatch.PutMetricAlarmType;
-import com.eucalyptus.cloudwatch.PutMetricDataResponseType;
-import com.eucalyptus.cloudwatch.PutMetricDataType;
-import com.eucalyptus.cloudwatch.SetAlarmStateResponseType;
-import com.eucalyptus.cloudwatch.SetAlarmStateType;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import org.apache.log4j.Logger;
+
+import com.eucalyptus.cloudwatch.domain.listmetrics.ListMetric;
+import com.eucalyptus.cloudwatch.domain.listmetrics.ListMetricDimension;
+import com.eucalyptus.cloudwatch.domain.listmetrics.ListMetricManager;
+import com.eucalyptus.context.Context;
+import com.eucalyptus.context.Contexts;
 import com.eucalyptus.util.EucalyptusCloudException;
+import com.eucalyptus.util.OwnerFullName;
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 
 public class CloudWatchService {
+  private static final Logger LOG = Logger.getLogger(CloudWatchService.class);
   public PutMetricAlarmResponseType putMetricAlarm(PutMetricAlarmType request) throws EucalyptusCloudException {
     PutMetricAlarmResponseType reply = request.getReply( );
     return reply;
@@ -38,7 +35,67 @@ public class CloudWatchService {
 
   public ListMetricsResponseType listMetrics(ListMetricsType request) throws EucalyptusCloudException {
     ListMetricsResponseType reply = request.getReply( );
+
+    final Context ctx = Contexts.lookup( );
+    final OwnerFullName ownerFullName = ctx.getUserFullName();
+    final String namespace = request.getNamespace();
+    final String metricName = request.getMetricName();
+    final Map<String, String> dimensionMap = transform(request.getDimensions());
+    
+    // take all stats updated after two weeks ago
+    final Date after = new Date(System.currentTimeMillis() - 2 * 7 * 24 * 60 * 60 * 1000L);
+    final Date before = null; // no bound on time before stats are updated  (though maybe 'now') 
+    
+    final Collection<ListMetric> results = ListMetricManager.listMetrics(ownerFullName.getAccountNumber(), 
+        metricName, 
+        namespace, 
+        dimensionMap, 
+        after, 
+        before);
+
+    final Metrics metrics = new Metrics();
+    metrics.setMember(Lists.newArrayList(Collections2.<ListMetric, Metric>transform(results, ListMetricFunction.INSTANCE)));
+    final ListMetricsResult listMetricsResult = new ListMetricsResult();
+    listMetricsResult.setMetrics(metrics);
+    reply.setListMetricsResult(listMetricsResult);
     return reply;
+  }
+
+  private enum ListMetricFunction implements Function<ListMetric, Metric> {
+    INSTANCE {
+      @Override
+      public Metric apply(@Nullable ListMetric listMetric) {
+        Metric metric = new Metric();
+        metric.setMetricName(listMetric.getMetricName());
+        metric.setNamespace(listMetric.getNamespace());
+        Dimensions dimensions = new Dimensions();
+        dimensions.setMember(Lists.newArrayList(Collections2.<ListMetricDimension, Dimension>transform(listMetric.getDimensions(), ListMetricDimensionFunction.INSTANCE)));
+        metric.setDimensions(dimensions);
+        return metric;
+      }
+    }
+  }
+  private enum ListMetricDimensionFunction implements Function<ListMetricDimension, Dimension> {
+    INSTANCE {
+      @Override
+      public Dimension apply(@Nullable ListMetricDimension listMetricDimension) {
+        Dimension dimension = new Dimension();
+        dimension.setName(listMetricDimension.getName());
+        dimension.setValue(listMetricDimension.getValue());
+        return dimension;
+      }
+    }
+  }
+
+  
+  private Map<String, String> transform(DimensionFilters dimensionFilters) {
+    final Map<String, String> result = Maps.newHashMap();
+    if (dimensionFilters != null && dimensionFilters.getMember() != null) {
+      for (DimensionFilter dimensionFilter: dimensionFilters.getMember()) {
+        result.put(dimensionFilter.getName(), dimensionFilter.getValue());
+      }
+    }
+    return result;
   }
 
   public GetMetricStatisticsResponseType getMetricStatistics(GetMetricStatisticsType request) throws EucalyptusCloudException {
