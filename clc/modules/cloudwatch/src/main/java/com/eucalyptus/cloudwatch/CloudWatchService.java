09a7a6d4a23d5b2acd9cd064ae7e8435a0aa9c70
Ken Edwards <ken.edwards@eucalyptus.com>
2013-02-08 16:50:00 -0800

    added IAM support for each cloud watch action type and cloud watch url to eucarc

--- clc/modules/cloudwatch/src/main/java/com/eucalyptus/cloudwatch/CloudWatchService.java
+++ clc/modules/cloudwatch/src/main/java/com/eucalyptus/cloudwatch/CloudWatchService.java
@@ -9,12 +9,15 @@ import javax.annotation.Nullable;
 import org.apache.log4j.Logger;
 
 import com.eucalyptus.cloudwatch.domain.dimension.DimensionEntity;
+import com.eucalyptus.auth.Permissions;
+import com.eucalyptus.auth.policy.PolicySpec;
 import com.eucalyptus.cloudwatch.domain.listmetrics.ListMetric;
 import com.eucalyptus.cloudwatch.domain.listmetrics.ListMetricManager;
 import com.eucalyptus.context.Context;
 import com.eucalyptus.context.Contexts;
 import com.eucalyptus.util.EucalyptusCloudException;
 import com.eucalyptus.util.OwnerFullName;
+import com.eucalyptus.util.RestrictedTypes;
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
@@ -25,18 +28,39 @@ public class CloudWatchService {
   private static final Logger LOG = Logger.getLogger(CloudWatchService.class);
   public PutMetricAlarmResponseType putMetricAlarm(PutMetricAlarmType request) throws EucalyptusCloudException {
     PutMetricAlarmResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_PUTMETRICALARM, ctx)) {
+   	throw new EucalyptusCloudException();
+    }
+    
     return reply;
   }
 
   public PutMetricDataResponseType putMetricData(PutMetricDataType request) throws EucalyptusCloudException {
     PutMetricDataResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_PUTMETRICDATA, ctx)) {
+   	throw new EucalyptusCloudException();
+    }
+    
+    final OwnerFullName ownerFullName = ctx.getUserFullName();
+    
     return reply;
   }
 
   public ListMetricsResponseType listMetrics(ListMetricsType request) throws EucalyptusCloudException {
     ListMetricsResponseType reply = request.getReply( );
-
     final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_LISTMETRICS, ctx)) {
+   	throw new EucalyptusCloudException();
+    }
+    
     final OwnerFullName ownerFullName = ctx.getUserFullName();
     final String namespace = request.getNamespace();
     final String metricName = request.getMetricName();
@@ -100,42 +124,103 @@ public class CloudWatchService {
 
   public GetMetricStatisticsResponseType getMetricStatistics(GetMetricStatisticsType request) throws EucalyptusCloudException {
     GetMetricStatisticsResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_GETMETRICSTATISTICS, ctx)) {
+   	throw new EucalyptusCloudException();
+    }
+  
     return reply;
   }
 
   public DisableAlarmActionsResponseType disableAlarmActions(DisableAlarmActionsType request) throws EucalyptusCloudException {
     DisableAlarmActionsResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_DISABLEALARMACTIONS, ctx)) {
+   	throw new EucalyptusCloudException();
+    }
+  
     return reply;
   }
 
   public DescribeAlarmsResponseType describeAlarms(DescribeAlarmsType request) throws EucalyptusCloudException {
     DescribeAlarmsResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_DESCRIBEALARMS, ctx)) {
+   	throw new EucalyptusCloudException();
+    }
+    
     return reply;
   }
 
   public DescribeAlarmsForMetricResponseType describeAlarmsForMetric(DescribeAlarmsForMetricType request) throws EucalyptusCloudException {
     DescribeAlarmsForMetricResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_DESCRIBEALARMSFORMETRIC, ctx)) {
+	throw new EucalyptusCloudException();
+    }
+    
     return reply;
   }
 
   public DescribeAlarmHistoryResponseType describeAlarmHistory(DescribeAlarmHistoryType request) throws EucalyptusCloudException {
     DescribeAlarmHistoryResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_DESCRIBEALARMHISTORY, ctx)) {
+	throw new EucalyptusCloudException();
+    }
+   
     return reply;
   }
 
   public EnableAlarmActionsResponseType enableAlarmActions(EnableAlarmActionsType request) throws EucalyptusCloudException {
     EnableAlarmActionsResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_ENABLEALARMACTIONS, ctx)) {
+	throw new EucalyptusCloudException();
+    }
+    
     return reply;
   }
 
   public DeleteAlarmsResponseType deleteAlarms(DeleteAlarmsType request) throws EucalyptusCloudException {
     DeleteAlarmsResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_DELETEALARMS, ctx)) {
+	throw new EucalyptusCloudException();
+    }
+	
     return reply;
   }
 
   public SetAlarmStateResponseType setAlarmState(SetAlarmStateType request) throws EucalyptusCloudException {
     SetAlarmStateResponseType reply = request.getReply( );
+    final Context ctx = Contexts.lookup( );
+    
+    //IAM Action Check
+    if(!hasActionPermission(PolicySpec.CLOUDWATCH_SETALARMSTATE, ctx)) {
+	throw new EucalyptusCloudException();
+    }
+	
     return reply;
   }
-
+  
+  private Boolean hasActionPermission(final String actionType, final Context ctx) {
+	return Permissions.isAuthorized(PolicySpec.VENDOR_CLOUDWATCH,
+		actionType, "", ctx.getAccount(), actionType, ctx.getUser());
+  }
+  
 }
