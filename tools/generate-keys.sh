cbd58000761dfb0f6f3f4d0145e3a597434d3cf4
Jeff Uphoff <juphoff@eucalyptus.com>
2013-02-21 09:20:44 -0800

    change to generate keys on NC rather than CC

--- tools/generate-keys.sh
+++ tools/generate-keys.sh
@@ -2,20 +2,27 @@
 
 # Generates keys for TLS migration of instances between NC nodes.
 #
-# Run on CC.
+# Run on NC with two arguments:
+#
+# 1. IP address of this host.
+# 2. Token unique to this migration.
 
 # Bail on error
 set -e
+
+LIBVIRTPID=/var/run/libvirtd.pid
 
 KEYDIR=$EUCALYPTUS/var/lib/eucalyptus/keys
 NCKEY=node-pk.pem
 NCCERT=node-cert.pem
 
-if [ $# -gt 0 ] ; then
+if [ $# -gt 1 ] ; then
 	NCADDR=$1
 	shift
+        MIGTOK=$1
+        shift
 else
-	echo "Usage $0 NC-address" >&2
+	echo "Usage $0 NC-address migration-token" >&2
 	exit 1
 fi
 
@@ -53,27 +60,34 @@ certtool --generate-certificate --load-privkey $WORKD/serverkey.pem --load-ca-ce
 certtool --generate-privkey > $WORKD/clientkey.pem
 
 # Create the client template.
+# I'm using the 'locality' field to hold the migration-specific
+# credential/token for now. This will probably change.
+#
 cat > $WORKD/client.info <<EOT
 country = US
-state = CA
-locality = Goleta
+locality = $MIGTOK
 organization = $NCORG
 cn = $NCADDR
 tls_www_client
 encryption_key
 signing_key
 EOT
+# state = CA
 
 certtool --generate-certificate --load-privkey $WORKD/clientkey.pem --load-ca-certificate $KEYDIR/$NCCERT --load-ca-privkey $KEYDIR/$NCKEY --template $WORKD/client.info --outfile $WORKD/clientcert.pem 2> $WORKD/clientcert.txt
 
 echo "Key generation complete."
 echo "Installing keys ..."
 
-ssh $NCADDR mkdir -p /etc/pki/libvirt/private
-ssh $NCADDR chown root.root /etc/pki/libvirt/private
-scp $KEYDIR/$NCCERT $NCADDR:/etc/pki/CA/cacert.pem
-scp $WORKD/clientcert.pem $WORKD/servercert.pem $NCADDR:/etc/pki/libvirt/
-scp $WORKD/clientkey.pem $WORKD/serverkey.pem $NCADDR:/etc/pki/libvirt/private/
-ssh $NCADDR chmod 600 /etc/pki/libvirt/private/serverkey.pem
+mkdir -p /etc/pki/libvirt/private
+chown root.root /etc/pki/libvirt/private
+cp $KEYDIR/$NCCERT /etc/pki/CA/cacert.pem
+cp $WORKD/clientcert.pem $WORKD/servercert.pem /etc/pki/libvirt/
+cp $WORKD/clientkey.pem $WORKD/serverkey.pem /etc/pki/libvirt/private/
+chmod 600 /etc/pki/libvirt/private/serverkey.pem
 
-echo "Key installation complete."
+echo "Key installation complete, sending SIGHUP to libvirtd"
+
+kill -HUP `cat $LIBVIRTPID`
+
+echo "Done."
