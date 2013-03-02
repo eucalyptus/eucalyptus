20797c115ffe08707c4da89d8eb5c12c7e6f4e68
Neil Soman <neil@eucalyptus.com>
2013-03-02 12:13:24 -0800

    Uncaught exception in AbstractConfigurableProperty causes euca-describe-properties to return a RuntimeException

--- clc/modules/msgs/src/main/java/com/eucalyptus/configurable/AbstractConfigurableProperty.java
+++ clc/modules/msgs/src/main/java/com/eucalyptus/configurable/AbstractConfigurableProperty.java
@@ -71,6 +71,7 @@ import java.util.NoSuchElementException;
 
 import javax.persistence.EntityTransaction;
 
+import org.apache.activemq.store.jdbc.adapter.TransactDatabaseLocker;
 import org.apache.log4j.Logger;
 import com.eucalyptus.configurable.PropertyDirectory.NoopEventListener;
 import com.eucalyptus.entities.Entities;
@@ -78,200 +79,210 @@ import com.eucalyptus.entities.EntityWrapper;
 import com.eucalyptus.records.Logs;
 
 public abstract class AbstractConfigurableProperty implements ConfigurableProperty {
-  
-  private static Logger                LOG = Logger.getLogger( AbstractConfigurableProperty.class );
-  private final String                 entrySetName;
-  private final String                 fieldName;
-  private final String                 qualifiedName;
-  private final String                 description;
-  private final PropertyTypeParser     typeParser;
-  private final String                 defaultValue;
-  private final Class                  definingClass;
-  private final Constructor            noArgConstructor;
-  private final Boolean                readOnly;
-  private final String                 displayName;
-  private final ConfigurableFieldType  widgetType;
-  private final String                 alias;
-  private final PropertyChangeListener changeListener;
-  private final Field                  field;
-  private final Method                 getter;
-  private final Method                 setter;
-  private final Class[]                setArgs;
-  private final boolean deferred;
-  
-  public AbstractConfigurableProperty( Class definingClass, String entrySetName, Field field, String defaultValue, String description,
-                                       PropertyTypeParser typeParser, Boolean readOnly, String displayName, ConfigurableFieldType widgetType, String alias ) {
-    this( definingClass, entrySetName, field, defaultValue, description, typeParser, readOnly, displayName, widgetType, alias,
-          NoopEventListener.NOOP );
-  }
-  
-  public AbstractConfigurableProperty( Class definingClass, String entrySetName, Field field, String defaultValue, String description,
-                                       PropertyTypeParser typeParser, Boolean readOnly, String displayName, ConfigurableFieldType widgetType, String alias,
-                                       PropertyChangeListener changeListener ) {
-    this.definingClass = definingClass;
-    this.field = field;
-    this.fieldName = this.field.getName( ).toLowerCase( );
-    this.entrySetName = entrySetName.toLowerCase( );
-    this.qualifiedName = this.entrySetName + "." + this.fieldName;
-    this.description = description;
-    this.typeParser = typeParser;
-    this.defaultValue = defaultValue;
-    this.readOnly = readOnly;
-    this.displayName = displayName;
-    this.widgetType = widgetType;
-    this.alias = alias;
-    this.changeListener = changeListener;
-    Constructor cons = null;
-    try {
-      cons = this.definingClass.getConstructor( new Class[] {} );
-      cons.setAccessible( true );
-    } catch ( Exception ex ) {
-      if ( !Modifier.isStatic( field.getModifiers( ) ) ) {
-        LOG.debug( "Known declared constructors: " + this.getDefiningClass( ).getDeclaredConstructors( ) );
-        LOG.debug( "Known constructors: " + this.getDefiningClass( ).getConstructors( ) );
-        LOG.debug( ex, ex );
-        throw new RuntimeException( ex );
-      } else {
-        //that a default no-arg constructor is required indicates there is too much specialized junk in here.
-      }
-    }
-    this.noArgConstructor = cons;
-    this.setArgs = new Class[] { this.field.getType( ) };
-    this.getter = this.getReflectedMethod( "get", this.field );
-    this.setter = this.getReflectedMethod( "set", this.field, this.setArgs );
-    ConfigurableClass configurableAnnot = ( ConfigurableClass ) definingClass.getAnnotation( ConfigurableClass.class );
-    this.deferred = configurableAnnot.deferred( );
-  }
-  
-  private Method getReflectedMethod( String namePrefix, Field field, Class... setArgs2 ) {
-    try {
-      String name = namePrefix + this.field.getName( ).substring( 0, 1 ).toUpperCase( ) + this.field.getName( ).substring( 1 );
-      Method m = definingClass.getDeclaredMethod( name, setArgs2 );
-      m.setAccessible( true );
-      return m;
-    } catch ( Exception e ) {
-      if( !Modifier.isStatic( field.getModifiers( ) ) ) {
-        LOG.debug( "Known declared methods: " + this.getDefiningClass( ).getDeclaredMethods( ) );
-        LOG.debug( "Known methods: " + this.getDefiningClass( ).getMethods( ) );
-        LOG.debug( e, e );
-      }
-      return null;
-    }
-  }
-  
-  protected abstract Object getQueryObject( ) throws Exception;
-  
-  public String getFieldName( ) {
-    return this.fieldName;
-  }
-  
-  public String getEntrySetName( ) {
-    return this.entrySetName;
-  }
-  
-  public String getQualifiedName( ) {
-    return this.qualifiedName;
-  }
-  
-  public String getDescription( ) {
-    return this.description;
-  }
-  
-  public PropertyTypeParser getTypeParser( ) {
-    return this.typeParser;
-  }
-  
-  public String getDefaultValue( ) {
-    return this.defaultValue;
-  }
-  
-  public String getValue( ) {
-    EntityTransaction trans = Entities.get( this.getDefiningClass( ) );
-    try {
-    	//Unique result gets first found value if multiple exist, should work if all are kept in sync
-      Object o = Entities.uniqueResult( this.getQueryObject( ) );
-      Object prop = this.getter.invoke( o );
-      String result = prop != null
-        ? prop.toString( )
-        : "<unset>";
-      trans.commit( );
-      return result;
-    } catch ( Exception e ) {
-      Logs.exhaust( ).error( e, e );
-      trans.rollback( );
-      return "<unset>";
-    }
-  }
-  
-  public String setValue( String s ) {
-  	EntityTransaction trans = Entities.get(this.getDefiningClass());
-    try {
-    	//This should return all matching objects
-      List<Object> resultList = Entities.query( this.getQueryObject( ) );
-      Object prop = this.getTypeParser( ).apply( s );
-      
-      if(resultList == null || resultList.size() == 0) {
-      	throw new NoSuchElementException("no entities found for property");
-      }
-      
-      this.fireChange( prop ); //Fire change only once
-      LOG.debug("Running setters.");
-      
-      for(Object obj : resultList) {      	
-      	this.setter.invoke( obj, prop );
-      }
-      trans.commit( );
-      return s;
-    } catch ( Exception e ) {
-      Logs.exhaust( ).error( e, e );
-      trans.rollback( );
-      return "Error: " + e.getMessage( );
-    }
-  }
-  
-  public void resetValue( ) {
-    this.setValue( this.defaultValue );
-  }
-  
-  public Class getDefiningClass( ) {
-    return this.definingClass;
-  }
-  
-  public Constructor getNoArgConstructor( ) {
-    return this.noArgConstructor;
-  }
-  
-  public String getDisplayName( ) {
-    return this.displayName;
-  }
-  
-  public ConfigurableFieldType getWidgetType( ) {
-    return this.widgetType;
-  }
-  
-  public String getAlias( ) {
-    return this.alias;
-  }
-  
-  protected void fireChange( Object newValue ) throws ConfigurablePropertyException {
-    if ( !NoopEventListener.class.equals( this.changeListener.getClass( ) ) ) {
-      this.changeListener.fireChange( this, newValue );
-    }
-  }
-  
-  public Boolean getReadOnly( ) {
-    return this.readOnly;
-  }
-  
-  public PropertyChangeListener getChangeListener( ) {
-    return this.changeListener;
-  }
-  
-  public Field getField( ) {
-    return this.field;
-  }
 
-  public boolean isDeferred( ) {
-    return this.deferred;
-  }
+	private static Logger                LOG = Logger.getLogger( AbstractConfigurableProperty.class );
+	private final String                 entrySetName;
+	private final String                 fieldName;
+	private final String                 qualifiedName;
+	private final String                 description;
+	private final PropertyTypeParser     typeParser;
+	private final String                 defaultValue;
+	private final Class                  definingClass;
+	private final Constructor            noArgConstructor;
+	private final Boolean                readOnly;
+	private final String                 displayName;
+	private final ConfigurableFieldType  widgetType;
+	private final String                 alias;
+	private final PropertyChangeListener changeListener;
+	private final Field                  field;
+	private final Method                 getter;
+	private final Method                 setter;
+	private final Class[]                setArgs;
+	private final boolean deferred;
+
+	public AbstractConfigurableProperty( Class definingClass, String entrySetName, Field field, String defaultValue, String description,
+			PropertyTypeParser typeParser, Boolean readOnly, String displayName, ConfigurableFieldType widgetType, String alias ) {
+		this( definingClass, entrySetName, field, defaultValue, description, typeParser, readOnly, displayName, widgetType, alias,
+				NoopEventListener.NOOP );
+	}
+
+	public AbstractConfigurableProperty( Class definingClass, String entrySetName, Field field, String defaultValue, String description,
+			PropertyTypeParser typeParser, Boolean readOnly, String displayName, ConfigurableFieldType widgetType, String alias,
+			PropertyChangeListener changeListener ) {
+		this.definingClass = definingClass;
+		this.field = field;
+		this.fieldName = this.field.getName( ).toLowerCase( );
+		this.entrySetName = entrySetName.toLowerCase( );
+		this.qualifiedName = this.entrySetName + "." + this.fieldName;
+		this.description = description;
+		this.typeParser = typeParser;
+		this.defaultValue = defaultValue;
+		this.readOnly = readOnly;
+		this.displayName = displayName;
+		this.widgetType = widgetType;
+		this.alias = alias;
+		this.changeListener = changeListener;
+		Constructor cons = null;
+		try {
+			cons = this.definingClass.getConstructor( new Class[] {} );
+			cons.setAccessible( true );
+		} catch ( Exception ex ) {
+			if ( !Modifier.isStatic( field.getModifiers( ) ) ) {
+				LOG.debug( "Known declared constructors: " + this.getDefiningClass( ).getDeclaredConstructors( ) );
+				LOG.debug( "Known constructors: " + this.getDefiningClass( ).getConstructors( ) );
+				LOG.debug( ex, ex );
+				throw new RuntimeException( ex );
+			} else {
+				//that a default no-arg constructor is required indicates there is too much specialized junk in here.
+			}
+		}
+		this.noArgConstructor = cons;
+		this.setArgs = new Class[] { this.field.getType( ) };
+		this.getter = this.getReflectedMethod( "get", this.field );
+		this.setter = this.getReflectedMethod( "set", this.field, this.setArgs );
+		ConfigurableClass configurableAnnot = ( ConfigurableClass ) definingClass.getAnnotation( ConfigurableClass.class );
+		this.deferred = configurableAnnot.deferred( );
+	}
+
+	private Method getReflectedMethod( String namePrefix, Field field, Class... setArgs2 ) {
+		try {
+			String name = namePrefix + this.field.getName( ).substring( 0, 1 ).toUpperCase( ) + this.field.getName( ).substring( 1 );
+			Method m = definingClass.getDeclaredMethod( name, setArgs2 );
+			m.setAccessible( true );
+			return m;
+		} catch ( Exception e ) {
+			if( !Modifier.isStatic( field.getModifiers( ) ) ) {
+				LOG.debug( "Known declared methods: " + this.getDefiningClass( ).getDeclaredMethods( ) );
+				LOG.debug( "Known methods: " + this.getDefiningClass( ).getMethods( ) );
+				LOG.debug( e, e );
+			}
+			return null;
+		}
+	}
+
+	protected abstract Object getQueryObject( ) throws Exception;
+
+	public String getFieldName( ) {
+		return this.fieldName;
+	}
+
+	public String getEntrySetName( ) {
+		return this.entrySetName;
+	}
+
+	public String getQualifiedName( ) {
+		return this.qualifiedName;
+	}
+
+	public String getDescription( ) {
+		return this.description;
+	}
+
+	public PropertyTypeParser getTypeParser( ) {
+		return this.typeParser;
+	}
+
+	public String getDefaultValue( ) {
+		return this.defaultValue;
+	}
+
+	public String getValue( ) {	  
+		try {
+			EntityTransaction trans = Entities.get( this.getDefiningClass( ) );
+			try {
+				//Unique result gets first found value if multiple exist, should work if all are kept in sync
+				Object o = Entities.uniqueResult( this.getQueryObject( ) );
+				Object prop = this.getter.invoke( o );
+				String result = prop != null
+						? prop.toString( )
+								: "<unset>";
+						trans.commit( );
+						return result;
+			} catch ( Exception e ) {
+				Logs.exhaust( ).error( e, e );
+				trans.rollback( );
+				return "<unset>";
+			}
+		} catch (Exception e) {
+			Logs.exhaust().error(e, e);
+			return "<unset>";
+		}
+	}
+
+	public String setValue( String s ) {
+		try {
+			EntityTransaction trans = Entities.get(this.getDefiningClass());
+			try {
+				//This should return all matching objects
+				List<Object> resultList = Entities.query( this.getQueryObject( ) );
+				Object prop = this.getTypeParser( ).apply( s );
+
+				if(resultList == null || resultList.size() == 0) {
+					throw new NoSuchElementException("no entities found for property");
+				}
+
+				this.fireChange( prop ); //Fire change only once
+				LOG.debug("Running setters.");
+
+				for(Object obj : resultList) {      	
+					this.setter.invoke( obj, prop );
+				}
+				trans.commit( );
+				return s;
+			} catch ( Exception e ) {
+				Logs.exhaust( ).error( e, e );
+				trans.rollback( );
+				return "Error: " + e.getMessage( );
+			}
+		} catch (Exception e) {
+			Logs.exhaust( ).error( e, e );
+			return "Error: " + e.getMessage( );
+		}
+	}
+
+	public void resetValue( ) {
+		this.setValue( this.defaultValue );
+	}
+
+	public Class getDefiningClass( ) {
+		return this.definingClass;
+	}
+
+	public Constructor getNoArgConstructor( ) {
+		return this.noArgConstructor;
+	}
+
+	public String getDisplayName( ) {
+		return this.displayName;
+	}
+
+	public ConfigurableFieldType getWidgetType( ) {
+		return this.widgetType;
+	}
+
+	public String getAlias( ) {
+		return this.alias;
+	}
+
+	protected void fireChange( Object newValue ) throws ConfigurablePropertyException {
+		if ( !NoopEventListener.class.equals( this.changeListener.getClass( ) ) ) {
+			this.changeListener.fireChange( this, newValue );
+		}
+	}
+
+	public Boolean getReadOnly( ) {
+		return this.readOnly;
+	}
+
+	public PropertyChangeListener getChangeListener( ) {
+		return this.changeListener;
+	}
+
+	public Field getField( ) {
+		return this.field;
+	}
+
+	public boolean isDeferred( ) {
+		return this.deferred;
+	}
 }
