417d20868b595e8a90f5cb01163aabf1eab12999
Jeff Uphoff <juphoff@eucalyptus.com>
2013-03-08 10:26:11 -0800

    clean up doModifyNode(), removing functionality duplicated in doMigrateInstances()

--- cluster/handlers.c
+++ cluster/handlers.c
@@ -3943,66 +3943,10 @@ int doModifyNode(ncMetadata * pMeta, char *nodeName, char *stateName)
         goto out;
     }
 
-    // find an instance running on the host
-    int found_instance = 0;
-    ccInstance cc_instance;
-    sem_mywait(INSTCACHE);
-    if (instanceCache->numInsts) {
-        for (i = 0; i < MAXINSTANCES_PER_CC; i++) {
-            if (instanceCache->cacheState[i] == INSTVALID
-                && instanceCache->instances[i].ncHostIdx == src_index
-                && (!strcmp(instanceCache->instances[i].state, "Extant"))) {
-                memcpy(&cc_instance, &(instanceCache->instances[i]), sizeof(ccInstance));
-                found_instance = 1;
-                break;
-            }
-        }
-    }
-    sem_mypost(INSTCACHE);
-    if (! found_instance) {
-        LOGINFO("no instances running on host %s\n", SP(nodeName));
-        goto out;
-    }
-
-    if (dst_index == -1) {
-        LOGERROR("have instances to migrate, but no destinations\n");
-        goto out;
-    }
-
-    ncInstance nc_instance;
-    ccInstance_to_ncInstance(&nc_instance, &cc_instance);
-    strncpy (nc_instance.migration_src, resourceCacheLocal.resources[src_index].hostname, sizeof(nc_instance.migration_src));
-    strncpy (nc_instance.migration_dst, resourceCacheLocal.resources[dst_index].hostname, sizeof(nc_instance.migration_dst));
-    ncInstance * instances = &nc_instance;
-
-    // notify the destination
-    timeout = ncGetTimeout(time(NULL), OP_TIMEOUT, 1, 0);
-    rc = ncClientCall(pMeta, timeout, resourceCacheLocal.resources[dst_index].lockidx, resourceCacheLocal.resources[dst_index].ncURL, "ncMigrateInstances",
-                      &instances, 1, "prepare", NULL);
-    if (rc) {
-        LOGERROR("failed to request prepare migration on destination\n");
-        ret = 1;
-        goto out;
-    }
-
-    // notify source
-    timeout = ncGetTimeout(time(NULL), OP_TIMEOUT, 1, 0);
-    rc = ncClientCall(pMeta, timeout, resourceCacheLocal.resources[src_index].lockidx, resourceCacheLocal.resources[src_index].ncURL, "ncMigrateInstances",
-                      &instances, 1, "prepare", NULL);
-    if (rc) {
-        LOGERROR("failed to request prepare migration on source\n");
-        ret = 1;
-        goto out;
-    }
-
-    // call commit on source
-    timeout = ncGetTimeout(time(NULL), OP_TIMEOUT, 1, 0);
-    rc = ncClientCall(pMeta, timeout, resourceCacheLocal.resources[src_index].lockidx, resourceCacheLocal.resources[src_index].ncURL, "ncMigrateInstances",
-                      &instances, 1, "commit", NULL);
-    if (rc) {
-        LOGERROR("failed to request migration on source\n");
-        ret = 1;
-        goto out;
+    // FIXME: This is only here for compatability with earlier demo
+    // development. Remove.
+    if (!doMigrateInstances(pMeta, nodeName)) {
+        LOGERROR("doModifyNode() call of doMigrateInstances() failed.\n");
     }
 
  out:
@@ -4065,6 +4009,7 @@ int doMigrateInstances(ncMetadata * pMeta, char *nodeName)
 
     LOGINFO("migrating from %s to %s\n", SP(resourceCacheLocal.resources[src_index].hostname), SP(resourceCacheLocal.resources[dst_index].hostname));
 
+    // FIXME: needs to find all instances running on host.
     // find an instance running on the host
     int found_instance = 0;
     ccInstance cc_instance;
@@ -4102,7 +4047,7 @@ int doMigrateInstances(ncMetadata * pMeta, char *nodeName)
     rc = ncClientCall(pMeta, timeout, resourceCacheLocal.resources[dst_index].lockidx, resourceCacheLocal.resources[dst_index].ncURL, "ncMigrateInstances",
                       &instances, 1, "prepare", NULL);
     if (rc) {
-        LOGERROR("failed to request prepare migration on destination\n");
+        LOGERROR("failed: request to prepare migration on destination\n");
         ret = 1;
         goto out;
     }
@@ -4112,7 +4057,7 @@ int doMigrateInstances(ncMetadata * pMeta, char *nodeName)
     rc = ncClientCall(pMeta, timeout, resourceCacheLocal.resources[src_index].lockidx, resourceCacheLocal.resources[src_index].ncURL, "ncMigrateInstances",
                       &instances, 1, "prepare", NULL);
     if (rc) {
-        LOGERROR("failed to request prepare migration on source\n");
+        LOGERROR("failed: request to prepare migration on source\n");
         ret = 1;
         goto out;
     }
@@ -4122,7 +4067,7 @@ int doMigrateInstances(ncMetadata * pMeta, char *nodeName)
     rc = ncClientCall(pMeta, timeout, resourceCacheLocal.resources[src_index].lockidx, resourceCacheLocal.resources[src_index].ncURL, "ncMigrateInstances",
                       &instances, 1, "commit", NULL);
     if (rc) {
-        LOGERROR("failed to request migration on source\n");
+        LOGERROR("failed: migration request on source\n");
         ret = 1;
         goto out;
     }
