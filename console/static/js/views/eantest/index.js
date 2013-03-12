52cfe5f1949ae7d8e8cec38ca6cd0c0a159dac0b
Tim Boudreau <tboudreau@chiliad.com>
2013-03-12 17:04:27 -0400

    Revert angular stuff

--- console/static/js/views/eantest/index.js
+++ console/static/js/views/eantest/index.js
@@ -20,9 +20,3 @@ define([
 		}
 	});
 });
-
-function AutoScaling ($scope, $http) {
-    http.post('/autoscaling?Action=DescribeAutoScalingGroups').success(function(data) {
-        $scope.results = data.results;
-    });
-}
