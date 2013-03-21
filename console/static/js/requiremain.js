f6a87327204fbe572398dbea42fa9bf8d679a71b
Ean Schuessler <ean@brainfood.com>
2013-03-21 17:35:45 -0400

    Improved rivets support with iterator context passed into function invocations.

--- console/static/js/requiremain.js
+++ console/static/js/requiremain.js
@@ -1,17 +1,19 @@
 console.log('REQUIRE CONFIG');
 require.config({
         baseUrl: 'js',
+/*
         paths: {
             'rivets': 'rivets.js',
             'rivetsbase': 'rivetsbase.js'
         },
+*/
         shim: {
-                //rivetsbase : {
-                 //       exports: 'rivets',
-                //},
+                rivetsbase : {
+                       exports: 'rivets',
+                },
                 rivets : {
-                  			deps: ['rivetsbase'],
-                        exports: 'rivets',
+       			deps: ['rivetsbase'],
+                        exports: 'rivets'
                 },
 	}
 });
