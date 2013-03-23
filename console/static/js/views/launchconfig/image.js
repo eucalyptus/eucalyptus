73c9a97a56fdb1b9dfdd0b5f32bb4e141ab68f98
Jerry Pommer <jpommer@gmail.com>
2013-03-23 12:39:28 -0400

    Bringing rivets in and implementing new look.

--- console/static/js/views/launchconfig/image.js
+++ console/static/js/views/launchconfig/image.js
@@ -1,12 +1,14 @@
 define([
 	'dataholder',
-	'text!./image_static.html!strip',
+	//'text!./image_static.html!strip',
+  'text!./image.html!strip',
   'rivets'
 	], function( dataholder, template, rivets ) {
 	return Backbone.View.extend({
 		initialize : function() {
 			this.view = this;
 			this.images = dataholder.images;
+      this.jerry = "POMMER";
 			this.render();
 		},
 		doit : function() {
@@ -14,8 +16,9 @@ define([
 			this.test.set({value: 'pressed the clicker'});
 		},
 		render : function() {
-			this.$el.html(template);
-			rivets.bind(this.$el, this);
+      this.$el.html(template);
+			//this.$el.html(_.template(template, {collection: this.collection}));
+			rivets.bind(this.$el, {images: this.images});
 			//$(':input[data-value]', this.$el).keyup(function() { $(this).trigger('change'); });
 			return this;
 		}
