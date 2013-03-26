3efff8570bb61300af8879dae0e9e67b79f952cb
Jerry Pommer <jpommer@gmail.com>
2013-03-26 16:57:20 -0400

    Rivetized template and Backbone view for wizard step.

--- console/static/js/views/launchconfig/image.js
+++ console/static/js/views/launchconfig/image.js
@@ -1,36 +1,41 @@
 define([
 	'dataholder',
-	'text!./image_static.html!strip',
+	//'text!./image_static.html!strip',
   'text!./image.html!strip',
   'rivets',
 	], function( dataholder, template, rivets ) {
 	return Backbone.View.extend({
+    title: 'Image',
 		initialize : function() {
-			this.view = this;
-			this.images = dataholder.images;
-      this.images.on('all', this.render, this);
-      this.imageNames = {};
-			this.render();
+  
+      var scope = {
+          view: this,
+          images: dataholder.images,
+          imageNames: {},
+          isOdd: function() {
+              return true;
+          },
+          setClass: function() {
+              console.log('ROW',this);
+              var image = this.image;
+              return inferImage(image.attributes.location, image.attributes.description, image.attributes.platform);
+          },
+      } 
+
+     $(this.el).html(template)
+     this.rView = rivets.bind(this.$el, scope);
+     this.render();
 		},
 
     setImageNames: function() {
         var view = this;
         _.each(this.images.models, function(image) {
-            console.log("IMAGES - model: ", image.attributes);
           view.imageNames[image.id] = inferImage(image.attributes.location, image.attributes.description, image.attributes.platform);
-          console.log("IMAGES - setname: ", view.imageNames[image.id]);
         });
     },
 
-
-		render : function() {
-      this.setImageNames();
-      this.$el.html(_.template(template,{images: this.images, imageNames: this.imageNames}));
-      //this.$el.html(template);
-
-			rivets.bind(this.$el, {images: this.images});
-			//$(':input[data-value]', this.$el).keyup(function() { $(this).trigger('change'); });
-			return this;
-		}
-	});
+  render: function() {
+    this.rView.sync();
+  }
+});
 });
