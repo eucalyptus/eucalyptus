f7642d12db705e8c9d8dfeb5e37ada3b25d5239c
dkavanagh <dkavanagh@gmail.com>
2013-03-28 09:10:10 -0400

    more features for scaling resources

--- console/static/js/models/scalinginst.js
+++ console/static/js/models/scalinginst.js
@@ -4,7 +4,47 @@
 define([
     './eucamodel'
 ], function(EucaModel) {
-    var model = EucaModel.extend({
-    });
-    return model;
+  var model = EucaModel.extend({
+    setInstanceHealth: function (instance_id, health_status, should_respect_grace_period) {
+      var data = "_xsrf="+$.cookie('_xsrf')+"&InstanceId="+instance_id+
+                 "&AutoScalingGroupName="+this.get('name')+
+                 "&DesiredCapacity="+desired_capacity;
+      if (honor_cooldown != undefined)
+        data += "&HonorCooldown=true";
+      $.ajax({
+        type:"POST",
+        url:"/autoscaling?Action=SetInstanceHealth",
+        data:data,
+        dataType:"json",
+        async:true,
+        success:
+          function(data, textStatus, jqXHR){
+          },
+        error:
+          function(jqXHR, textStatus, errorThrown){
+          },
+      });
+    },
+    terminateInstance: function (instance_id, decrement_capacity) {
+      var data = "_xsrf="+$.cookie('_xsrf')+"&InstanceId="+instance_id+
+                 "&AutoScalingGroupName="+this.get('name')+
+                 "&DesiredCapacity="+desired_capacity;
+      if (honor_cooldown != undefined)
+        data += "&HonorCooldown=true";
+      $.ajax({
+        type:"POST",
+        url:"/autoscaling?Action=SetInstanceHealth",
+        data:data,
+        dataType:"json",
+        async:true,
+        success:
+          function(data, textStatus, jqXHR){
+          },
+        error:
+          function(jqXHR, textStatus, errorThrown){
+          },
+      });
+    }
+  });
+  return model;
 });
