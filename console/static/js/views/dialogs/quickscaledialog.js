352d248093f957e829cb883570ae36e0d441f501
Ean Schuessler <ean@brainfood.com>
2013-03-28 19:20:44 -0400

    New app top level utility object. Fixed up the layout of the quickscale dialog. Use app.dialog() in testbed.

--- console/static/js/views/dialogs/quickscaledialog.js
+++ console/static/js/views/dialogs/quickscaledialog.js
@@ -3,17 +3,32 @@ define([
    'text!./quickscaledialog.html!strip',
 ], function(EucaDialogView, template) {
     return EucaDialogView.extend({
-        scope: {
-            qscale: new Backbone.Model({
-                name: 'some name',
-                size: 4,
-                minimum: 1,
-                maximum: 10,
-                desired: 4,
-            }),
-        },
-        initialize : function() {
-            this.$el.html(template);
+        initialize : function(args) {
+            var self = this;
+            this.template = template;
+            this.scope = {
+                qscale: new Backbone.Model({
+                    name: 'some name',
+                    size: 4,
+                    minimum: 1,
+                    maximum: 10,
+                    desired: 4,
+                }),
+
+                cancelButton: {
+                    click: function() {
+                       self.close();
+                    }
+                },
+
+                submitButton: {
+                    click: function() {
+                       self.scope.status = 'Deleting';
+                       self.close();
+                    }
+                }
+            }
+
             this._do_init();
         },
 	});
