4612ff4505d17a440b8473776ebb7157b9b9f8a5
Tim Boudreau <niftiness@gmail.com>
2013-03-29 20:53:10 -0400

    Search stubs for the remaining landing pages

--- console/static/js/views/searches/instance.js
+++ console/static/js/views/searches/instance.js
@@ -2,6 +2,6 @@ define([
     'views/searches/generic',
 ], function(Search) {
     return function(images) {
-      return new Search(images, ['availability_zones', 'launch_config_name', 'name', 'health_check_type'], {}, null);
+      return new Search(images, ['group_name', 'root_device_name', 'name', 'placement', 'owner_id', 'image_id'], {}, null);
     }
 });
