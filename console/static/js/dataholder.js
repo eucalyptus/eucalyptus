30659f16d70c3ae1093f0eb6e94e56a8b244f77a
Jerry Pommer <jpommer@gmail.com>
2013-03-30 14:56:31 -0400

    Security groups wizard step so far. Going to pull from Ean's branch and see what improvements I can get for Rivets.

--- console/static/js/dataholder.js
+++ console/static/js/dataholder.js
@@ -4,9 +4,11 @@ define([
 	'models/volumes', 
 	'models/images',
 	'models/launchconfigs',
-	'models/instances'
+	'models/instances',
+  'models/sgroups',
+  'models/keypairs'
 ], 
-function(Backbone, ScalingGroups,Volumes,Images,LaunchConfigs,Instances) {
+function(Backbone, ScalingGroups,Volumes,Images,LaunchConfigs,Instances, SecurityGroups, KeyPairs) {
 	var shared = {
 		launchConfigs: new LaunchConfigs(),
 		scalingGroups: new ScalingGroups(),
@@ -18,19 +20,25 @@ function(Backbone, ScalingGroups,Volumes,Images,LaunchConfigs,Instances) {
 		  new Backbone.Model({name: "LB B"}),
 		  new Backbone.Model({name: "LB C"}),
 		  new Backbone.Model({name: "LB D"}),
-		])
+		]),
+    securityGroups: new SecurityGroups(),
+    keypairs: new KeyPairs()
 	};
 
     shared.image = shared.images;
     shared.volume = shared.images;
     shared.launchConfig = shared.launchConfigs;
     shared.scalingGroup = shared.scalingGroups;
+    shared.securityGroup = shared.securityGroups;
+    shared.keypair = shared.keypairs;
 
 	shared.launchConfigs.fetch();
 	shared.scalingGroups.fetch();
 	shared.volumes.fetch();
 	shared.images.fetch();
 	shared.instance.fetch();
+  shared.securityGroups.fetch();
+  shared.keypairs.fetch();
 
 	return shared;
 });
--- console/static/js/views/launchconfig/security.js
+++ console/static/js/views/launchconfig/security.js
@@ -1,16 +1,45 @@
 define([
+  'app',
 	'dataholder',
   'text!./security.html!strip',
   'rivets',
-	], function( dataholder, template, rivets ) {
+	], function( app, dataholder, template, rivets ) {
 	return Backbone.View.extend({
     title: 'Security',
 
 		initialize : function() {
 
+      var self = this;
       var scope = {
+        configmodel: this.model,
+        sgroups: dataholder.securityGroups,
+        keypairs: dataholder.keypairs,
+
+        setSecurityGroup: function(e, item) {
+          var groupWhere = this.sgroups.where({name: e.target.value});
+          var group = _.find(groupWhere, function(sg) {
+                        return sg.get('name') == e.target.value;
+          });
+          console.log("GROUPSET", this.selectedGroup);
+          self.model.set('security_group', group);
+          self.model.set('security_group_name', group.get('name'));
+          self.model.set('security_group_rules', group.get('rules'));
+          console.log("GROUPSET", this.selectedGroup);
+          console.log("GROUPSET", this.groupName);
+          console.log("GROUPSET", this.rules);
+        }
 
       };
+     
+      this.model.bind('change:security_group', function() { 
+          scope.selectedGroup = this.model.get('security_group');
+          scope.groupName = this.model.get('security_group').get('name');
+          scope.rules = this.model.get('security_group').get('rules');
+          }, this);
+
+console.log("SGROUPS", dataholder.securityGroups);
+console.log("KEYPAIRS", dataholder.keypairs);
+
       $(this.el).html(template)
       this.rView = rivets.bind(this.$el, scope);
       this.render();
