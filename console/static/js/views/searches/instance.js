0120398bcc20f9fe6acfd6432a5499873c084ebb
Tim Boudreau <niftiness@gmail.com>
2013-03-31 21:35:33 -0400

    Address Jenny's comments in EUCA-5574 - however, flying blind since no properties with names resembling  'scaling_group', 'load_balancer' or 'availability_zone' exist in the sample data

--- console/static/js/views/searches/instance.js
+++ console/static/js/views/searches/instance.js
@@ -2,6 +2,17 @@ define([
   'views/searches/generic',
 ], function(Search) {
   return function(images) {
-    return new Search(images, ['all_text', 'group_name', 'root_device_name', 'name', 'placement', 'owner_id', 'image_id'], {}, null);
+    
+    return new Search(images, ['all_text', 'root_device_name', 'status', 
+      
+      // FIXME - these are guesses based on the wireframe at 
+      // https://eucalyptus.atlassian.net/wiki/display/3UD/Manage+Instances+Landing
+      //
+      // since there are no such fields in the mock data, no idea what they 
+      // should actually be
+      
+      'scaling_group', 'load_balancer', 'availability_zone', 
+      
+      'instance_type'], {}, null);
   }
 });
