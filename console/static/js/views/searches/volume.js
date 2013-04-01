cdebb509ae213fb5062f906efc3677d3ff7c37b1
Tim Boudreau <niftiness@gmail.com>
2013-03-31 22:37:33 -0400

    Make volume facets match wireframe

--- console/static/js/views/searches/volume.js
+++ console/static/js/views/searches/volume.js
@@ -2,6 +2,6 @@ define([
   'views/searches/generic',
 ], function(Search) {
   return function(images) {
-    return new Search(images, ['all_text', 'snapshot_id', 'display_id', 'zone', 'region'], {}, null);
+    return new Search(images, ['all_text', 'attach_data'], { attach_data : 'Attachment' }, null);
   }
 });
