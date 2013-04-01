0e0db667bb6f9343edc78d179f4ee5401b1f8ed7
Tim Boudreau <niftiness@gmail.com>
2013-03-31 21:25:27 -0400

    Attempt to address Jenny's comments in EUCA-5575 without actual data to work with

--- console/static/js/views/searches/eip.js
+++ console/static/js/views/searches/eip.js
@@ -2,6 +2,6 @@ define([
   'views/searches/generic',
 ], function(Search) {
   return function(images) {
-    return new Search(images, ['all_text', 'name'], {}, null);
+    return new Search(images, ['all_text', 'name'], { assigned_to_instance_name : 'Assignment'}, null);
   }
 });
