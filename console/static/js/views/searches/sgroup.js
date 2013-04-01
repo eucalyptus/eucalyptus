6860f7e0c124bccc7210f8e96d8c091afbfe2886
Tim Boudreau <niftiness@gmail.com>
2013-03-31 22:00:42 -0400

    EUCA-5578 - make facets match wireframe;  flying blind since the data contains no properties 'platform' or 'architecture'

--- console/static/js/views/searches/sgroup.js
+++ console/static/js/views/searches/sgroup.js
@@ -2,6 +2,19 @@ define([
   'views/searches/generic',
 ], function(Search) {
   return function(images) {
-    return new Search(images, ['all_text', 'name'], {}, null);
+    
+    var facets = ['all_text', 'owner_id', 
+      // FIXME:  there is no mock data for platform or architecture,
+      // so no idea what properties are intended to be referenced by the
+      // wireframe at 
+      // https://eucalyptus.atlassian.net/wiki/display/3UD/Manage+Security+Groups+Landing
+      
+      'platform', 'architecture', 
+      
+      'root_device'
+    ];
+    // PENDING:  tag facets
+    
+    return new Search(images, facets, {}, null);
   }
 });
