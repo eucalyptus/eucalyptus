a8169ad5855539b2ccb96d82884332e762b18fb6
Jerry Pommer <jpommer@gmail.com>
2013-04-02 04:06:29 -0400

    Storage lister/adder working. Add/delete only at this time, no edits.

--- console/static/js/views/launchconfig/advanced.js
+++ console/static/js/views/launchconfig/advanced.js
@@ -26,7 +26,7 @@ define([
         enableSnapshot: false,
         snapshot: '',
         size: '',
-        deleteOnTerm: false,
+        deleteOnTerm: true,
 
         setKernel: function() {
           self.model.set('ramdisk_id', e.target.value);
@@ -48,16 +48,58 @@ define([
 
         },
 
-        setStorageVolume: function() {
+        setStorageVolume: function(e, obj) {
+          var m = new Backbone.Model();
+          var tr = $(e.target).closest('tr');
+          var vol = tr.find('.launch-wizard-advanced-storage-volume-selector').val();
+          var dev = "/dev/" + tr.find('.launch-wizard-advanced-storage-mapping-input').val();
+          var snap = tr.find('.launch-wizard-advanced-storage-snapshot-input').val();
+          var size = tr.find('.launch-wizard-advanced-storage-size-input').val();
+          var del = tr.find('.launch-wizard-advanced-storage-delOnTerm').prop('checked');
 
+          m.set({
+              map_name: dev,
+              status: null,
+              __obj_name__: "BlockDeviceType",
+              attach_time: null,
+              no_device: false,
+              volume_id: null,
+              connection: null,
+              snapshot_id: snap,
+              size: size,
+              ebs: "",
+              delete_on_termination: del,
+              ephemeral_name: null    
+          });
+
+          this.blockDeviceMappings.add(m);
+           console.log("DEVMAP", this.blockDeviceMappings); 
         },
 
-        setSnapshot: function() {
-
+        delStorageVol: function(e, obj) {
+          console.log('DEL', obj);
+          this.blockDeviceMappings.remove(obj.volume); 
         },
 
-        setBlockDeviceMapping: {
+        getVolLabel: function(obj) {
+          if (obj.volume.get('map_name') == '/dev/sda1') {
+              return 'Root';
+          } else {
+              return 'EBS';
+          }
+        },
 
+        deleteButtonIf: function(obj) {
+          if(this.getVolLabel(obj) != 'Root') {
+            return 'icon_delete';
+          }
+        },
+
+        checkDisabledIf: function(obj) {
+          if(this.getVolLabel(obj) != 'Root') {
+            return false;
+          }
+          return true;
         },
 
         mapName: function() {
@@ -71,6 +113,7 @@ define([
 
       };
 
+      scope.blockDeviceMappings.on('change add reset remove', function() {self.render();});
       $(this.el).html(template)
       this.rView = rivets.bind(this.$el, scope);
       this.render();
