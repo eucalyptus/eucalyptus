1059521e969260966d73b941af0248b6bb962ebe
dkavanagh <dkavanagh@gmail.com>
2013-04-03 11:41:09 -0400

    EUCA-5485 filling in instance expando

--- console/static/js/dataholder.js
+++ console/static/js/dataholder.js
@@ -1,6 +1,7 @@
 define([
     'backbone',
 	'models/scalinggrps',
+	'models/scalinginsts',
 	'models/volumes', 
 	'models/images',
 	'models/launchconfigs',
@@ -9,25 +10,24 @@ define([
 	'models/keypairs',
 	'models/sgroups',
 	'models/snapshots',
+	'models/balancers',
+	'models/insthealths',
     'sharedtags'
 ], 
-function(Backbone, ScalingGroups, Volumes, Images, LaunchConfigs, Instances, Eips, KeyPairs, SecurityGroups, Snapshots, tags) {
+function(Backbone, ScalingGroups, ScalingInstances, Volumes, Images, LaunchConfigs, Instances, Eips, KeyPairs, SecurityGroups, Snapshots, Balancers, InstHealths, tags) {
 	var shared = {
 		launchConfigs: new LaunchConfigs(),
 		scalingGroups: new ScalingGroups(),
+		scalinginsts: new ScalingInstances(),
 		volumes: new Volumes(),
 		images: new Images(),
 		instance: new Instances(),
-		loadBalancers: new Backbone.Collection([
-		  new Backbone.Model({name: "LB A"}),
-		  new Backbone.Model({name: "LB B"}),
-		  new Backbone.Model({name: "LB C"}),
-		  new Backbone.Model({name: "LB D"}),
-		]),
+		loadBalancers: new Balancers(),
+		instHealths: new InstHealths(),
         eip : new Eips(),
         keypair : new KeyPairs(),
         sgroup : new SecurityGroups(),
-        snapshot : new Snapshots()
+        snapshot : new Snapshots(),
 	};
 
 	shared.tags = tags;
@@ -41,9 +41,12 @@ function(Backbone, ScalingGroups, Volumes, Images, LaunchConfigs, Instances, Eip
 
 	shared.launchConfigs.fetch();
 	shared.scalingGroups.fetch();
+	shared.scalinginsts.fetch();
 	shared.volumes.fetch();
 	shared.images.fetch();
 	shared.instance.fetch();
+    shared.loadBalancers.fetch();
+    shared.instHealths.fetch();
     shared.eip.fetch();
     shared.keypair.fetch();
     shared.sgroup.fetch();
