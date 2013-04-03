18e7233dbb3ee6bfcff80e19ef704c53068e39b5
dkavanagh <dkavanagh@gmail.com>
2013-04-03 09:06:06 -0400

    re-enabled vmtypes from server

--- console/static/js/views/expandos/instance.js
+++ console/static/js/views/expandos/instance.js
@@ -1,12 +1,18 @@
 define([
-   'underscore',
-   './eucaexpandoview',
-   'text!./instance.html!strip',
-], function(_, EucaExpandoView, template) {
+ 'app',
+ 'underscore',
+ './eucaexpandoview',
+ 'text!./instance.html!strip',
+], function(app, _, EucaExpandoView, template) {
     return EucaExpandoView.extend({
         initialize : function(args) {
             this.template = template;
             this.model = this.model ? this.model : {};
+            var id = this.model.get('id');
+            this.model.set('volumes', app.data.volume.reduce(function(c, v) {
+                        //return v.get('attach_data').instance_id = id ? c.add(v) : c;
+                        return c.add(v);
+                      }, new Backbone.Collection()));
             this.scope = _.extend(this.model, {});
             this._do_init();
         },
