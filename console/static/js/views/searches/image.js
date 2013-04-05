105ebb418e08f80b60bdd73040761b2be333151f
Tim Boudreau <niftiness@gmail.com>
2013-04-05 02:46:01 -0400

    Look up user id on security group, so owned by 'me' facet now works

--- console/static/js/views/searches/image.js
+++ console/static/js/views/searches/image.js
@@ -1,11 +1,17 @@
 define([
   'views/searches/generic',
-  'views/searches/tagsearch'
-], function(Search, TagSearch) {
+  'views/searches/tagsearch',
+  'app'
+], function(Search, TagSearch, app) {
   return function(images) {
 
-    var USER_ID = "601265054777"; //FIXME
-
+    var USER_ID = "601265054777";
+    app.data.sgroup.each(function(securityGroup) {
+      securityGroup = securityGroup.toJSON();
+      if ('default' === securityGroup.name) {
+        USER_ID = securityGroup.owner_id;
+      }
+    });
     var config = {
       facets: ['all_text', 'architecture', 'description', 'name',
         'ownerId', 'platform', 'root_device_type']
--- console/static/js/views/searches/sgroup.js
+++ console/static/js/views/searches/sgroup.js
@@ -1,10 +1,18 @@
 define([
   'views/searches/generic',
   'views/searches/tagsearch',
-], function(Search, TagSearch) {
+  'app'
+], function(Search, TagSearch, app) {
   return function(sgroups) {
     
-    var USER_ID = '072279894205'; // XXX where do I get this?
+    var USER_ID = '072279894205';
+    
+    app.data.sgroup.each(function(securityGroup) {
+      securityGroup = securityGroup.toJSON();
+      if ('default' === securityGroup.name) {
+        USER_ID = securityGroup.owner_id;
+      }
+    });
     
     var config = {
       facets: ['all_text', 'owner_id']
@@ -19,7 +27,7 @@ define([
       }
       ,search : {
         owner_id : function(search, facetSearch, item, itemsFacetValue, hit) {
-          if (facetValue === USER_ID) {
+          if (itemsFacetValue === USER_ID) {
             hit();
           }
         }
