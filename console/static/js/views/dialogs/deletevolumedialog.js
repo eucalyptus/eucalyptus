4745f5fc7a34f31843265ea3b1b835023527fd82
Kyo Lee <kyo.lee@eucalyptus.com>
2013-04-08 00:37:41 -0700

    Minor Adjustment - Moving 'app' to be included in the header

--- console/static/js/views/dialogs/deletevolumedialog.js
+++ console/static/js/views/dialogs/deletevolumedialog.js
@@ -1,7 +1,8 @@
 define([
    './eucadialogview',
    'text!./deletevolumedialog.html!strip',
-], function(EucaDialogView, template) {
+   'app',
+], function(EucaDialogView, template, App) {
     return EucaDialogView.extend({
         initialize : function(args) {
             var self = this;
@@ -16,48 +17,45 @@ define([
                 },
 
                 deleteButton: function() {
-                  require(['app'], function(app) {
-                      // FOR EACH VOLUME IN THE LIST 'items'
-                      _.each(self.scope.items, function(item) {
-                        console.log("Volume Item: " + item);
-                        // FIND THE MODEL THAT MATCHES THE VOLUME ID WITH 'item'
-                        var match = app.data.volume.find(function(model){ return model.get('id') == item; });
-//                        console.log("Volume Create Time: " + match.get('create_time'));
+	          // FOR EACH VOLUME IN THE LIST 'items'
+	          _.each(self.scope.items, function(item) {
+		    console.log("Volume Item: " + item);
+		    // FIND THE MODEL THAT MATCHES THE VOLUME ID WITH 'item'
+		    var match = App.data.volume.find(function(model){ return model.get('id') == item; });
 
-                        // PERFORM DELETE CALL OM THE MODEL
-                        match.sync('delete', match, {
-                          // IN CASE OF AJAX CALL'S SUCCESS
-                          success: function(data, response, jqXHR){
-                            console.log("Callback " + response + " for " + item);
-                            if(data.results){
-                              console.log("sync: Volume " +item+ " Deleted = " + data.results );
-                              notifySuccess(null, $.i18n.prop('volume_create_success', DefaultEncoder().encodeForHTML(item)));  // incorrect prop
-                            }else{
-                              notifyError($.i18n.prop('delete_volume_error', DefaultEncoder().encodeForHTML(item)), undefined_error);  // incorrect prop
-                            }
-                          },
-                          // IN CASE OD AJAX CALL'S ERROR
-                          error: function(jqXHR, textStatus, errorThrown){
-                            notifyError($.i18n.prop('delete_volume_error', DefaultEncoder().encodeForHTML(item)), getErrorMessage(jqXHR));  // incorrect prop 
-                          }
-                        });
+		    // PERFORM DELETE CALL OM THE MODEL
+		    match.sync('delete', match, {
+		      // IN CASE OF AJAX CALL'S SUCCESS
+		      success: function(data, response, jqXHR){
+		        console.log("Callback " + response + " for " + item);
+		        if(data.results){
+		          console.log("sync: Volume " +item+ " Deleted = " + data.results );
+		          notifySuccess(null, $.i18n.prop('volume_create_success', DefaultEncoder().encodeForHTML(item)));  // incorrect prop
+		        }else{
+		          notifyError($.i18n.prop('delete_volume_error', DefaultEncoder().encodeForHTML(item)), undefined_error);  // incorrect prop
+		        }
+		      },
+		      // IN CASE OD AJAX CALL'S ERROR
+		      error: function(jqXHR, textStatus, errorThrown){
+		        notifyError($.i18n.prop('delete_volume_error', DefaultEncoder().encodeForHTML(item)), getErrorMessage(jqXHR));  // incorrect prop 
+		      }
+		    });
 
-                        // DESTORY THIS MODEL
-                        match.destroy({wait: true});
-                      });
+		    // DESTORY THIS MODEL
+		    match.destroy({wait: true});
+	          });
 
-                      // DISPLAY THE MODEL LIST FOR VOLUME AFTER THE DESTROY OPERATION
-                      app.data.volume.each(function(item){
-                        console.log("Volume After Delete: " + item.toJSON().id);
-                      });
-                  });
+	          // DISPLAY THE MODEL LIST FOR VOLUME AFTER THE DESTROY OPERATION -- FOR DEBUG
+	          App.data.volume.each(function(item){
+		    console.log("Volume After Delete: " + item.toJSON().id);
+	          });
 
                   // CLOSE THE DIALOG
                   self.close();
                 }
-            }
+          }
 
-            this._do_init();
+          this._do_init();
         },
 	});
 });
