232b64a3e5069f5287c484d0817b292b22cdb5aa
dkavanagh <dkavanagh@gmail.com>
2013-04-10 14:54:54 -0400

    EUCA-5652 inital conversion of 1 use case. factored most code into helper function to minimize code re-work

--- console/static/js/views/dialogs/delete_volume_dialog.js
+++ console/static/js/views/dialogs/delete_volume_dialog.js
@@ -19,51 +19,12 @@ define([
         },
         deleteButton: {
           click: function() {
-              // FOR EACH VOLUME IN THE LIST 'items'
-              var done = 0;
-              var all = self.scope.items.length;
-              var error = [];
-              doMultiAjax(self.scope.items, function(item, dfd) {
-                console.log("Volume to be deleted: "+item);
-                var volumeId = item;
-                var volume = App.data.volumes.get(item);
-                volume.destroy({
-                  wait: true,
-                  success:
-                    function(model, response, options) {
-                      console.log("response = "+JSON.stringify(response));
-                      if (response.results && data.results == true) {
-                        ;
-                      } else {
-                        error.push({id:volumeId, reason: undefined_error});
-                      }
-                      options.complete(model, response);
-                    },
-                  error:
-                    function(model, xhr, options) {
-                      console.log("response = "+JSON.stringify(xhr));
-                      error.push({id:volumeId, reason: getErrorMessage(xhr)});
-                      options.complete(model, xhr);
-                    },
-                  complete:
-                    function(model, response) {
-                      done++;
-                      if (done < all) {
-                        notifyMulti(100*(done/all), $.i18n.prop('volume_delete_progress', all));
-                      }
-                      else {
-                        var $msg = $('<div>').addClass('multiop-summary').append(
-                                   $('<div>').addClass('multiop-summary-success').
-                                       html($.i18n.prop('volume_delete_done', (all-error.length), all)));
-                        if (error.length > 0)
-                            $msg.append($('<div>').addClass('multiop-summary-failure').
-                                       html($.i18n.prop('volume_delete_fail', error.length)));
-                        notifyMulti(100, $msg.html(), error);
-                      }
-                      dfd.resolve();
-                    }
-                });
-              });
+              doMultiAction(self.scope.items, App.data.volumes,
+                            function(model, options) {
+                              options['wait'] = true;
+                              model.destroy(options);
+                            },
+                            volume_delete_progress, volume_delete_done, volume_delete_fail);
               self.close();
           }
         },
