a58e07cff239d839ee1a664370850f8648357749
dkavanagh <dkavanagh@gmail.com>
2013-04-11 20:11:32 -0400

    implemented doMultiAction in snapshot and volume dialogs that needed it. Implemented sync(create/delete) for image model. got snapshot id to appear on image expando

--- console/static/js/views/expandos/image.js
+++ console/static/js/views/expandos/image.js
@@ -11,6 +11,12 @@ define([
       var tmp = this.model ? this.model : new Backbone.Model();
       this.model = new Backbone.Model();
       this.model.set('image', tmp);
+      var bdm = tmp.get('block_device_mapping');
+      if (bdm) {
+        if (bdm['/dev/sda1']) {
+          this.model.set('snapshot_id', bdm['/dev/sda1'].snapshot_id);
+        }
+      }
       this.model.set('kernel', app.data.images.get(this.model.get('image').get('kernel_id')));
       this.model.set('ramdisk', app.data.images.get(this.model.get('image').get('ramdisk_id')));
       this.scope = this.model;
