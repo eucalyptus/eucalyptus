048a0b4646fd301508349744540c0476e4115466
Ean Schuessler <ean@brainfood.com>
2013-04-16 14:40:25 -0500

    Provide tag editing on security groups.

--- console/static/js/sgroup.js
+++ console/static/js/sgroup.js
@@ -214,13 +214,11 @@
       (function(){
          menuItems['edit'] = {"name":sgroup_action_edit, callback: function(key, opt) { ; }, disabled: function(){ return true; }};
          menuItems['delete'] = {"name":sgroup_action_delete, callback: function(key, opt) { thisObj._deleteAction(); }};
-         menuItems['newcreate'] = {"name":'new create', callback: function(key, opt) { thisObj._newCreateAction(); }};
+         menuItems['tag'] = {"name":'Tag Resource', callback: function(key, opt) { thisObj._tagResourceAction(); }};
       })();
-      if(numSelected == 0){
-         menuItems['newcreate'] = {"name":'new create', callback: function(key, opt) { thisObj._newCreateAction(); }};
-      }
       if(numSelected == 1){
         menuItems['edit'] = {"name":sgroup_action_edit, callback: function(key, opt) { thisObj._editAction(); }}
+        menuItems['tag'] = {"name":'Tag Resource', callback: function(key, opt) { thisObj._tagResourceAction(); }}
       }
       return menuItems;
     },
@@ -786,6 +784,14 @@
       this.tableWrapper.eucatable('reDrawTable');
     },
 
+    _tagResourceAction : function(){
+      var selected = this.tableWrapper.eucatable('getSelectedRows', 7);
+      if ( selected.length > 0 ) {
+        require(['app'], function(app) {
+           app.dialog('edittags', app.data.sgroup.get(selected[0]));
+        });
+       }
+    },
 
     _deleteSelectedSecurityGroups : function (groupsToDelete) {
       var thisObj = this;
