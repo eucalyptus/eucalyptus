158e37dd3bff1f2f30f59dae21c7e36333f320c4
Kyo Lee <kyo.lee@eucalyptus.com>
2013-04-16 20:53:38 -0700

    Adjusted the detach volume dialog to pass the correct parameter to doMultiAction()    However, the AJAX return values need to adjusted for detach volume call.        Be aware of that there are 2 dialogs, old and new, exist that use the same AJAX call from the proxy

--- console/static/js/views/dialogs/detach_volume_dialog.js
+++ console/static/js/views/dialogs/detach_volume_dialog.js
@@ -1,27 +1,26 @@
 define([
   './eucadialogview',
   'text!./detach_volume_dialog.html!strip',
+  'models/volume',
   'app',
   'backbone',
-], function(EucaDialogView, template, App, Backbone) {
+], function(EucaDialogView, template, Volume, App, Backbone) {
    return EucaDialogView.extend({
      initialize : function(args) {
        var self = this;
        this.template = template;
 
+       var volume_list = new Backbone.Collection();
+       _.each(args.volume_ids, function(vid){
+         var instance_id = App.data.volume.get(vid).get('attach_data').instance_id;
+         volume_list.push(new Volume({volume_id: vid, instance_id: instance_id}));
+         console.log("Volume: " + vid + " Instance: " + instance_id);
+       });
+
        this.scope = {
          status: '',
-         items: function(){                   // Feels very hacky -- Kyo 040813
-           var volume_list = [];
-           _.each(args.volume_ids, function(v){
-             var match = App.data.volume.find(function(model){ return model.get('id') == v; });
-             var instance_id = match.toJSON().attach_data.instance_id;
-             volume_list.push({volume_id: v, instance_id: instance_id});
-             console.log("Volume: " + v + " Instance: " + instance_id);
-           });
-           return volume_list;
-         },
-
+         items: volume_list,
+        
          cancelButton: {
            click: function() {
              self.close();
@@ -30,8 +29,10 @@ define([
 
          detachButton: {
            click: function() {
-              doMultiAction(self.scope.items, App.data.volumes,
+
+              doMultiAction(args.volume_ids, App.data.volumes,
                             function(model, options) {
+                              options['wait'] = true;
                               model.detach(options);
                             },
                             'volume_detach_progress', 'volume_detach_done', 'volume_detach_fail');
