ff6c5e4905e03f478bca074e0910d7b487bbad63
Kyo Lee <kyo.lee@eucalyptus.com>
2013-04-16 21:47:01 -0700

    Adjusted the created snapshot dialog to display the name tag for the volumes

--- console/static/js/views/dialogs/create_snapshot_dialog.js
+++ console/static/js/views/dialogs/create_snapshot_dialog.js
@@ -7,16 +7,23 @@ define([
 ], function(EucaDialogView, template, Snapshot, App, Backbone) {
     return EucaDialogView.extend({
 
+        disableVolumeInputBox: function(){
+          var $volumeSelector = this.$el.find('#snapshot-create-volume-id');
+          $volumeSelector.attr('disabled', 'disabled');
+        },
 
         // SET UP AUTOCOMPLETE FOR THE VOLUME INPUT BOX
         setupAutoComplete: function(args){
             var self = this;
             
             if( args.volume_id == undefined ){
+              // CASE: CALLED FROM THE SNAPSHOT LANDING PAGE
               var vol_ids = [];
               App.data.volume.each(function(v){
                 console.log("Volume ID: " + v.get('id') + "  Status:" + v.get('status'));
-                vol_ids.push(v.get('id'));
+                var nameTag = self.findNameTag(v);
+                var autocomplete_string = String(self.createIdNameTagString(v.get('id'), nameTag));
+                vol_ids.push(autocomplete_string);
               });
 
               var sorted = sortArray(vol_ids);
@@ -26,7 +33,34 @@ define([
               $volumeSelector.autocomplete({
                 source: sorted
               });
-            }; 
+            }else{
+              // CASE: CALLED FROM THE VOLUME LANDING PAGE
+              // DISABLE THE VOLUME INPUT BOX
+              this.disableVolumeInputBox();
+              // DISPLAY ITS NAME TAG FOR VOLUME ID
+              var foundNameTag = self.findNameTag(App.data.volume.get(args.volume_id));
+              self.scope.snapshot.set({volume_id: String(self.createIdNameTagString(args.volume_id, foundNameTag))});
+            } 
+        },
+
+        // CONSTRUCT A STRING THAT DISPLAYS BOTH RESOURCE ID AND ITS NAME TAG
+        createIdNameTagString: function(resource_id, name_tag){
+          var this_string = resource_id;
+          if( name_tag != null ){
+            this_string += " (" + name_tag + ")";
+          }
+          return this_string;
+        },
+
+        // UTILITY FUNCTION TO DISCOVER THE NAME TAG OF CLOUD RESOURCE MODEL
+        findNameTag: function(model){
+          var nameTag = null;
+          model.get('tags').each(function(tag){
+            if( tag.get('name').toLowerCase() == 'name' ){
+              nameTag = tag.get('value');
+            };
+          });
+          return nameTag;
         },
 
         initialize : function(args) {
@@ -35,8 +69,7 @@ define([
 
             this.scope = {
                 status: '',
-                snapshot: new Snapshot(),
-                item: {volume_id: args.volume_id, description: ''},
+                snapshot: new Snapshot({volume_id: args.volume_id, description: ''}),
 
                 cancelButton: {
                   click: function() {
@@ -47,10 +80,16 @@ define([
                 createButton: {
                   click: function() {
 	            // GET THE INPUT FROM HTML VIEW
-	            var volumeId = self.scope.item.volume_id;
-	            var description = self.scope.item.description;
+	            var volumeId = self.scope.snapshot.get('volume_id');
+	            var description = self.scope.snapshot.get('description');
 		    console.log("Selected Volume ID: " + volumeId);
 		    console.log("Volume Description: " + description);
+
+                    // EXTRACT THE RESOURCE ID IF THE NAME TAG WAS FOLLOWED
+                    if( volumeId.match(/^\w+-\w+\s+/) ){
+                      volumeId = volumeId.split(" ")[0];
+		      console.log("Volume ID: " + volumeId);
+                    }
 
 	            // CONSTRUCT AJAX CALL RESPONSE OPTIONS
 	            var createAjaxCallResponse = {
@@ -70,8 +109,8 @@ define([
 	            };
 
 	            // PERFORM CREATE CALL OM THE MODEL
-	            self.scope.snapshot = new Snapshot({volume_id: volumeId, description: description}); 
-	            self.scope.snapshot.sync('create', self.scope.snapshot, createAjaxCallResponse);
+	            var new_snapshot = new Snapshot({volume_id: volumeId, description: description}); 
+	            new_snapshot.sync('create', new_snapshot, createAjaxCallResponse);
 
 	            // DISPLAY THE MODEL LIST FOR VOLUME AFTER THE DESTROY OPERATION
 	            App.data.snapshot.each(function(item){
