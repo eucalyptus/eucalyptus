e452fa6c2805c95cb6a807b28cb591898506b422
dkavanagh <dkavanagh@gmail.com>
2013-04-22 19:53:34 -0400

    EUCA-5784 added common date formatter that pulls from i18n, referenced in templates

--- console/static/js/views/expandos/eucaexpandoview.js
+++ console/static/js/views/expandos/eucaexpandoview.js
@@ -1,18 +1,21 @@
 define([
-   'backbone',
-   'rivets',
-   'dataholder',
+  'backbone',
+  'rivets',
+  'dataholder',
 ], function(Backbone, rivets, dataholder) {
-    return Backbone.View.extend({
-        _do_init : function() {
-            var self = this;
-            this.$el.html(this.template);
-            this.rivetsView = rivets.bind(this.$el, this.scope);
-            this.render();
-        },
-        render : function() {
-            this.rivetsView.sync();
-            return this;
-        },
-	});
+  return Backbone.View.extend({
+    _do_init : function() {
+      var self = this;
+      this.$el.html(this.template);
+      this.rivetsView = rivets.bind(this.$el, this.scope);
+      this.render();
+      rivets.formatters.date = function(value){
+        return moment(value).format(TIME_FORMAT)
+      };
+    },
+    render : function() {
+      this.rivetsView.sync();
+      return this;
+    },
+  });
 });
