c609560086cf7dd3099aab93b4cae3c6ada4be0b
chris grzegorczyk <grze@eucalyptus.com>
2013-04-26 01:54:36 -0700

    Bound concurrent syscalls; avoid I/O starvation EUCA-3032
    - Add system.exec.restricted_concurrent_ops property
      (default: 2)
    - Add system.exec.max_restricted_concurrent_ops property
      (default: 'dd,tar,gunzip')
    - Restrict number of concurrent calls to anything in the list
      system.exec.restricted_concurrent_ops to no more than
      system.exec.max_restricted_concurrent_ops concurrent executions

--- clc/modules/core/src/main/java/edu/ucsb/eucalyptus/util/SystemUtil.java
+++ clc/modules/core/src/main/java/edu/ucsb/eucalyptus/util/SystemUtil.java
@@ -62,18 +62,130 @@
 
 package edu.ucsb.eucalyptus.util;
 
-import org.apache.log4j.Logger;
-
-import com.eucalyptus.util.EucalyptusCloudException;
-import com.google.common.base.Joiner;
-
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.log4j.Logger;
+import com.eucalyptus.configurable.ConfigurableClass;
+import com.eucalyptus.configurable.ConfigurableField;
+import com.eucalyptus.configurable.ConfigurableProperty;
+import com.eucalyptus.configurable.ConfigurablePropertyException;
+import com.eucalyptus.configurable.PropertyChangeListener;
+import com.eucalyptus.empyrean.Empyrean;
+import com.eucalyptus.system.Threads;
+import com.eucalyptus.util.EucalyptusCloudException;
+import com.eucalyptus.util.Exceptions;
+import com.google.common.base.Joiner;
+import com.google.common.base.Objects;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Primitives;
 
+@ConfigurableClass( root = "system.exec",
+                    description = "Parameters controlling the usage of system processes." )
 public class SystemUtil {
-	private static Logger LOG = Logger.getLogger(SystemUtil.class);
+  private static Logger              LOG                           = Logger.getLogger( SystemUtil.class );
+  
+  @ConfigurableField( description = "Maximum number of concurrent processes which match any of the patterns in system.exec.restricted_concurrent_ops.",
+                      initial = "2",
+                      changeListener = RestrictedOpChangeListener.class )
+  public static Integer              MAX_RESTRICTED_CONCURRENT_OPS = 2;
+  
+  @ConfigurableField( description = "Comma-separated list of commands which are restricted by system.exec.max_restricted_concurrent_ops.",
+                      initial = "dd,gunzip,tar" )
+  public static String               RESTRICTED_CONCURRENT_OPS     = "dd,gunzip,tar";
+  
+  private static final AtomicBoolean acquiringPermits              = new AtomicBoolean( false );
+  
+  private static final Semaphore     concurrentOps                 = new Semaphore( MAX_RESTRICTED_CONCURRENT_OPS );
 
+  public static class RestrictedOpChangeListener implements PropertyChangeListener<String> {
+    @Override
+    public void fireChange( ConfigurableProperty t, String newValueString ) throws ConfigurablePropertyException {
+      int value = Integer.parseInt( t.getValue( ) );
+      int newValue = Integer.parseInt( newValueString );
+      if ( newValue < 1 ) {
+        throw new ConfigurablePropertyException( t.getDisplayName( ) + " must be greater than or equal to 1." );
+      } 
+      final int permitDelta = newValue - value;
+      LOG.debug( "Adjust number of RESTRICTED_CONCURRENT_OPS to be: "
+                 + newValue + " from " + value + "@"
+                 + concurrentOps.availablePermits( ) + "/" + concurrentOps.getQueueLength( )
+                 + " (" + permitDelta + ")" );
+      if ( permitDelta > 0 ) {
+        concurrentOps.release( permitDelta );
+      } else if ( permitDelta == 0 ) {
+        //NOOP.
+      } else if ( permitDelta < 0 ) {
+        //need to remove permits from the semaphore.  this involves calling .acquire() and may block if they are in use.  do it in a thread.
+        if ( acquiringPermits.compareAndSet( false, true ) ) {
+          Threads.lookup( Empyrean.class ).submit( new Runnable( ) {
+            
+            @Override
+            public void run ( ) {
+              for( int i = 0 ; i < Math.abs( permitDelta ); i++ ) {
+                  try {
+                    concurrentOps.acquire( );
+                  } catch ( InterruptedException ex ) {
+                    Exceptions.maybeInterrupted( ex );
+                    break;
+                  }
+              }
+              acquiringPermits.set( false );
+            }
+          } );
+        } else {
+          throw new ConfigurablePropertyException( t.getDisplayName( ) + " is still draining permits from a previous request to change the settings.  Please wait." );
+        }
+      }
+    }    
+  }
+  
+  private static final Supplier<Predicate<CharSequence>> restrictionFilter = Suppliers.memoizeWithExpiration( new Supplier<Predicate<CharSequence>>( ) {
+                                                                             
+                                                                             @Override
+                                                                             public Predicate<CharSequence> get( ) {
+                                                                               List<Predicate<CharSequence>> restrictionFilter = Lists.newArrayList( );
+                                                                               for ( String patternString : Arrays.asList( RESTRICTED_CONCURRENT_OPS.split( "," ) ) ) {
+                                                                                 Predicate<CharSequence> predicate = Predicates.containsPattern( patternString.trim( ) );
+                                                                                 restrictionFilter.add( predicate );
+                                                                               }
+                                                                               return Predicates.or( restrictionFilter );
+                                                                             }
+                                                                           },
+                                                                             30l,
+                                                                             TimeUnit.SECONDS );
+  
+  private static boolean maybeRestrictedOp(String... command) {
+    if ( Iterables.any( Arrays.asList( command ).subList( 0, Math.min( command.length, 2 ) ), restrictionFilter.get( ) ) ) {
+      try {
+        concurrentOps.acquire( );
+        LOG.debug( "Started concurrent op: " + Joiner.on( " " ).join( command ) );
+        return true;
+      } catch ( Exception ex ) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  
+  private static void releaseRestrictedOp(String... command) {
+    concurrentOps.release( );
+    LOG.debug( "Ended concurrent op: " + Joiner.on( " " ).join( command ) );
+  }
+  
 	public static String run(String[] command) {
-		try
+    boolean hasTicket = maybeRestrictedOp( command );
+		try 
 		{
 			String commandString = "";
 			for(String part : command) {
@@ -94,11 +206,14 @@ public class SystemUtil {
 			return output.getReturnValue();
 		} catch (Exception t) {
 			LOG.error(t, t);
+		} finally {
+		  if ( hasTicket ) releaseRestrictedOp( command );
 		}
 		return "";
 	}
 
 	public static int runAndGetCode(String[] command) {
+    boolean hasTicket = maybeRestrictedOp( command );
 		try
 		{
 			String commandString = "";
@@ -116,7 +231,9 @@ public class SystemUtil {
 			return returnValue;
 		} catch (Exception t) {
 			LOG.error(t, t);
-		}
+		} finally {
+      if ( hasTicket ) releaseRestrictedOp( command );
+    }
 		return -1;
 	}
 
