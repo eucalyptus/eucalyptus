a762f07038e4943b88c492470ac67d425b5828c7
dkavanagh <dkavanagh@gmail.com>
2013-04-29 19:25:01 -0400

    make tag collection on models an actual Tags object

--- console/static/js/models/tags.js
+++ console/static/js/models/tags.js
@@ -24,6 +24,21 @@ define([
           }
         });
       }
+    },
+    clone: function(clean, exclude) {  // specifying clean=true indicates only key and value will be copied
+      if (typeof clean != undefined && clean == true) {
+        var ret = new this.constructor();
+        for (var i=0; i<this.length; i+=1) {
+          var tmp = this.at(i);
+          if (typeof exclude != undefined && exclude(tmp)) {
+            continue;
+          }
+          ret.add(new Tag({name:tmp.get('name'), value:tmp.get('value')}));
+        }
+        return ret;
+      } else {
+        return this.clone();
+      }
     }
   });
 });
