24d32be0022d83360a08f5c805b61b03eb541214
Vasiliy Kochergin <vasya@eucalyptus.com>
2013-05-14 15:11:23 -0700

    Added HTML tags and subs {} check to the format UI properties tool

--- console/static/custom/format_properties.py
+++ console/static/custom/format_properties.py
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     import sys
     import os
     import commands
+    import re
     from optparse import OptionParser
     from argparse import ArgumentParser
 
@@ -62,10 +63,11 @@ if __name__ == "__main__":
     if not options.language or len(options.language) <= 0:
         parser.print_help()
         exit(-1)
+
+    # open en_US properties file and build a map
     source_arr = []
     source_map= build_map('./Messages.properties', source_arr);
     
-    # open en_US properties file and build a map
     for lang in options.language:
         print "Processing lanugage: %s" % lang
         dest_path = 'Messages_%s.properties' % lang
@@ -75,6 +77,11 @@ if __name__ == "__main__":
         contents.append('#auto-formatted from Messages_en_US.properties\n')
         for k in source_arr:
             if dest_map.has_key(k):
+                # check if there are any special chars in the destination that do not exist in the source
+                ds = re.sub('[^{}<>]','',dest_map[k])
+                ss = re.sub('[^{}<>]','',source_map[k])
+                if ds != ss:
+                    contents.append('#warning - special characters <> {} mismatch. Original: %s\n' % source_map[k])
                 contents.append('%s = %s\n' % (k, dest_map[k]))
             elif source_map.has_key(k):
                 contents.append('#warning - translation not found\n')
