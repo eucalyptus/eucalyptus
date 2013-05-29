fa0ef5469a6b1f655db4178f4792c29dda91d843
dkavanagh <dkavanagh@gmail.com>
2013-05-29 13:13:26 -0400

    EUCA-6186, expando in better shape

--- console/static/js/views/expandos/scaling.js
+++ console/static/js/views/expandos/scaling.js
@@ -7,9 +7,9 @@ define([
     initialize : function(args) {
       this.template = template;
       var tmp = this.model ? this.model : new Backbone.Model();
-      var id = tmp.get('name');
       this.model = new Backbone.Model();
       this.model.set('group', tmp);
+      this.model.set('current', tmp.get('instances').length);
       this.scope = this.model;
       this._do_init();
     }
