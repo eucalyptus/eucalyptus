5898cfa801e5aea7436fcf18705cc21e405abb1e
dkavanagh <dkavanagh@gmail.com>
2013-06-07 16:40:39 -0700

    EUCA-5651 scaling instances code updated

--- console/static/js/models/scalinginst.js
+++ console/static/js/models/scalinginst.js
@@ -7,57 +7,23 @@ define([
   var model = EucaModel.extend({
     idAttribute: 'instance_id',
     setInstanceHealth: function (health_status, should_respect_grace_period) {
+      var url = "/autoscaling?Action=SetInstanceHealth";
       var id = this.get('instance_id');
       var data = "_xsrf="+$.cookie('_xsrf')+"&InstanceId="+id+
                  "&HealthStatus="+health_status;
       if (should_respect_grace_period != undefined)
         data += "&ShouldRespectGracePeriod=true";
-      $.ajax({
-        type:"POST",
-        url:"/autoscaling?Action=SetInstanceHealth",
-        data:data,
-        dataType:"json",
-        async:true,
-        success:
-          function(data, textStatus, jqXHR){
-            if ( data.results ) {
-              notifySuccess(null, $.i18n.prop('manage_scaling_group_set_health_success', DefaultEncoder().encodeForHTML(id)));
-            } else {
-              notifyError($.i18n.prop('manage_scaling_group_set_health_error', DefaultEncoder().encodeForHTML(id)), undefined_error);
-            }
-          },
-        error:
-          function(jqXHR, textStatus, errorThrown){
-            notifyError($.i18n.prop('manage_scaling_group_set_health_error', DefaultEncoder().encodeForHTML(id)), getErrorMessage(jqXHR));
-          },
-      });
+      this.makeAjaxCall(url, data, options);
     },
     terminateInstance: function (decrement_capacity) {
+      var url = "/autoscaling?Action=TerminateInstanceInAutoScalingGroup";
       var id = this.get('instance_id');
       var data = "_xsrf="+$.cookie('_xsrf')+"&InstanceId="+id+
                  "&ShouldDecrementDesiredCapacity=";
       if (decrement_capacity != undefined)
         data += 'true';
       else data += 'false';
-      $.ajax({
-        type:"POST",
-        url:"/autoscaling?Action=TerminateInstanceInAutoScalingGroup",
-        data:data,
-        dataType:"json",
-        async:true,
-        success:
-          function(data, textStatus, jqXHR){
-            if ( data.results ) {
-              notifySuccess(null, $.i18n.prop('manage_scaling_group_terminate_success', DefaultEncoder().encodeForHTML(id)));
-            } else {
-              notifyError($.i18n.prop('manage_scaling_group_terminate_error', DefaultEncoder().encodeForHTML(id)), undefined_error);
-            }
-          },
-        error:
-          function(jqXHR, textStatus, errorThrown){
-            notifyError($.i18n.prop('manage_scaling_group_terminate_error', DefaultEncoder().encodeForHTML(id)), getErrorMessage(jqXHR));
-          },
-      });
+      this.makeAjaxCall(url, data, options);
     }
   });
   return model;
