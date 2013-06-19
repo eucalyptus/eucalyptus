22000b9e8d63b30b2cb0e9b71249ea5aff2a0566
dkavanagh <dkavanagh@gmail.com>
2013-06-19 09:34:24 -0400

    EUCA-6516 updated vol id to be display_id, changed volume tab to BDM tab for launch config

--- console/static/js/views/expandos/launchconfig.js
+++ console/static/js/views/expandos/launchconfig.js
@@ -10,6 +10,18 @@ define([
       this.model = new Backbone.Model();
       this.model.set('config', tmp);
       this.model.set('image', app.data.images.get(tmp.get('image_id')));
+      var mappings = [];
+      var bdm = tmp.get('block_device_mapping');
+      if (bdm) {
+        if (bdm['/dev/sda']) {
+          this.model.set('snapshot_id', bdm['/dev/sda'].snapshot_id);
+        }
+        for (var device in bdm) {
+          var tmp = bdm[device];
+          mappings.push({device:device, snapshot_id:tmp.snapshot_id, size:tmp.size, delete_on_terminate:tmp.delete_on_termination});
+        }
+      }
+      this.model.set('bdm', mappings);
       this.scope = this.model;
       this._do_init();
     },
