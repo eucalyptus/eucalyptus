2015fe3ea2ec5e0dd28b33822c23ce5ba6e42d48
Sang-Min Park <smpark.uva@gmail.com>
2013-08-06 22:37:59 -0700

    add virtualiza-type in describe-image response

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/images/Images.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/images/Images.java
@@ -300,6 +300,7 @@ public class Images {
       i.setKernelId( arg0.getKernelId( ) );
       i.setRamdiskId( arg0.getRamdiskId( ) );
       i.setPlatform( arg0.getPlatform( ).toString( ) );
+      i.setVirtualizationType(arg0.getVirtualizationType().toString());
       i.getBlockDeviceMappings( ).addAll( Collections2.transform( arg0.getDeviceMappings( ), DeviceMappingDetails.INSTANCE ) );
 //      i.setStateReason( arg0.getStateReason( ) );//TODO:GRZE:NOW
 //      i.setVirtualizationType( arg0.getVirtualizationType( ) );//TODO:GRZE:NOW
