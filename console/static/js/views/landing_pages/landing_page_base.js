5436c885388c4360ebdfb682a77368fb6ccff1e0
Kyo Lee <kyo.lee@eucalyptus.com>
2013-08-20 14:52:08 -0700

    checkbox and expanded row fixes

--- console/static/js/views/landing_pages/landing_page_base.js
+++ console/static/js/views/landing_pages/landing_page_base.js
@@ -89,6 +89,27 @@ define([
             self.activate_more_actions_button();
           };
 
+          this.scope.expand_row = function(context, event){
+            var thisModel = '';
+            var this_id = event.item.id;
+            // SPECIAL CASE FOR EIP LANDING PAGE WHERE THERE IS NO ID FOR THE MODEL
+            if ( self.scope.id === "eips" ){
+              this_id = event.item.get('public_ip');
+              thisModel = self.scope.items.where({public_ip: this_id})[0];
+            }else{
+              thisModel = self.scope.items.get(this_id);
+            }
+            console.log("Clicked to expand: " + this_id);
+            var is_expanded = true;
+            // IF ALREADY EXPANDED, CLOSE IT
+            if( thisModel.get('expanded') === true ){
+              is_expanded = false;
+            }
+            thisModel.set('expanded', is_expanded);
+            self.set_expanded_item(this_id, is_expanded);
+            self.refresh_view();
+          };
+
           // DISPLAY COUNT ADJUSTMENT BAR (TOP-RIGHT) CALLBACK
           this.scope.adjust_display_count = function(context, event){
             console.log("Clicked: " + context.srcElement.innerText);
@@ -183,6 +204,7 @@ define([
           // PROB: REFRESHMENT OF THE COLLECTION ENDS UP REMOVING THE CHECKED LIST - KYO 081613
           this.adjust_page();
           this.recover_checked_items();
+          this.recover_expanded_items();
           this.activate_more_actions_button();
           this.setup_page_info();
           this.render();
@@ -212,8 +234,29 @@ define([
             var is_clicked = item_view.get('clicked');
             console.log("ITEM VIEW ID: " + this_id + " IS_CLICKED: " + is_clicked);
             var this_model = self.scope.items.get(this_id)
+            // SPECIAL CASE FOR EIP LANDING PAGE WHERE THERE IS NO ID FOR THE MODEL
+            if ( self.scope.id === "eips" ){
+                this_model = self.scope.items.where({public_ip: this_id})[0];
+            }
             if( this_model !== undefined ){
               this_model.set('clicked', is_clicked);
+            }
+          })
+        },
+        // IN CASE OF REFRESH, RECOVER THE EXPANDED ITEMS FOR THIS VIEW
+        recover_expanded_items: function(){
+          var self = this;
+          this.scope.item_views.each(function(item_view){
+            var this_id = item_view.get('id');
+            var is_expanded = item_view.get('expanded');
+            console.log("ITEM VIEW ID: " + this_id + " IS_EXPANDED: " + is_expanded);
+            var this_model = self.scope.items.get(this_id)
+            // SPECIAL CASE FOR EIP LANDING PAGE WHERE THERE IS NO ID FOR THE MODEL
+            if ( self.scope.id === "eips" ){
+                this_model = self.scope.items.where({public_ip: this_id})[0];
+            }
+            if( this_model !== undefined ){
+              this_model.set('expanded', is_expanded);
             }
           })
         },
@@ -227,6 +270,18 @@ define([
           }else{
             this_model.set('clicked', is_clicked);
             console.log("THIS MODEL: " + this_id + " CLICKED: " + this_model.get('clicked')); 
+          }  
+        },
+        // TRACK THE EXPANDED STATE OF THIS ITEM IN THE 'ITEM_VIEWS' COLLECTION
+        set_expanded_item: function(this_id, is_expanded){
+          var this_model = this.scope.item_views.get(this_id);
+          if( this_model === undefined ){
+            this_model = new Backbone.Model({id: this_id, expanded: is_expanded});
+            this.scope.item_views.add(this_model);
+            console.log("THIS MODEL: " + this_id + " EXPANDED: " + is_expanded); 
+          }else{
+            this_model.set('expanded', is_expanded);
+            console.log("THIS MODEL: " + this_id + " EXPANDED: " + this_model.get('expanded')); 
           }  
         },
         count_checked_items: function(){
