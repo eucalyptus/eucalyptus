0cf384c9c606b1d9f40f6d7653129962c49415c4
mmunn <mmunn@eucalyptus.com>
2013-11-19 19:49:40 -0800

    EUCA-6429 DNS TCP handler should have read timeout

--- clc/modules/dns/src/main/java/com/eucalyptus/cloud/ws/TCPHandler.java
+++ clc/modules/dns/src/main/java/com/eucalyptus/cloud/ws/TCPHandler.java
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright 2009-2012 Eucalyptus Systems, Inc.
+ * Copyright 2009-2013 Eucalyptus Systems, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -62,7 +62,14 @@
 
 package com.eucalyptus.cloud.ws;
 
+import com.eucalyptus.component.id.Dns;
+import com.eucalyptus.configurable.ConfigurableClass;
+import com.eucalyptus.configurable.ConfigurableField;
+import com.eucalyptus.system.Threads;
 import com.eucalyptus.util.DNSProperties;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+import com.google.common.util.concurrent.TimeLimiter;
+import com.google.common.util.concurrent.UncheckedTimeoutException;
 import org.apache.log4j.Logger;
 import org.xbill.DNS.Message;
 import org.xbill.DNS.Rcode;
@@ -71,30 +78,43 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
-
+@ConfigurableClass( root = "dns.tcp",
+        description = "Handles dns TCP requests." )
 public class TCPHandler extends ConnectionHandler {
-	private static Logger LOG = Logger.getLogger( TCPHandler.class );
-	Socket socket;
-	public TCPHandler(Socket s) {
-		this.socket = s;
-	}
+  @ConfigurableField( description = "Parameter controlling tcp handler timeout in seconds." )
+  public static Integer timeout_seconds = 30;
+  private static Logger LOG = Logger.getLogger( TCPHandler.class );
 
-	public void run() {
-		try {
-			int inputLength;
-			DataInputStream inStream = new DataInputStream(socket.getInputStream());
-			DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
-			inputLength = inStream.readUnsignedShort();
-			if(inputLength > DNSProperties.MAX_MESSAGE_SIZE) {
-				LOG.error("Maximum message size exceeded. Ignoring request.");
-			}
-			byte[] inBytes = new byte[inputLength];
-			inStream.readFully(inBytes);
-			Message query;
-			byte [] response = null;
-			try {
-				query = new Message(inBytes);
+  private final Socket socket;
+
+  public TCPHandler( final Socket s ) {
+    this.socket = s;
+  }
+
+  public void run() {
+    try ( final Socket socket = this.socket ) {
+      socket.setSoTimeout( timeout_seconds * 1000 );
+      final TimeLimiter limiter = new SimpleTimeLimiter( Threads.lookup( Dns.class, TCPHandler.class ) );
+      final byte[] inBytes = limiter.callWithTimeout( new Callable<byte[]>( ) {
+        @Override
+        public byte[] call() throws Exception {
+          final DataInputStream inStream = new DataInputStream(socket.getInputStream());
+          final int inputLength = inStream.readUnsignedShort();
+          if( inputLength > DNSProperties.MAX_MESSAGE_SIZE ) {
+            throw new IOException("Maximum message size exceeded. Ignoring request.");
+          }
+          byte[] inBytes = new byte[inputLength];
+          inStream.readFully(inBytes);
+          return inBytes;
+        }
+      }, timeout_seconds, TimeUnit.SECONDS, false );
+
+      byte [] response = null;
+      try {
+        final Message query = new Message(inBytes);
         ConnectionHandler.setRemoteInetAddress( socket.getInetAddress( ) );
         try {
           response = generateReply(query, inBytes, inBytes.length, socket);
@@ -104,18 +124,19 @@ public class TCPHandler extends ConnectionHandler {
         } finally {
           ConnectionHandler.removeRemoteInetAddress( );
         }
-				if (response == null)
-					return;
-			}
-			catch (IOException exception) {
-				LOG.error(exception);
-			}
-			outStream.writeShort(response.length);
-			outStream.write(response);
-		} catch(IOException ex) {
-			LOG.error(ex);
-		}
-	}
-
-
+        if (response == null)
+          return;
+      } catch ( IOException exception ) {
+        LOG.error(exception);
+      }
+      final DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+      outStream.writeShort(response.length);
+      outStream.write(response);
+      outStream.flush();
+    } catch ( UncheckedTimeoutException e ) {
+      LOG.debug( "Timeout reading request." );
+    } catch(Exception ex) {
+      LOG.error(ex);
+    }
+  }
 }
