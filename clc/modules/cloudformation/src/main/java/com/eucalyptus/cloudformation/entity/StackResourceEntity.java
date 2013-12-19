cd61f23bb62d6472f4f793f382cbe4215ad0c508
Evan Thomas <ethomas@eucalyptus.com>
2013-12-18 17:25:26 -0800

    adding some cloudformation CRUD

---
+++ clc/modules/cloudformation/src/main/java/com/eucalyptus/cloudformation/entity/StackResourceEntity.java
@@ -0,0 +1,135 @@
+package com.eucalyptus.cloudformation.entity;
+
+import com.eucalyptus.entities.AbstractPersistent;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by ethomas on 12/18/13.
+ */
+@Entity
+@PersistenceContext( name = "eucalyptus_cloudformation" )
+@Table( name = "stack_resources" )
+public class StackResourceEntity extends AbstractPersistent {
+
+  @Column(name = "description")
+  String description;
+  @Column(name = "update_timestamp" )
+  Date lastUpdatedTimestamp;
+  @Column(name = "logical_resource_id", nullable = false )
+  String logicalResourceId;
+  @Column(name = "metadata" )
+  @Lob
+  @Type(type="org.hibernate.type.StringClobType")
+  String metadata;
+  @Column(name = "physical_resource_id", nullable = false )
+  String physicalResourceId;
+  @Column(name = "resource_status", nullable = false )
+  @Enumerated(EnumType.STRING)
+  Status resourceStatus;
+  @Column(name = "resource_status_reason" )
+  String resourceStatusReason;
+  @Column(name = "resource_type", nullable = false )
+  String resourceType;
+  @Column(name = "stack_id", nullable = false )
+  String stackId;
+  @Column(name = "stack_name", nullable = false )
+  String stackName;
+
+  public enum Status {
+    CREATE_IN_PROGRESS,
+    CREATE_FAILED,
+    CREATE_COMPLETE,
+    DELETE_IN_PROGRESS,
+    DELETE_FAILED,
+    DELETE_COMPLETE,
+    UPDATE_IN_PROGRESS,
+    UPDATE_FAILED,
+    UPDATE_COMPLETE
+  }
+
+  public StackResourceEntity() {
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Date getLastUpdatedTimestamp() {
+    return lastUpdatedTimestamp;
+  }
+
+  public void setLastUpdatedTimestamp(Date lastUpdatedTimestamp) {
+    this.lastUpdatedTimestamp = lastUpdatedTimestamp;
+  }
+
+  public String getLogicalResourceId() {
+    return logicalResourceId;
+  }
+
+  public void setLogicalResourceId(String logicalResourceId) {
+    this.logicalResourceId = logicalResourceId;
+  }
+
+  public String getMetadata() {
+    return metadata;
+  }
+
+  public void setMetadata(String metadata) {
+    this.metadata = metadata;
+  }
+
+  public String getPhysicalResourceId() {
+    return physicalResourceId;
+  }
+
+  public void setPhysicalResourceId(String physicalResourceId) {
+    this.physicalResourceId = physicalResourceId;
+  }
+
+  public Status getResourceStatus() {
+    return resourceStatus;
+  }
+
+  public void setResourceStatus(Status resourceStatus) {
+    this.resourceStatus = resourceStatus;
+  }
+
+  public String getResourceStatusReason() {
+    return resourceStatusReason;
+  }
+
+  public void setResourceStatusReason(String resourceStatusReason) {
+    this.resourceStatusReason = resourceStatusReason;
+  }
+
+  public String getResourceType() {
+    return resourceType;
+  }
+
+  public void setResourceType(String resourceType) {
+    this.resourceType = resourceType;
+  }
+
+  public String getStackId() {
+    return stackId;
+  }
+
+  public void setStackId(String stackId) {
+    this.stackId = stackId;
+  }
+
+  public String getStackName() {
+    return stackName;
+  }
+
+  public void setStackName(String stackName) {
+    this.stackName = stackName;
+  }
+}
