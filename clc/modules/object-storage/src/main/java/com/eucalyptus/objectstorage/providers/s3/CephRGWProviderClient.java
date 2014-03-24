9ff412bf21b69af91df5069e55cc9fae271984ea
Neil Soman <neil@eucalyptus.com>
2014-03-24 08:16:21 -0700

    add new provider clients for RiakCS and Ceph RGW. Fixes EUCA-8922.

---
+++ clc/modules/object-storage/src/main/java/com/eucalyptus/objectstorage/providers/s3/CephRGWProviderClient.java
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * Copyright 2009-2013 Eucalyptus Systems, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+ * CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+ * additional information or have any questions.
+ ************************************************************************/
+
+package com.eucalyptus.objectstorage.providers.s3;
+
+import com.eucalyptus.objectstorage.providers.ObjectStorageProviders.ObjectStorageProviderClientProperty;
+
+/**
+ * Implementation for Ceph Rados Gateway. Add any additional Ceph RGW specific behavior here.
+ */
+@ObjectStorageProviderClientProperty("ceph-rgw")
+public class CephRGWProviderClient extends S3ProviderClient {
+}
