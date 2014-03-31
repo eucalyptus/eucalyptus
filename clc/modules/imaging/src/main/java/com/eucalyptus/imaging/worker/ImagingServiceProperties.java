b924f54c3acd5d65024184187e683af0660f7939
Sang-Min Park <smpark.uva@gmail.com>
2014-03-31 13:43:08 -0700

    EUCA-8972: remove debug messages when ELB and ImagingService emi is not set

--- clc/modules/imaging/src/main/java/com/eucalyptus/imaging/worker/ImagingServiceProperties.java
+++ clc/modules/imaging/src/main/java/com/eucalyptus/imaging/worker/ImagingServiceProperties.java
@@ -167,10 +167,6 @@ public class ImagingServiceProperties {
         return true;
       } else {
         imageNotConfiguredFaultRunnable.run( );
-        //TODO: the name of the service is TBD, change message later
-        LOG.debug("Imaging service EMI property is unset.  \"\n" +
-            "              + \"Use euca-modify-property -p imaging.imaging_emi=<imaging service emi> \"\n" +
-            "              + \"where the emi should point to the image provided in the eucalyptus-TBD package.\" ");
         return false;
       }
     }
