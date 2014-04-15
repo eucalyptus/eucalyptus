a8c15c4d57e1d456a54f1d1fdf0dddc2341f8004
chris grzegorczyk <grze@eucalyptus.com>
2014-04-15 06:43:45 -0700

    EUCA-8710, EUCA-8924: @TechPreview for ServiceGroups
    - Add @TechPreview annotation for use by ServiceGroups
      during registration
    - Annotate CloudFormation

--- clc/modules/cloudformation-common/src/main/java/com/eucalyptus/cloudformation/CloudFormation.java
+++ clc/modules/cloudformation-common/src/main/java/com/eucalyptus/cloudformation/CloudFormation.java
@@ -26,12 +26,14 @@ import com.eucalyptus.component.annotation.FaultLogPrefix;
 import com.eucalyptus.component.annotation.Partition;
 import com.eucalyptus.component.annotation.PolicyVendor;
 import com.eucalyptus.component.annotation.PublicService;
+import com.eucalyptus.util.techpreview.TechPreview;
 
 @PublicService
 @AwsServiceName( "cloudformation" )
 @PolicyVendor( PolicySpec.VENDOR_CLOUDFORMATION )
 @Partition( value = CloudFormation.class, manyToOne = true )
 @FaultLogPrefix( "services" )
+@TechPreview
 public class CloudFormation extends ComponentId {
   private static final long serialVersionUID = 1L;
 
