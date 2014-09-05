09bdfc5efe99be39b3a6cb63a21fa5e0c0358d69
Garrett Holmstrom <gholms@devzero.com>
2014-09-04 17:43:43 -0700

    Update license headers
    Fixes EUCA-9914 #submit

--- clc/modules/object-storage-common/src/main/java/com/eucalyptus/objectstorage/util/ObjectStorageProperties.java
+++ clc/modules/object-storage-common/src/main/java/com/eucalyptus/objectstorage/util/ObjectStorageProperties.java
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright 2009-2012 Eucalyptus Systems, Inc.
+ * Copyright 2009-2014 Eucalyptus Systems, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -58,6 +58,23 @@
  *   REPLACEMENT OF THE CODE SO IDENTIFIED, LICENSING OF THE CODE SO
  *   IDENTIFIED, OR WITHDRAWAL OF THE CODE CAPABILITY TO THE EXTENT
  *   NEEDED TO COMPLY WITH ANY SUCH LICENSES OR RIGHTS.
+ *
+ * This file may incorporate work covered under the following copyright
+ * and permission notice:
+ *
+ *   Copyright 2010-2014 Amazon.com, Inc. or its affiliates. All Rights
+ *   Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *   http://aws.amazon.com/apache2.0
+ *
+ *   or in the "license" file accompanying this file. This file is
+ *   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ *   ANY KIND, either express or implied. See the License for the specific
+ *   language governing permissions and limitations under the License.
  ************************************************************************/
 
 package com.eucalyptus.objectstorage.util;
