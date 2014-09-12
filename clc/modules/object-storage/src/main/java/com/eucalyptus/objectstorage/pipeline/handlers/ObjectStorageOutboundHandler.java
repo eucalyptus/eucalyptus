1749bb96b01eb0c833b5a069506e971388932492
Swathi Gangisetty <swathi@eucalyptus.com>
2014-09-11 17:49:18 -0700

    Fix S3 responses from OSG
    Fixes EUCA-9939 and EUCA-9966

--- clc/modules/object-storage/src/main/java/com/eucalyptus/objectstorage/pipeline/handlers/ObjectStorageOutboundHandler.java
+++ clc/modules/object-storage/src/main/java/com/eucalyptus/objectstorage/pipeline/handlers/ObjectStorageOutboundHandler.java
@@ -62,17 +62,8 @@
 
 package com.eucalyptus.objectstorage.pipeline.handlers;
 
-import com.eucalyptus.http.MappingHttpResponse;
-import com.eucalyptus.objectstorage.msgs.CopyObjectResponseType;
-import com.eucalyptus.objectstorage.msgs.CreateBucketResponseType;
-import com.eucalyptus.objectstorage.msgs.ObjectStorageDataResponseType;
-import com.eucalyptus.objectstorage.msgs.PostObjectResponseType;
-import com.eucalyptus.objectstorage.msgs.PutObjectResponseType;
-import com.eucalyptus.objectstorage.util.OSGUtil;
-import com.eucalyptus.objectstorage.util.ObjectStorageProperties;
-import com.eucalyptus.storage.common.DateFormatter;
-import com.eucalyptus.ws.handlers.MessageStackHandler;
-import edu.ucsb.eucalyptus.msgs.BaseMessage;
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 import org.jboss.netty.channel.ChannelEvent;
 import org.jboss.netty.channel.ChannelFutureListener;
@@ -83,93 +74,130 @@ import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.handler.codec.http.HttpHeaders;
 import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 
+import com.eucalyptus.http.MappingHttpResponse;
+import com.eucalyptus.objectstorage.msgs.AbortMultipartUploadResponseType;
+import com.eucalyptus.objectstorage.msgs.CopyObjectResponseType;
+import com.eucalyptus.objectstorage.msgs.CreateBucketResponseType;
+import com.eucalyptus.objectstorage.msgs.ObjectStorageDataGetResponseType;
+import com.eucalyptus.objectstorage.msgs.ObjectStorageDataResponseType;
+import com.eucalyptus.objectstorage.msgs.ObjectStorageResponseType;
+import com.eucalyptus.objectstorage.msgs.PostObjectResponseType;
+import com.eucalyptus.objectstorage.msgs.PutObjectResponseType;
+import com.eucalyptus.objectstorage.msgs.SetBucketAccessControlPolicyResponseType;
+import com.eucalyptus.objectstorage.msgs.SetBucketLifecycleResponseType;
+import com.eucalyptus.objectstorage.msgs.SetBucketLoggingStatusResponseType;
+import com.eucalyptus.objectstorage.msgs.SetBucketVersioningStatusResponseType;
+import com.eucalyptus.objectstorage.msgs.SetObjectAccessControlPolicyResponseType;
+import com.eucalyptus.objectstorage.msgs.UploadPartResponseType;
+import com.eucalyptus.objectstorage.util.ObjectStorageProperties;
+import com.eucalyptus.storage.common.DateFormatter;
+import com.eucalyptus.ws.handlers.MessageStackHandler;
+
+import edu.ucsb.eucalyptus.msgs.BaseMessage;
+
 @ChannelPipelineCoverage("one")
 public class ObjectStorageOutboundHandler extends MessageStackHandler {
-    @Override
-    public void handleUpstream(ChannelHandlerContext ctx,
-                               ChannelEvent channelEvent) throws Exception {
-        if (channelEvent instanceof ExceptionEvent) {
-            exceptionCaught(ctx, (ExceptionEvent) channelEvent);
-        } else {
-            ctx.sendUpstream(channelEvent);
-        }
-    }
+	private static Logger LOG = Logger.getLogger(ObjectStorageOutboundHandler.class);
 
-    private void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
-        //The next stage should be the exception handler that will catch this, marshall as needed, and send it back down.
-        ctx.sendUpstream(e);
-    }
+	@Override
+	public void handleUpstream(ChannelHandlerContext ctx, ChannelEvent channelEvent) throws Exception {
+		if (channelEvent instanceof ExceptionEvent) {
+			exceptionCaught(ctx, (ExceptionEvent) channelEvent);
+		} else {
+			ctx.sendUpstream(channelEvent);
+		}
+	}
 
-    @Override
-    public void handleDownstream(ChannelHandlerContext ctx,
-                                 ChannelEvent channelEvent) throws Exception {
-        if (channelEvent instanceof MessageEvent) {
-            final MessageEvent msgEvent = (MessageEvent) channelEvent;
-            this.outgoingMessage(ctx, msgEvent);
-        }
-        ctx.sendDownstream(channelEvent);
-    }
+	private void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+		// The next stage should be the exception handler that will catch this, marshall as needed, and send it back down.
+		ctx.sendUpstream(e);
+	}
 
-    private static Logger LOG = Logger.getLogger(ObjectStorageOutboundHandler.class);
+	@Override
+	public void handleDownstream(ChannelHandlerContext ctx, ChannelEvent channelEvent) throws Exception {
+		if (channelEvent instanceof MessageEvent) {
+			final MessageEvent msgEvent = (MessageEvent) channelEvent;
+			this.outgoingMessage(ctx, msgEvent);
+		}
+		ctx.sendDownstream(channelEvent);
+	}
 
-    @Override
-    public void outgoingMessage(ChannelHandlerContext ctx, MessageEvent event) throws Exception {
-        if (event.getMessage() instanceof MappingHttpResponse) {
-            MappingHttpResponse httpResponse = (MappingHttpResponse) event.getMessage();
-            BaseMessage msg = (BaseMessage) httpResponse.getMessage();
+	@Override
+	public void outgoingMessage(ChannelHandlerContext ctx, MessageEvent event) throws Exception {
+		if (event.getMessage() instanceof MappingHttpResponse) {
+			MappingHttpResponse httpResponse = (MappingHttpResponse) event.getMessage();
+			BaseMessage msg = (BaseMessage) httpResponse.getMessage();
 
-            if (msg instanceof PutObjectResponseType) {
-                PutObjectResponseType putObjectResponse = (PutObjectResponseType) msg;
-                httpResponse.setHeader(HttpHeaders.Names.ETAG, '\"' + putObjectResponse.getEtag() + '\"');
-                if (putObjectResponse.getLastModified() != null) {
-                    httpResponse.setHeader(HttpHeaders.Names.LAST_MODIFIED, DateFormatter.dateToHeaderFormattedString(putObjectResponse.getLastModified()));
-                }
-                if (putObjectResponse.getVersionId() != null) {
-                    httpResponse.setHeader(ObjectStorageProperties.X_AMZ_VERSION_ID, putObjectResponse.getVersionId());
-                }
-            } else if (msg instanceof PostObjectResponseType) {
-                PostObjectResponseType postObjectResponse = (PostObjectResponseType) msg;
-                String redirectUrl = postObjectResponse.getRedirectUrl();
-                if (redirectUrl != null) {
-                    httpResponse.addHeader(HttpHeaders.Names.LOCATION, redirectUrl);
-                    httpResponse.setStatus(HttpResponseStatus.SEE_OTHER);
-                    httpResponse.setMessage(null);
-                } else {
-                    Integer successCode = postObjectResponse.getSuccessCode();
-                    if (successCode != null) {
-                        if (successCode != 201) {
-                            httpResponse.setMessage(null);
-                            httpResponse.setStatus(new HttpResponseStatus(successCode, "OK"));
-                        } else {
-                            httpResponse.setStatus(new HttpResponseStatus(successCode, "Created"));
-                        }
-                    }
-                }
-                //have to force a close for browsers
-                event.getFuture().addListener(ChannelFutureListener.CLOSE);
-            } else if (msg instanceof ObjectStorageDataResponseType) {
-                ObjectStorageDataResponseType response = (ObjectStorageDataResponseType) msg;
-                if (response.getEtag() != null) {
-                    httpResponse.addHeader(HttpHeaders.Names.ETAG, '\"' + response.getEtag() + '\"');
-                }
-                if (response.getLastModified() != null) {
-                    httpResponse.addHeader(HttpHeaders.Names.LAST_MODIFIED, DateFormatter.dateToHeaderFormattedString(response.getLastModified()));
-                }
-                if (response.getVersionId() != null) {
-                    httpResponse.addHeader(ObjectStorageProperties.X_AMZ_VERSION_ID, response.getVersionId());
-                }
-            } else if (msg instanceof CopyObjectResponseType) {
-                CopyObjectResponseType copyResponse = (CopyObjectResponseType) msg;
-                if (copyResponse.getVersionId() != null)
-                    httpResponse.addHeader("x-amz-version-id", copyResponse.getVersionId());
-                if (copyResponse.getCopySourceVersionId() != null)
-                    httpResponse.addHeader("x-amz-copy-source-version-id", copyResponse.getCopySourceVersionId());
-            } else if (msg instanceof CreateBucketResponseType) {
-                httpResponse.setStatus(HttpResponseStatus.OK);
-                httpResponse.setMessage(null);
-                event.getFuture().addListener(ChannelFutureListener.CLOSE);
-            }
-        }
-    }
+			// Ordering if-else conditions from most to least restrictive i.e. narrow to broad filters
+			if (msg instanceof PostObjectResponseType) {
+				PostObjectResponseType postObjectResponse = (PostObjectResponseType) msg;
+				String redirectUrl = postObjectResponse.getRedirectUrl();
+				if (redirectUrl != null) {
+					httpResponse.addHeader(HttpHeaders.Names.LOCATION, redirectUrl);
+					httpResponse.setStatus(HttpResponseStatus.SEE_OTHER);
+					httpResponse.setMessage(null);
+				} else {
+					Integer successCode = postObjectResponse.getSuccessCode();
+					if (successCode != null) {
+						if (successCode != 201) {
+							httpResponse.setMessage(null);
+							httpResponse.setStatus(new HttpResponseStatus(successCode, "OK"));
+						} else {
+							httpResponse.setStatus(new HttpResponseStatus(successCode, "Created"));
+						}
+					}
+				}
+				// have to force a close for browsers
+				event.getFuture().addListener(ChannelFutureListener.CLOSE);
+			} else if (msg instanceof CopyObjectResponseType) {
+				CopyObjectResponseType copyResponse = (CopyObjectResponseType) msg;
+				if (copyResponse.getVersionId() != null)
+					httpResponse.addHeader("x-amz-version-id", copyResponse.getVersionId());
+				if (copyResponse.getCopySourceVersionId() != null)
+					httpResponse.addHeader("x-amz-copy-source-version-id", copyResponse.getCopySourceVersionId());
+			} else if (msg instanceof CreateBucketResponseType) {
+				httpResponse.setStatus(HttpResponseStatus.OK);
+				removeResponseBody(msg, httpResponse);
+				event.getFuture().addListener(ChannelFutureListener.CLOSE);
+			} else if (msg instanceof ObjectStorageDataResponseType) { // Filter for GETs and PUTs related to data
+				ObjectStorageDataResponseType response = (ObjectStorageDataResponseType) msg;
+				if (response.getEtag() != null) {
+					httpResponse.setHeader(HttpHeaders.Names.ETAG, '\"' + response.getEtag() + '\"');
+				}
+				if (response.getVersionId() != null) {
+					httpResponse.setHeader(ObjectStorageProperties.X_AMZ_VERSION_ID, response.getVersionId());
+				}
+				if (msg instanceof ObjectStorageDataGetResponseType && response.getLastModified() != null) {
+					httpResponse.setHeader(HttpHeaders.Names.LAST_MODIFIED, DateFormatter.dateToHeaderFormattedString(response.getLastModified()));
+				}
+				// Remove the content in response for certain operations
+				if (msg instanceof PutObjectResponseType || msg instanceof UploadPartResponseType) {
+					removeResponseBody(msg, httpResponse);
+				}
+			} else if (msg instanceof ObjectStorageResponseType) { // Filter for GETs and PUTs *NOT* related to data
+				// TODO uncomment when PUT object acl response is fixed and contains version ID
+				// if (msg instanceof SetObjectAccessControlPolicyResponseType && ((SetObjectAccessControlPolicyResponseType) msg).getVersionId() != null) {
+				// httpResponse.setHeader(ObjectStorageProperties.X_AMZ_VERSION_ID, ((SetObjectAccessControlPolicyResponseType) msg).getVersionId());
+				// }
+				// Remove the content in response for certain operations
+				if (msg instanceof SetBucketAccessControlPolicyResponseType || msg instanceof SetBucketLifecycleResponseType
+						|| msg instanceof SetBucketLoggingStatusResponseType || msg instanceof SetBucketVersioningStatusResponseType
+						|| msg instanceof SetObjectAccessControlPolicyResponseType) {
+					removeResponseBody(msg, httpResponse);
+				}
+			}
+		}
+	}
 
+	private void removeResponseBody(BaseMessage msg, MappingHttpResponse httpResponse) {
+		// Populate all the common fields. These fields normally get populated in the outbound stage of the parent handler but get skipped in this case due to
+		// an empty message body
+		httpResponse.setHeader(HttpHeaders.Names.DATE, DateFormatter.dateToHeaderFormattedString(new Date()));
+		httpResponse.setHeader(HttpHeaders.Names.CONTENT_LENGTH, String.valueOf(0));
+		if (msg.getCorrelationId() != null) {
+			httpResponse.setHeader(ObjectStorageProperties.AMZ_REQUEST_ID, msg.getCorrelationId());
+		}
+		// Null the message body to remove it
+		httpResponse.setMessage(null);
+	}
 }
