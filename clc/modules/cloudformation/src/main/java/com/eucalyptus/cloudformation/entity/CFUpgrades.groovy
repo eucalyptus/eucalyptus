749c8919ccc68df6ff2a9b2928f4504970f6407f
Evan Thomas <ethomas@eucalyptus.com>
2014-11-19 17:21:06 -0800

    EUCA-9760 Widened columns physical_resource_id in stack_events and stack_resources (CF) to 'text'.  Also widened template_body and stack_policy in stacks to 'text'

---
+++ clc/modules/cloudformation/src/main/java/com/eucalyptus/cloudformation/entity/CFUpgrades.groovy
@@ -0,0 +1,43 @@
+package com.eucalyptus.cloudformation.entity
+
+import com.eucalyptus.component.id.Eucalyptus
+import groovy.sql.Sql
+import groovy.transform.CompileStatic
+import groovy.transform.TypeCheckingMode
+import org.apache.log4j.Logger
+
+import java.util.concurrent.Callable
+
+/**
+ * Created by ethomas on 11/19/14.
+ */
+public class CFUpgrades {
+
+  @com.eucalyptus.upgrade.Upgrades.PreUpgrade( value = Eucalyptus.class, since = com.eucalyptus.upgrade.Upgrades.Version.v4_1_0 )
+  @CompileStatic( TypeCheckingMode.SKIP )
+  public static class CloudFormation410PreUpgrade implements Callable<Boolean> {
+    private static Logger LOG = Logger.getLogger( CloudFormation410PreUpgrade.class );
+
+    @Override
+    public Boolean call( ) throws Exception {
+      Sql sql = null;
+      try {
+        sql = com.eucalyptus.upgrade.Upgrades.DatabaseFilters.NEWVERSION.getConnection( "eucalyptus_cloudformation" );
+        sql.execute( "ALTER TABLE stacks ALTER COLUMN stack_policy TYPE text" );
+        sql.execute( "ALTER TABLE stacks ALTER COLUMN template_body TYPE text" );
+        sql.execute( "ALTER TABLE stack_events ALTER COLUMN physical_resource_id TYPE text" );
+        sql.execute( "ALTER TABLE stack_resources ALTER COLUMN physical_resource_id TYPE text" );
+        return true;
+      } catch ( Exception ex ) {
+        LOG.error( ex, ex );
+        return false;
+      } finally {
+        if ( sql != null ) {
+          sql.close( );
+        }
+      }
+    }
+  }
+
+
+}
