7bc09206380533289083fa874a99307e5e184549
Steve Jones <steve.jones@eucalyptus.com>
2015-05-05 18:24:31 -0700

    EC2 VPC account initialization now occurs on service use - EUCA-10809

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/compute/vpc/VpcComputeAccountInitializer.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/compute/vpc/VpcComputeAccountInitializer.java
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright 2009-2014 Eucalyptus Systems, Inc.
+ * Copyright 2009-2015 Eucalyptus Systems, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,23 +19,18 @@
  ************************************************************************/
 package com.eucalyptus.compute.vpc;
 
-import java.util.concurrent.ExecutionException;
 import org.apache.log4j.Logger;
 import com.eucalyptus.auth.Accounts;
-import com.eucalyptus.auth.euare.events.AccountCreatedEvent;
 import com.eucalyptus.auth.policy.key.Keys;
 import com.eucalyptus.component.ServiceConfiguration;
 import com.eucalyptus.component.Topology;
 import com.eucalyptus.compute.common.Compute;
-import com.eucalyptus.compute.common.CreateVpcResponseType;
 import com.eucalyptus.compute.common.CreateVpcType;
+import com.eucalyptus.compute.common.internal.account.ComputeAccounts;
 import com.eucalyptus.compute.common.network.Networking;
 import com.eucalyptus.compute.common.network.NetworkingFeature;
-import com.eucalyptus.event.EventListener;
-import com.eucalyptus.event.Listeners;
 import com.eucalyptus.util.Internets;
 import com.eucalyptus.util.async.AsyncRequests;
-import com.eucalyptus.util.concurrent.ListenableFuture;
 import com.google.common.collect.Lists;
 import edu.ucsb.eucalyptus.msgs.BaseCallerContext;
 import edu.ucsb.eucalyptus.msgs.EvaluatedIamConditionKey;
@@ -43,41 +38,25 @@ import edu.ucsb.eucalyptus.msgs.EvaluatedIamConditionKey;
 /**
  *
  */
-public class VpcNewAccountEventListener implements EventListener<AccountCreatedEvent> {
+public class VpcComputeAccountInitializer implements ComputeAccounts.ComputeAccountInitializer {
 
-  private static final Logger logger = Logger.getLogger( VpcNewAccountEventListener.class );
-
-  public static void register( ) {
-    Listeners.register( AccountCreatedEvent.class, new VpcNewAccountEventListener() );
-  }
+  private final Logger logger = Logger.getLogger( VpcComputeAccountInitializer.class );
 
   @Override
-  public void fireEvent( final AccountCreatedEvent event ) {
+  public void initialize( final String accountNumber ) {
     try {
-      if ( VpcConfiguration.getDefaultVpc( ) && Networking.getInstance( ).supports( NetworkingFeature.Vpc ) ) {
+      if ( VpcConfiguration.getDefaultVpc( ) && Networking.getInstance().supports( NetworkingFeature.Vpc ) ) {
         final ServiceConfiguration config = Topology.lookup( Compute.class );
         final CreateVpcType createVpc = new CreateVpcType( );
-        createVpc.setCidrBlock( event.getMessage( ) );
-        createVpc.setUserId( Accounts.lookupSystemAdmin( ).getUserId( ) );
+        createVpc.setCidrBlock( accountNumber );
+        createVpc.setUserId( Accounts.lookupSystemAdmin().getUserId( ) );
         createVpc.setCallerContext( new BaseCallerContext( Lists.newArrayList(
-            new EvaluatedIamConditionKey( Keys.AWS_SOURCEIP, Internets.localHostAddress( ) )
+            new EvaluatedIamConditionKey( Keys.AWS_SOURCEIP, Internets.localHostAddress() )
         ) ) );
-        final ListenableFuture<CreateVpcResponseType> dispatchFuture = AsyncRequests.dispatch( config, createVpc );
-        dispatchFuture.addListener( new Runnable( ) {
-          @Override
-          public void run() {
-            try {
-              dispatchFuture.get( );
-            } catch ( final InterruptedException e ) {
-              logger.info( "Interrupted while creating default vpc for account " + event.getMessage( ), e );
-            } catch ( final ExecutionException e ) {
-              logger.error( "Error creating default vpc for account " + event.getMessage( ), e );
-            }
-          }
-        } );
+        AsyncRequests.sendSync( config, createVpc );
       }
     } catch ( final Exception e ) {
-      logger.error( "Error creating default vpc for account " + event.getMessage( ), e );
+      logger.error( "Error creating default vpc for account " + accountNumber, e );
     }
   }
 }
