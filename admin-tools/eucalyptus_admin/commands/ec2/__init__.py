4785f76881f8cf49dda1912066621f00dfb972a7
Garrett Holmstrom <gholms@devzero.com>
2015-06-24 16:01:49 -0700

    Implement empyrean-describe-node-controllers
    Fixes EUCA-11016

---
+++ admin-tools/eucalyptus_admin/commands/ec2/__init__.py
@@ -0,0 +1,49 @@
+# Copyright 2015 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg
+import requestbuilder.auth.aws
+import requestbuilder.request
+import requestbuilder.service
+
+from eucalyptus_admin.commands import EucalyptusAdmin
+
+
+class EC2(requestbuilder.service.BaseService):
+    NAME = 'ec2'
+    DESCRIPTION = 'Compute service'
+    REGION_ENVVAR = 'AWS_DEFAULT_REGION'
+    URL_ENVVAR = 'EC2_URL'
+
+    ARGS = [Arg('-U', '--url', metavar='URL',
+                help='compute service endpoint URL')]
+
+
+class EC2Request(requestbuilder.request.AWSQueryRequest):
+    SUITE = EucalyptusAdmin
+    SERVICE_CLASS = EC2
+    AUTH_CLASS = requestbuilder.auth.aws.HmacV4Auth
+    API_VERSION = '2014-10-01'
+    METHOD = 'POST'
---
+++ admin-tools/eucalyptus_admin/commands/empyrean/describenodecontrollers.py
@@ -0,0 +1,81 @@
+# Copyright 2015 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg, Filter, MutuallyExclusiveArgList
+
+from eucalyptus_admin.commands.ec2.describeinstances import DescribeInstances
+from eucalyptus_admin.commands.empyrean import EmpyreanRequest
+from eucalyptus_admin.commands.empyrean.describeservices import \
+    DescribeServices
+from eucalyptus_admin.commands.mixins import TableOutputMixin
+
+
+class DescribeNodeControllers(EmpyreanRequest, TableOutputMixin):
+    DESCRIPTION = "List the cloud's node controllers and their instances"
+
+    def main(self):
+        req = DescribeServices.from_other(self, ListAll=True)
+        services = req.main().get('serviceStatuses') or []
+        nodes = {}
+        for service in services:
+            svcid = service['serviceId']
+            if svcid.get('type') == 'node':
+                nodes[svcid.get('name')] = {
+                    'zone': svcid.get('partition'),
+                    'host': svcid.get('host'),
+                    'state': service.get('localState'),
+                    'details': service.get('details') or {},
+                    'instances': {}}
+        req = DescribeInstances.from_other(self, InstanceId=['verbose'])
+        reservations = req.main().get('reservationSet')
+        for reservation in reservations:
+            for instance in reservation.get('instancesSet') or []:
+                inst_info = {}
+                for tag in instance.get('tagSet') or []:
+                    if tag['key'] == 'euca:node' and tag['value'] in nodes:
+                        node = nodes[tag['value']]
+                        node['instances'][instance['instanceId']] = inst_info
+                    elif tag['key'] == 'euca:node:migration:destination':
+                        inst_info['migration_dest'] = tag['value']
+                    # 'euca:node:migration:source' also exists
+        return nodes
+
+    def print_result(self, nodes):
+        for node_name, node in sorted(nodes.items()):
+            table = self.get_table(('NODE', 'zone', 'host', 'state',
+                                    'details'))
+            table.add_row(('NODE', node['zone'], node['host'],
+                           node['state'].lower(),
+                           ', '.join('='.join((key, val)) for key, val in
+                           node['details'].iteritems())))
+            print table
+            table = self.get_table(('INSTANCE', 'ID', 'MIGRATING-TO', 'dest'))
+            for instance_id, instance in node['instances'].iteritems():
+                if 'migration_dest' in instance:
+                    table.add_row(('INSTANCE', instance_id, 'MIGRATING-TO',
+                                   instance['migration_dest']))
+                else:
+                    table.add_row(('INSTANCE', instance_id, '', ''))
+            print table
