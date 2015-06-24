b8a942fc1a0746523f68d690fc6314376a784844
Garrett Holmstrom <gholms@devzero.com>
2015-06-24 16:22:34 -0700

    Implement empyrean-migrate-instances
    I don't like this name because it's mainly a compute service command.
    Better suggestions are definitely welcome.
    
    Fixes EUCA-11018

---
+++ admin-tools/eucalyptus_admin/commands/ec2/migrateinstances.py
@@ -0,0 +1,70 @@
+# Copyright 2015 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg, MutuallyExclusiveArgList
+from requestbuilder.exceptions import ArgumentError
+
+from eucalyptus_admin.commands.ec2 import EC2Request
+from eucalyptus_admin.commands.empyrean.modifyservice import ModifyService
+
+
+class MigrateInstances(EC2Request):
+    DESCRIPTION = ('Migrate one instance from its current host, or '
+                   'migrate all instances from a specific host')
+    ARGS = [MutuallyExclusiveArgList(
+                Arg('-s', '--source', dest='SourceHost', metavar='HOST',
+                    help='remove all instances from a specific host'),
+                Arg('-i', '--instance', dest='InstanceId', metavar='INSTANCE',
+                    help='remove one instance from its current host'))
+            .required(),
+            MutuallyExclusiveArgList(
+                Arg('--include-dest', action='append', route_to=None,
+                    metavar='HOST', help='''allow migration to only a
+                    specific host (may be used more than once)'''),
+                Arg('--exclude-dest', action='append', route_to=None,
+                    metavar='HOST', help='''allow migration to any host
+                    except a specific one (may be used more than once)''')),
+            Arg('--stop-source', action='store_true', route_to=None,
+                help='''also stop the source host to prevent new instances
+                from running on it (requires -s/--source)''')]
+
+    def configure(self):
+        EC2Request.configure(self)
+        if self.args.get('stop_source') and not self.args.get('source'):
+            raise ArgumentError('argument --stop-source: only allowed '
+                                'with -s/--source')
+
+    def preprocess(self):
+        if self.args.get('include_dest'):
+            self.params['AllowHosts'] = True
+            self.params['DestinationHost'] = self.args['include_dest']
+        elif self.args.get('exclude_dest'):
+            self.params['AllowHosts'] = False
+            self.params['DestinationHost'] = self.args['exclude_dest']
+
+    def postprocess(self):
+        req = ModifyService.from_other(self, Name=self.args['source'],
+                                       State='STOP')
+        req.main()
