f27903e44aa243f3426f4c808131c0cfc4e49bc0
Evan Thomas <ethomas@eucalyptus.com>
2015-08-16 21:52:19 -0700

    EUCA-11050 Created a new way to update absolute metrics.  Rather than repeated queries against new data points, and updates/inserts based on values found in the db, one big full table scan is done. Values returned from the table that are in the new set are updated and removed from the new set, values not in the new set are left alone, and at the end, values in the new set are inserted.  (clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/reporting/FullTableScanAbsoluteMetricConverter.java). This allows batch operations.  The original method was refactored (clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/reporting/DefaultAbsoluteMetricConverter.java), and now both methods can be chosen by changing a single line of code.

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/reporting/AbsoluteMetricHelper.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/reporting/AbsoluteMetricHelper.java
@@ -20,6 +20,7 @@
 
 package com.eucalyptus.cluster.callback.reporting;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
@@ -27,17 +28,109 @@ import java.util.concurrent.TimeUnit;
 
 import javax.persistence.EntityTransaction;
 
+import com.eucalyptus.cloudwatch.common.internal.domain.metricdata.Units;
+import com.eucalyptus.cloudwatch.common.msgs.Dimension;
+import com.eucalyptus.cloudwatch.common.msgs.Dimensions;
+import com.eucalyptus.cloudwatch.common.msgs.MetricDatum;
+import com.eucalyptus.cloudwatch.common.msgs.StatisticSet;
+import com.google.common.collect.ImmutableMap;
 import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 
-import com.eucalyptus.cluster.callback.reporting.AbsoluteMetricQueue.AbsoluteMetricCache;
+import com.eucalyptus.cluster.callback.reporting.DefaultAbsoluteMetricConverter.AbsoluteMetricCache;
 import com.eucalyptus.entities.Entities;
 import com.eucalyptus.records.Logs;
 
 public class AbsoluteMetricHelper {
+  static final double TOLERANCE = 0.0000001; // arbitrary to check double "equality"
+  static final Map<String, String> EBS_ABSOLUTE_METRICS =
+    new ImmutableMap.Builder<String, String>()
+      .put("VolumeReadOpsAbsolute", "VolumeReadOps")
+      .put("VolumeWriteOpsAbsolute", "VolumeWriteOps")
+      .put("VolumeReadBytesAbsolute", "VolumeReadBytes")
+      .put("VolumeWriteBytesAbsolute", "VolumeWriteBytes")
+      .put("VolumeConsumedReadWriteOpsAbsolute", "VolumeConsumedReadWriteOps")
+      .put("VolumeTotalReadTimeAbsolute", "VolumeTotalReadTime")
+      .put("VolumeTotalWriteTimeAbsolute", "VolumeTotalWriteTime")
+      .put("VolumeTotalReadWriteTimeAbsolute", "VolumeTotalReadWriteTime")
+      .build();
+  static final Map<String, String> EC2_ABSOLUTE_METRICS =
+    new ImmutableMap.Builder<String, String>()
+      .put("DiskReadOpsAbsolute", "DiskReadOps")
+      .put("DiskWriteOpsAbsolute", "DiskWriteOps")
+      .put("DiskReadBytesAbsolute", "DiskReadBytes")
+      .put("DiskWriteBytesAbsolute", "DiskWriteBytes")
+      .put("NetworkInAbsolute", "NetworkIn")
+      .put("NetworkOutAbsolute", "NetworkOut")
+      .build();
+  static final long MAX_DIFFERENCE_DURATION_MS = TimeUnit.MINUTES.toMillis(15L); // 15 minutes, also max reporting time
+  static final String AWS_EBS_NAMESPACE = "AWS/EBS";
+  static final String VOLUME_ID_DIM_NAME = "VolumeId";
+  static final String AWS_EC2_NAMESPACE = "AWS/EC2";
+  static final String INSTANCE_ID_DIM_NAME = "InstanceId";
+  static final String VOLUME_READ_OPS_METRIC_NAME = "VolumeReadOps";
+  static final String VOLUME_QUEUE_LENGTH_METRIC_NAME = "VolumeQueueLength";
+  static final String VOLUME_TOTAL_READ_WRITE_TIME_METRIC_NAME = "VolumeTotalReadWriteTime";
+  static final String VOLUME_QUEUE_LENGTH_PLACEHOLDER_METRIC_NAME = "VolumeQueueLengthPlaceHolder";
+
+  static final String VOLUME_QUEUE_LENGTH_PLACEHOLDER_ABSOLUTE_METRIC_NAME = "VolumeQueueLengthPlaceHolderAbsolute";
+
+
+  static final String CPU_UTILIZATION_MS_ABSOLUTE_METRIC_NAME = "CPUUtilizationMSAbsolute";
+  static final String CPU_UTILIZATION_METRIC_NAME = "CPUUtilization";
   private static final Logger LOG = Logger.getLogger(AbsoluteMetricHelper.class);
-  private static final long MAX_DIFFERENCE_DURATION_MS = TimeUnit.MINUTES.toMillis(15L); // 15 minutes, also max reporting time
+
+  static void convertVolumeTotalReadWriteTimeToVolumeIdleTime(final MetricDatum datum) {
+    // we convert this to VolumeIdleTime = Period Length - VolumeTotalReadWriteTime on the period (though won't be negative)
+    datum.setMetricName("VolumeIdleTime");
+    double totalReadWriteTime = datum.getStatisticValues().getSum(); // value is in seconds
+    double totalPeriodTime = 60.0 * datum.getStatisticValues().getSampleCount();
+    double totalIdleTime = totalPeriodTime - totalReadWriteTime;
+    if (totalIdleTime < 0) totalIdleTime = 0; // if we have read and written more than in the period, don't go negative
+    datum.getStatisticValues().setSum(totalIdleTime);
+    double averageIdleTime = totalIdleTime / datum.getStatisticValues().getSampleCount();
+    datum.getStatisticValues().setMaximum(averageIdleTime);
+    datum.getStatisticValues().setMinimum(averageIdleTime);
+  }
+
+  static AbsoluteMetricQueueItem createVolumeThroughputMetric(String accountId, String nameSpace, MetricDatum datum) {
+    // add volume throughput percentage.  (The guess is that there will be a set of volume metrics.
+    // Attach it to one so it will be sent as many times as the others.
+    // add one
+    MetricDatum vtpDatum = new MetricDatum();
+    vtpDatum.setMetricName("VolumeThroughputPercentage");
+    vtpDatum.setTimestamp(datum.getTimestamp());
+    vtpDatum.setUnit(Units.Percent.toString());
+    // should be 100% but weigh it the same
+    if (datum.getValue() != null) {
+      vtpDatum.setValue(100.0); // Any time we have a volume data, this value is 100%
+    } else if (datum.getStatisticValues() != null) {
+      StatisticSet statisticSet = new StatisticSet();
+      statisticSet.setMaximum(100.0);
+      statisticSet.setMinimum(100.0);
+      statisticSet.setSum(100.0 * datum.getStatisticValues().getSampleCount());
+      statisticSet.setSampleCount(datum.getStatisticValues().getSampleCount());
+      vtpDatum.setStatisticValues(statisticSet);
+    }
+    // use the same dimensions as current metric
+    Dimensions vtpDimensions = new Dimensions();
+    ArrayList<Dimension> vtpDimensionsMember = new ArrayList<Dimension>();
+    if ( datum.getDimensions( ) != null ) for ( final Dimension dimension: datum.getDimensions( ).getMember( ) ) {
+      Dimension vtpDimension = new Dimension();
+      vtpDimension.setName(dimension.getName());
+      vtpDimension.setValue(dimension.getValue());
+      vtpDimensionsMember.add(vtpDimension);
+    }
+    vtpDimensions.setMember(vtpDimensionsMember);
+    vtpDatum.setDimensions(vtpDimensions);
+    AbsoluteMetricQueueItem vtpQueueItem = new AbsoluteMetricQueueItem();
+    vtpQueueItem.setAccountId(accountId);
+    vtpQueueItem.setNamespace(nameSpace);
+    vtpQueueItem.setMetricDatum(vtpDatum);
+    return vtpQueueItem;
+  }
+
   public static class MetricDifferenceInfo {
     private Double valueDifference;
     private Long elapsedTimeInMillis;
@@ -71,7 +164,6 @@ public class AbsoluteMetricHelper {
       cache.put(namespace, metricName, dimensionName, dimensionValue, lastEntity);
       returnValue =  null;
     } else {
-      double TOLERANCE = 0.0000001; // arbitrary to check double "equality"
       long elapsedTimeInMillis = newTimestamp.getTime() - lastEntity.getTimestamp().getTime();
       LOG.trace("lastTimestamp="+lastEntity.getTimestamp());
       double valueDifference = newMetricValue - lastEntity.getLastMetricValue();
@@ -84,7 +176,7 @@ public class AbsoluteMetricHelper {
           LOG.warn("Getting different values " + newMetricValue + " and " + lastEntity.getLastMetricValue() + " for absolute metric " + metricName + " at the same timestamp " + newTimestamp + ", keeping the second value.");
         }
         return null; // not a useful data point either
-      } else if (elapsedTimeInMillis > MAX_DIFFERENCE_DURATION_MS) { 
+      } else if (elapsedTimeInMillis > MAX_DIFFERENCE_DURATION_MS) {
         // Too much time has passed, a useful data point, but we will not report the 'difference'.  We will reset.
         LOG.trace("too much time has passed, (" + elapsedTimeInMillis + " ms), starting over");
         lastEntity.setTimestamp(newTimestamp);
@@ -99,7 +191,7 @@ public class AbsoluteMetricHelper {
           // assume the previous point had a 0 value.  Not sure which is the better choice, but for now, we will make it a "first"
           // point again
           returnValue = null;
-        } else { // truncate differences within TOLERANCE to zero
+        } else { // truncate differences within AbsoluteMetricCommon.TOLERANCE to zero
           if (Math.abs(valueDifference) < TOLERANCE) {
             valueDifference = 0.0;
           } 
@@ -139,7 +231,6 @@ public class AbsoluteMetricHelper {
         Entities.persist(lastEntity);
         returnValue =  null;
       } else {
-        double TOLERANCE = 0.0000001; // arbitrary to check double "equality"
         long elapsedTimeInMillis = newTimestamp.getTime() - lastEntity.getTimestamp().getTime();
         LOG.trace("lastTimestamp="+lastEntity.getTimestamp());
         double valueDifference = newMetricValue - lastEntity.getLastMetricValue();
@@ -152,7 +243,7 @@ public class AbsoluteMetricHelper {
             LOG.warn("Getting different values " + newMetricValue + " and " + lastEntity.getLastMetricValue() + " for absolute metric " + metricName + " at the same timestamp " + newTimestamp + ", keeping the second value.");
           }
           returnValue = null; // not a useful data point either
-        } else if (elapsedTimeInMillis > MAX_DIFFERENCE_DURATION_MS) { 
+        } else if (elapsedTimeInMillis > MAX_DIFFERENCE_DURATION_MS) {
           // Too much time has passed, a useful data point, but we will not report the 'difference'.  We will reset.
           LOG.trace("too much time has passed, (" + elapsedTimeInMillis + " ms), starting over");
           lastEntity.setTimestamp(newTimestamp);
@@ -167,7 +258,7 @@ public class AbsoluteMetricHelper {
             // assume the previous point had a 0 value.  Not sure which is the better choice, but for now, we will make it a "first"
             // point again
             returnValue = null;
-          } else { // truncate differences within TOLERANCE to zero
+          } else { // truncate differences within AbsoluteMetricCommon.TOLERANCE to zero
             if (Math.abs(valueDifference) < TOLERANCE) {
               valueDifference = 0.0;
             } 
