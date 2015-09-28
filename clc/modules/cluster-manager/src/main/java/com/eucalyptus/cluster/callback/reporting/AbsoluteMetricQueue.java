434ad593684007da93612ea8205321fb0f8b0cb6
Steve Jones <steve.jones@hpe.com>
2015-09-27 22:59:46 -0700

    EC2 put system metrics to all UFS with higher concurrency - EUCA-11413

--- clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/reporting/AbsoluteMetricQueue.java
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/cluster/callback/reporting/AbsoluteMetricQueue.java
@@ -21,7 +21,6 @@ package com.eucalyptus.cluster.callback.reporting;
 
 import com.eucalyptus.bootstrap.Bootstrap;
 import com.eucalyptus.cloudwatch.common.CloudWatch;
-import com.eucalyptus.cloudwatch.common.CloudWatchBackend;
 import com.eucalyptus.cloudwatch.common.internal.domain.metricdata.Units;
 import com.eucalyptus.cloudwatch.common.msgs.Dimension;
 import com.eucalyptus.cloudwatch.common.msgs.Dimensions;
@@ -32,8 +31,12 @@ import com.eucalyptus.cloudwatch.common.msgs.PutMetricDataType;
 import com.eucalyptus.component.ServiceConfiguration;
 import com.eucalyptus.component.Topology;
 import com.eucalyptus.component.id.Eucalyptus;
-import com.eucalyptus.util.EucalyptusCloudException;
+import com.eucalyptus.util.CollectionUtils;
 import com.eucalyptus.util.async.AsyncRequests;
+import com.eucalyptus.util.async.CheckedListenableFuture;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -42,12 +45,15 @@ import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -58,6 +64,7 @@ public class AbsoluteMetricQueue {
   public static volatile Integer ABSOLUTE_METRIC_NUM_DB_OPERATIONS_PER_TRANSACTION = 10000;
   public static volatile Integer ABSOLUTE_METRIC_NUM_DB_OPERATIONS_UNTIL_SESSION_FLUSH = 50;
 
+  static boolean useScanningConverter = true;
 
   static {
     ScheduledExecutorService dbCleanupService = Executors
@@ -68,7 +75,7 @@ public class AbsoluteMetricQueue {
 
 
   private static final Logger LOG = Logger.getLogger(AbsoluteMetricQueue.class);
-  final static LinkedBlockingQueue<AbsoluteMetricQueueItem> dataQueue = new LinkedBlockingQueue<AbsoluteMetricQueueItem>();
+  private static final LinkedBlockingQueue<AbsoluteMetricQueueItem> dataQueue = new LinkedBlockingQueue<>( );
 
   private static final ScheduledExecutorService dataFlushTimer = Executors
     .newSingleThreadScheduledExecutor();
@@ -83,39 +90,49 @@ public class AbsoluteMetricQueue {
     return singleton;
   }
 
-  private static Runnable safeRunner = new Runnable() {
+  private static Runnable safeRunner = new Runnable( ) {
     @Override
     public void run() {
+      if ( !Bootstrap.isOperational( ) ) return;
       long before = System.currentTimeMillis();
       try {
         List<AbsoluteMetricQueueItem> dataBatch = Lists.newArrayList();
         dataQueue.drainTo(dataBatch);
-//        dataQueue.drainTo(dataBatch, 15000);
-        LOG.debug("Cluster:Timing:dataBatch.size()="+dataBatch.size());
+        LOG.trace( "Cluster:Timing:dataBatch.size()=" + dataBatch.size( ) );
+        final Predicate<AbsoluteMetricQueueItem> expired =
+            AbsoluteMetricQueueItem.createdBefore( System.currentTimeMillis( ) - TimeUnit.MINUTES.toMillis( 5 ) );
+        int expiredQueueItems = CollectionUtils.reduce( dataBatch, 0, CollectionUtils.count( expired ) );
+        if ( expiredQueueItems > 0 ) {
+          LOG.error( "Dropping " + expiredQueueItems + " expired items from system metrics queue" );
+        }
+        LOG.trace( "Cluster:Timing:dataBatch.size()=" + dataBatch.size( ) );
         long t1 = System.currentTimeMillis();
-//        dataBatch = DefaultAbsoluteMetricConverter.dealWithAbsoluteMetrics(dataBatch);
-        dataBatch = FullTableScanAbsoluteMetricConverter.dealWithAbsoluteMetrics(dataBatch);
+        if ( useScanningConverter ) {
+          dataBatch = FullTableScanAbsoluteMetricConverter.dealWithAbsoluteMetrics(
+              Iterables.filter( dataBatch, Predicates.not( expired ) ) );
+        } else {
+          dataBatch = DefaultAbsoluteMetricConverter.dealWithAbsoluteMetrics(
+              Iterables.filter( dataBatch, Predicates.not( expired ) ) );
+        }
         long t2 = System.currentTimeMillis();
-        LOG.debug("Cluster:Timing:dataBatch.dealWithAbsoluteMetrics():time="+(t2-t1));
+        LOG.trace( "Cluster:Timing:dataBatch.dealWithAbsoluteMetrics():time=" + ( t2 - t1 ) );
         dataBatch = foldMetrics(dataBatch);
         long t3 = System.currentTimeMillis();
-        LOG.debug("Cluster:Timing:dataBatch.foldMetrics():time="+(t3-t2));
+        LOG.trace( "Cluster:Timing:dataBatch.foldMetrics():time=" + ( t3 - t2 ) );
         List<PutMetricDataType> putMetricDataTypeList =convertToPutMetricDataList(dataBatch);
         long t4 = System.currentTimeMillis();
-        LOG.debug("Cluster:Timing:dataBatch.convertToPutMetricDataList():time="+(t4-t3));
+        LOG.trace( "Cluster:Timing:dataBatch.convertToPutMetricDataList():time=" + ( t4 - t3 ) );
         putMetricDataTypeList = CloudWatchHelper.consolidatePutMetricDataList(putMetricDataTypeList);
         long t5 = System.currentTimeMillis();
-        LOG.debug("Cluster:Timing:dataBatch.consolidatePutMetricDataList():time="+(t5-t4));
+        LOG.trace( "Cluster:Timing:dataBatch.consolidatePutMetricDataList():time=" + ( t5 - t4 ) );
         callPutMetricData(putMetricDataTypeList);
         long t6 = System.currentTimeMillis();
-        LOG.debug("Cluster:Timing:ListMetricManager.callPutMetricData():time="+(t6-t5));
+        LOG.trace( "Cluster:Timing:ListMetricManager.callPutMetricData():time=" + ( t6 - t5 ) );
       } catch (Throwable ex) {
-        LOG.debug("error");
-        ex.printStackTrace();
         LOG.error(ex,ex);
       } finally {
         long after = System.currentTimeMillis();
-        LOG.debug("Cluster:Timing:time="+(after-before));
+        LOG.trace( "Cluster:Timing:time=" + ( after - before ) );
       }
     }
   };
@@ -124,11 +141,12 @@ public class AbsoluteMetricQueue {
     final List<PutMetricDataType> putMetricDataTypeList = Lists.newArrayList();
     for (AbsoluteMetricQueueItem item: dataBatch) {
       PutMetricDataType putMetricDataType = new PutMetricDataType();
+      //noinspection deprecation
       putMetricDataType.setUserId(item.getAccountId());
       putMetricDataType.markPrivileged();
       putMetricDataType.setNamespace(item.getNamespace());
       MetricData metricData = new MetricData();
-      ArrayList member = Lists.newArrayList(item.getMetricDatum());
+      ArrayList<MetricDatum> member = Lists.newArrayList(item.getMetricDatum());
       metricData.setMember(member);
       putMetricDataType.setMetricData(metricData);
       putMetricDataTypeList.add(putMetricDataType);
@@ -136,16 +154,40 @@ public class AbsoluteMetricQueue {
     return putMetricDataTypeList;
   }
 
-  private static ServiceConfiguration createServiceConfiguration() {
-    return Topology.lookup(CloudWatch.class);
-  }
-
-  private static void callPutMetricData(List<PutMetricDataType> putMetricDataList) throws Exception {
-    ServiceConfiguration serviceConfiguration = createServiceConfiguration();
-    for (PutMetricDataType putMetricData: putMetricDataList) {
-      BaseMessage reply = AsyncRequests.dispatch(serviceConfiguration, putMetricData).get();
-      if (!(reply instanceof PutMetricDataResponseType)) {
-        throw new EucalyptusCloudException("Unable to send put metric data to cloud watch");
+  private static void callPutMetricData( final List<PutMetricDataType> putMetricDataList ) throws Exception {
+    final List<ServiceConfiguration> serviceConfigurations =
+        Lists.newArrayList( Topology.lookupMany( CloudWatch.class ) );
+    final Map<ServiceConfiguration, Semaphore> semaphoreMap = Maps.newHashMap( );
+    for ( final ServiceConfiguration serviceConfiguration: serviceConfigurations ) {
+      semaphoreMap.put( serviceConfiguration, new Semaphore( 8 ) );
+    }
+    final Iterator<ServiceConfiguration> putToServiceConfiguration =
+        Iterables.cycle( serviceConfigurations ).iterator( );
+    for ( final PutMetricDataType putMetricData : putMetricDataList ) {
+      final ServiceConfiguration serviceConfiguration = putToServiceConfiguration.next( );
+      final Semaphore activePuts = semaphoreMap.get( serviceConfiguration );
+      activePuts.acquire( );
+      try {
+        final CheckedListenableFuture<BaseMessage> replyFuture =
+            AsyncRequests.dispatch( serviceConfiguration, putMetricData );
+        replyFuture.addListener( new Runnable( ) {
+          @Override
+          public void run( ) {
+            try {
+              final BaseMessage reply = replyFuture.get( );
+              if ( !( reply instanceof PutMetricDataResponseType ) ) {
+                LOG.error( "Error putting compute system metric data" );
+              }
+            } catch ( ExecutionException | InterruptedException e ) {
+              LOG.error( "Error putting compute system metric data", e );
+            } finally {
+              activePuts.release( );
+            }
+          }
+        } );
+      } catch ( Throwable t ) {
+        activePuts.release( );
+        LOG.error( "Error putting compute system metric data", t );
       }
     }
   }
@@ -195,21 +237,10 @@ public class AbsoluteMetricQueue {
     dataFlushTimer.scheduleAtFixedRate(safeRunner, 0, 1, TimeUnit.MINUTES);
   }
 
-
   private void scrub(AbsoluteMetricQueueItem absoluteMetricQueueItem, Date now) {
     MetricDatum datum = absoluteMetricQueueItem.getMetricDatum();
     if (datum.getUnit() == null || datum.getUnit().trim().isEmpty()) datum.setUnit(Units.None.toString());
     if (datum.getTimestamp() == null) datum.setTimestamp(now);
-  }
-
-  private static Map<String, String> makeDimensionMap(
-    final List<Dimension> dimensions
-  ) {
-    Map<String,String> returnValue = Maps.newTreeMap();
-    for (Dimension dimension: dimensions) {
-      returnValue.put(dimension.getName(), dimension.getValue());
-    }
-    return returnValue;
   }
 
   public void addQueueItems(List<AbsoluteMetricQueueItem> queueItems) {
@@ -220,7 +251,6 @@ public class AbsoluteMetricQueue {
       dataQueue.offer(queueItem);
     }
   }
-
 
   private static class DBCleanupService implements Runnable {
     @Override
