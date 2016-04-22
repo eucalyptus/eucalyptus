bb77082aa0c6b8e3867baa9a253a64a482e335e8
Steve Jones <steve.jones@hpe.com>
2016-04-22 01:20:55 -0700

    EC2 VPC update route active/blackhole state - EUCA-12230

---
+++ clc/modules/cluster-manager/src/main/java/com/eucalyptus/compute/vpc/RouteKey.java
@@ -0,0 +1,44 @@
+/*************************************************************************
+ * Copyright 2009-2016 Eucalyptus Systems, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+ * CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+ * additional information or have any questions.
+ ************************************************************************/
+package com.eucalyptus.compute.vpc;
+
+import javax.annotation.Nonnull;
+import com.eucalyptus.util.Pair;
+
+/**
+ *
+ */
+public class RouteKey extends Pair<String,String> {
+
+  /**
+   *
+   */
+  public RouteKey( @Nonnull final String routeTableId, @Nonnull final String cidr ) {
+    super( routeTableId, cidr );
+  }
+
+  public String getRouteTableId( ) {
+    return getLeft( );
+  }
+
+  public String getCidr( ) {
+    return getRight( );
+  }
+}
