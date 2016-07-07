6c194e332452de3140885560fde9f6f674bd3645
Mauricio Tsugawa <tsugawa@hpe.com>
2016-07-07 16:25:44 -0700

    Implements destination IP check for EUCA-11951

--- net/euca-to-mido.c
+++ net/euca-to-mido.c
@@ -2204,7 +2204,7 @@ int do_midonet_update_pass3_insts(globalNetworkInfo *gni, mido_config *mido) {
                 "type", "drop", "dlType", "2048", "nwSrcAddress", instIp, "nwSrcLength", "32", "invNwSrc", "true", NULL);
         if ((rc == 0) && ptmpmn && (ptmpmn->init == 1)) {
             if ((mido->disable_l2_isolation) || (!gniif->srcdstcheck)) {
-                LOGTRACE("\tdeleting L3 rule for %s\n", gniif->name);
+                LOGTRACE("\tdeleting L3 rule src for %s\n", gniif->name);
                 rc = mido_delete_rule(vpcif->prechain, ptmpmn);
                 if (rc) {
                     LOGWARN("Failed to delete src IP check rule for %s\n", gniif->name);
@@ -2213,12 +2213,38 @@ int do_midonet_update_pass3_insts(globalNetworkInfo *gni, mido_config *mido) {
             }
         } else {
             if ((!mido->disable_l2_isolation) && (gniif->srcdstcheck)) {
-                LOGTRACE("\tcreating L3 rule for %s\n", gniif->name);
+                LOGTRACE("\tcreating L3 rule src for %s\n", gniif->name);
                 rc = mido_create_rule(vpcif->prechain, vpcif->midos[INST_PRECHAIN],
                         NULL, &rulepos, "position", pos_str, "type", "drop", "dlType", "2048",
                         "nwSrcAddress", instIp, "nwSrcLength", "32", "invNwSrc", "true", NULL);
                 if (rc) {
                     LOGWARN("Failed to create src IP check rule for %s\n", gniif->name);
+                    ret++;
+                }
+            }
+        }
+
+        // block any incoming IP traffic that isn't to the VM private IP
+        // Check if the rule is already in place
+        rc = mido_find_rule_from_list(vpcif->postchain->rules, vpcif->postchain->max_rules, &ptmpmn,
+                "type", "drop", "dlType", "2048", "nwDstAddress", instIp, "nwDstLength", "32", "invNwDst", "true", NULL);
+        if ((rc == 0) && ptmpmn && (ptmpmn->init == 1)) {
+            if ((mido->disable_l2_isolation) || (!gniif->srcdstcheck)) {
+                LOGTRACE("\tdeleting L3 rule dst for %s\n", gniif->name);
+                rc = mido_delete_rule(vpcif->postchain, ptmpmn);
+                if (rc) {
+                    LOGWARN("Failed to delete dst IP check rule for %s\n", gniif->name);
+                    ret++;
+                }
+            }
+        } else {
+            if ((!mido->disable_l2_isolation) && (gniif->srcdstcheck)) {
+                LOGTRACE("\tcreating L3 rule dst for %s\n", gniif->name);
+                rc = mido_create_rule(vpcif->postchain, vpcif->midos[INST_POSTCHAIN],
+                        NULL, &rulepos, "position", pos_str, "type", "drop", "dlType", "2048",
+                        "nwDstAddress", instIp, "nwDstLength", "32", "invNwDst", "true", NULL);
+                if (rc) {
+                    LOGWARN("Failed to create dst IP check rule for %s\n", gniif->name);
                     ret++;
                 }
             }
