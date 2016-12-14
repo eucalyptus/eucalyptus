5451cae77a6b571a04bd36642ab2949f731cda33
Mauricio Tsugawa <tsugawa@hpe.com>
2016-12-14 12:58:10 -0800

    Cosmetic updates to EDGE driver upgrade process.

--- net/eucanetd_edge.c
+++ net/eucanetd_edge.c
@@ -331,9 +331,9 @@ static int network_driver_upgrade(eucanetdConfig *pConfig, globalNetworkInfo *pG
     // dhcpd
     char cmd[EUCA_MAX_PATH] = "";
     snprintf(cmd, EUCA_MAX_PATH, "%s pkill -f euca-dhcp.pid", pConfig->cmdprefix);
-    rc = timeshell_nb(cmd, 10, TRUE);
+    rc = timeshell_nb(cmd, 10, FALSE);
     if (rc != 0) {
-        LOGERROR("Failed to terminate dhcpd\n");
+        LOGDEBUG("Failed to terminate dhcpd\n");
     }
     // iptables
     ipt_handler_repopulate(pConfig->ipt);
