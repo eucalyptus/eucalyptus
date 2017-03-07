ad8dc8ee62cb4d5817eca61da32fb129eefb0749
Steve Jones <steve.jones@hpe.com>
2017-03-06 16:32:57 -0800

    IAM policy variables as condition keys - EUCA-13276

---
+++ clc/modules/msgs/src/main/java/com/eucalyptus/auth/policy/key/PolicyVariableKey.java
@@ -0,0 +1,56 @@
+/*************************************************************************
+ * (c) Copyright 2017 Hewlett Packard Enterprise Development Company LP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ ************************************************************************/
+package com.eucalyptus.auth.policy.key;
+
+import com.eucalyptus.auth.AuthException;
+import com.eucalyptus.auth.policy.condition.ConditionOp;
+import com.eucalyptus.auth.policy.condition.StringConditionOp;
+import com.eucalyptus.auth.policy.variable.PolicyVariables;
+import net.sf.json.JSONException;
+
+/**
+ *
+ */
+public class PolicyVariableKey implements Key {
+
+  private final String name;
+
+  public PolicyVariableKey( final String name ) {
+    this.name = name;
+  }
+
+  @Override
+  public String name( ) {
+    return name;
+  }
+
+  @Override
+  public String value( ) throws AuthException {
+    return PolicyVariables.getPolicyVariable( name( ) ).evaluate( );
+  }
+
+  @Override
+  public void validateConditionType( final Class<? extends ConditionOp> conditionClass ) throws JSONException {
+    if ( !StringConditionOp.class.isAssignableFrom( conditionClass ) ) {
+      throw new JSONException( name() + " is not allowed in condition " + conditionClass.getName( ) + ". String conditions are required." );
+    }
+  }
+
+  @Override
+  public boolean canApply( final String action ) {
+    return true;
+  }
+}
