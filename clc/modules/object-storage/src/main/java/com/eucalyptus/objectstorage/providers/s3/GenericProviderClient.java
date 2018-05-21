c82d88426f3338d4ae9772d7226ad3e97d40f935
Steve Jones <steve@iaascream.cloud>
2018-05-21 01:23:24 -0700

    Object storage gateway generic and minio providers

---
+++ clc/modules/object-storage/src/main/java/com/eucalyptus/objectstorage/providers/s3/GenericProviderClient.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2018 AppScale Systems, Inc
+ *
+ * Use of this source code is governed by a BSD-2-Clause
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/BSD-2-Clause
+ */
+package com.eucalyptus.objectstorage.providers.s3;
+
+import com.eucalyptus.objectstorage.providers.ObjectStorageProviders.ObjectStorageProviderClientProperty;
+
+/**
+ * Implementation for a generic S3 provider.
+ */
+@ObjectStorageProviderClientProperty("generic")
+public class GenericProviderClient extends S3ProviderClient {
+}
