457e20955d5567e343436ed4310c234601157d06
Steve Jones <steve@iaascream.cloud>
2018-10-15 02:09:08 -0700

    Use SPDX short license identifiers in new source files

--- clc/modules/compute-common/src/main/java/com/eucalyptus/compute/common/internal/network/NetworkCidr.java
+++ clc/modules/compute-common/src/main/java/com/eucalyptus/compute/common/internal/network/NetworkCidr.java
@@ -1,9 +1,7 @@
 /*
  * Copyright 2018 AppScale Systems, Inc
  *
- * Use of this source code is governed by a BSD-2-Clause
- * license that can be found in the LICENSE file or at
- * https://opensource.org/licenses/BSD-2-Clause
+ * SPDX-License-Identifier: BSD-2-Clause
  */
 package com.eucalyptus.compute.common.internal.network;
 
