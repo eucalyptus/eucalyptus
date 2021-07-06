00f4267bbc18c6c2316859c126148122eef01e67
Steve Jones <steve@iaascream.cloud>
2021-07-06 13:03:36 -0700

    CloudFormation RDS database parameter group resource

---
+++ clc/modules/cloudformation/src/main/java/com/eucalyptus/cloudformation/resources/standard/info/AWSRDSDBParameterGroupResourceInfo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 AppScale Systems, Inc
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+package com.eucalyptus.cloudformation.resources.standard.info;
+
+import com.eucalyptus.cloudformation.resources.ResourceInfo;
+import com.eucalyptus.cloudformation.resources.annotations.AttributeJson;
+import com.google.common.base.MoreObjects;
+
+public class AWSRDSDBParameterGroupResourceInfo extends ResourceInfo {
+
+  @AttributeJson
+  private String arn;
+
+  public AWSRDSDBParameterGroupResourceInfo( ) {
+    setType( "AWS::RDS::DBParameterGroup" );
+  }
+
+  public String getArn() {
+    return arn;
+  }
+
+  public void setArn(String arn) {
+    this.arn = arn;
+  }
+
+  @Override
+  public boolean supportsTags( ) {
+    return true;
+  }
+
+  @Override
+  public String toString( ) {
+    return MoreObjects.toStringHelper( this )
+        .add( "arn", arn )
+        .toString( );
+  }
+}
